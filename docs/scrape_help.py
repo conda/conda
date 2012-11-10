195d5a6b637db6b6c9a997edd9afe69b6760c21d
Maggie Mari <maggie.mari@continuum.io>
2012-11-09 18:00:59 -0600

    Improved scrape_help script.

--- docs/scrape_help.py
+++ docs/scrape_help.py
@@ -1,6 +1,24 @@
 from subprocess import *
+from os.path import join
 import re
 import sys
+
+cmd_names = [
+    'info',
+    'list',
+    'depends',
+    'search',
+    'locations',
+    'envs',
+    'create',
+    'install',
+    'upgrade',
+    'activate',
+    'deactivate',
+    'download',
+    'remove',
+    #'upgrade2pro'
+]
 
 def scrape_help(cmd_name):
 
@@ -25,23 +43,46 @@ def scrape_help(cmd_name):
     # groups:                            ---1--
     optional_pat = re.compile(r'(optional(.*\n)*)$')
     opt = optional_pat.search(output)
-    options = opt.group(1)
 
-    yn_pat = re.compile(r'{.*}')
+    if opt:
+        options = opt.group(1)
 
-    options = yn_pat.sub('', options)
+        yn_pat = re.compile(r'{.*}')
+        options = yn_pat.sub('', options)
 
+        rd_pat = re.compile(r'(default: )(.*/anaconda)')
+        options = rd_pat.sub(r"\1ROOT_DIR", options)
+
+        in_pat = re.compile(r'(in )(.*/anaconda)')
+        options = in_pat.sub(r"\1ROOT_DIR", options)
+
+    else:
+        options = ''
 
     output = desc.group(1)
 
-    output += "\n\n**%s** ``%s``\n\n" % (usage.group(1), usage.group(2))
+    output += "\n\n**%s**: ``%s``\n\n" % (usage.group(1), usage.group(2))
 
     if pos:
         output += "*%s*\n\t%s\n\n" % (pos.group(1), pos.group(2))
 
     output += options
 
-    print output
+    return output
+
+    
 
 if __name__ == '__main__':
-    scrape_help(sys.argv[1])
+    for name in cmd_names:
+        output = scrape_help(name)
+
+        path = "/tmp"
+        if len(sys.argv) > 1:
+            path = sys.argv[1]
+        outpath = join(path, "%s.txt" % name)
+
+        print "Scraping help for '%s' -> %s" % (name, outpath)
+
+        outfile = open(outpath, "w")
+        outfile.write(output)
+        outfile.close()
