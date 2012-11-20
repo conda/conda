845a7b14e25287903331dc36d3bff2ba509a3cf1
Bryan Van de Ven <bryanv@continuum.io>
2012-11-20 16:10:28 -0600

    better versioning for dev

--- conda/__init__.py
+++ conda/__init__.py
@@ -1,5 +1,7 @@
-__version__ = '1.1.1'
 
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
 
 # This is deprecated, do not use in new code
 from envs import get_installed
--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 import sys
 from distutils.core import setup
 
+import versioneer
+
 
 if sys.version[:3] != '2.7':
     raise Exception("conda is only meant for Python 2.7, current version: %s" %
                     sys.version[:3])
 
+versioneer.versionfile_source = 'conda/_version.py'
+versioneer.versionfile_build = 'conda/_version.py'
+versioneer.tag_prefix = '' # tags are like 1.2.0
+versioneer.parentdir_prefix = 'conda-' # dirname like 'myproject-1.2.0'
 
 setup(
     name = "conda",
-    version = '1.1.1',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     author = "Continuum Analytics, Inc.",
     author_email = "ilan@continuum.io",
     description = "Conda tool",
