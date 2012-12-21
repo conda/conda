bc6479317f46ae58b4b342e1335025f22e3853c4
Bryan Van de Ven <bryanv@continuum.io>
2012-12-20 18:15:09 -0600

    small fixes and tests for new wakari build target

--- tests/test_package.py
+++ tests/test_package.py
@@ -21,9 +21,6 @@ with open(join(dirname(__file__), 'index.json')) as fi:
 
 class test_package(unittest.TestCase):
 
-    def test_(self):
-        pass
-
     def test_is_meta(self):
         self.assertEqual(
             package(info["baz-2.0.1-0.tar.bz2"]).is_meta,
@@ -66,6 +63,16 @@ class test_sort_packages_by_name(unittest.TestCase):
             [foo, baz, bar]
         )
 
+    def test_with_build_target(self):
+        none, ce, pro, w = (
+            package(info["baz-2.0.1-0.tar.bz2"]),
+            package(info["baz-2.0.1-ce0.tar.bz2"]),
+            package(info["baz-2.0.1-pro0.tar.bz2"]),
+            package(info["baz-2.0.1-w0.tar.bz2"]),
+        )
+        self.assertTrue(none < ce)
+        self.assertTrue(ce < pro)
+        self.assertTrue(pro < w)
 
 class test_group_packages_by_name(unittest.TestCase):
 
@@ -88,6 +95,27 @@ class test_group_packages_by_name(unittest.TestCase):
             }
         )
 
+    def test_with_build_target(self):
+        f1, f2, f3, b1, b2, b3, b4, m  = [
+            package(info["foo-0.8.0-0.tar.bz2"]),
+            package(info["foo-0.8.0-1.tar.bz2"]),
+            package(info["foo-0.9.0-0.tar.bz2"]),
+            package(info["baz-2.0.1-0.tar.bz2"]),
+            package(info["baz-2.0.1-ce0.tar.bz2"]),
+            package(info["baz-2.0.1-pro0.tar.bz2"]),
+            package(info["baz-2.0.1-w0.tar.bz2"]),
+            package(info["meta-0.2-0.tar.bz2"]),
+        ]
+        pkgs = [f2, f1, b2, f3, b1, m, b4, b3]
+        self.assertEqual(
+            group_packages_by_name(pkgs),
+            {
+                'foo' : set([f1, f2, f3]),
+                'baz' : set([b1, b2, b3, b4]),
+                'meta' : set([m]),
+            }
+        )
+
 
 class test_find_newest_packages(unittest.TestCase):
 
@@ -103,6 +131,23 @@ class test_find_newest_packages(unittest.TestCase):
         self.assertEqual(
             newest_packages(pkgs),
             set([f2, b2, m])
+        )
+
+    def test_with_build_target(self):
+        f1, f2, f3, b1, b2, b3, b4, m  = [
+            package(info["foo-0.8.0-0.tar.bz2"]),
+            package(info["foo-0.8.0-1.tar.bz2"]),
+            package(info["foo-0.9.0-0.tar.bz2"]),
+            package(info["baz-2.0.1-0.tar.bz2"]),
+            package(info["baz-2.0.1-ce0.tar.bz2"]),
+            package(info["baz-2.0.1-pro0.tar.bz2"]),
+            package(info["baz-2.0.1-w0.tar.bz2"]),
+            package(info["meta-0.2-0.tar.bz2"]),
+        ]
+        pkgs = [f2, f1, b2, f3, b1, m, b4, b3]
+        self.assertEqual(
+            newest_packages(pkgs),
+            set([f3, b4, m])
         )
 
 
