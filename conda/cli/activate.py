7b7f05e32f32593c3b06bc8979e06203465dded4
Ilan Schnell <ilanschnell@gmail.com>
2013-02-20 01:03:46 -0600

    add activate and deactivate commands on Unix

---
+++ conda/cli/activate.py
@@ -0,0 +1,50 @@
+import os
+import sys
+from os.path import isdir, join
+
+
+ROOT_BINPATH = join(sys.prefix, 'bin')
+
+def help():
+    if sys.argv[1] == '..activate':
+        sys.exit("""Usage: source activate [ENV]
+
+adds the environment ENV to the front of PATH.
+ENV may either refer to just the name of the environment, or the full
+prefix path.  defaults to: %s""" % ROOT_BINPATH)
+    else: # ..deactivate
+        sys.exit("""Usage: source deactivate [ENV]
+
+removes the environment ENV from PATH.
+ENV may either refer to just the name of the environment, or the full
+prefix path.  defaults to: %s""" % ROOT_BINPATH)
+
+
+def main():
+    assert sys.argv[1] in ('..activate', '..deactivate')
+    if '-h' in sys.argv or '--help' in sys.argv:
+        help()
+
+    if len(sys.argv) == 2:
+        binpath = ROOT_BINPATH
+    elif len(sys.argv) == 3:
+        binpath = join(sys.prefix, 'envs', sys.argv[2], 'bin')
+    else:
+        sys.exit("Error: did not expect more than one argument")
+
+    if sys.argv[1] == '..activate':
+        if not isdir(binpath):
+            sys.exit("Error: no such directory: %s" % binpath)
+        paths = [binpath]
+    else: # ..deactivate
+        paths = []
+
+    for path in os.getenv('PATH').split(os.pathsep):
+        if path != binpath:
+            paths.append(path)
+    sys.stderr.write("prepending %s to PATH\n" % binpath)
+    print os.pathsep.join(paths)
+        
+
+if __name__ == '__main__':
+    main()
