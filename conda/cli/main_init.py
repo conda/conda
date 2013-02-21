3e98570e0a6474666faba55720435bd34325ec8c
Maggie Mari <maggie.mari@continuum.io>
2013-02-21 15:40:01 -0600

    Rough working version of conda init.

--- conda/cli/main_init.py
+++ conda/cli/main_init.py
@@ -4,8 +4,24 @@
 # conda is distributed under the terms of the BSD 3-clause license.
 # Consult LICENSE.txt or http://opensource.org/licenses/BSD-3-Clause.
 
+import json
 import os
+<<<<<<< Updated upstream
 from os.path import abspath, exists, expanduser, join
+=======
+import platform
+import sys
+from os.path import abspath, exists, expanduser, join
+from subprocess import check_call
+from urllib2 import urlopen
+
+
+from conda.constraints import AllOf, Requires, Satisfies
+from conda.package_index import PackageIndex
+from conda.package_spec import PackageSpec
+from conda.install import make_available, link
+from conda.remote import fetch_file
+>>>>>>> Stashed changes
 
 
 def configure_parser(sub_parsers):
@@ -31,8 +47,61 @@ def execute(args, parser):
     if exists(prefix):
         raise RuntimeError("Install directory '%s' already exists." % prefix)
 
+<<<<<<< Updated upstream
     os.makedirs(prefix)
     for sd in 'envs', 'pkgs':
         os.mkdir(join(prefix, sd))
 
     print "Anaconda initalized into directory '%s'" % prefix
+=======
+    os.mkdir(prefix)
+    for sd in subdirs:
+        os.mkdir("%s/%s" % (prefix, sd))
+
+    channel = "http://repo.continuum.io/pkgs/free/%s/" % get_platform()
+
+    index = PackageIndex(fetch_index(channel))
+
+    py_spec     = PackageSpec("python 2.7")
+    yaml_spec   = PackageSpec("yaml")
+    pyyaml_spec = PackageSpec("pyyaml") 
+    conda_spec  = PackageSpec("conda") 
+
+    py = max(index.find_matches(Satisfies(py_spec)))
+    yaml = max(index.find_matches(Satisfies(yaml_spec)))
+    pyyaml = max(index.find_matches(AllOf(Requires(py_spec), Satisfies(pyyaml_spec))))
+    conda = max(index.find_matches(AllOf(Requires(py_spec), Satisfies(conda_spec))))
+
+    pkgs_dir = join(prefix, 'pkgs')
+    pkgs = [py, yaml, pyyaml, conda]
+
+    for pkg in pkgs:
+        fetch_file(pkg.filename, [channel], md5=pkg.md5, size=pkg.size, pkgs_dir=pkgs_dir)
+        make_available(pkgs_dir, pkg.canonical_name)
+        link(pkgs_dir, pkg.canonical_name, prefix)
+
+    if exists(prefix):
+        print "Anaconda installed into directory '%s'" % prefix
+
+def fetch_index(url):
+    try:
+        fi = urlopen(url + 'repodata.json')
+        repodata = json.loads(fi.read())
+    except IOError:
+        raise RuntimeError("Failed to fetch index for channel '%s' (bad url or network issue?)" % url)
+
+    fi.close()
+    
+    index = repodata['packages']
+
+    for pkg_info in index.itervalues():
+        pkg_info['channel'] = url
+
+    return index
+
+def get_platform():
+    sys_map = {'linux2': 'linux', 'darwin': 'osx', 'win32': 'win'}
+    bits = int(platform.architecture()[0][:2])
+    system = sys_map.get(sys.platform, 'unknown')
+    return '%s-%d' % (system, bits)
+>>>>>>> Stashed changes
