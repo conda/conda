91e934b9c5e88fd0dc7990046c0e506c7874d1be
Maggie Mari <maggie.mari@continuum.io>
2013-03-06 12:26:12 -0600

    Added additional test cases.

--- utils/smoketest.py
+++ utils/smoketest.py
@@ -8,6 +8,10 @@ from tempfile import mkdtemp
 base = mkdtemp()
 myenv = join(base, "env")
 
+# CIO_TEST needs to be set to 2 if any of the packages tested below are only found in the test repo.
+
+# os.environ['CIO_TEST'] = 2
+
 cmds = [
     (
         "conda info"
@@ -19,13 +23,16 @@ cmds = [
         "conda search ^m.*lib$"
     ),
     (
+        "conda search --all -v numpy"
+    ),
+    (
         "conda depends numpy"
     ),
     (
         "conda info -e"
     ),
     (
-        "conda create --yes -p %s sqlite" % myenv
+        "conda create --yes -p %s sqlite python=2.6" % myenv
     ),
     (
         "conda install --yes -p %s pandas=0.8.1" % myenv
@@ -40,16 +47,28 @@ cmds = [
         "conda install --yes -p %s cython=0.17.3" % myenv
     ),
     (
+        "conda install --yes -p %s accelerate" % myenv
+    ),
+    (
+        "conda remove --yes -p %s accelerate" % myenv
+    ),
+    (
         "conda update --yes -p %s pandas" % myenv
+    ),
+    (
+        "conda install --yes -p %s iopro" % myenv
+    ),
+    (
+        "conda remove --yes -p %s iopro" % myenv
     ),
     (
         "conda local --yes -d numba-0.3.1-np17py27_0"
     ),
     (
-        "conda env --yes -ap %s numba-0.3.1-np17py27_0" % myenv
+        "conda env --yes -lp %s numba-0.3.1-np17py27_0" % myenv
     ),
     (
-        "conda env --yes -dp %s sqlite-3.7.13-0" % myenv
+        "conda env --yes -up %s sqlite-3.7.13-0" % myenv
     ),
     (
         "conda local --yes -r zeromq-2.2.0-0"
