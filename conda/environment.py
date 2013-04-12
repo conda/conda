85e269173007f422186e6de2b4fa2df57e519efc
Ilan Schnell <ilanschnell@gmail.com>
2013-04-12 12:53:02 -0500

    loosen Python contraint to allow updating Python

--- conda/environment.py
+++ conda/environment.py
@@ -129,9 +129,8 @@ class Environment(object):
     def _python_constraint(self):
         try:
             pkg = self.find_linked_package('python')
-            req = make_package_spec('%s %s.%s' % (pkg.name, pkg.version.version[0], pkg.version.version[1]))
-            sat = make_package_spec('%s %s %s' % (pkg.name, pkg.version.vstring, pkg.build))
-            return AnyOf(Requires(req), Satisfies(sat))
+            spec = make_package_spec('%s %s.%s' % (pkg.name, pkg.version.version[0], pkg.version.version[1]))
+            return AnyOf(Requires(spec), Satisfies(spec))
         except: # TODO
             log.debug('no python constraint, returning Wildcard()')
             return Wildcard()
