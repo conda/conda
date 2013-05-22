be4917c424884880348038c8b96a40b889ea19f5
Ilan Schnell <ilanschnell@gmail.com>
2013-05-22 17:10:48 -0500

    move execute module into plan.py

--- conda/plan.py
+++ conda/plan.py
@@ -3,7 +3,9 @@ from collections import defaultdict
 import install
 from config import PKGS_DIR
 from naming import name_dist
+from remote import fetch_file
 from resolve import MatchSpec, Resolve
+from progressbar import Bar, ETA, FileTransferSpeed, Percentage, ProgressBar
 
 
 
@@ -41,7 +43,7 @@ def plan_from_actions(actions):
         if not actions[op]:
             continue
         res.append('PRINT %sing packages ...' % op.capitalize())
-        if op != 'FETCH':
+        if op not in ('FETCH', 'RM_FETCHED'):
             res.append('PROGRESS %d' % len(actions[op]))
         for dist in actions[op]:
             res.append('%s %s' % (op, dist))
@@ -99,6 +101,72 @@ def remove_actions(prefix, args):
             actions['UNLINK'].append(dist)
 
     return actions
+
+# ---------------------------- EXECUTION --------------------------
+
+def fetch(index, dist, progress):
+    fn = dist + '.tar.bz2'
+    info = index[fn]
+    fetch_file(info['channel'], fn, md5=info['md5'], size=info['size'],
+               progress=progress)
+
+def cmds_from_plan(plan):
+    res = []
+    for line in plan:
+        line = line.strip()
+        if not line or line.startswith('#'):
+            continue
+        res.append(line.split(None, 1))
+    return res
+
+def execute_plan(plan, index=None, enable_progress=True):
+    if enable_progress:
+        fetch_progress = ProgressBar(
+            widgets=['', ' ', Percentage(), ' ', Bar(), ' ', ETA(), ' ',
+                     FileTransferSpeed()])
+        progress = ProgressBar(
+            widgets=['', ' ', Bar(), ' ', Percentage()])
+    else:
+        fetch_progress = None
+        progress = None
+
+    progress_cmds = set(['EXTRACT', 'RM_EXTRACTED', 'LINK', 'UNLINK'])
+    prefix = i = None
+    for cmd, arg in cmds_from_plan(plan):
+        if enable_progress and cmd in progress_cmds:
+            i += 1
+            progress.widgets[0] = '[%-20s]' % name_dist(arg)
+            progress.update(i)
+
+        if cmd == 'PREFIX':
+            prefix = arg
+        elif cmd == 'PRINT':
+            print arg
+        elif cmd == 'FETCH':
+            fetch(index or {}, arg, fetch_progress)
+        elif cmd == 'PROGRESS':
+            if enable_progress:
+                progress.maxval = int(arg)
+                progress.start()
+                i = 0
+        elif cmd == 'EXTRACT':
+            install.extract(PKGS_DIR, arg)
+        elif cmd == 'RM_EXTRACTED':
+            install.rm_extracted(PKGS_DIR, arg)
+        elif cmd == 'LINK':
+            install.link(PKGS_DIR, arg, prefix)
+        elif cmd == 'UNLINK':
+            install.unlink(arg, prefix)
+        else:
+            raise Exception("Did not expect command: %r" % cmd)
+
+        if enable_progress and cmd in progress_cmds and progress.maxval == i:
+            progress.widgets[0] = '[      COMPLETE      ]'
+            progress.finish()
+
+def execute_actions(actions, index=None, enable_progress=True):
+    plan = plan_from_actions(actions)
+    execute_plan(plan, index, enable_progress)
 
 
 if __name__ == '__main__':
