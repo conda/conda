e5d064fa3564eec56af3e79fb5929449cfc6ce05
Ilan Schnell <ilanschnell@gmail.com>
2013-05-23 15:03:12 -0500

    refactor conda create

--- conda/cli/main_create.py
+++ conda/cli/main_create.py
@@ -9,8 +9,6 @@ import sys
 from argparse import RawDescriptionHelpFormatter
 from os.path import abspath, exists
 
-from conda.anaconda import Anaconda
-from conda.planners import create_create_plan
 from utils import (add_parser_prefix, get_prefix, add_parser_yes, confirm,
                    add_parser_quiet)
 
@@ -21,33 +19,42 @@ descr = ("Create a new conda environment from a list of specified "
          "look in that directory first.  This command requires either "
          "the -n NAME or -p PREFIX option.")
 
+example = """
+examples:
+    conda create -n myenv sqlite
+
+"""
+
 def configure_parser(sub_parsers):
     p = sub_parsers.add_parser(
         'create',
         formatter_class = RawDescriptionHelpFormatter,
-        description     = descr,
-        help            = descr,
-        epilog          = example,
+        description = descr,
+        help = descr,
+        epilog  = example,
     )
     add_parser_yes(p)
     p.add_argument(
         '-f', "--file",
-        action  = "store",
-        help    = "filename to read package specs from",
+        action = "store",
+        help = "filename to read package specs from",
     )
     add_parser_prefix(p)
     add_parser_quiet(p)
     p.add_argument(
         'package_specs',
         metavar = 'package_spec',
-        action  = "store",
-        nargs   ='*',
-        help    = "specification of package to install into new Anaconda environment",
+        action = "store",
+        nargs = '*',
+        help = "specification of package to install into new environment",
     )
     p.set_defaults(func=execute)
 
 
 def execute(args, parser):
+    import conda.plan as plan
+    from conda.api import get_index
+
     if len(args.package_specs) == 0 and not args.file:
         raise RuntimeError('too few arguments, must supply command line '
                            'package specs or --file')
@@ -56,15 +63,15 @@ def execute(args, parser):
         raise RuntimeError('either -n NAME or -p PREFIX option required, '
                            'try "conda create -h" for more details')
 
-    conda = Anaconda()
-
     prefix = get_prefix(args)
 
     if exists(prefix):
         if args.prefix:
-            raise RuntimeError("'%s' already exists, must supply new directory for -p/--prefix" % prefix)
+            raise RuntimeError("'%s' already exists, must supply new "
+                               "directory for -p/--prefix" % prefix)
         else:
-            raise RuntimeError("'%s' already exists, must supply new directory for -n/--name" % prefix)
+            raise RuntimeError("'%s' already exists, must supply new "
+                               "directory for -n/--name" % prefix)
 
     if args.file:
         try:
@@ -76,43 +83,35 @@ def execute(args, parser):
     else:
         spec_strings = args.package_specs
 
-    if any(s == 'conda' or s.startswith('conda ') for s in spec_strings):
+    if any(s == 'conda' or s.startswith('conda=') for s in spec_strings):
         raise RuntimeError("Package 'conda' may only be installed in the "
                            "root environment")
 
     if len(spec_strings) == 0:
         raise RuntimeError('no package specifications supplied')
 
-    plan = create_create_plan(prefix, conda, spec_strings)
+    index = get_index()
+    actions = plan.install_actions(prefix, index, spec_strings)
 
-    if plan.empty():
+    if plan.nothing_to_do(actions):
         print 'No matching packages could be found, nothing to do'
         return
 
     print
     print "Package plan for creating environment at %s:" % prefix
-
-    print plan
-
+    plan.display_actions(actions)
 
     confirm(args)
     os.makedirs(prefix)
-    env = conda.lookup_environment(prefix)
-
-    plan.execute(env, not args.quiet)
-
-    #Activate and deactivate won't work on Windows
+    # env = conda.lookup_environment(prefix) ???
+    plan.execute_actions(actions, index, enable_progress=not args.quiet)
 
     if sys.platform != 'win32':
         activate_name = prefix
         if args.name:
             activate_name = args.name
+        print "#"
         for cmd in ('activate', 'deactivate'):
-            print "\nTo %s this environment, type 'source %s %s'" % (cmd, cmd, activate_name)
-        print
-
-example = '''
-examples:
-    conda create -n myenv sqlite
-
-'''
+            print "# To %s this environment, use:" % cmd
+            print "# $ source %s %s" % (cmd, activate_name)
+            print "#"
