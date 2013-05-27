3191ac3dc253368fdf209d353f83205d74b46255
Ilan Schnell <ilanschnell@gmail.com>
2013-05-27 15:27:18 -0500

    fix imports

--- conda/cli/main_index.py
+++ conda/cli/main_index.py
@@ -4,12 +4,6 @@
 # conda is distributed under the terms of the BSD 3-clause license.
 # Consult LICENSE.txt or http://opensource.org/licenses/BSD-3-Clause.
 
-import os
-from os.path import abspath
-
-from conda.builder.index import update_index
-
-
 descr = "Updates repodata.json in channel directories. (ADVANCED)"
 
 
@@ -35,6 +29,12 @@ def configure_parser(sub_parsers):
 
 
 def execute(args, parser):
+    import os
+    from os.path import abspath
+
+    from conda.builder.index import update_index
+
+
     if len(args.directories) == 0:
         dir_paths = [os.getcwd()]
     else:
