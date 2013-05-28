a46225e062c8dce62ae6ed254616b9f0e09d7b8c
Ilan Schnell <ilanschnell@gmail.com>
2013-05-28 18:08:11 -0500

    add todo items

--- conda/api.py
+++ conda/api.py
@@ -19,6 +19,7 @@ def get_index():
         new_index = repodata['packages']
         for info in new_index.itervalues():
             info['channel'] = url
+            # TODO: icons should point to separate files
             if 'icon' in info:
                 md5 = info['icon']
                 icondata = base64.b64decode(repodata['icons'][md5])
@@ -32,6 +33,30 @@ def get_index():
 def get_app_index():
     return {fn: info for fn, info in get_index().iteritems()
             if info.get('type') == 'app'}
+
+
+def remaining_packages(fn):
+    """
+    given the filename of a package, return which packages (and their sizes)
+    still need to be downloaded, in order to install the package.  That is,
+    the package itself and it's dependencies (unless already in cache).
+    Returns a list of tuples, (fn, size)
+    """
+    # TODO
+    return
+
+
+def launch_app(app_dir, add_args):
+    return
+
+
+def is_installed(fn):
+    return # list of envs where app is installed
+
+
+def install(fn):
+    # link the app `fn` into a new environment and return the environments name
+    return
 
 
 if __name__ == '__main__':
