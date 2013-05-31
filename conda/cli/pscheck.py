a8051cd483cdcaa78db560a07da00995cfd1ca52
Aaron Meurer <asmeurer@gmail.com>
2013-05-31 16:59:12 -0500

    Rearrange the try statement

--- conda/cli/pscheck.py
+++ conda/cli/pscheck.py
@@ -11,13 +11,13 @@ def main():
             p = psutil.Process(n)
         except psutil._error.NoSuchProcess:
             continue
-        for f in p.get_open_files():
-            try:
+        try:
+            for f in p.get_open_files():
                 if os.path.relpath(f.path).startswith(os.path.realpath(root_dir)):
                     print "Warning: the process %s is running" % p.name
                     conflicts = True
-            except psutil._error.AccessDenied:
-                pass
+        except psutil._error.AccessDenied:
+            pass
     return conflicts
 
 if __name__ == '__main__':
