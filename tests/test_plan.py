eb248d78769e48ed22eab3d53431a76a7770b8a8
Ilan Schnell <ilanschnell@gmail.com>
2013-05-30 22:40:30 -0500

    simplified how tests are run

--- tests/test_plan.py
+++ tests/test_plan.py
@@ -16,44 +16,45 @@ def solve(specs):
 class TestAddDeaultsToSpec(unittest.TestCase):
     # tests for plan.add_defaults_to_specs(r, linked, specs)
 
+    def check(self, specs, added):
+        new_specs = list(specs + added)
+        plan.add_defaults_to_specs(r, self.linked, specs)
+        self.assertEqual(specs, new_specs)
+
     def test_1(self):
-        linked = solve(['anaconda 1.5.0', 'python 2.7*', 'numpy 1.7*'])
-        for specs, output in [
-            (['python 3*'],  ['python 3*']),
-            (['python'],     ['python', 'python 2.7*']),
-            (['scipy'],      ['scipy', 'python 2.7*', 'numpy 1.7*']),
+        self.linked = solve(['anaconda 1.5.0', 'python 2.7*', 'numpy 1.7*'])
+        for specs, added in [
+            (['python 3*'],  []),
+            (['python'],     ['python 2.7*']),
+            (['scipy'],      ['python 2.7*', 'numpy 1.7*']),
             ]:
-            plan.add_defaults_to_specs(r, linked, specs)
-            self.assertEqual(specs, output)
+            self.check(specs, added)
 
     def test_2(self):
-        linked = solve(['anaconda 1.5.0', 'python 2.6*', 'numpy 1.6*'])
-        for specs, output in [
-            (['python'],     ['python', 'python 2.6*']),
-            (['numpy'],      ['numpy',  'python 2.6*', 'numpy 1.6*']),
-            (['pandas'],     ['pandas', 'python 2.6*', 'numpy 1.6*']),
+        self.linked = solve(['anaconda 1.5.0', 'python 2.6*', 'numpy 1.6*'])
+        for specs, added in [
+            (['python'],     ['python 2.6*']),
+            (['numpy'],      ['python 2.6*', 'numpy 1.6*']),
+            (['pandas'],     ['python 2.6*', 'numpy 1.6*']),
             ]:
-            plan.add_defaults_to_specs(r, linked, specs)
-            self.assertEqual(specs, output)
+            self.check(specs, added)
 
     def test_3(self):
-        linked = solve(['anaconda 1.5.0', 'python 3.3*'])
-        for specs, output in [
-            (['python'],     ['python', 'python 3.3*']),
-            (['scipy'],      ['scipy', 'python 3.3*', 'numpy 1.7*']),
+        self.linked = solve(['anaconda 1.5.0', 'python 3.3*'])
+        for specs, added in [
+            (['python'],     ['python 3.3*']),
+            (['scipy'],      ['python 3.3*', 'numpy 1.7*']),
             ]:
-            plan.add_defaults_to_specs(r, linked, specs)
-            self.assertEqual(specs, output)
+            self.check(specs, added)
 
     def test_4(self):
-        linked = []
-        for specs, output in [
-            (['python'],     ['python', 'python 2.7*']),
-            (['numpy'],      ['numpy', 'python 2.7*', 'numpy 1.7*']),
-            (['scipy'],      ['scipy', 'python 2.7*', 'numpy 1.7*']),
+        self.linked = []
+        for specs, added in [
+            (['python'],     ['python 2.7*']),
+            (['numpy'],      ['python 2.7*', 'numpy 1.7*']),
+            (['scipy'],      ['python 2.7*', 'numpy 1.7*']),
             ]:
-            plan.add_defaults_to_specs(r, linked, specs)
-            self.assertEqual(specs, output)
+            self.check(specs, added)
 
 
 if __name__ == '__main__':
