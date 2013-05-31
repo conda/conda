176d2d39fa0c4cfbf78d4db3067705df42721497
Ilan Schnell <ilanschnell@gmail.com>
2013-05-30 22:05:18 -0500

    adding tests for add_defaults_to_specs

---
+++ tests/test_plan.py
@@ -0,0 +1,47 @@
+import json
+import unittest
+from os.path import dirname, join
+
+import conda.plan as plan
+from conda.resolve import Resolve
+
+
+with open(join(dirname(__file__), 'index.json')) as fi:
+    r = Resolve(json.load(fi))
+
+def solve(specs):
+    return [fn[:-8] for fn in r.solve(specs)]
+
+
+class TestAddDeaultsToSpec(unittest.TestCase):
+    # tests for plan.add_defaults_to_specs(r, linked, specs)
+
+    def test_1(self):
+        linked = solve(['anaconda 1.5.0', 'python 2.7*', 'numpy 1.7*'])
+        for specs, output in [
+            (['python 3*'],  ['python 3*']),
+            (['python'],     ['python', 'python 2.7*']),
+            ]:
+            plan.add_defaults_to_specs(r, linked, specs)
+            self.assertEqual(specs, output)
+
+    def test_2(self):
+        linked = solve(['anaconda 1.5.0', 'python 2.6*', 'numpy 1.6*'])
+        for specs, output in [
+            (['python'],     ['python', 'python 2.6*']),
+            (['numpy'],      ['numpy',  'python 2.6*', 'numpy 1.6*']),
+            ]:
+            plan.add_defaults_to_specs(r, linked, specs)
+            self.assertEqual(specs, output)
+
+    def test_3(self):
+        linked = solve(['anaconda 1.5.0', 'python 3.3*'])
+        for specs, output in [
+            (['python'],     ['python', 'python 3.3*']),
+            ]:
+            plan.add_defaults_to_specs(r, linked, specs)
+            self.assertEqual(specs, output)
+
+
+if __name__ == '__main__':
+    unittest.main()
