cbe2b38bef69f8e86dea4a33fb290508d7d554d5
Aaron Meurer <asmeurer@gmail.com>
2013-05-31 16:56:14 -0500

    First try at a psutil based process checker

--- conda/cli/pscheck.py
+++ conda/cli/pscheck.py
@@ -1,11 +1,24 @@
+import os
+
 import psutil
 
-
+from conda.config import root_dir
 
 def main():
-    # TODO
-    pass
-
+    conflicts = False
+    for n in psutil.get_pid_list():
+        try:
+            p = psutil.Process(n)
+        except psutil._error.NoSuchProcess:
+            continue
+        for f in p.get_open_files():
+            try:
+                if os.path.relpath(f.path).startswith(os.path.realpath(root_dir)):
+                    print "Warning: the process %s is running" % p.name
+                    conflicts = True
+            except psutil._error.AccessDenied:
+                pass
+    return conflicts
 
 if __name__ == '__main__':
     main()
