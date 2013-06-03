365bd91f5307fa165e4de751789311d0a49ba42d
Maggie Mari <maggie.mari@continuum.io>
2013-06-03 12:52:36 -0500

    Improved smoketest script, made logging optional.

--- utils/smoketest.py
+++ utils/smoketest.py
@@ -1,5 +1,7 @@
 import os
 import subprocess as sp
+import sys
+import time
 
 from os.path import exists, join
 from shutil import rmtree
@@ -13,73 +15,31 @@ myenv = join(base, "env")
 # os.environ['CIO_TEST'] = 2
 
 cmds = [
-    (
-        "conda info"
-    ),
-    (
-        "conda list ^m.*lib$"
-    ),
-    (
-        "conda search ^m.*lib$"
-    ),
-    (
-        "conda search --all -v numpy"
-    ),
-    (
-        "conda depends numpy"
-    ),
-    (
-        "conda info -e"
-    ),
-    (
-        "conda create --yes -p %s sqlite python=2.6" % myenv
-    ),
-    (
-        "conda install --yes -p %s pandas=0.8.1" % myenv
-    ),
-    (
-        "conda install --yes -p %s numba=0.1.1" % myenv
-    ),
-    (
-        "conda install --yes -p %s cython=0.16" % myenv
-    ),
-    (
-        "conda install --yes -p %s cython=0.17.3" % myenv
-    ),
-    (
-        "conda install --yes -p %s accelerate" % myenv
-    ),
-    (
-        "conda remove --yes -p %s accelerate" % myenv
-    ),
-    (
-        "conda update --yes -p %s pandas" % myenv
-    ),
-    (
-        "conda install --yes -p %s iopro" % myenv
-    ),
-    (
-        "conda remove --yes -p %s iopro" % myenv
-    ),
-    (
-        "conda local --yes -d numba-0.3.1-np17py27_0"
-    ),
-    (
-        "conda env --yes -lp %s numba-0.3.1-np17py27_0" % myenv
-    ),
-    (
-        "conda env --yes -up %s sqlite-3.7.13-0" % myenv
-    ),
-    (
-        "conda local --yes -r zeromq-2.2.0-0"
-    ),
-    (
-        "conda local --yes -d zeromq-2.2.0-0"
-    )
+    "conda info",
+    "conda list ^m.*lib$",
+    "conda search ^m.*lib$",
+    "conda search -v numpy",    
+    "conda search -c numpy",
+    "conda info -e",
+    "conda creae --yes -p %s sqlite python=2.6" % myenv,
+    "conda install --yes -p %s pandas=0.8.1" % myenv,
+    "conda install --yes -p %s numba=0.1.1" % myenv,
+    "conda install --yes -p %s cython=0.16" % myenv,
+    "conda install --yes -p %s cython=0.17.3" % myenv,
+    "conda install --yes -p %s accelerate" % myenv,
+    "conda remove --yes -p %s accelerate" % myenv,
+    "conda update --yes -p %s pandas" % myenv,
+    "conda install --yes -p %s iopro" % myenv,
+    "conda remove --yes -p %s iopro" % myenv,
+    "conda info -e",
+    "conda info -a",
+    "conda info --license",
+    "conda info -s",
 ]
 
 def tester(commands):
     cmds = commands
+    errs = []
     fails = []
     for cmd in cmds:
         print "-"*120
@@ -91,7 +51,7 @@ def tester(commands):
             ret = child.returncode
             if ret != 0:
                 print "\nFAILED\n"
-                f.write("\n%s\n \n%s" % (cmd, err))
+                errs.append("\n%s\n \n%s" % (cmd, err))
                 fails.append(cmd)
             else:
                 print "\nPASSED\n"
@@ -99,21 +59,25 @@ def tester(commands):
             print e
             f.write("\nThe script had the following error running %s: %s" % (cmd, e))
 
-    return fails
+    return (fails, errs)
 
 
 if __name__ == '__main__':
-    TESTLOG="conda-testlog.txt"
-    if exists(TESTLOG):
-        os.remove(TESTLOG)
-    f = open(TESTLOG, "w")
-    fails = tester(cmds)
-    f.close()
+    TESTLOG = ''
+    if len(sys.argv) == 2 and sys.argv[1] == 'log':
+        TESTLOG="conda-testlog.txt"
+    fails, errs = tester(cmds)
     if fails:
         print "These commands failed: \n"
         for line, fail in enumerate(fails, 1):
             print "%d: %s\n" % (line, fail)
         print "Writing failed commands to conda-testlog.txt"
+        header = 'Test Results For %s' % time.asctime()
+        if TESTLOG:
+            with open(TESTLOG, "a") as f:
+                f.write('%s\n%s\n' % (header, '-'*len(header)))
+                for error in errs:
+                    f.write(error)
 
     try:
         rmtree(myenv)
