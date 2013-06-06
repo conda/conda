29888d93bc7fe0fc34b43f75a431edca192805bf
Ilan Schnell <ilanschnell@gmail.com>
2013-06-06 14:57:42 -0500

    add functions to conda.builder.utils

--- conda/builder/utils.py
+++ conda/builder/utils.py
@@ -1,4 +1,13 @@
-from os.path import normpath, getmtime, getsize
+import os
+import sys
+import hashlib
+import shutil
+import tarfile
+import urllib2
+import zipfile
+import subprocess
+from os.path import (dirname, getmtime, getsize, isdir, isfile, islink,
+                     join, normpath)
 
 from conda.utils import md5_file
 
@@ -10,6 +19,69 @@ def rel_lib(f):
         return normpath((f.count('/') - 1) * '../')
     else:
         return normpath(f.count('/') * '../') + '/lib'
+
+
+def download(url, dst_path, md5=None):
+    try:
+        fi = urllib2.urlopen(url)
+    except urllib2.URLError:
+        raise urllib2.URLError("Error could not open URL: %r" % url)
+    data = fi.read()
+    fi.close()
+    if md5:
+        assert hashlib.md5(data).hexdigest() == md5
+    with open(dst_path, 'wb') as fo:
+        fo.write(data)
+
+
+def bzip2(path, verbose=True):
+    if verbose:
+        print "bz2ing:", path
+    subprocess.check_call(['bzip2', '-f', '-k', path])
+    assert isfile(path + '.bz2')
+
+
+def bunzip2(bz2path, verbose=False):
+    assert bz2path.endswith('.bz2')
+    path = bz2path[:-4]
+    if verbose:
+        print "bunz2ing:", bz2path
+    subprocess.check_call(['bunzip2', '-f', '-k', bz2path])
+    assert isfile(path)
+
+
+def tar_xf(tarball, dir_path, mode='r:*'):
+    if tarball.endswith('.tar.xz'):
+        subprocess.check_call(['unxz', '-f', '-k', tarball])
+        tarball = tarball[:-3]
+    t = tarfile.open(tarball, mode)
+    t.extractall(path=dir_path)
+    t.close()
+
+
+def unzip(zip_path, dir_path):
+    z = zipfile.ZipFile(zip_path)
+    for name in z.namelist():
+        if name.endswith('/'):
+            continue
+        path = join(dir_path, *name.split('/'))
+        dp = dirname(path)
+        if not isdir(dp):
+            os.makedirs(dp)
+        with open(path, 'wb') as fo:
+            fo.write(z.read(name))
+    z.close()
+
+
+def rm_rf(path):
+    if islink(path) or isfile(path):
+        os.unlink(path)
+
+    elif isdir(path):
+        if sys.platform == 'win32':
+            subprocess.check_call(['cmd', '/c', 'rd', '/s', '/q', path])
+        else:
+            shutil.rmtree(path)
 
 
 def file_info(path):
