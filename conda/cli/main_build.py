8cb725cf2323e4b4a34cbfc9f5cd69866cb3d30b
Ilan Schnell <ilanschnell@gmail.com>
2013-06-08 17:44:00 -0500

    update build CLI

--- conda/cli/main_build.py
+++ conda/cli/main_build.py
@@ -1,76 +1,45 @@
-# (c) 2012 Continuum Analytics, Inc. / http://continuum.io
+# (c) 2012-2013 Continuum Analytics, Inc. / http://continuum.io
 # All Rights Reserved
 #
 # conda is distributed under the terms of the BSD 3-clause license.
 # Consult LICENSE.txt or http://opensource.org/licenses/BSD-3-Clause.
 
-import sys
-import common
-
-
-descr = "Build a package from source. (EXPERIMENTAL)"
-
-source_types = 'git', 'tar', 'zip', 'svn', 'dir'
+descr = "Build a package from recipe. (EXPERIMENTAL)"
 
 
 def configure_parser(sub_parsers):
     p = sub_parsers.add_parser('build', description=descr, help=descr)
-    npgroup = p.add_mutually_exclusive_group()
-    npgroup.add_argument("--git",
-                         action="store_true",
-                         help="build from git url")
-    npgroup.add_argument("--tar",
-                         action="store_true",
-                         help="build from local source tarball")
-    npgroup.add_argument("--zip",
-                         action="store_true",
-                         help="build from local source zipfile")
-    npgroup.add_argument("--svn",
-                         action="store_true",
-                         help = "build from svn")
-    npgroup.add_argument("--dir",
-                         action="store_true",
-                         help="build from local source directory")
-    common.add_parser_prefix(p)
-    p.add_argument('url',
+
+    p.add_argument(
+        '-s', "--source",
+        action  = "store_true",
+        help    = "only obtain the source (but don't build)",
+    )
+    p.add_argument(
+        '-t', "--test",
+        action  = "store_true",
+        help    = "test package (assumes package is already build)",
+    )
+    p.add_argument('recipe',
                    action="store",
-                   metavar='URL',
-                   nargs=1,
-                   help="name of source (url, tarball, path, etc...)",
+                   metavar='PATH',
+                   nargs='+',
+                   help="path to recipe directory",
     )
     p.set_defaults(func=execute)
 
 
-def get_source_type(args):
-    from os.path import isdir, isfile
-
-
-    for opt_name in source_types:
-        if getattr(args, opt_name):
-            return opt_name
-    # try to determine source from url
-    url = args.url[0]
-    if isfile(url):
-        if url.endswith(('.tar.gz', '.tar.bz2', '.tgz', '.tar')):
-            return 'tar'
-        if url.endswith('.zip'):
-            return 'zip'
-    if isdir(url):
-        return 'dir'
-    if url.endswith('.git'):
-        return 'git'
-    # we don't know
-    return None
-
-
 def execute(args, parser):
-    from conda.builder.commands import build
+    import conda.builder.build as build
+    import conda.builder.source as source
+    from conda.builder.metadata import MetaData
 
-    prefix = common.get_prefix(args)
-    source_type = get_source_type(args)
-    if source_type is None:
-        sys.exit('Error: Could not determine source type, please provide one\n'
-                 'of the following options: %s' %
-                 ', '.join('--' + s for s in source_types))
-
-    build(prefix, args.url[0], source_type)
+    for arg in args.recipe:
+        m = MetaData(arg)
+        if args.test:
+            build.test(m)
+        elif args.source:
+            source.provide(m.path, m.get_section('source'))
+            print 'Source tree in:', source.get_dir()
+        else:
+            build.build(m)
