33644efd5d1bd62535511b5c842809adc18c1a39
Ilan Schnell <ilanschnell@gmail.com>
2013-06-09 22:41:00 -0500

    better names

--- conda/console.py
+++ conda/console.py
@@ -3,29 +3,29 @@ import logging
 from progressbar import Bar, ETA, FileTransferSpeed, Percentage, ProgressBar
 
 
-progress = ProgressBar(
+fetch_progress = ProgressBar(
     widgets=['', ' ', Percentage(), ' ', Bar(), ' ', ETA(), ' ',
              FileTransferSpeed()])
 
 
-class ConsoleProgressHandler(logging.Handler):
+class FetchProgressHandler(logging.Handler):
 
     def emit(self, record):
-        if record.name == 'progress.start':
+        if record.name == 'fetch.start':
             d = record.msg
-            progress.widgets[0] = d['filename']
-            progress.maxval = d['maxval']
-            progress.start()
+            fetch_progress.widgets[0] = d['filename']
+            fetch_progress.maxval = d['maxval']
+            fetch_progress.start()
 
-        elif record.name == 'progress.update':
+        elif record.name == 'fetch.update':
             n = record.msg
-            progress.update(n)
+            fetch_progress.update(n)
 
-        elif record.name == 'progress.stop':
-            progress.finish()
+        elif record.name == 'fetch.stop':
+            fetch_progress.finish()
 
 
 def setup_handlers():
-    prog_logger = logging.getLogger('progress')
+    prog_logger = logging.getLogger('fetch')
     prog_logger.setLevel(logging.INFO)
-    prog_logger.addHandler(ConsoleProgressHandler())
+    prog_logger.addHandler(FetchProgressHandler())
