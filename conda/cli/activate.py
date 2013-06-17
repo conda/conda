a1528601f20a4058252c27eb15578cc57fc85b98
Aaron Meurer <asmeurer@gmail.com>
2013-06-17 14:49:55 -0500

    Change the semantics of activate and deactivate
    Now you cannot activate twice, and you cannot deactivate unless you first
    activate.  Furthermore, activate takes exactly one argument (so activating the
    default environment is no longer possible), and deactivate takes no arguments
    (it always deactivates the activated environment).
    
    This fixes #146.

--- conda/cli/activate.py
+++ conda/cli/activate.py
@@ -2,7 +2,7 @@ import os
 import sys
 from os.path import isdir, join
 
-
+import conda.config
 
 def help():
     if sys.argv[1] == '..activate':
@@ -12,7 +12,7 @@ adds the 'bin' directory of the environment ENV to the front of PATH.
 ENV may either refer to just the name of the environment, or the full
 prefix path.""")
     else: # ..deactivate
-        sys.exit("""Usage: source deactivate ENV
+        sys.exit("""Usage: source deactivate
 
 removes the 'bin' directory of the environment ENV from PATH.
 ENV may either refer to just the name of the environment, or the full
@@ -24,19 +24,26 @@ def main():
     if '-h' in sys.argv or '--help' in sys.argv:
         help()
 
-    if len(sys.argv) == 2:
-        sys.exit("Error: no environment provided.")
-    elif len(sys.argv) == 3:
-        binpath = join(sys.prefix, 'envs', sys.argv[2], 'bin')
-    else:
-        sys.exit("Error: did not expect more than one argument")
-
     if sys.argv[1] == '..activate':
+        if len(sys.argv) == 2:
+            sys.exit("Error: no environment provided.")
+        elif len(sys.argv) == 3:
+            binpath = join(conda.config.root_dir, 'envs', sys.argv[2], 'bin')
+        else:
+            sys.exit("Error: did not expect more than one argument")
+
         if not isdir(binpath):
             sys.exit("Error: no such directory: %s" % binpath)
         paths = [binpath]
         sys.stderr.write("prepending %s to PATH\n" % binpath)
     else: # ..deactivate
+        if len(sys.argv) != 2:
+            sys.exit("Error: too many arguments.")
+
+        if 'CONDA_DEFAULT_ENV' not in os.environ:
+            sys.exit("Error: No environment to deactivate")
+        binpath = join(conda.config.root_dir, 'envs',
+            os.getenv('CONDA_DEFAULT_ENV'), 'bin')
         paths = []
         sys.stderr.write("discarding %s from PATH\n" % binpath)
 
