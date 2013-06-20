4d700fb5608277db9c772b03cab4a737b44623fd
Aaron Meurer <asmeurer@gmail.com>
2013-06-20 15:59:18 -0500

    Use %PYTHON% in conda skeleton pypi bld.bat

--- conda/builder/pypi.py
+++ conda/builder/pypi.py
@@ -84,7 +84,7 @@ $PYTHON setup.py install
 """
 
 PYPI_BLD_BAT = """\
-python setup.py install
+"%PYTHON%" setup.py install
 if errorlevel 1 exit 1
 
 :: Add more build steps here, if they are necessary.
