9c26c6004e0b665f1d9ac8e12dec75d7b803290a
Ilan Schnell <ilanschnell@gmail.com>
2013-08-13 18:17:39 -0500

    add LOCKFN variable, simplified some locking code

--- conda/lock.py
+++ conda/lock.py
@@ -25,49 +25,52 @@ import errno
 from conda import config
 
 
-def create_lock(path, name):
+LOCKFN = '.conda_lock'
+
+
+def create_lock(lock_path):
     """
     Creates a lock at `path`. Returns True if the file was created and
     False if the file already exists.
     """
     # Note, we do this instead of os.path.exists to avoid race conditions
     try:
-        os.makedirs(join(path, name))
+        os.makedirs(lock_path)
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise RuntimeError("LOCKERROR: Could not create the lock %s: %s" %
-                (join(path, name), e.strerror))
+                               lock_path, e.strerror)
         return False
     return True
 
 
-def remove_lock(path, name):
+def remove_lock(lock_path):
     # Intentionally raise an exception if the directory is not there, or if it
     # is nonempty.
-    os.rmdir(join(path, name))
+    os.rmdir(lock_path)
 
 
 class Locked(object):
     """
     Context manager to handle locks.
     """
-    def __init__(self, path=config.root_dir, name=".conda_lock"):
+    def __init__(self, path=config.root_dir):
         self.path = path
-        self.name = name
+        self.lock_path = join(self.path, LOCKFN)
 
     def __enter__(self):
-        lock = create_lock(self.path, self.name)
+        lock = create_lock(self.lock_path)
         if not lock:
             # Keep the string "LOCKERROR" in this string so that external
             # programs can look for it.
-            raise RuntimeError(
-                "LOCKERROR: It looks like conda is already doing "
-                "something.  The lock %s was found. Wait for it to finish "
-                "before continuing. If you are sure that conda is not running, "
-                "remove it and try again." % join(self.path, self.name, ''))
+            raise RuntimeError("""\
+LOCKERROR: It looks like conda is already doing something.
+The lock %s was found. Wait for it to finish before continuing.
+If you are sure that conda is not running, remove it and try again.""" %
+                               self.lock_path)
 
     def __exit__(self, exc_type, exc_value, traceback):
-        remove_lock(self.path, self.name)
+        remove_lock(self.lock_path)
         try:
             # Remove the locked path if it is empty, since this means that it
             # did not exist when we created the lock.
