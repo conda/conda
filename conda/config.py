155e12ab101d4de4db55f21eabc2b09d68a90406
Ilan Schnell <ilanschnell@gmail.com>
2013-09-30 09:03:20 -0500

    add error handling

--- conda/config.py
+++ conda/config.py
@@ -24,10 +24,15 @@ default_numpy = '1.7'
 
 # ----- operating system and architecture -----
 
+_ARCH = os.getenv('ARCH')
+if _ARCH not in (None, '', '32', '64'):
+    sys.exit("Error: unreasonable value for ARCH: %s  (expected 32 or 64)"
+             % _ARCH)
+
 _sys_map = {'linux2': 'linux', 'linux': 'linux',
             'darwin': 'osx', 'win32': 'win'}
 platform = _sys_map.get(sys.platform, 'unknown')
-bits = int(os.getenv('ARCH', 8 * tuple.__itemsize__))
+bits = int(_ARCH or 8 * tuple.__itemsize__)
 
 if platform == 'linux' and machine() == 'armv6l':
     subdir = 'linux-armv6l'
