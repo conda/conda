d36307acde00b648bffebaacb4fa29af49856b97
Travis E. Oliphant <teoliphant@gmail.com>
2013-10-24 23:50:30 -0500

    Add pip installed packages to conda list.

--- conda/cli/main_list.py
+++ conda/cli/main_list.py
@@ -8,7 +8,8 @@ from __future__ import print_function, division, absolute_import
 
 import re
 import sys
-from os.path import isdir
+from os.path import isdir, join
+import subprocess
 
 import conda.install as install
 import conda.config as config
@@ -37,6 +38,12 @@ def configure_parser(sub_parsers):
                   "(output may be used by conda create --file)",
     )
     p.add_argument(
+        "--no-pip",
+        action = "store_false",
+        default=True,
+        dest="pip",
+        help = "Do not include pip-only installed packages")
+    p.add_argument(
         'regex',
         action = "store",
         nargs = "?",
@@ -51,7 +58,39 @@ def print_export_header():
     print('# platform: %s' % config.subdir)
 
 
-def list_packages(prefix, regex=None, format='human'):
+def _linked(prefix, piplist):
+    installed = install.linked(prefix)
+    if piplist:
+        pip_path = join(prefix,'bin','pip')
+        pipinst = subprocess.check_output([pip_path,'list']).split('\n')
+        # For every package in pipinst that is not already represented
+        # in installed append a fake name to installed with 'pip'
+        # as the build string
+        for pkg in pipinst:
+            if not pkg:
+                continue
+            name, version = pkg.split()
+            name = name.lower()
+            # remove '()'
+            version = version[1:-1]
+            # This search could be made faster with a sorted 
+            # list and binary search
+            # The matching is not always perfect
+            found = False
+            for condapkg in installed:
+                cname, cversion = condapkg.rsplit('-',2)[:2]
+                if name == cname and version == cversion:
+                    found = True
+                    break
+            if not found:
+                installed.add('%s-%s-*pip*'%(name, version))
+
+    return installed
+
+def _is_linked(prefix, dist, piplist):
+    pass
+
+def list_packages(prefix, regex=None, format='human', piplist=True):
     if not isdir(prefix):
         sys.exit("""\
 Error: environment does not exist: %s
@@ -66,7 +105,7 @@ Error: environment does not exist: %s
     if format == 'export':
         print_export_header()
 
-    for dist in sorted(install.linked(prefix)):
+    for dist in sorted(_linked(prefix, piplist)):
         name = dist.rsplit('-', 2)[0]
         if pat and pat.search(name) is None:
             continue
@@ -78,6 +117,7 @@ Error: environment does not exist: %s
             print('='.join(dist.rsplit('-', 2)))
             continue
         try:
+            # Returns None if no meta-file found (e.g. pip install)
             info = install.is_linked(prefix, dist)
             features = set(info.get('features', '').split())
             print('%-25s %-15s %15s  %s' % (info['name'],
@@ -99,4 +139,4 @@ def execute(args, parser):
         format = 'export'
     else:
         format = 'human'
-    sys.exit(list_packages(prefix, args.regex, format=format))
+    sys.exit(list_packages(prefix, args.regex, format=format, piplist=args.pip))
