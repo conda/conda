ad4b4adcb11592824fac16ee908f6bf78a5b44c6
Ilan Schnell <ilanschnell@gmail.com>
2013-11-10 16:25:55 -0600

    remove unused code

--- conda/builder/header_test.py
+++ conda/builder/header_test.py
@@ -4,16 +4,10 @@ import subprocess
 from distutils.spawn import find_executable
 
 
-if sys.platform == 'win32':
-    bin_dir = sys.prefix + r'\Scripts'
-else:
-    bin_dir = sys.prefix + '/bin'
-
-
 def call_args(string):
     args = string.split()
     arg0 = args[0]
-    args[0] = find_executable(arg0, path=os.environ['PATH'])
+    args[0] = find_executable(arg0)
     if not args[0]:
         sys.exit("Command not found: '%s'" % arg0)
 
