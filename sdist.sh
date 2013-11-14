a222db921dd15834137337ef07748b331cfb0a85
Ilan Schnell <ilanschnell@gmail.com>
2013-11-14 17:55:49 -0600

    udpate work on sdist

--- sdist.sh
+++ sdist.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-rm -rf build dist docs/build conda.egg-info
-
-VERSION=$(git describe)
+VERSION=$(git describe --dirty)
 echo "VERSION: '$VERSION'"
+
+echo $VERSION | grep dirty
+if (( $? )); then
+    echo "CLEAN"
+else
+    echo "DIRTY"
+    echo "Error: You must commit your changes before creating a tarball." 
+    exit 1
+fi
+
+rm -rf build dist docs/build conda.egg-info
 
 cat <<EOF >conda/__init__.py
 __version__ = '$VERSION'
 EOF
 
 replace 'version=versioneer.get_version(),' "version='$VERSION'," setup.py
-
 sdist
-
 git reset --hard
