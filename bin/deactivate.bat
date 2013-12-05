3dc2a4f8788d94a31a1ff34ff429eee3b501eeab
Mark Wiebe <mwwiebe@gmail.com>
2013-12-04 18:19:54 -0800

    Add a deactivate script for windows

---
+++ bin/deactivate.bat
@@ -0,0 +1,25 @@
+@echo off
+
+for /f %%i in ("%~dp0..\envs") do (
+    set ANACONDA_ENVS=%%~fi
+)
+
+if "%1" == "" goto skipmissingarg
+    echo Usage: deactivate
+    echo.
+    echo Deactivates previously activated Conda
+    echo environment.
+    exit /b 1
+:skipmissingarg
+
+REM Deactivate a previous activation if it is live
+if "%CONDA_DEFAULT_ENV%" == "" goto skipdeactivate
+    REM This search/replace removes the previous env from the path
+    echo Deactivating environment "%CONDA_DEFAULT_ENV%"...
+    set CONDACTIVATE_PATH=%ANACONDA_ENVS%\%CONDA_DEFAULT_ENV%;%ANACONDA_ENVS%\%CONDA_DEFAULT_ENV%\Scripts;
+    call set PATH=%%PATH:%CONDACTIVATE_PATH%=%%
+    set CONDA_DEFAULT_ENV=
+    set CONDACTIVATE_PATH=
+:skipdeactivate
+
+set PROMPT=$P$G
