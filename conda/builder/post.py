conda/conda

commit 75980ea3a3974d0138256bf2bfbc352805a5fe3b

Fix building when some of the built files are symbolic links

conda/builder/post.py
@@ -175,8 +175,8 @@ def fix_permissions(files):
 
     for f in files:
         path = join(build_prefix, f)
-        st = os.stat(path)
-        os.chmod(path, stat.S_IMODE(st.st_mode) | stat.S_IWUSR) # chmod u+w
+        st = os.lstat(path)
+        os.lchmod(path, stat.S_IMODE(st.st_mode) | stat.S_IWUSR) # chmod u+w
 
 
 def post_build(files):

conda/builder/build.py
@@ -57,6 +57,8 @@ def have_prefix_files(files):
             continue
         if is_obj(path):
             continue
+        if os.path.islink(path):
+            continue
         try:
             with open(path) as fi:
                 data = fi.read()

conda/install.py
@@ -170,7 +170,7 @@ def update_prefix(path, new_prefix):
                             new_prefix.encode('utf-8'))
     if new_data == data:
         return
-    st = os.stat(path)
+    st = os.lstat(path)
     os.unlink(path)
     with open(path, 'wb') as fo:
         fo.write(new_data)

commit 3f19c2eb74e04040ea81ef8f966a8ad221499ba7

Fix bug introduced in 75980ea re: chmod/lchmod.
os.lchmod does not exist on Windows or Linux.

conda/builder/post.py
@@ -19,6 +19,11 @@ if sys.platform.startswith('linux'):
 elif sys.platform == 'darwin':
     from conda.builder import macho
 
+try:
+    # lchmod tends to be a BSD thing; it's not present on Linux/Windows
+    chmod = os.lchmod
+except AttributeError:
+    chmod = os.chmod
 
 
 def is_obj(path):
@@ -184,7 +189,7 @@ def fix_permissions(files):
     for f in files:
         path = join(build_prefix, f)
         st = os.lstat(path)
-        os.lchmod(path, stat.S_IMODE(st.st_mode) | stat.S_IWUSR) # chmod u+w
+        chmod(path, stat.S_IMODE(st.st_mode) | stat.S_IWUSR) # chmod u+w
 
 
 def post_build(files):