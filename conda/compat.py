bcf5d8651e986693351e8fdf609846a904e180e6
Aaron Meurer <asmeurer@gmail.com>
2014-01-03 11:54:36 -0600

    follow_symlinks raises NotImplementedError

--- conda/compat.py
+++ conda/compat.py
@@ -20,7 +20,7 @@ if PY3:
     def lchmod(path, mode):
         try:
             os.chmod(path, mode, follow_symlinks=False)
-        except TypeError:
+        except (TypeError, NotImplementedError):
             # On systems that don't allow permissions on symbolic links, skip
             # this entirely.
             pass
