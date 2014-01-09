e0cd43c5abec0aaf68cd257c9e23478d3aec5742
Aaron Meurer <asmeurer@gmail.com>
2014-01-09 14:27:30 -0600

    Use the basicstrap theme, as it supports font awesome

--- web/source/conf.py
+++ web/source/conf.py
@@ -92,16 +92,19 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-import sphinx_bootstrap_theme
-html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+# import sphinx_bootstrap_theme
+# html_theme = 'bootstrap'
+# html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+
+extensions += ['sphinxjp.themes.basicstrap']
+html_theme = 'basicstrap'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    'bootswatch_theme': "spacelab",
-}
+# html_theme_options = {
+#     'bootswatch_theme': "spacelab",
+# }
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
