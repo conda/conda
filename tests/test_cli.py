4c8780bb25cf7e0a232b0d68383596974bd1d9b1
Aaron Meurer <asmeurer@gmail.com>
2014-02-07 11:21:29 -0600

    Make sure conda tests always run this conda
    Remove setup.py develop from Travis so that we test this there.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -26,7 +26,11 @@ python = sys.executable
 conda = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'bin', 'conda')
 
 def run_conda_command(*args):
-    p= subprocess.Popen((python, conda,) + args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    env = os.environ.copy()
+    # Make sure bin/conda imports *this* conda.
+    env['PYTHONPATH'] = os.path.dirname(os.path.dirname(__file__))
+    p= subprocess.Popen((python, conda,) + args, stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE, env=env)
     stdout, stderr = p.communicate()
     return (stdout.decode('utf-8').replace('\r\n', '\n'),
         stderr.decode('utf-8').replace('\r\n', '\n'))
