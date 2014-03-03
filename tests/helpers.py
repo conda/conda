7ab8991b3238a9ea524ff37116a7c9dc66f26b68
Aaron Meurer <asmeurer@gmail.com>
2014-03-03 14:34:02 -0600

    Add ability for raises to check for a substring
    This is necessary because so many different errors are just RuntimeError.

--- tests/helpers.py
+++ tests/helpers.py
@@ -2,9 +2,11 @@
 Helpers for the tests
 """
 
-def raises(exception, func):
+def raises(exception, func, string=None):
     try:
         a = func()
-    except exception:
+    except exception as e:
+        if string:
+            assert string in e.args[0]
         return True
     raise Exception("did not raise, gave %s" % a)
