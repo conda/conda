db600e19f4209df7422e29517784e26088442772
Aaron Meurer <asmeurer@gmail.com>
2014-03-03 15:07:44 -0600

    Fix imports in the tests
    Previously they did not work when py.test was called from a different
    location. This required making the tests a package (adding __init__.py).

--- tests/test_resolve.py
+++ tests/test_resolve.py
@@ -5,7 +5,7 @@ from os.path import dirname, join
 
 from conda.resolve import MatchSpec, Package, Resolve
 
-from tests.helpers import raises
+from .helpers import raises
 
 
 with open(join(dirname(__file__), 'index.json')) as fi:
