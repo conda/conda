5de6bc0a9ba6c5de512ed23172d16c824a0f18dd
Aaron Meurer <asmeurer@gmail.com>
2014-03-11 17:28:25 -0500

    Split up and add more activate tests

--- tests/test_activate.py
+++ tests/test_activate.py
@@ -22,31 +22,65 @@ activate = join(dirname(dirname(__file__)), 'bin', 'activate')
 deactivate = join(dirname(dirname(__file__)), 'bin', 'deactivate')
 # Make sure the subprocess activate calls this python
 syspath = dirname(sys.executable)
+PATH = ':'.join([syspath, '/bin', '/usr/bin'])
 
-def test_activate():
+setup = """\
+export PATH="{PATH}"
+export PS1='$'
+cd {here}
+""".format(here=dirname(__file__), PATH=PATH)
+
+setup = setup + """
+mkdir -p {envs}/test1/bin
+mkdir -p {envs}/test2/bin
+"""
+
+def test_activate_test1():
     for shell in shells:
         with TemporaryDirectory(prefix='envs', dir=dirname(__file__)) as envs:
-            PATH = ':'.join([syspath, '/bin', '/usr/bin'])
-            setup = """\
-            export PATH="{PATH}"
-            export PS1='$'
-            cd {here}
-            """.format(here=dirname(__file__), PATH=PATH)
-
-            setup_envs = setup + """\
-            mkdir -p {envs}/test1/bin
-            mkdir -p {envs}/test2/bin
-            """.format(envs=envs)
-
-            stdout, stderr = run_in(setup_envs, shell)
-            assert not stdout
-            assert not stderr
-
-            activate_test1 = setup + """
+            commands = (setup + """
             source {activate} {envs}/test1
             printf $PATH
-            """.format(envs=envs, activate=activate)
+            """).format(envs=envs, activate=activate)
 
-            stdout, stderr = run_in(activate_test1, shell)
+            stdout, stderr = run_in(commands, shell)
             assert stdout == envs + "/test1/bin:" + PATH
             assert stderr == 'prepending {envs}/test1/bin to PATH\n'.format(envs=envs)
+
+def test_activate_test1_test2():
+    for shell in shells:
+        with TemporaryDirectory(prefix='envs', dir=dirname(__file__)) as envs:
+            commands = (setup + """
+            source {activate} {envs}/test1 2> /dev/null
+            source {activate} {envs}/test2
+            printf $PATH
+            """).format(envs=envs, activate=activate)
+
+            stdout, stderr = run_in(commands, shell)
+            assert stdout == envs + "/test2/bin:" + PATH
+            assert stderr == 'discarding {envs}/test1/bin from PATH\nprepending {envs}/test2/bin to PATH\n'.format(envs=envs)
+
+def test_activate_test3():
+    for shell in shells:
+        with TemporaryDirectory(prefix='envs', dir=dirname(__file__)) as envs:
+            commands = (setup + """
+            source {activate} {envs}/test3
+            printf $PATH
+            """).format(envs=envs, activate=activate)
+
+            stdout, stderr = run_in(commands, shell)
+            assert stdout == PATH
+            assert stderr == 'Error: no such directory: {envs}/test3/bin\n'.format(envs=envs)
+
+def test_activate_test1_test3():
+    for shell in shells:
+        with TemporaryDirectory(prefix='envs', dir=dirname(__file__)) as envs:
+            commands = (setup + """
+            source {activate} {envs}/test1 2> /dev/null
+            source {activate} {envs}/test3
+            printf $PATH
+            """).format(envs=envs, activate=activate)
+
+            stdout, stderr = run_in(commands, shell)
+            assert stdout == envs + "/test1/bin:" + PATH
+            assert stderr == 'Error: no such directory: {envs}/test3/bin\n'.format(envs=envs)
