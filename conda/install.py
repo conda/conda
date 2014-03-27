1a2018c12de9c4514216c46734bd153f43da4fa8
Dan Blanchard <dan.blanchard@gmail.com>
2014-03-27 14:54:17 -0400

    Don't try to maintain hard links on Windows

--- conda/install.py
+++ conda/install.py
@@ -398,11 +398,12 @@ def link(pkgs_dir, prefix, dist, linktype=LINK_HARD):
             no_softlink = set()
 
     with Locked(prefix), Locked(pkgs_dir):
-        # Build mapping from inode to filenames to maintain hardlinks
-        inode_dict = defaultdict(list)
-        for f in files:
-            inode = os.lstat(os.path.realpath(join(source_dir, f))).st_ino
-            inode_dict[inode].append(f)
+        if not on_win:
+            # Build mapping from inode to filenames to maintain hardlinks
+            inode_dict = defaultdict(list)
+            for f in files:
+                inode = os.lstat(os.path.realpath(join(source_dir, f))).st_ino
+                inode_dict[inode].append(f)
 
         # Use while loop so we can modify file_set on the fly
         file_set = set(files)
@@ -431,19 +432,21 @@ def link(pkgs_dir, prefix, dist, linktype=LINK_HARD):
                           (src, dst, lt, e))
             # If this was a hard link in archive, make sure we maintain those
             # links even when link type is copy
-            inode = os.lstat(os.path.realpath(src)).st_ino
-            link_src = dst
-            for link_file in inode_dict[inode]:
-                if link_file != f:
-                    link_dst = join(prefix, link_file)
-                    try:
-                        # Hard link to newly created file
-                        _link(link_src, link_dst, LINK_HARD)
-                    except OSError as e:
-                        log.error(('failed to link (src=%r, dst=%r, type=%r, ' +
-                                   'error=%r)') % (link_src, link_dst, 
-                                                   LINK_HARD, e))
-                    file_set.remove(link_file)
+            if not on_win:
+                inode = os.lstat(os.path.realpath(src)).st_ino
+                link_src = dst
+                for link_file in inode_dict[inode]:
+                    if link_file != f:
+                        link_dst = join(prefix, link_file)
+                        try:
+                            # Hard link to newly created file
+                            _link(link_src, link_dst, LINK_HARD)
+                        except OSError as e:
+                            log.error(('failed to link (src=%r, dst=%r, ' + 
+                                       'type=%r, error=%r)') % (link_src, 
+                                                               link_dst, 
+                                                               LINK_HARD, e))
+                        file_set.remove(link_file)
 
         if name_dist(dist) == '_cache':
             return
