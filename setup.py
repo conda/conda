c8e197aed2ce6b91ff9307694cdb6b6170db3790
Ilan Schnell <ilanschnell@gmail.com>
2014-05-08 21:30:52 -0500

    add new dependency to setup.py - next release will be 3.5.0

--- setup.py
+++ setup.py
@@ -70,6 +70,6 @@ setup(
     description = "package management tool",
     long_description = open('README.rst').read(),
     packages = ['conda', 'conda.cli', 'conda.progressbar'],
-    install_requires = ['pycosat', 'pyyaml'],
+    install_requires = ['pycosat', 'pyyaml', 'requests'],
     **kwds
 )
