6721421eaaff0e64ffd85dbea7b0963c93d563a5
Aaron Meurer <asmeurer@gmail.com>
2014-08-22 11:11:23 -0500

    install and remove do not have --platform

--- conda/cli/main_remove.py
+++ conda/cli/main_remove.py
@@ -105,10 +105,10 @@ def execute(args, parser):
                                       prepend=not args.override_channels,
                                       use_cache=args.use_index_cache,
                                       unknown=args.unknown,
-                                      json=args.json, platform=args.platform)
+                                      json=args.json)
     else:
         index = common.get_index_trap(channel_urls=channel_urls, prepend=not
-                                      args.override_channels, platform=args.platform,
+                                      args.override_channels,
                                       use_cache=args.use_index_cache,
                                       unknown=args.unknown, json=args.json)
     if args.features:
