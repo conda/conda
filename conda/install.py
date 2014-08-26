de95a7737a0c11667397641631d3a892fa012d45
Aaron Meurer <asmeurer@gmail.com>
2014-08-26 14:26:59 -0500

    Use re.escape instead of replace('.', '\\.')

--- conda/install.py
+++ conda/install.py
@@ -217,7 +217,7 @@ def binary_replace(data, a, b):
         if padding < 1:
             raise PaddingError
         return b + match.group(1) + b'\0' * padding
-    pat = re.compile(a.replace(b'.', b'\.') + b'([^\0\\s]*?)\0')
+    pat = re.compile(re.escape(a) + b'([^\0\\s]*?)\0')
     res = pat.sub(replace, data)
     assert len(res) == len(data)
     return res
