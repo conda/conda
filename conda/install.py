009731ef52cfc1eeb7348319140df3d6d1bd23f1
Darren Dale <dsdale24@gmail.com>
2014-08-27 16:29:33 -0400

    supports unix-style prefix paths in regular text files on windows

--- conda/install.py
+++ conda/install.py
@@ -229,16 +229,17 @@ def binary_replace(data, a, b):
 def update_prefix(path, new_prefix, placeholder=prefix_placeholder,
                   mode='text'):
     path = os.path.realpath(path)
+    if on_win and '/' in placeholder:
+        # original prefix uses unix-style path separators
+        # replace with unix-style path separators
+        new_prefix = new_prefix.replace('\\', '/')
+
     with open(path, 'rb') as fi:
         data = fi.read()
     if mode == 'text':
         new_data = data.replace(placeholder.encode('utf-8'),
                                 new_prefix.encode('utf-8'))
     elif mode == 'binary':
-        if on_win and '/' in placeholder:
-            # windows binary contains prefix with unix-style path separators
-            # replace with unix-style path separators
-            new_prefix = new_prefix.replace('\\', '/')
         new_data = binary_replace(data, placeholder.encode('utf-8'),
                                   new_prefix.encode('utf-8'))
     else:
