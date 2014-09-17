bb3a9ec686e54ef97c9639344e77d44bed8c255e
Aaron Meurer <asmeurer@gmail.com>
2014-09-17 11:18:23 -0500

    Give special error message if binstar tokens are expired

--- conda/fetch.py
+++ conda/fetch.py
@@ -105,6 +105,9 @@ def fetch_repodata(url, cache_dir=None, use_cache=False, session=None):
                    config.remove_binstar_tokens(url).split(config.DEFAULT_CHANNEL_ALIAS)[1].split('/')[0])
             else:
                 msg = 'Could not find URL: %s' % config.remove_binstar_tokens(url)
+        elif e.response.status_code == 401 and config.binstar_channel_alias() in url:
+            msg = ("UNAUTHORIZED (401): %s, you may need to login to binstar "
+            "again with 'binstar login' (%s)") % (url, e)
         else:
             msg = "HTTPError: %s: %s\n" % (e, config.remove_binstar_tokens(url))
         log.debug(msg)
