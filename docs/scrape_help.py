77ee5efd8f80e6c7c3312e853be9d0d460565653
Aaron Meurer <asmeurer@gmail.com>
2014-10-15 16:20:07 -0500

    Start rewriting scrape_help.py to generate manpages

--- docs/scrape_help.py
+++ docs/scrape_help.py
@@ -5,109 +5,78 @@
 # conda is distributed under the terms of the BSD 3-clause license.
 # Consult LICENSE.txt or http://opensource.org/licenses/BSD-3-Clause.
 
-from subprocess import Popen, STDOUT, PIPE
-from os.path import join
-import re
+from subprocess import check_output
+from os.path import join, dirname, abspath, isdir
+from os import makedirs, chdir
 import sys
 
-cmd_names = [
-    'info',
-    'help',
-    'list',
-    'search',
-    'create',
-    'install',
-    'update',
-    'remove',
-    'config',
-    'init',
-    'clean',
-    'build',
-    'skeleton',
-    'package',
-    'bundle',
-    'index',
-]
+def conda_help(cache=[]):
+    if cache:
+        return cache[0]
+    cache.append(check_output(['conda', '--help']))
+    return cache[0]
 
-def scrape_help(cmd_name):
+def conda_commands():
+    help = conda_help()
+    commands = []
+    start = False
+    for line in help.splitlines():
+        # Commands start after "command" header
+        if line.strip() == 'command':
+            start = True
+            continue
+        if start:
+            # The end of the commands
+            if not line:
+                break
+            if line[4] != ' ':
+                commands.append(line.split()[0])
+    return commands
 
-    cmd = "CIO_TARGET='ce' COLUMNS=1000 conda %s -h" % cmd_name
+def external_commands():
+    help = conda_help()
+    commands = []
+    start = False
+    for line in help.splitlines():
+        # Commands start after "command" header
+        if line.strip() == 'external commands:':
+            start = True
+            continue
+        if start:
+            # The end of the commands
+            if not line:
+                break
+            if line[4] != ' ':
+                commands.append(line.split()[0])
+    return commands
 
-    p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+def generate_man(command):
+    chdir(abspath(dirname(__file__)))
+    manpath = join('build', 'man')
+    if not isdir(manpath):
+        makedirs(manpath)
+    conda_version = check_output(['conda', '--version'])
+    print("Generating manpage for conda %s" % command)
+    check_output([
+        'help2man',
+        '--name', 'conda %s' % command,
+        '--section', '1',
+        '--source', 'Continuum Analytics',
+        '--version-string', conda_version,
+        '--no-info',
+        'conda %s' % command,
+        '-o', join(manpath, 'conda-%s.1' % command)
+        ])
 
-    output = p.stdout.read().decode('utf-8')
-
-
-    if cmd_name in ['remove','package','install', 'config']:
-
-        # groups:
-        usage_pat = re.compile(r'(usage): (conda .*\n\s*.*\n\s*.*)')
-
-        # groups:                           -----1----
-        desc_pat = re.compile(r'usage.*\n\s*(.*\n\s*.*)')
-    else:
-        # groups:                ----1---- -----2----
-        usage_pat = re.compile(r'(usage): (conda .*)\n')
-
-        # groups:                          --1-
-        desc_pat = re.compile(r'usage.*\n\n(.*)\n\n')
-
-
-    usage = usage_pat.search(output)
-    desc = desc_pat.search(output)
-
+def generate_html(command):
+    # Make sure to replace hard-coded paths
+    pass
 
 
-
-    # groups:                                               --1--   --2-
-    positional_pat = re.compile(r'positional arguments:\n\s+(\w*)\s+(.*)\n')
-    pos = positional_pat.search(output)
-
-    # groups:                            ---1--
-    optional_pat = re.compile(r'(optional(.*\n)*)$')
-    opt = optional_pat.search(output)
-
-    if opt:
-        options = opt.group(1)
-
-        yn_pat = re.compile(r'{.*}')
-        options = yn_pat.sub('', options)
-
-        rd_pat = re.compile(r'(default: )(.*/anaconda)')
-        options = rd_pat.sub(r"\1ROOT_DIR", options)
-
-        in_pat = re.compile(r'(in )(.*/anaconda)')
-        options = in_pat.sub(r"\1ROOT_DIR", options)
-
-    else:
-        options = ''
-
-    output = desc.group(1)
-
-    output += "\n\n**%s**: ``%s``\n\n" % (usage.group(1), usage.group(2))
-
-    if pos:
-        output += "*%s*\n\t%s\n\n" % (pos.group(1), pos.group(2))
-
-    output += options
-
-    return output
-
+def main():
+    for command in conda_commands():
+        generate_man(command)
 
 
 if __name__ == '__main__':
-
-    for name in cmd_names:
-        path = "source/commands/"
-        if len(sys.argv) > 1:
-            path = sys.argv[1]
-        outpath = join(path, "%s.txt" % name)
-
-        print("Scraping help for '%s' -> %s" % (name, outpath))
-
-        output = scrape_help(name)
-
-
-        outfile = open(outpath, "w")
-        outfile.write(output)
-        outfile.close()
+    sys.exit(main())
