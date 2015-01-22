83d582f99fe7212d85d4241ca69f3e632ced7181
Travis Swicegood <development@domain51.com>
2015-01-21 23:06:45 -0800

    Move all of the conda-env scripts over top of the old conda ones

--- bin/activate.bat
+++ bin/activate.bat
@@ -4,10 +4,9 @@ for /f "delims=" %%i in ("%~dp0..\envs") do (
     set ANACONDA_ENVS=%%~fi
 )
 
-set CONDA_NEW_ENV=%1
-set CONDA_NEW_ENV=%CONDA_NEW_ENV:"=%
+set CONDA_NEW_ENV=%~1
 
-if "%2" == "" goto skiptoomanyargs
+if "%~2" == "" goto skiptoomanyargs
     echo ERROR: Too many arguments provided
     goto usage
 :skiptoomanyargs
@@ -21,7 +20,7 @@ if not "%CONDA_NEW_ENV%" == "" goto skipmissingarg
     exit /b 1
 :skipmissingarg
 
-if exist "%ANACONDA_ENVS%\%CONDA_NEW_ENV%\Python.exe" goto skipmissingenv
+if exist "%ANACONDA_ENVS%\%CONDA_NEW_ENV%\conda-meta" goto skipmissingenv
     echo No environment named "%CONDA_NEW_ENV%" exists in %ANACONDA_ENVS%
     set CONDA_NEW_ENV=
     exit /b 1
@@ -31,6 +30,14 @@ REM Deactivate a previous activation if it is live
 if "%CONDA_DEFAULT_ENV%" == "" goto skipdeactivate
     REM This search/replace removes the previous env from the path
     echo Deactivating environment "%CONDA_DEFAULT_ENV%"...
+
+    REM Run any deactivate scripts
+    if not exist %ANACONDA_ENVS%\%CONDA_DEFAULT_ENV%\etc\conda\deactivate.d goto nodeactivate
+        pushd %ANACONDA_ENVS%\%CONDA_DEFAULT_ENV%\etc\conda\deactivate.d
+        for %%g in (*.bat) do call "%%g"
+        popd
+    :nodeactivate
+
     set CONDACTIVATE_PATH=%ANACONDA_ENVS%\%CONDA_DEFAULT_ENV%;%ANACONDA_ENVS%\%CONDA_DEFAULT_ENV%\Scripts;
     call set PATH=%%PATH:%CONDACTIVATE_PATH%=%%
     set CONDA_DEFAULT_ENV=
@@ -42,3 +49,10 @@ set CONDA_NEW_ENV=
 echo Activating environment "%CONDA_DEFAULT_ENV%"...
 set PATH=%ANACONDA_ENVS%\%CONDA_DEFAULT_ENV%;%ANACONDA_ENVS%\%CONDA_DEFAULT_ENV%\Scripts;%PATH%
 set PROMPT=[%CONDA_DEFAULT_ENV%] $P$G
+
+REM Run any activate scripts
+if not exist %ANACONDA_ENVS%\%CONDA_DEFAULT_ENV%\etc\conda\activate.d goto noactivate
+    pushd %ANACONDA_ENVS%\%CONDA_DEFAULT_ENV%\etc\conda\activate.d
+    for %%g in (*.bat) do call "%%g"
+    popd
+:noactivate
