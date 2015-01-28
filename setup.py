985fa772d136600a13336ffed6dff6d146bcb602
Travis Swicegood <development@domain51.com>
2015-01-28 15:38:22 -0600

    Bump the version numbers so the automated builds are handled properly
    cc: @ilanschnell

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ else:
 
 setup(
     name="conda-env",
-    version="2.0.0",
+    version="2.0.1",
     author="Continuum Analytics, Inc.",
     author_email="support@continuum.io",
     url="https://github.com/conda/conda-env",
