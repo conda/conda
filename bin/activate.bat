eb98cfcd4c7a940ff1ce917af7cb99eb4f66e007
Ilan Schnell <ilanschnell@gmail.com>
2015-02-03 16:58:40 -0600

    copy (de)activate scripts from conda-env

--- bin/activate.bat
+++ bin/activate.bat
@@ -1,20 +1,58 @@
 @echo off
-for /f "delims=" %%a in ('conda info --root') do @set root=%%a
 
-if exist "%root%\Scripts\env-activate.bat" goto activate
+for /f "delims=" %%i in ("%~dp0..\envs") do (
+    set ANACONDA_ENVS=%%~fi
+)
 
-    echo We're making conda environments even better!  Part of this process
-    echo is changing the way environment activation works.  You need to
-    echo install the new conda-env package before you can use these features:
-    echo .
-    echo     conda install -c conda conda-env
-    echo .
-    echo For more information, please visit: https://github.com/conda/conda-env
-    echo .
-    goto done
+set CONDA_NEW_ENV=%~1
 
-:activate
-    set args=%*
-    call "%root%\Scripts\env-activate.bat" %args%
+if "%~2" == "" goto skiptoomanyargs
+    echo ERROR: Too many arguments provided
+    goto usage
+:skiptoomanyargs
 
-:done
+if not "%CONDA_NEW_ENV%" == "" goto skipmissingarg
+:usage
+    echo Usage: activate envname
+    echo.
+    echo Deactivates previously activated Conda
+    echo environment, then activates the chosen one.
+    exit /b 1
+:skipmissingarg
+
+if exist "%ANACONDA_ENVS%\%CONDA_NEW_ENV%\conda-meta" goto skipmissingenv
+    echo No environment named "%CONDA_NEW_ENV%" exists in %ANACONDA_ENVS%
+    set CONDA_NEW_ENV=
+    exit /b 1
+:skipmissingenv
+
+REM Deactivate a previous activation if it is live
+if "%CONDA_DEFAULT_ENV%" == "" goto skipdeactivate
+    REM This search/replace removes the previous env from the path
+    echo Deactivating environment "%CONDA_DEFAULT_ENV%"...
+
+    REM Run any deactivate scripts
+    if not exist "%ANACONDA_ENVS%\%CONDA_DEFAULT_ENV%\etc\conda\deactivate.d" goto nodeactivate
+        pushd "%ANACONDA_ENVS%\%CONDA_DEFAULT_ENV%\etc\conda\deactivate.d"
+        for %%g in (*.bat) do call "%%g"
+        popd
+    :nodeactivate
+
+    set CONDACTIVATE_PATH=%ANACONDA_ENVS%\%CONDA_DEFAULT_ENV%;%ANACONDA_ENVS%\%CONDA_DEFAULT_ENV%\Scripts;
+    call set PATH=%%PATH:%CONDACTIVATE_PATH%=%%
+    set CONDA_DEFAULT_ENV=
+    set CONDACTIVATE_PATH=
+:skipdeactivate
+
+set CONDA_DEFAULT_ENV=%CONDA_NEW_ENV%
+set CONDA_NEW_ENV=
+echo Activating environment "%CONDA_DEFAULT_ENV%"...
+set PATH=%ANACONDA_ENVS%\%CONDA_DEFAULT_ENV%;%ANACONDA_ENVS%\%CONDA_DEFAULT_ENV%\Scripts;%PATH%
+set PROMPT=[%CONDA_DEFAULT_ENV%] $P$G
+
+REM Run any activate scripts
+if not exist "%ANACONDA_ENVS%\%CONDA_DEFAULT_ENV%\etc\conda\activate.d" goto noactivate
+    pushd "%ANACONDA_ENVS%\%CONDA_DEFAULT_ENV%\etc\conda\activate.d"
+    for %%g in (*.bat) do call "%%g"
+    popd
+:noactivate
