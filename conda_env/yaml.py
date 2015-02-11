4ef60dfc23488a5043764dac0ee9087749544112
Travis Swicegood <development@domain51.com>
2015-02-11 11:15:47 -0800

    Remove unused code

--- conda_env/yaml.py
+++ conda_env/yaml.py
@@ -8,16 +8,6 @@ from collections import OrderedDict
 import yaml
 
 
-class UnsortableList(list):
-    def sort(self, *args, **kwargs):
-        pass
-
-
-class UnsortableOrderedDict(OrderedDict):
-    def items(self, *args, **kwargs):
-        return UnsortableList(OrderedDict.items(self, *args, **kwargs))
-
-
 def represent_ordereddict(dumper, data):
     value = []
 
