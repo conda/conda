29bf31a447e7faea48d904473c5cb379c59c707f
Travis Swicegood <development@domain51.com>
2015-02-11 11:14:58 -0800

    Fix issue with Python3 support and conda env export
    I swear I wrote this code before, but its not here.

---
+++ conda_env/compat.py
@@ -0,0 +1,17 @@
+import sys
+
+PY3 = sys.version_info[0] == 3
+
+
+def u(some_str):
+    if PY3:
+        return some_str
+    else:
+        return unicode(some_str)
+
+
+def b(some_str, encoding="utf-8"):
+    try:
+        return bytes(some_str, encoding=encoding)
+    except TypeError:
+        return some_str
