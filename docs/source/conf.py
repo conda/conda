d5aab34b9d2fc366f2c3e4f0d2bd211a9ab4d830
Aaron Meurer <asmeurer@gmail.com>
2015-03-31 16:03:03 -0500

    Update copyright year in the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Conda'
-copyright = u'2014, Continuum Analytics'
+copyright = u'2015, Continuum Analytics'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
