f510ac4141aa8b9c4eeb74ba8a950ddd4d1c724e
Ilan Schnell <ilanschnell@gmail.com>
2015-04-06 10:37:25 -0500

    update test

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -7,8 +7,7 @@ class TestMisc(unittest.TestCase):
 
     def test_cache_fn_url(self):
         url = "http://repo.continuum.io/pkgs/pro/osx-64/"
-        self.assertEqual(cache_fn_url(url),
-                         '7618c8b65f9329feb96d07caa0751cc6.json')
+        self.assertEqual(cache_fn_url(url), '7618c8b6.json')
 
 
 if __name__ == '__main__':
