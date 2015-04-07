25e41ee45db49674250cde8ec49f7fc481e89cc9
Travis Swicegood <development@domain51.com>
2015-04-06 17:38:50 -0700

    Bump to dev bugfix version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ else:
 
 setup(
     name="conda-env",
-    version="2.1.3",
+    version="2.1.4alpha",
     author="Continuum Analytics, Inc.",
     author_email="support@continuum.io",
     url="https://github.com/conda/conda-env",
