e1a86db9af7cbcfd2d2b3d883310d1a4d9d29774
malev <marcosvanetta@gmail.com>
2015-04-24 14:38:32 -0500

    Add create --binstar exceptions

--- conda_env/exceptions.py
+++ conda_env/exceptions.py
@@ -10,7 +10,22 @@ class EnvironmentFileNotFound(CondaEnvException):
 
 
 class EnvironmentFileNotDownloaded(CondaEnvException):
-    def __init__(self, filename, *args, **kwargs):
-        msg = '{} file not downloaded'.format(filename)
-        self.filename = filename
+    def __init__(self, username, packagename, *args, **kwargs):
+        msg = '{}/{} file not downloaded'.format(username, packagename)
+        self.username = username
+        self.packagename = packagename
         super(EnvironmentFileNotDownloaded, self).__init__(msg, *args, **kwargs)
+
+
+class EnvironmentUsernameRequired(CondaEnvException):
+    def __init__(self, *args, **kwargs):
+        msg = 'Username is required'
+        super(EnvironmentUsernameRequired, self).__init__(msg, *args, **kwargs)
+
+
+class PackageNotFound(CondaEnvException):
+    def __init__(self, username, packagename, *args, **kwargs):
+        msg = '{}/{} not found'.format(username, packagename)
+        self.username = username
+        self.packagename = packagename
+        super(PackageNotFound, self).__init__(msg, *args, **kwargs)
