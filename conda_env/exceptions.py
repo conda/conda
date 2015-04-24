06b99c4415a6605cbd6123271d44af96585fbb9d
malev <marcosvanetta@gmail.com>
2015-04-23 19:39:00 -0500

    Add environment not found exception

--- conda_env/exceptions.py
+++ conda_env/exceptions.py
@@ -7,3 +7,10 @@ class EnvironmentFileNotFound(CondaEnvException):
         msg = '{} file not found'.format(filename)
         self.filename = filename
         super(EnvironmentFileNotFound, self).__init__(msg, *args, **kwargs)
+
+
+class EnvironmentFileNotDownloaded(CondaEnvException):
+    def __init__(self, filename, *args, **kwargs):
+        msg = '{} file not downloaded'.format(filename)
+        self.filename = filename
+        super(EnvironmentFileNotDownloaded, self).__init__(msg, *args, **kwargs)
