1c931a828031d4e1d4085961855a12f73a492fab
malev <marcosvanetta@gmail.com>
2015-04-24 14:40:13 -0500

    Refactor create --binstar into OO

--- conda_env/utils/tests/test_downloader.py
+++ conda_env/utils/tests/test_downloader.py
@@ -1,24 +1,23 @@
 import unittest
 from conda_env.utils.downloader import Downloader
+from conda_env.exceptions import EnvironmentUsernameRequired
 
 
 class TestDownloader(unittest.TestCase):
     def test_parse_full_spec(self):
         username, packagename, version = Downloader.parse("anaconda/conda-env==2.1")
         self.assertEqual(username, 'anaconda')
-        self.assertEqual(packagename, 'anaconda/conda-env')
+        self.assertEqual(packagename, 'conda-env')
         self.assertEqual(version, '2.1')
 
     def test_parse_missing_username(self):
-        username, packagename, version = Downloader.parse("conda-env==2.1")
-        self.assertEqual(username, None)
-        self.assertEqual(packagename, 'conda-env')
-        self.assertEqual(version, '2.1')
+        with self.assertRaises(EnvironmentUsernameRequired):
+            Downloader.parse("conda-env==2.1")
 
     def test_parse_partial_spec(self):
         username, packagename, version = Downloader.parse("anaconda/conda-env")
         self.assertEqual(username, 'anaconda')
-        self.assertEqual(packagename, 'anaconda/conda-env')
+        self.assertEqual(packagename, 'conda-env')
         self.assertEqual(version, None)
 
 if __name__ == '__main__':
