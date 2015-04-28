e943d57f4fffad10def67b071fada89bf80232aa
malev <marcosvanetta@gmail.com>
2015-04-28 18:42:47 -0500

    Raise exception if binstar is not installed

--- conda_env/exceptions.py
+++ conda_env/exceptions.py
@@ -2,8 +2,18 @@ class CondaEnvException(Exception):
     pass
 
 
+class CondaEnvRuntimeError(RuntimeError, CondaEnvException):
+    pass
+
+
 class EnvironmentFileNotFound(CondaEnvException):
     def __init__(self, filename, *args, **kwargs):
         msg = '{} file not found'.format(filename)
         self.filename = filename
         super(EnvironmentFileNotFound, self).__init__(msg, *args, **kwargs)
+
+
+class NoBinstar(CondaEnvRuntimeError):
+    def __init__(self):
+        msg = 'The binstar client must be installed to perform this action'
+        super(NoBinstar, self).__init__(msg)
