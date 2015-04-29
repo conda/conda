faf47662ef973f66fd68c2ddfa3447f9b5f8fde9
Travis Swicegood <development@domain51.com>
2015-04-29 12:26:17 -0700

    Add a detect() function to finding specs

--- conda_env/specs/__init__.py
+++ conda_env/specs/__init__.py
@@ -1,4 +1,14 @@
 from .binstar import BinstarSpec
 
 
-all_specs = [BinstarSpec]
+def detect(**kwargs):
+    for SpecClass in all_specs:
+        spec = SpecClass(**kwargs)
+        if spec.can_handle():
+            return spec
+
+
+all_specs = [
+    BinstarSpec,
+    # YamlFileSpec
+]
