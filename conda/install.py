7fdc89469a0994fc7d0ca9e6eec8727c7a96f964
Aaron Meurer <asmeurer@gmail.com>
2015-05-05 17:39:25 -0500

    Re-enable win_ignore_root for now
    Once we are sure there are no other issues with the trash stuff we can remove
    it again.

--- conda/install.py
+++ conda/install.py
@@ -64,7 +64,7 @@ if on_win:
 
     # on Windows we cannot update these packages in the root environment
     # because of the file lock problem
-    win_ignore_root = set()
+    win_ignore_root = set(['python', 'pycosat', 'psutil'])
 
     CreateHardLink = ctypes.windll.kernel32.CreateHardLinkW
     CreateHardLink.restype = wintypes.BOOL
