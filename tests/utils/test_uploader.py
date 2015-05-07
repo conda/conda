32c161964177295ccb37fa3fbd1bba1368a4654b
malev <marcosvanetta@gmail.com>
2015-05-07 15:55:09 -0500

    Add uploder tests

--- tests/utils/test_uploader.py
+++ tests/utils/test_uploader.py
@@ -17,5 +17,13 @@ class UploaderTestCase(unittest.TestCase):
     def test_authorized(self):
         uploader = Uploader('package', 'filename')
         with mock.patch.object(uploader.binstar, 'user') as get_user_mock:
-            get_user_mock.user.return_value = {}
+            get_user_mock.return_value = {}
             self.assertEqual(uploader.authorized(), True)
+
+    def test_package_already_exist(self):
+        uploader = Uploader('package', 'filename')
+        with mock.patch.object(uploader.binstar, 'user') as user_mock:
+            user_mock.return_value = {'login': 'user'}
+            with mock.patch.object(uploader.binstar, 'distribution') as distribution_mock:
+                distribution_mock.return_value = True
+                self.assertEqual(uploader.ensure_distribution(), False)
