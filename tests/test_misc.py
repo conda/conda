5eee8c06d808488dee093d37ca6f87ed97b86d53
Ilan Schnell <ilanschnell@gmail.com>
2015-06-01 21:24:59 -0500

    add some tests

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,6 +1,7 @@
 import unittest
 
 from conda.fetch import cache_fn_url
+from conda.signature import ascii2sig, sig2ascii
 
 
 class TestMisc(unittest.TestCase):
@@ -8,6 +9,16 @@ class TestMisc(unittest.TestCase):
     def test_cache_fn_url(self):
         url = "http://repo.continuum.io/pkgs/pro/osx-64/"
         self.assertEqual(cache_fn_url(url), '7618c8b6.json')
+
+    def test_ascii2sig(self):
+        self.assertEqual(sig2ascii(1234), 'BNI=')
+
+    def test_sig2ascii(self):
+        self.assertEqual(ascii2sig('BNI='), 1234)
+
+    def test_sig2ascii2sig(self):
+        for i in range(10000):
+            self.assertEqual(ascii2sig(sig2ascii(i)), i)
 
 
 if __name__ == '__main__':
