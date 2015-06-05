083dbba55675361374e5a0780f6c7e3421f2d0db
malev <marcosvanetta@gmail.com>
2015-06-05 14:13:22 -0500

    Add py3.4 as a classifier in setup.py

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
     ],
     description="tools for interacting with conda environments",
     long_description=open('README.rst').read(),
