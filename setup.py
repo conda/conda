4b78b576f6c798a438d1b3c53015f13a657d1e8d
malev <marcosvanetta@gmail.com>
2015-06-08 14:55:59 -0500

    Bump version to 2.2.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ else:
 
 setup(
     name="conda-env",
-    version="2.2alpha.0",
+    version="2.2.1",
     author="Continuum Analytics, Inc.",
     author_email="support@continuum.io",
     url="https://github.com/conda/conda-env",
