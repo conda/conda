bfc9d6f0c95d21213c6485a9da5db99ef1d9d64e
malev <marcosvanetta@gmail.com>
2015-06-15 17:23:15 -0500

    Display better error when env already uploaded

--- conda_env/exceptions.py
+++ conda_env/exceptions.py
@@ -19,7 +19,7 @@ class NoBinstar(CondaEnvRuntimeError):
         super(NoBinstar, self).__init__(msg)
 
 
-class AlreadyExist(CondaEnvException):
+class AlreadyExist(CondaEnvRuntimeError):
     def __init__(self):
         msg = 'The environment path already exists'
         super(AlreadyExist, self).__init__(msg)
