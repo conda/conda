dd6e3077ce35d3fc62b25a23d99a2e41a3056650
malev <marcosvanetta@gmail.com>
2015-06-16 16:38:58 -0500

    Bump version to 2.3.0alpha

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ else:
 
 setup(
     name="conda-env",
-    version="2.2alpha.0",
+    version="2.3.0alpha",
     author="Continuum Analytics, Inc.",
     author_email="support@continuum.io",
     url="https://github.com/conda/conda-env",
