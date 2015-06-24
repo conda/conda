65f1913f05985897e428ec5c9790ddd898794518
malev <marcosvanetta@gmail.com>
2015-06-23 20:15:29 -0500

    Notebook injector class

--- conda_env/utils/notebooks.py
+++ conda_env/utils/notebooks.py
@@ -1,6 +1,39 @@
+import json
 from os.path import basename
 import conda.config as config
 from conda.cli import common
+from ..exceptions import EnvironmentAlreadyInNotebook
+
+
+class Notebook(object):
+    """Inject environment into a notebook"""
+    def __init__(self, notebook):
+        self.msg = ""
+        self.notebook = notebook
+
+    def inject(self, environment, force=False):
+        try:
+            return self._inject(environment, force)
+        except PermissionError:
+            self.msg = "Please verify permission in {}".format(self.notebook)
+        except FileNotFoundError:
+            self.msg = "{} does not exist".format(self.notebook)
+        except EnvironmentAlreadyInNotebook:
+            self.msg = "There is already an environment in {}. Consider '--force'".\
+                format(self.notebook)
+        return False
+
+    def _inject(self, environment, force=False):
+        with open(self.notebook) as fb:
+            data = json.loads(fb.read())
+            if force or 'environment' not in data['metadata']:
+                data['metadata']['environment'] = environment.to_yaml()
+            else:
+                raise EnvironmentAlreadyInNotebook(self.notebook)
+
+        with open(self.notebook, 'w') as fb:
+            fb.write(json.dumps(data))
+        return True
 
 
 def current_env():
@@ -9,4 +42,4 @@ def current_env():
         name = config.root_env_name
     else:
         name = basename(config.default_prefix)
-    return {'name': name, 'prefix': config.default_prefix}
+    return name
