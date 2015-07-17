c298eec9d9d1d037b4a940fae344ddb29b863ec1
Ilan Schnell <ilanschnell@gmail.com>
2015-07-17 16:29:13 -0500

    add note

--- conda/cli/pscheck.py
+++ conda/cli/pscheck.py
@@ -1,3 +1,7 @@
+# NOTE:
+#   This is deprecated, and will be removed in the future.
+#   it is not imported anywhere in conda.
+
 from __future__ import print_function, division, absolute_import
 
 import os
