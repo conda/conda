3431eb62c9379a0b74f170f3c2702dced3069b40
Aaron Meurer <asmeurer@gmail.com>
2015-07-20 16:02:15 -0500

    Add completer for subcommands (like conda bui<TAB>)

--- conda/cli/main.py
+++ conda/cli/main.py
@@ -161,6 +161,9 @@ In short:
     from conda.cli import main_bundle
     main_bundle.configure_parser(sub_parsers)
 
+    from conda.cli.find_commands import find_commands
+    sub_parsers.completer = lambda prefix, **kwargs: [i for i in
+        list(sub_parsers.choices) + find_commands() if i.startswith(prefix)]
     args = p.parse_args()
 
     if getattr(args, 'json', False):
