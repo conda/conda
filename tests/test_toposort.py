11847eb1927cb95f972580a0158f40ef922a44d9
Stuart Berg <bergs@janelia.hhmi.org>
2015-09-14 13:42:50 -0400

    toposort: Added special case to remove 'pip' dependency from 'python'. Fixes conda/conda-build#401, at least in the specific (and common) case of python.

--- tests/test_toposort.py
+++ tests/test_toposort.py
@@ -40,6 +40,44 @@ class TopoSortTests(unittest.TestCase):
         # Results do not have an guaranteed order
         self.assertEqual(set(results[3:]), {'1', '2'})
 
+    def test_python_is_prioritized(self):
+        """
+        This test checks a special invariant related to 'python' specifically.
+        Python is part of a cycle (pip <--> python), which can cause it to be
+        installed *after* packages that need python (possibly in 
+        post-install.sh).
+        
+        A special case in toposort() breaks the cycle, to ensure that python
+        isn't installed too late.  Here, we verify that it works.
+        """
+        # This is the actual dependency graph for python (as of the time of this writing, anyway)
+        data = {'python' : ['pip', 'openssl', 'readline', 'sqlite', 'tk', 'xz', 'zlib'],
+                'pip': ['python', 'setuptools', 'wheel'],
+                'setuptools' : ['python'],
+                'wheel' : ['python'],
+                'openssl' : [],
+                'readline' : [],
+                'sqlite' : [],
+                'tk' : [],
+                'xz' : [],
+                'zlib' : []}
+
+        # Here are some extra pure-python libs, just for good measure.
+        data.update({'psutil' : ['python'],
+                     'greenlet' : ['python'],
+                     'futures' : ['python'],
+                     'six' : ['python']})
+
+        results = toposort(data)
+
+        # Python always comes before things that need it!
+        self.assertLess(results.index('python'), results.index('setuptools'))
+        self.assertLess(results.index('python'), results.index('wheel'))
+        self.assertLess(results.index('python'), results.index('pip'))
+        self.assertLess(results.index('python'), results.index('psutil'))
+        self.assertLess(results.index('python'), results.index('greenlet'))
+        self.assertLess(results.index('python'), results.index('futures'))
+        self.assertLess(results.index('python'), results.index('six'))
 
 if __name__ == '__main__':
     unittest.main()
