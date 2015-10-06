be7f32a3e7e9842e8a2401b5073fc4baece04796
Ilan Schnell <ilanschnell@gmail.com>
2015-10-06 00:38:45 -0500

    add fetching packages

--- conda/cli/install.py
+++ conda/cli/install.py
@@ -171,7 +171,7 @@ def install(args, parser, command='install'):
         for fpath in args.file:
             specs.extend(common.specs_from_url(fpath, json=args.json))
         if '@EXPLICIT' in specs:
-            misc.explicit(specs)
+            misc.explicit(specs, prefix)
             return
     elif getattr(args, 'all', False):
         linked = ci.linked(prefix)
