a0905ba77be1aaf8a4488644839d05544d6920c9
Ilan Schnell <ilanschnell@gmail.com>
2015-10-24 21:19:54 -0500

    add file extension to regex

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -11,20 +11,20 @@ class TestMisc(unittest.TestCase):
         self.assertEqual(cache_fn_url(url), '7618c8b6.json')
 
     def test_url_pat_1(self):
-        m = url_pat.match('http://www.cont.io/pkgs/linux-64/foo.tar'
+        m = url_pat.match('http://www.cont.io/pkgs/linux-64/foo.tar.bz2'
                           '#d6918b03927360aa1e57c0188dcb781b')
         self.assertEqual(m.group('url'), 'http://www.cont.io/pkgs/linux-64')
-        self.assertEqual(m.group('fn'), 'foo.tar')
+        self.assertEqual(m.group('fn'), 'foo.tar.bz2')
         self.assertEqual(m.group('md5'), 'd6918b03927360aa1e57c0188dcb781b')
 
     def test_url_pat_2(self):
-        m = url_pat.match('http://www.cont.io/pkgs/linux-64/foo.tar')
+        m = url_pat.match('http://www.cont.io/pkgs/linux-64/foo.tar.bz2')
         self.assertEqual(m.group('url'), 'http://www.cont.io/pkgs/linux-64')
-        self.assertEqual(m.group('fn'), 'foo.tar')
+        self.assertEqual(m.group('fn'), 'foo.tar.bz2')
         self.assertEqual(m.group('md5'), None)
 
     def test_url_pat_3(self):
-        m = url_pat.match('http://www.cont.io/pkgs/linux-64/foo.tar#1234')
+        m = url_pat.match('http://www.cont.io/pkgs/linux-64/foo.tar.bz2#1234')
         self.assertEqual(m, None)
 
 
