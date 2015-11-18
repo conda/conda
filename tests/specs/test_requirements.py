2b4fdb238cddd0b80a6cb2f5f6a45ef8e5407e58
Marcos Vanetta <marcosvanetta@gmail.com>
2015-11-18 11:43:52 -0600

    Requirements spec

---
+++ tests/specs/test_requirements.py
@@ -0,0 +1,26 @@
+import unittest
+from conda_env import env
+from conda_env.specs.requirements import RequirementsSpec
+from .. import utils
+
+
+class TestRequiremets(unittest.TestCase):
+    def test_no_environment_file(self):
+        spec = RequirementsSpec(name=None, filename='not-a-file')
+        self.assertEqual(spec.can_handle(), False)
+
+    def test_no_name(self):
+        spec = RequirementsSpec(filename=utils.support_file('requirements.txt'))
+        self.assertEqual(spec.can_handle(), False)
+
+    def test_req_file_and_name(self):
+        spec = RequirementsSpec(filename=utils.support_file('requirements.txt'), name='env')
+        self.assertTrue(spec.can_handle())
+
+    def test_environment(self):
+        spec = RequirementsSpec(filename=utils.support_file('requirements.txt'), name='env')
+        self.assertIsInstance(spec.environment, env.Environment)
+        self.assertEqual(
+            spec.environment.dependencies['conda'][0],
+            'flask ==0.10.1'
+        )
