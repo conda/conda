5f0b35a9261dc8b103b6fef24794df6ab17b2ce5
Eric Dill <edill@bnl.gov>
2015-11-23 18:05:54 -0500

    ENH: Allow different failure modes
    Allow a different failure mode for a url failing to be created if the
    url was originally not present versus if the url was originally a file.

--- conda/cli/main_list.py
+++ conda/cli/main_list.py
@@ -208,13 +208,26 @@ def print_explicit(prefix, add_md5=False):
         with open(join(meta_dir, fn)) as fi:
             meta = json.load(fi)
         url = meta.get('url')
-        if not url or url.startswith('file'):
-            # try to format the correct url
+        def format_url():
+            return '%s%s-%s-%s.tar.bz2' % (meta['channel'], meta['name'],
+                                           meta['version'], meta['build'])
+        # two cases in which we want to try to format the url:
+        # 1. There is no url key in the metadata
+        # 2. The url key in the metadata is referencing a file on the local
+        #    machine
+        if not url:
             try:
-                url = '%s%s-%s-%s.tar.bz2' % (meta['channel'], meta['name'],
-                                              meta['version'], meta['build'])
+                url = format_url()
             except KeyError:
+                # Declare failure :-(
                 print('# no URL for: %s' % fn[:-5])
+                continue
+        if url.startswith('file'):
+            try:
+                url = format_url()
+            except KeyError:
+                # declare failure and allow the url to be the file from which it was
+                # originally installed
                 continue
         md5 = meta.get('md5')
         print(url + ('#%s' % md5 if add_md5 and md5 else ''))
