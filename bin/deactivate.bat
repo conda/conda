85ae6e4060283abd6bc32a0663fb95ef7ccd0d0a
Michael Sarahan <msarahan@continuum.io>
2015-12-07 09:58:57 -0600

    keep Scripts dir on PATH

--- bin/deactivate.bat
+++ bin/deactivate.bat
@@ -1,8 +1,5 @@
 @echo off
-
-for /f "delims=" %%i in ("%~dp0..\envs") do (
-    set ANACONDA_ENVS=%%~fi
-)
+SETLOCAL ENABLEDELAYEDEXPANSION
 
 if "%1" == "" goto skipmissingarg
     echo Usage: deactivate
@@ -12,6 +9,11 @@ if "%1" == "" goto skipmissingarg
     exit /b 1
 :skipmissingarg
 
+REM special case for root env:
+REM   Checks for Library\bin on PATH.  If exists, we have root env on PATH.
+call :NORMALIZEPATH ROOT_PATH "%~dp0.."
+CALL SET "PATH_NO_ROOT=%%PATH:%ROOT_PATH%;=%%"
+IF NOT "%PATH_NO_ROOT%"=="%PATH%" SET "CONDA_DEFAULT_ENV=%ROOT_PATH%"
 
 REM Deactivate a previous activation if it is live
 if "%CONDA_DEFAULT_ENV%" == "" goto skipdeactivate
@@ -25,15 +27,26 @@ if "%CONDA_DEFAULT_ENV%" == "" goto skipdeactivate
         popd
     :nodeactivate
 
-    set CONDACTIVATE_PATH=%CONDA_DEFAULT_ENV%;%CONDA_DEFAULT_ENV%\Scripts;%CONDA_DEFAULT_ENV%\Library\bin;
-    call set PATH=%%PATH:%CONDACTIVATE_PATH%=%%
-
     REM Remove env name from PROMPT
-    for /F %%i in ("%CONDA_DEFAULT_ENV%") do set CONDA_OLD_ENV_NAME=%%~ni
+    FOR /F "tokens=* delims=\" %%i IN ("%CONDA_DEFAULT_ENV%") DO SET "CONDA_OLD_ENV_NAME=%%~ni"
     call set PROMPT=%%PROMPT:[%CONDA_OLD_ENV_NAME%] =%%
-    set CONDA_OLD_ENV_NAME=
 
-    set CONDA_DEFAULT_ENV=
-    set CONDA_ENV_PATH=
-    set CONDACTIVATE_PATH=
+    SET "CONDACTIVATE_PATH=%CONDA_DEFAULT_ENV%;%CONDA_DEFAULT_ENV%\Scripts;%CONDA_DEFAULT_ENV%\Library\bin;"
+    CALL SET "PATH=%%PATH:%CONDACTIVATE_PATH%=%%"
 :skipdeactivate
+
+REM Make sure that root's Scripts dir is on PATH, for sake of keeping activate/deactivate available.
+CALL SET "PATH_NO_SCRIPTS=%%PATH:%~dp0;=%%"
+IF "%PATH_NO_SCRIPTS%"=="%PATH%" SET "PATH=%PATH%;%~dp0;"
+
+ENDLOCAL & (
+    SET "PATH=%PATH%"
+    SET "PROMPT=%PROMPT%"
+    SET CONDA_DEFAULT_ENV=
+)
+
+EXIT /B
+
+:NORMALIZEPATH
+    SET "%1=%~dpfn2"
+    EXIT /B
