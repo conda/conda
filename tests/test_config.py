8827526705c14eda015a51f3211a713e7f708491
Søren Fuglede Jørgensen <s@fuglede.dk>
2016-01-01 17:09:04 +0100

    Change http://continuum.io to https://continuum.io
    Continuum.io supports SSL; let's use that.

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,4 @@
-# (c) 2012-2014 Continuum Analytics, Inc. / http://continuum.io
+# (c) 2012-2014 Continuum Analytics, Inc. / https://continuum.io
 # All Rights Reserved
 #
 # conda is distributed under the terms of the BSD 3-clause license.
