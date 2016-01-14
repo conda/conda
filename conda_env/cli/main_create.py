a5aa40d9e5bdafcec09e114fa03dfeb793e22f61
Korijn van Golen <korijn@gmail.com>
2016-01-14 10:59:45 +0100

    Improved CLI help text

--- conda_env/cli/main_create.py
+++ conda_env/cli/main_create.py
@@ -70,7 +70,7 @@ def configure_parser(sub_parsers):
     p.add_argument(
         '--select',
         action='append',
-        help='preprocessing selectors (can be passed multiple times)',
+        help="toggle preprocessing selectors. pass multiple times to toggle multiple selectors. pass 'all' to toggle all selectors",
         metavar="SELECTORS",
     )
     common.add_parser_json(p)
