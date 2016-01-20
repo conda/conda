7e81ce2bfd296a0cd1a48164198bc9ea9c3059cf
Kale Franz <kfranz@continuum.io>
2016-01-19 21:36:20 -0600

    add codecov

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
     ],
     description = "package management tool",
     long_description = open('README.rst').read(),
