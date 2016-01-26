7e94fca40c7b5d1daf2449dc5b942fcecdf33e58
David Kent <davidkent@fastmail.com.au>
2016-01-26 14:59:52 +1100

    Check return code when calling pip.

--- conda_env/installers/pip.py
+++ conda_env/installers/pip.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 import subprocess
 
+from conda.cli import common
 from conda.pip import pip_args
 
 
@@ -8,3 +9,6 @@ def install(prefix, specs, args, env):
     pip_cmd = pip_args(prefix) + ['install', ] + specs
     process = subprocess.Popen(pip_cmd, universal_newlines=True)
     process.communicate()
+
+    if process.returncode != 0:
+        common.exception_and_exit(ValueError("pip returned an error."))
