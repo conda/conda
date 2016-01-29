74ade1e32eaa686f23999d6dbed4625673ef4c0c
Kale Franz <kfranz@continuum.io>
2016-01-29 15:44:44 -0600

    remove pycosat as install dependency

--- setup.py
+++ setup.py
@@ -66,6 +66,6 @@ setup(
     description = "package management tool",
     long_description = open('README.rst').read(),
     packages = ['conda', 'conda.cli', 'conda.progressbar'],
-    install_requires = ['pycosat', 'pyyaml', 'requests'],
+    install_requires = ['pyyaml', 'requests'],
     **kwds
 )
