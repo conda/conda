31e149909aa06c4c4217f31e9ea41e4252a646f2
Michael C. Grant <mcg@cvxr.com>
2016-02-03 17:55:50 -0600

    Don't call full get_dists in add_defaults_to_specs; optimize generate_constraints

--- conda/plan.py
+++ conda/plan.py
@@ -348,7 +348,10 @@ def add_defaults_to_specs(r, linked, specs):
             log.debug('H1 %s' % name)
             continue
 
-        any_depends_on = any(pkg.name == name for pkg in itervalues(r.get_dists(specs)[0]))
+        any_depends_on = any(ms2.name == name
+                             for spec in specs
+                             for fn in r.find_matches(spec)
+                             for ms2 in r.ms_depends(fn))
         log.debug('H2 %s %s' % (name, any_depends_on))
 
         if not any_depends_on and name not in names_ms:
