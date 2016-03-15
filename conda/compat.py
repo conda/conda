16a310d7bbb252906ecca3fe034eeffb551084b8
Kale Franz <kfranz@continuum.io>
2016-03-15 15:37:59 -0500

    use memoized from auxlib

--- conda/compat.py
+++ conda/compat.py
@@ -4,31 +4,7 @@ This module is DEPRECATED. Please use `conda.common.compat`.
 """
 from __future__ import absolute_import, division, print_function
 
-from .common.utils import deprecated_import
+from .common.utils import deprecate_module_with_proxy
+from conda.common.compat import *  # NOQA
 
-deprecated_import('conda.common.compat')
-
-import conda.common.compat
-from conda.common.compat import *
-
-import sys, warnings
-
-def WrapMod(mod, deprecated):
-    """Return a wrapped object that warns about deprecated accesses"""
-    deprecated = set(deprecated)
-
-    class Wrapper(object):
-        def __getattr__(self, attr):
-            if attr in deprecated:
-                warnings.warn("Property %s is deprecated" % attr)
-
-            return getattr(mod, attr)
-
-        def __setattr__(self, attr, value):
-            if attr in deprecated:
-                warnings.warn("Property %s is deprecated" % attr)
-            return setattr(mod, attr, value)
-    return Wrapper()
-
-
-sys.modules[__name__] = WrapMod(sys.modules[__name__], deprecated=conda.common.compat.__all__)
+deprecate_module_with_proxy(__name__, locals())
