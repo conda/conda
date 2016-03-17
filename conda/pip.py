c70ee4498e2bfd74484091745ae0163a7e2b6a2a
Ilan Schnell <ilanschnell@gmail.com>
2016-03-16 20:49:47 -0500

    first working version

--- conda/pip.py
+++ conda/pip.py
@@ -2,108 +2,79 @@
 Functions related to core conda functionality that relates to pip
 """
 from __future__ import absolute_import, print_function
-from os.path import isfile, join
+
+import os
 import re
-import subprocess
 import sys
+from os.path import isdir, isfile, join
+
+from conda.compat import itervalues
+from conda.install import linked_data
+from conda.misc import rel_path
 
 
-def pip_args(prefix):
-    """
-    return the arguments required to invoke pip (in prefix), or None if pip
-    is not installed
-    """
-    if sys.platform == 'win32':
-        pip_path = join(prefix, 'Scripts', 'pip.exe')
-    else:
-        pip_path = join(prefix, 'bin', 'pip')
-    if isfile(pip_path):
-        ret = [pip_path]
 
-        # Check the version of pip
-        # --disable-pip-version-check was introduced in pip 6.0
-        # If older than that, they should probably get the warning anyway.
-        pip_version = subprocess.check_output(ret + ['-V']).decode('utf-8').split()[1]
-        major_ver = pip_version.split('.')[0]
-        if int(major_ver) >= 6:
-            ret.append('--disable-pip-version-check')
-        return ret
-    else:
-        return None
+def get_site_packages_dir(installed_pkgs):
+    for info in itervalues(installed_pkgs):
+        if info['name'] == 'python':
+            if sys.platform == 'win32':
+                stdlib_dir = 'Lib'
+            else:
+                py_ver = info['version'][:3]
+                stdlib_dir = 'lib/python%s' % py_ver
+            return join(stdlib_dir, 'site-packages')
+    return None
 
 
-class PipPackage(dict):
-    def __str__(self):
-        if 'path' in self:
-            return '%s (%s)-%s-<pip>' % (
-                self['name'],
-                self['path'],
-                self['version']
-            )
-        return '%s-%s-<pip>' % (self['name'], self['version'])
+def get_egg_info_files(sp_dir):
+    for fn in os.listdir(sp_dir):
+        if not fn.endswith(('.egg', '.egg-info')):
+            continue
+        path = join(sp_dir, fn)
+        if isfile(path):
+            yield path
+        elif isdir(path):
+            path2 = join(path, 'PKG-INFO')
+            if isfile(path2):
+                yield path2
 
 
-def installed(prefix, output=True):
-    args = pip_args(prefix)
-    if args is None:
-        return
-    args.append('list')
-    try:
-        pipinst = subprocess.check_output(
-            args, universal_newlines=True
-        ).splitlines()
-    except Exception:
-        # Any error should just be ignored
-        if output:
-            print("# Warning: subprocess call to pip failed")
-        return
-
-    # For every package in pipinst that is not already represented
-    # in installed append a fake name to installed with 'pip'
-    # as the build string
-    pat = re.compile('([\w.-]+)\s+\((.+)\)')
-    for line in pipinst:
+pat = re.compile(r'(\w+):\s*(\S+)', re.I)
+def read_egg_info(path):
+    info = {}
+    for line in open(path):
         line = line.strip()
-        if not line:
-            continue
         m = pat.match(line)
-        if m is None:
-            if output:
-                print('Could not extract name and version from: %r' % line)
-            continue
-        name, version = m.groups()
-        name = name.lower()
-        kwargs = {
-            'name': name,
-            'version': version,
-        }
-        if ', ' in version:
-            # Packages installed with setup.py develop will include a path in
-            # the version. They should be included here, even if they are
-            # installed with conda, as they are preferred over the conda
-            # version. We still include the conda version, though, because it
-            # is still installed.
-
-            version, path = version.split(', ')
-            # We do this because the code below uses rsplit('-', 2)
-            version = version.replace('-', ' ')
-            kwargs.update({
-                'path': path,
-                'version': version,
-            })
-        yield PipPackage(**kwargs)
+        if m:
+            info[m.group(1).lower()] = m.group(2)
+        try:
+            return '%(name)s-%(version)s-<egg.info>' % info
+        except KeyError:
+            pass
+    return None
 
 
-def add_pip_installed(prefix, installed_pkgs, json=None, output=True):
-    # Defer to json for backwards compatibility
-    if type(json) is bool:
-        output = not json
+def pip_installed(prefix):
+    installed_pkgs = linked_data(prefix)
+    sp_dir = get_site_packages_dir(installed_pkgs)
+    if sp_dir is None:
+        return
 
-    # TODO Refactor so installed is a real list of objects/dicts
-    #      instead of strings allowing for direct comparison
-    conda_names = {d.rsplit('-', 2)[0] for d in installed_pkgs}
+    conda_files = set()
+    for info in itervalues(installed_pkgs):
+        conda_files.update(info.get('files', []))
+    print(len(conda_files))
 
-    for pip_pkg in installed(prefix, output=output):
-        if pip_pkg['name'] in conda_names and not 'path' in pip_pkg:
-            continue
-        installed_pkgs.add(str(pip_pkg))
+    res = set()
+    for path in get_egg_info_files(join(prefix, sp_dir)):
+        f = rel_path(prefix, path)
+        if f not in conda_files:
+            #print(f)
+            dist = read_egg_info(path)
+            if dist:
+                res.add(dist)
+    return res
+
+
+if __name__ == '__main__':
+    print(pip_installed(sys.prefix))
