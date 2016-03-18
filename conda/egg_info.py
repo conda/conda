144d4998518aef800d7fde54f960c6e1e0bd4c58
Ilan Schnell <ilanschnell@gmail.com>
2016-03-17 21:19:17 -0500

    fix return value when Python is not installed in environment

--- conda/egg_info.py
+++ conda/egg_info.py
@@ -70,7 +70,7 @@ def get_egg_info(prefix, all_pkgs=False):
     installed_pkgs = linked_data(prefix)
     sp_dir = get_site_packages_dir(installed_pkgs)
     if sp_dir is None:
-        return
+        return set()
 
     conda_files = set()
     for info in itervalues(installed_pkgs):
