7583faa587d1b0ff4fb8b5f8f743215179e62663
Michael Sarahan <msarahan@gmail.com>
2016-03-28 16:04:07 -0500

    add symlinking to exec folder.  add scripts back (bin install)

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -1,3 +1,9 @@
 unlink $PREFIX/bin/conda
 CONDA_DEFAULT_ENV='' $PYTHON setup.py install
 CONDA_DEFAULT_ENV='' python setup.py --version > __conda_version__.txt
+
+# link to exec folder as a more contained proxy.  Idea is that people can add exec folder to PATH
+#    instead of bin, and have only activate & conda on PATH - no trampling other stuff.
+mkdir -p $PREFIX/exec
+ln -s $PREFIX/bin/activate $PREFIX/exec/activate
+ln -s $PREFIX/bin/conda $PREFIX/exec/conda
