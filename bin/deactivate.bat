f531272c24b8c123cab9ddd7c66154cba277d5cc
Michael Sarahan <msarahan@continuum.io>
2016-04-15 14:50:19 -0500

    add forgotten activate scripts

--- bin/deactivate.bat
+++ bin/deactivate.bat
@@ -1,24 +1,40 @@
-@echo off
-setlocal
+@REM @ symbols in this file indicate that output should not be printed.
+@REM   Setting it this way allows us to not touch the user's echo setting.
+@REM   For debugging, remove the @ on the section you need to study.
+@SETLOCAL
 
-if not "%1" == "--help" goto skipusage
+@IF NOT "%1" == "--help" @GOTO skipusage
     (
-    echo Usage: deactivate
-    echo.
-    echo Deactivates previously activated Conda
-    echo environment.
-    echo.
-    echo Additional arguments are ignored.
+    @ECHO Usage: deactivate
+    @ECHO.
+    @ECHO Deactivates previously activated Conda
+    @ECHO environment.
+    @ECHO.
+    @ECHO Additional arguments are ignored.
     ) 1>&2
-    exit /b 1
+    @EXIT /b 1
 :skipusage
 
-REM Deactivate a previous activation if it is live
-IF NOT "%CONDA_PATH_BACKUP%" == "" (SET "PATH=%CONDA_PATH_BACKUP%" && SET "CONDA_PATH_BACKUP=")
-IF NOT "%CONDA_OLD_PS1%" == "" (SET "PROMPT=%CONDA_OLD_PS1%" && SET "CONDA_OLD_PS1=")
+@REM Deactivate a previous activation if it is live
+@IF "%CONDA_PATH_BACKUP%"=="" @GOTO NOPATH
+   @SET "PATH=%CONDA_PATH_BACKUP%"
+   @SET CONDA_PATH_BACKUP=
+   :NOPATH
 
-endlocal & (
-            set "CONDA_DEFAULT_ENV="
-            set "PATH=%PATH%"
-            set "PROMPT=%PROMPT%"
+@IF "%CONDA_OLD_PS1%"=="" @GOTO NOPROMPT
+   @SET "PROMPT=%CONDA_OLD_PS1%"
+   @SET CONDA_OLD_PS1=
+   :NOPROMPT
+
+@ENDLOCAL & (
+            REM Run any deactivate scripts
+            @IF EXIST "%CONDA_DEFAULT_ENV%\etc\conda\deactivate.d" (
+                @PUSHD "%CONDA_DEFAULT_ENV%\etc\conda\deactivate.d"
+                @FOR %%g IN (*.bat) DO @CALL "%%g"
+                @POPD
+            )
+
+            @SET "CONDA_DEFAULT_ENV="
+            @SET "PATH=%PATH%"
+            @SET "PROMPT=%PROMPT%"
            )
