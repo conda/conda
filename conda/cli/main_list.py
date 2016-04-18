39a7c89e026bd4bf4875f28ea2b58d09b21e5e7e
Michael C. Grant <mcg@cvxr.com>
2016-04-18 14:27:57 -0500

    document new package and meta cache; fixed package listing

--- conda/cli/main_list.py
+++ conda/cli/main_list.py
@@ -115,7 +115,7 @@ def get_packages(installed, regex):
     pat = re.compile(regex, re.I) if regex else None
 
     for dist in sorted(installed, key=str.lower):
-        name = dist.rsplit('-', 2)[0]
+        name = install.name_dist(dist)
         if pat and pat.search(name) is None:
             continue
 
