92ff4c8839874c8560ede7e77b5e33e3f2caffaf
Ilan Schnell <ilanschnell@gmail.com>
2016-04-18 22:32:03 -0500

    remove dead code

--- conda/misc.py
+++ conda/misc.py
@@ -283,68 +283,6 @@ def environment_for_conda_environment(prefix=config.root_dir):
     return binpath, env
 
 
-def launch(fn, prefix=config.root_dir, additional_args=None, background=False):
-    info = install.is_linked(prefix, fn[:-8])
-    if info is None:
-        return None
-
-    if not info.get('type') == 'app':
-        raise TypeError('Not an application: %s' % fn)
-
-    binpath, env = environment_for_conda_environment(prefix)
-    # allow updating environment variables from metadata
-    if 'app_env' in info:
-        env.update(info['app_env'])
-
-    # call the entry command
-    args = info['app_entry'].split()
-    args = [a.replace('${PREFIX}', prefix) for a in args]
-    arg0 = find_executable(args[0], env['PATH'])
-    if arg0 is None:
-        raise Exception('Executable not found: %s' % args[0])
-    args[0] = arg0
-
-    cwd = abspath(expanduser('~'))
-    if additional_args:
-        args.extend(additional_args)
-    if sys.platform == 'win32' and background:
-        return subprocess.Popen(args, cwd=cwd, env=env, close_fds=False,
-                                creationflags=subprocess.CREATE_NEW_CONSOLE)
-    else:
-        return subprocess.Popen(args, cwd=cwd, env=env, close_fds=False)
-
-
-def execute_in_environment(cmd, prefix=config.root_dir, additional_args=None,
-                           inherit=True):
-    """Runs ``cmd`` in the specified environment.
-
-    ``inherit`` specifies whether the child inherits stdio handles (for JSON
-    output, we don't want to trample this process's stdout).
-    """
-    binpath, env = environment_for_conda_environment(prefix)
-
-    if sys.platform == 'win32' and cmd == 'python':
-        # python is located one directory up on Windows
-        cmd = join(binpath, '..', cmd)
-
-    args = shlex.split(cmd)
-    if additional_args:
-        args.extend(additional_args)
-
-    if inherit:
-        stdin, stdout, stderr = None, None, None
-    else:
-        stdin, stdout, stderr = subprocess.PIPE, subprocess.PIPE, subprocess.PIPE
-
-    if sys.platform == 'win32' and not inherit:
-        return subprocess.Popen(args, env=env, close_fds=False,
-                                stdin=stdin, stdout=stdout, stderr=stderr,
-                                creationflags=subprocess.CREATE_NEW_CONSOLE)
-    else:
-        return subprocess.Popen(args, env=env, close_fds=False,
-                                stdin=stdin, stdout=stdout, stderr=stderr)
-
-
 def make_icon_url(info):
     if 'channel' in info and 'icon' in info:
         base_url = dirname(info['channel'].rstrip('/'))
