conda/conda

commit 1cc6481195492c2f757cc34d5bfb1799d79dafed

CLI flag disclaimer

conda/cli/common.py
@@ -267,14 +267,16 @@ def add_parser_install(p):
         action="store_true",
         dest="channel_priority",
         default=config.channel_priority,
-        help="Channel priority takes precedence over packaage version (default: %(defaults))."
+        help="Channel priority takes precedence over packaage version (default: %(defaults)). "
+             "Note: This feature is in beta and may change in a future release."
     )
     p.add_argument(
         "--no-channel-priority", "--no-channel-pri", "--no-chan-pri",
         action="store_true",
         dest="channel_priority",
         default=not config.channel_priority,
-        help="Package version takes precedence over channel priority (default: %(defaults))."
+        help="Package version takes precedence over channel priority (default: %(defaults)). "
+             "Note: This feature is in beta and may change in a future release."
     )
     add_parser_show_channel_urls(p)
 

commit 1a49ec50aed114229f26aaf6e8b9ef55bdb23993

fix help in install. broken from 1cc64811

.travis.yml
@@ -30,6 +30,7 @@ script:
   - radon cc --ignore "build,tests,ve,_vendor,auxlib,progressbar" --no-assert -nb --total-average -s conda
   - radon mi --ignore "build,tests,ve,_vendor,auxlib,progressbar" -na -m -s conda
   - flake8 --statistics
+  - conda install --help
 
 sudo: false
 

conda/cli/common.py
@@ -267,7 +267,7 @@ def add_parser_install(p):
         action="store_true",
         dest="channel_priority",
         default=config.channel_priority,
-        help="Channel priority takes precedence over packaage version (default: %(defaults)). "
+        help="Channel priority takes precedence over packaage version (default: %(default)s). "
              "Note: This feature is in beta and may change in a future release."
     )
     p.add_argument(
@@ -275,7 +275,7 @@ def add_parser_install(p):
         action="store_true",
         dest="channel_priority",
         default=not config.channel_priority,
-        help="Package version takes precedence over channel priority (default: %(defaults)). "
+        help="Package version takes precedence over channel priority (default: %(default)s). "
              "Note: This feature is in beta and may change in a future release."
     )
     add_parser_show_channel_urls(p)