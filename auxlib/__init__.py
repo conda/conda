6a43adcf7d1ba2904a3d36e2a7461f879996336a
Kale Franz <kfranz@continuum.io>
2016-05-11 13:43:58 -0500

    don't use git rev-parse at all

--- auxlib/__init__.py
+++ auxlib/__init__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """auxiliary library to the python standard library"""
 from __future__ import absolute_import, division, print_function
-from logging import getLogger, StreamHandler, DEBUG
+from logging import getLogger, StreamHandler, INFO
 
 # don't mess up logging for users
 log = getLogger(__name__)
 log.addHandler(StreamHandler())
-log.setLevel(DEBUG)
+log.setLevel(INFO)
 
 from .packaging import BuildPyCommand, SDistCommand, Tox, get_version  # NOQA
 
