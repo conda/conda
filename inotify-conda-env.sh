a12a7cca6e24fbf99d8c155587976ffaa2472238
Jillian Rowe <jillian.e.rowe@gmail.com>
2016-05-31 12:27:07 +0400

    got rid of a few files

--- inotify-conda-env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#module load anaconda conda-env
-
-inotify-hookable \
-    --watch-directories /home/jillian/projects/python/conda-env \
-    --on-modify-command "python setup.py install"
