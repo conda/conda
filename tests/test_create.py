29ad28a31449c8bdde787b75e1867e7fab693992
Kale Franz <kfranz@continuum.io>
2016-06-07 14:08:48 -0500

    cleanup test_create

--- tests/test_create.py
+++ tests/test_create.py
@@ -1,35 +1,41 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
 
-import pytest
 from contextlib import contextmanager
-import os
 from logging import getLogger
+from os.path import exists, isdir, isfile, join
 from shlex import split
 from shutil import rmtree
-from tempfile import mkdtemp
+from tempfile import gettempdir
+from uuid import uuid1
 
+import pytest
+
+from conda import config
 from conda.cli import conda_argparse
 from conda.cli.main_create import configure_parser
-from conda import config
 
 log = getLogger(__name__)
 
 
+def make_temp_env_path():
+    tempdir = gettempdir()
+    dirname = str(uuid1())[:8]
+    env_path = join(tempdir, dirname)
+    if exists(env_path):
+        # rm here because create complains if directory exists
+        rmtree(env_path)
+    assert isdir(tempdir)
+    return env_path
+
+
 @contextmanager
-def make_env(*packages):
+def make_temp_env(*packages):
+    env_path = make_temp_env_path()
     try:
         # try to clear any config that's been set by other tests
         config.rc = config.load_condarc('')
-
-        env_path = mkdtemp()
-        try:
-            rmtree(env_path)  # rm here because create complains if directory exists
-        except OSError as e:
-            print("OSError", e)
-        if not os.path.exists(os.path.dirname(env_path)):
-            os.mkdir(os.path.dirname(env_path))
-
+        
         p = conda_argparse.ArgumentParser()
         sub_parsers = p.add_subparsers(metavar='command', dest='cmd')
         configure_parser(sub_parsers)
@@ -41,33 +47,28 @@ def make_env(*packages):
 
         yield env_path
     finally:
-        try:
-            rmtree(env_path)
-        except OSError:
-            print("{0} does not exist".format(env_path))
-        print(env_path)
+        rmtree(env_path, ignore_errors=True)
 
 
 @pytest.mark.timeout(600)
 def test_just_python3():
-    with make_env("python=3") as env_path:
-        assert os.path.exists(os.path.join(env_path, 'bin/python3'))
+    with make_temp_env("python=3") as env_path:
+        assert exists(join(env_path, 'bin/python3'))
 
 
 @pytest.mark.timeout(600)
 def test_just_python2():
-    with make_env("python=2") as env_path:
-        assert os.path.exists(os.path.join(env_path, 'bin/python2'))
+    with make_temp_env("python=2") as env_path:
+        assert exists(join(env_path, 'bin/python2'))
 
 
 @pytest.mark.timeout(600)
 def test_python2_anaconda():
-    with make_env("python=2 anaconda") as env_path:
-        assert os.path.isfile(os.path.join(env_path, 'bin/python2'))
-        assert os.path.isfile(os.path.join(env_path, 'bin/numba'))
+    with make_temp_env("python=2 anaconda") as env_path:
+        assert isfile(join(env_path, 'bin/python2'))
+        assert isfile(join(env_path, 'bin/numba'))
 
 
 if __name__ == '__main__':
     test_just_python3()
     test_just_python2()
-    # test_python2_anaconda()
