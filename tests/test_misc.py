f456f38871a2d51155407ef89ca095204d0f86a1
Kale Franz <kfranz@continuum.io>
2016-06-09 00:59:11 -0500

    fix explicit for windows paths with backslash

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -15,13 +15,13 @@ class TestMisc(unittest.TestCase):
     def test_url_pat_1(self):
         m = url_pat.match('http://www.cont.io/pkgs/linux-64/foo.tar.bz2'
                           '#d6918b03927360aa1e57c0188dcb781b')
-        self.assertEqual(m.group('url'), 'http://www.cont.io/pkgs/linux-64')
+        self.assertEqual(m.group('url_p'), 'http://www.cont.io/pkgs/linux-64')
         self.assertEqual(m.group('fn'), 'foo.tar.bz2')
         self.assertEqual(m.group('md5'), 'd6918b03927360aa1e57c0188dcb781b')
 
     def test_url_pat_2(self):
         m = url_pat.match('http://www.cont.io/pkgs/linux-64/foo.tar.bz2')
-        self.assertEqual(m.group('url'), 'http://www.cont.io/pkgs/linux-64')
+        self.assertEqual(m.group('url_p'), 'http://www.cont.io/pkgs/linux-64')
         self.assertEqual(m.group('fn'), 'foo.tar.bz2')
         self.assertEqual(m.group('md5'), None)
 
