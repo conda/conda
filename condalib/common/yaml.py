d0b042b63893f5a98dd7501a660cf813ef7d3d69
Kale Franz <kfranz@continuum.io>
2016-06-11 02:14:04 -0500

    test yaml

--- condalib/common/yaml.py
+++ condalib/common/yaml.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
 from logging import getLogger
-import sys
 
 log = getLogger(__name__)
 
@@ -11,30 +10,17 @@ def get_yaml():
     try:
         import ruamel_yaml as yaml
     except ImportError:
-        try:
-            import ruamel.yaml as yaml
-        except ImportError:
-            try:
-                import yaml
-            except ImportError:
-                sys.exit("No yaml library available.\n"
-                         "To proceed, please conda install ruamel_yaml")
+        import ruamel.yaml as yaml
     return yaml
 
 
 def yaml_load(filehandle):
     yaml = get_yaml()
-    try:
-        return yaml.load(filehandle, Loader=yaml.RoundTripLoader, version="1.2")
-    except AttributeError:
-        return yaml.load(filehandle)
+    return yaml.load(filehandle, Loader=yaml.RoundTripLoader, version="1.2")
 
 
-def yaml_dump(string):
+def yaml_dump(obj):
     yaml = get_yaml()
-    try:
-        return yaml.dump(string, Dumper=yaml.RoundTripDumper,
-                         block_seq_indent=2, default_flow_style=False,
-                         indent=4)
-    except AttributeError:
-        return yaml.dump(string, default_flow_style=False)
+    return yaml.dump(obj, Dumper=yaml.RoundTripDumper,
+                     block_seq_indent=2, default_flow_style=False,
+                     indent=2)
---
+++ tests/condalib/common/test_yaml.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+from logging import getLogger
+
+from auxlib.ish import dals
+
+from condalib.common.yaml import yaml_dump, yaml_load
+
+log = getLogger(__name__)
+
+
+def test_dump():
+    obj = dict([
+        ('a_seq', [1, 2, 3]),
+        ('a_map', {'a_key': 'a_value'}),
+    ])
+    assert obj == yaml_load(yaml_dump(obj))
+
+
+def test_seq():
+    test_string = dals("""
+    a_seq:
+      - 1
+      - 2
+      - 3
+    """)
+    assert test_string == yaml_dump({'a_seq': [1, 2, 3]})
+
+
+def test_map():
+    test_string = dals("""
+    a_map:
+      a_key: a_value
+    """)
+    assert test_string == yaml_dump({'a_map': {'a_key': 'a_value'}})
