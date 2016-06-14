300038f425b176e3674f5a604459d5da4c6eeede
Zhang Tian <zhangtian1000@gmail.com>
2016-06-14 09:51:15 -0500

    remove unused futures

--- conda/install.py
+++ conda/install.py
@@ -926,8 +926,8 @@ def linked_data(prefix):
         else:
             try:
                 json_files_tuple = tuple(json_files)
-                futures = tuple(executor.submit(load_linked_data, prefix,
-                                                json_file) for json_file in json_files_tuple)
+                tuple(executor.submit(load_linked_data, prefix,
+                                      json_file) for json_file in json_files_tuple)
             finally:
                 executor.shutdown(wait=True)
     return recs
