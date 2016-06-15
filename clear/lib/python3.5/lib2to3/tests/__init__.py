11de7f472d536628c5449cb8787e589a37196b21
Zhang Tian <zhangtian1000@gmail.com>
2016-06-15 15:08:17 -0500

    remove clear

--- clear/lib/python3.5/lib2to3/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-# Author: Collin Winter
-
-import os
-import unittest
-
-from test.support import load_package_tests
-
-def load_tests(*args):
-    return load_package_tests(os.path.dirname(__file__), *args)
