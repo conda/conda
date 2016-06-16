a7e113af27e8d1d23b14413c994b51024f3adef5
Michael C. Grant <mcg@cvxr.com>
2016-06-15 23:25:47 -0500

    Restore explicit file installs

--- conda/config.py
+++ conda/config.py
@@ -297,6 +297,8 @@ def url_channel(url):
         return None, '<unknown>'
     channel = url.rsplit('/', 2)[0]
     schannel = canonical_channel_name(channel)
+    if url.startswith('file://') and schannel != 'local':
+        channel = schannel = url.rsplit('/', 1)[0]
     return channel, schannel
 
 # ----- allowed channels -----
