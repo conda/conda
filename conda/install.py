3d71b141834a9834663275ef1139f00038d09f93
Kale Franz <kfranz@continuum.io>
2016-06-15 22:19:05 -0500

    fix #2703 ignore activate symlink error if links already exist

--- conda/install.py
+++ conda/install.py
@@ -573,13 +573,20 @@ def symlink_conda_hlp(prefix, root_dir, where, symlink_fn):
     for f in scripts:
         root_file = join(root_dir, where, f)
         prefix_file = join(prefix_where, f)
-        # try to kill stale links if they exist
-        if os.path.lexists(prefix_file):
-            os.remove(prefix_file)
-        # if they're in use, they won't be killed.  Skip making new symlink.
-        if not os.path.lexists(prefix_file):
-            symlink_fn(root_file, prefix_file)
-
+        try:
+            # try to kill stale links if they exist
+            if os.path.lexists(prefix_file):
+                os.remove(prefix_file)
+            # if they're in use, they won't be killed.  Skip making new symlink.
+            if not os.path.lexists(prefix_file):
+                symlink_fn(root_file, prefix_file)
+        except (IOError, OSError) as e:
+            if (os.path.lexists(prefix_file) and
+                    (e.errno == errno.EPERM or e.errno == errno.EACCES)):
+                log.debug("Cannot symlink {0} to {1}. Ignoring since link already exists."
+                          .format(root_file, prefix_file))
+            else:
+                raise
 
 # ========================== begin API functions =========================
 
