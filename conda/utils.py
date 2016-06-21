9760bdde0c947d52dc7c3c217677443fe8a8d794
Pete Bachant <petebachant@gmail.com>
2016-06-21 11:59:10 -0400

    Git Bash executable is called bash on Windows
    Wasn't able to use `source activate` on Windows with Git Bash, but this fixed it.

--- conda/utils.py
+++ conda/utils.py
@@ -401,6 +401,9 @@ if sys.platform == "win32":
         "bash.exe": dict(
             msys2_shell_base, exe="bash.exe",
         ),
+        "bash": dict(
+            msys2_shell_base, exe="bash",
+        ),
         "sh.exe": dict(
             msys2_shell_base, exe="sh.exe",
         ),
