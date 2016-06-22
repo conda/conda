b22c7493a3fc6d46a632ab8f24b402e389b0cb07
Michael C. Grant <mcg@cvxr.com>
2016-06-22 13:44:44 -0500

    Handle both file-based channels and explicit file-based URLs

--- conda/cli/main_list.py
+++ conda/cli/main_list.py
@@ -17,8 +17,8 @@ from .common import (add_parser_help, add_parser_prefix, add_parser_json,
                      get_prefix)
 from ..config import show_channel_urls, subdir, use_pip
 from ..egg_info import get_egg_info
-from ..install import dist2quad
-from ..install import name_dist, is_linked, linked
+from ..install import dist2quad, load_meta, linked
+from ..install import name_dist, is_linked, linked_data
 
 
 descr = "List linked packages in a conda environment."
@@ -192,35 +192,11 @@ def print_explicit(prefix, add_md5=False):
     print_export_header()
     print("@EXPLICIT")
 
-    meta_dir = join(prefix, 'conda-meta')
-    for fn in sorted(os.listdir(meta_dir)):
-        if not fn.endswith('.json'):
-            continue
-        with open(join(meta_dir, fn)) as fi:
-            meta = json.load(fi)
+    for meta in sorted(linked_data(prefix).values(), key=lambda x: x['name']):
         url = meta.get('url')
-
-        def format_url():
-            return '%s%s-%s-%s.tar.bz2' % (meta['channel'], meta['name'],
-                                           meta['version'], meta['build'])
-        # two cases in which we want to try to format the url:
-        # 1. There is no url key in the metadata
-        # 2. The url key in the metadata is referencing a file on the local
-        #    machine
-        if not url:
-            try:
-                url = format_url()
-            except KeyError:
-                # Declare failure :-(
-                print('# no URL for: %s' % fn[:-5])
-                continue
-        if url.startswith('file'):
-            try:
-                url = format_url()
-            except KeyError:
-                # declare failure and allow the url to be the file from which it was
-                # originally installed
-                continue
+        if not url or url.startswith('<unknown>'):
+            print('# no URL for: %s' % meta['fn'])
+            continue
         md5 = meta.get('md5')
         print(url + ('#%s' % md5 if add_md5 and md5 else ''))
 
--- conda/config.py
+++ conda/config.py
@@ -295,12 +295,18 @@ def canonical_channel_name(channel):
         return channel
 
 def url_channel(url):
-    if url is None:
-        return None, '<unknown>'
-    channel = url.rsplit('/', 2)[0]
-    schannel = canonical_channel_name(channel)
-    if url.startswith('file://') and schannel != 'local':
-        channel = schannel = url.rsplit('/', 1)[0]
+    parts = (url or '').rsplit('/', 2)
+    if len(parts) == 1:
+        return '<unknown>', '<unknown>'
+    if len(parts) == 2:
+        return parts[0], parts[0]
+    if url.startswith('file://') and parts[1] not in ('noarch', subdir):
+        # Explicit file-based URLs are denoted with a '/' in the schannel
+        channel = parts[0] + '/' + parts[1]
+        schannel = channel + '/'
+    else:
+        channel = parts[0]
+        schannel = canonical_channel_name(channel)
     return channel, schannel
 
 # ----- allowed channels -----
