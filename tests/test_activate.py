a9d0e0041c209088755ea57008d19642d5b07cae
Kale Franz <kfranz@continuum.io>
2016-06-24 04:36:29 -0500

    bring a shortcut test in test_create into the framework

--- tests/test_activate.py
+++ tests/test_activate.py
@@ -621,6 +621,7 @@ def test_activate_does_not_leak_echo_setting(shell):
         assert_equals(stdout, u'ECHO is on.', stderr)
 
 
+@pytest.mark.installed
 def test_activate_non_ascii_char_in_path(shell):
     if not PY3:
         pytest.xfail("subprocess with python 2.7 is broken with unicode")
