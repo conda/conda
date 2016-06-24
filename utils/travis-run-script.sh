7353483ffa9bb91c2a893f0bb89fafb4b3689f14
Kale Franz <kfranz@continuum.io>
2016-06-24 06:08:04 -0500

    cleanup

--- utils/travis-run-script.sh
+++ utils/travis-run-script.sh
@@ -5,10 +5,13 @@ main_test() {
     export PYTHONHASHSEED=$(python -c "import random as r; print(r.randint(0,4294967296))")
     echo $PYTHONHASHSEED
     py.test --cov-report xml tests --shell=bash --shell=zsh -m "not installed"
+    python setup.py --version
     python setup.py install
+    hash -r
     conda info
     py.test --cov-report xml tests --shell=bash --shell=zsh -m "installed"
-    conda install -y conda-build
+    conda install -y -q conda-build
+    set +x
     conda build conda.recipe
 }
 
