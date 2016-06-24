dbc7d8ebfd33116d5ae0f424f4ef9ca3ffda900b
Kale Franz <kfranz@continuum.io>
2016-06-24 09:30:05 -0500

    append coverage

--- utils/travis-run-script.sh
+++ utils/travis-run-script.sh
@@ -4,12 +4,12 @@ set -x
 main_test() {
     export PYTHONHASHSEED=$(python -c "import random as r; print(r.randint(0,4294967296))")
     echo $PYTHONHASHSEED
-    py.test --cov-report xml tests --shell=bash --shell=zsh -m "not installed"
+    py.test --cov-report xml --shell=bash --shell=zsh -m "not installed" tests
     python setup.py --version
     python setup.py install
     hash -r
     conda info
-    py.test --cov-report xml tests --shell=bash --shell=zsh -m "installed"
+    py.test --cov-report xml --cov-append --shell=bash --shell=zsh -m "installed" tests
     # conda install -y -q conda-build
     # set +x
     # conda build conda.recipe
