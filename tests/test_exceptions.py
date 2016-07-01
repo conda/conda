1a73b1240d7f6eb4bd51fbdf8ed40d59d546f3ab
Dylan Orzel <dylan.orzel@gmail.com>
2016-07-01 12:06:51 -0500

    added tests for CondaError and modified output to include newlines

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -4,7 +4,7 @@ import unittest
 from conda import exceptions
 
 
-class InvalidExceptionTestCase(unittest.TestCase):
+class InvalidInstructionTestCase(unittest.TestCase):
     def test_requires_an_instruction(self):
         with self.assertRaises(TypeError):
             exceptions.InvalidInstruction()
@@ -16,5 +16,42 @@ class InvalidExceptionTestCase(unittest.TestCase):
     def test_creates_message_with_instruction_name(self):
         random_instruction = random.randint(100, 200)
         e = exceptions.InvalidInstruction(random_instruction)
-        expected = "No handler for instruction: %s" % random_instruction
+        expected = "No handler for instruction: %s\n" % random_instruction
         self.assertEqual(expected, str(e))
+
+
+class CondaErrorTestCase(unittest.TestCase):
+    def test_repr_is_correct(self):
+        e = exceptions.CondaError("Can't do that")
+        self.assertEqual(repr(e), "Can't do that")
+
+    def test_inherited_repr_is_correct(self):
+        try:
+            raise exceptions.CondaValueError('value incorrect')
+        except exceptions.CondaValueError as e:
+            err = exceptions.CondaError("Can't do that", e)
+
+        self.assertEqual(repr(err), "Can't do that Value error: value incorrect\n")
+
+    def test_str_is_correct(self):
+        e = exceptions.CondaError("Can't do that")
+        self.assertEqual(str(e), "Can't do that")
+
+    def test_inherited_str_is_correct(self):
+        try:
+            raise exceptions.CondaValueError('value incorrect')
+        except exceptions.CondaValueError as e:
+            err = exceptions.CondaError("Can't do that", e)
+
+        self.assertEqual(str(err), "Can't do that Value error: value incorrect\n")
+
+    def test_json_flags_not_in_output(self):
+        try:
+            raise exceptions.CondaValueError('value incorrect', False)
+        except exceptions.CondaValueError as e:
+            err = exceptions.CondaError("Can't do that", e, True)
+
+        self.assertNotIn('False', repr(err))
+        self.assertNotIn('True', repr(err))
+        self.assertNotIn('False', str(err))
+        self.assertNotIn('True', str(err))
