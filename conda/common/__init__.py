e9a8cd9b7afdcf89e20603b118b9eb6e2f106dab
Kale Franz <kfranz@continuum.io>
2016-07-06 17:57:56 -0500

    update conda.common README

--- conda/common/__init__.py
+++ conda/common/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Code in ``conda.common`` is not conda-specific.  It is able to stand independently on its own.
+Code in ``conda.common`` is not conda-specific.  Technically, it sits *aside* the application
+stack and not *within* the stack.  It is able to stand independently on its own.
 The *only* allowed imports of conda code in ``conda.common`` modules are imports of other
 ``conda.common`` modules and imports from ``conda._vendor``.
 
