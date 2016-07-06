3dfc2ead2d3a57ff013d9f36301ba81fc1149464
Kale Franz <kfranz@continuum.io>
2016-07-06 17:55:25 -0500

    travis-ci cleanup

---
+++ utils/travis-run-install.sh
@@ -0,0 +1,31 @@
+set -e
+set -x
+
+
+flake8_install() {
+    pip install flake8
+}
+
+
+main_install() {
+    pip install psutil ruamel.yaml pycosat pycrypto
+    case $TRAVIS_PYTHON in
+      '2.7')
+          pip install enum34 futures
+          ;;
+      *) ;;
+}
+
+
+test_install() {
+    pip install mock pytest pytest-cov pytest-timeout radon responses
+}
+
+
+if [[ $FLAKE8 == true ]]; then
+    main_install
+    flake8_install
+else
+    main_install
+    test_install
+fi
