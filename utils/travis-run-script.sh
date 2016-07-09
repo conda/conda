5a3a179c02b6e9e52d6a68da00a2ec7e663de82e
Kale Franz <kfranz@continuum.io>
2016-07-09 18:41:35 -0500

    travis osx

--- utils/travis-run-script.sh
+++ utils/travis-run-script.sh
@@ -4,12 +4,12 @@ set -x
 main_test() {
     export PYTHONHASHSEED=$(python -c "import random as r; print(r.randint(0,4294967296))")
     echo $PYTHONHASHSEED
-    py.test --cov-report xml --shell=bash --shell=zsh -m "not installed" tests
-    python setup.py --version
-    python setup.py install
+    $PYTHON_EXE -m py.test --cov-report xml --shell=bash --shell=zsh -m "not installed" tests
+    $PYTHON_EXE setup.py --version
+    $PYTHON_EXE setup.py install
     hash -r
     conda info
-    py.test --cov-report xml --cov-append --shell=bash --shell=zsh -m "installed" tests
+    $PYTHON_EXE -m py.test --cov-report xml --cov-append --shell=bash --shell=zsh -m "installed" tests
     # conda install -y -q conda-build
     # set +x
     # conda build conda.recipe
@@ -18,6 +18,9 @@ main_test() {
 flake8_test() {
     flake8 --statistics
 }
+
+which -a python
+env | sort
 
 if [[ $FLAKE8 == true ]]; then
     flake8_test
