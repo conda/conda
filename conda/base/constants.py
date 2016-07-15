664ce97cc8a879b7ad4034bf19ea825ad4a10a8b
Kale Franz <kfranz@continuum.io>
2016-07-15 14:32:59 -0500

    linux fix

--- conda/base/constants.py
+++ conda/base/constants.py
@@ -24,14 +24,21 @@ class Arch(Enum):
 
 
 class Platform(Enum):
-    linux = 'linux2'
+    linux = 'linux'
     win = 'win32'
     openbsd = 'openbsd5'
     osx = 'darwin'
 
     @classmethod
     def from_sys(cls):
-        return cls(sys.platform)
+        p = sys.platform
+        if p.startswith('linux'):
+            # Changed in version 2.7.3: Since lots of code check for sys.platform == 'linux2',
+            # and there is no essential change between Linux 2.x and 3.x, sys.platform is always
+            # set to 'linux2', even on Linux 3.x. In Python 3.3 and later, the value will always
+            # be set to 'linux'
+            p = 'linux'
+        return cls(p)
 
 machine_bits = 8 * tuple.__itemsize__
 
