06f2003a470cf8bf577600486f440f02644c30e1
Kale Franz <kfranz@continuum.io>
2016-07-15 10:33:24 -0500

    fix all test_plan; make context truly resetable

--- tests/test_plan.py
+++ tests/test_plan.py
@@ -1,3 +1,5 @@
+import os
+
 from contextlib import contextmanager
 import sys
 import json
@@ -8,7 +10,7 @@ from collections import defaultdict
 
 import pytest
 
-from conda.base.context import context
+from conda.base.context import context, reset_context
 from conda.install import LINK_HARD
 import conda.plan as plan
 import conda.instructions as inst
@@ -134,8 +136,8 @@ class TestAddDeaultsToSpec(unittest.TestCase):
             self.check(specs, added)
 
 def test_display_actions():
-    import conda.plan
-    conda.plan.config_show_channel_urls = False
+    os.environ['CONDA_SHOW_CHANNEL_URLS'] = 'False'
+    reset_context(())
     actions = defaultdict(list, {"FETCH": ['sympy-0.7.2-py27_0', "numpy-1.7.1-py27_0"]})
     # The older test index doesn't have the size metadata
     index['sympy-0.7.2-py27_0.tar.bz2']['size'] = 4374752
@@ -144,7 +146,6 @@ def test_display_actions():
     with captured() as c:
         display_actions(actions, index)
 
-    import pdb; pdb.set_trace()
     assert c.stdout == """
 The following packages will be downloaded:
 
@@ -278,8 +279,8 @@ The following packages will be DOWNGRADED due to dependency conflicts:
 
 
 def test_display_actions_show_channel_urls():
-    import conda.plan
-    conda.plan.config_show_channel_urls = True
+    os.environ['CONDA_SHOW_CHANNEL_URLS'] = 'True'
+    reset_context(())
     actions = defaultdict(list, {"FETCH": ['sympy-0.7.2-py27_0',
         "numpy-1.7.1-py27_0"]})
     # The older test index doesn't have the size metadata
@@ -453,8 +454,8 @@ The following packages will be DOWNGRADED due to dependency conflicts:
 
 
 def test_display_actions_link_type():
-    import conda.plan
-    conda.plan.config_show_channel_urls = False
+    os.environ['CONDA_SHOW_CHANNEL_URLS'] = 'False'
+    reset_context(())
 
     actions = defaultdict(list, {'LINK': ['cython-0.19.1-py33_0 2', 'dateutil-1.5-py33_0 2',
     'numpy-1.7.1-py33_0 2', 'python-3.3.2-0 2', 'readline-6.2-0 2', 'sqlite-3.7.13-0 2', 'tk-8.5.13-0 2', 'zlib-1.2.7-0 2']})
@@ -605,8 +606,8 @@ The following packages will be DOWNGRADED due to dependency conflicts:
     dateutil: 2.1-py33_1    --> 1.5-py33_0  (copy)
 
 """
-    import conda.plan
-    conda.plan.config_show_channel_urls = True
+    os.environ['CONDA_SHOW_CHANNEL_URLS'] = 'True'
+    reset_context(())
 
     index['cython-0.19.1-py33_0.tar.bz2']['channel'] = 'my_channel'
     index['dateutil-1.5-py33_0.tar.bz2']['channel'] = 'my_channel'
@@ -662,8 +663,8 @@ The following packages will be DOWNGRADED due to dependency conflicts:
 """
 
 def test_display_actions_features():
-    import conda.plan
-    conda.plan.config_show_channel_urls = False
+    os.environ['CONDA_SHOW_CHANNEL_URLS'] = 'False'
+    reset_context(())
 
     actions = defaultdict(list, {'LINK': ['numpy-1.7.1-py33_p0', 'cython-0.19-py33_0']})
 
@@ -739,8 +740,8 @@ The following packages will be UPDATED:
     numpy: 1.7.1-py33_p0 [mkl] --> 1.7.1-py33_0
 
 """
-    import conda.plan
-    conda.plan.config_show_channel_urls = True
+    os.environ['CONDA_SHOW_CHANNEL_URLS'] = 'True'
+    reset_context(())
 
     actions = defaultdict(list, {'LINK': ['numpy-1.7.1-py33_p0', 'cython-0.19-py33_0']})
 
