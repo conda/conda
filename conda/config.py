316b57fea0d6a46f43b13b4b3b408f6ac18c1163
Kale Franz <kfranz@continuum.io>
2016-07-22 17:53:53 -0500

    Revert "handle failures in binstar_client more general"
    This reverts commit 810010020910901c3f9b78ec63e0ecc012c247b1.

--- conda/config.py
+++ conda/config.py
@@ -257,9 +257,11 @@ def init_binstar(quiet=False):
             # Turn off output in offline mode so people don't think we're going online
             args = namedtuple('args', 'log_level')(0) if quiet or offline else None
             binstar_client = get_binstar(args)
-        except Exception as e:
-            stderrlog.info("Warning: somethings is wrong with binstar_client (%s)" % e)
+        except ImportError:
+            log.debug("Could not import binstar")
             binstar_client = ()
+        except Exception as e:
+            stderrlog.info("Warning: could not import binstar_client (%s)" % e)
     if binstar_client == ():
         binstar_domain = DEFAULT_CHANNEL_ALIAS
         binstar_domain_tok = None
