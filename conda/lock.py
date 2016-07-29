0fefedf85a23600be0a7bed36fe289575c3a16ad
Zhang Tian <zhangtian1000@gmail.com>
2016-07-29 15:32:19 -0500

    tmp lock for filr

--- conda/lock.py
+++ conda/lock.py
@@ -4,25 +4,76 @@
 # conda is distributed under the terms of the BSD 3-clause license.
 # Consult LICENSE.txt or http://opensource.org/licenses/BSD-3-Clause.
 
+#!/usr/bin/python3
+
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# For more information, please refer to <http://unlicense.org>
+
 """
-Tools for working with locks
-
-A lock is just an empty directory. We use directories because this lets us use
-the race condition-proof os.makedirs.
-
-For now, there is one global lock for all of conda, because some things happen
-globally (such as downloading packages).
-
-We don't raise an error if the lock is named with the current PID
+A platform independent file lock that supports the with-statement.
 """
+
+
+# Modules
+# ------------------------------------------------
+import os
+import threading
+import time
 from __future__ import absolute_import, division, print_function
 import logging
-import os
-import time
-from glob import glob
-from os.path import abspath, isdir, dirname, basename, join
-from .compat import range
 from .exceptions import LockError
+
+try:
+    import warnings
+except ImportError:
+    warnings = None
+
+try:
+    import msvcrt
+except ImportError:
+    msvcrt = None
+
+try:
+    import fcntl
+except ImportError:
+    fcntl = None
+
+
+# Data
+# ------------------------------------------------
+__all__ = [
+    "Timeout",
+    "BaseFileLock",
+    "WindowsFileLock",
+    "UnixFileLock",
+    "SoftFileLock",
+    "FileLock"
+]
+
+__version__ = "2.0.6"
+
 
 LOCK_EXTENSION = 'conda_lock'
 
@@ -38,70 +89,315 @@ You can also use: $ conda clean --lock
 stdoutlog = logging.getLogger('stdoutlog')
 log = logging.getLogger(__name__)
 
-def touch(file_name, times=None):
-    """ Touch function like touch in Unix shell
-    :param file_name: the name of file
-    :param times: the access and modified time
-    Examples:
-        touch("hello_world.py")
+# Classes
+# ------------------------------------------------
+class BaseFileLock(object):
     """
-    with open(file_name, 'a'):
-        os.utime(file_name, times)
-
-
-class FileLock(object):
-    """Lock a path (file or directory) with the lock file sitting *beside* path.
-
-    :param path_to_lock: the path to be locked
-    :param retries: max number of retries
+    Implements the base class of a file lock.
     """
-    def __init__(self, path_to_lock, retries=10):
+
+    def __init__(self, lock_file, timeout = 100):
         """
         """
-        self.path_to_lock = abspath(path_to_lock)
-        self.retries = retries
-        self.lock_file_path = "%s.pid{0}.%s" % (self.path_to_lock, LOCK_EXTENSION)
-        # e.g. if locking path `/conda`, lock file will be `/conda.pidXXXX.conda_lock`
-        self.lock_file_glob_str = "%s.pid*.%s" % (self.path_to_lock, LOCK_EXTENSION)
-        assert isdir(dirname(self.path_to_lock)), "{0} doesn't exist".format(self.path_to_lock)
-        assert "::" not in self.path_to_lock, self.path_to_lock
+        # The path to the lock file.
+        self._lock_file = lock_file
+
+        # The file descriptor for the *_lock_file* as it is returned by the
+        # os.open() function.
+        # This file lock is only NOT None, if the object currently holds the
+        # lock.
+        self._lock_file_fd = None
+
+        # The default timeout value.
+        self.timeout = timeout
+
+        # We use this lock primarily for the lock counter.
+        self._thread_lock = threading.Lock()
+
+        # The lock counter is used for implementing the nested locking
+        # mechanism. Whenever the lock is acquired, the counter is increased and
+        # the lock is only released, when this value is 0 again.
+        self._lock_counter = 0
+        return None
+
+    @property
+    def lock_file(self):
+        """
+        The path to the lock file.
+        """
+        return self._lock_file
+
+    @property
+    def timeout(self):
+        """
+        You can set a default timeout for the filelock. It will be used as
+        fallback value in the acquire method, if no timeout value (*None*) is
+        given.
+
+        If you want to disable the timeout, set it to a negative value.
+
+        A timeout of 0 means, that there is exactly one attempt to acquire the
+        file lock.
+
+        .. versionadded:: 2.0.0
+        """
+        return self._timeout
+
+    @timeout.setter
+    def timeout(self, value):
+        """
+        """
+        self._timeout = float(value)
+        return None
+
+    # Platform dependent locking
+    # --------------------------------------------
+
+    def _acquire(self):
+        """
+        Platform dependent. If the file lock could be
+        acquired, self._lock_file_fd holds the file descriptor
+        of the lock file.
+        """
+        raise NotImplementedError()
+
+    def _release(self):
+        """
+        Releases the lock and sets self._lock_file_fd to None.
+        """
+        raise NotImplementedError()
+
+    # Platform independent methods
+    # --------------------------------------------
+
+    @property
+    def is_locked(self):
+        """
+        True, if the object holds the file lock.
+
+        .. versionchanged:: 2.0.0
+
+            This was previously a method and is now a property.
+        """
+        return self._lock_file_fd is not None
+
+    def acquire(self, timeout=None, poll_intervall=1):
+        """
+        Acquires the file lock or fails with a :exc:`Timeout` error.
+
+        .. code-block:: python
+
+            # You can use this method in the context manager (recommended)
+            with lock.acquire():
+                pass
+
+            # Or you use an equal try-finally construct:
+            lock.acquire()
+            try:
+                pass
+            finally:
+                lock.release()
+
+        :arg float timeout:
+            The maximum time waited for the file lock.
+            If ``timeout <= 0``, there is no timeout and this method will
+            block until the lock could be acquired.
+            If ``timeout`` is None, the default :attr:`~timeout` is used.
+
+        :arg float poll_intervall:
+            We check once in *poll_intervall* seconds if we can acquire the
+            file lock.
+
+        :raises Timeout:
+            if the lock could not be acquired in *timeout* seconds.
+
+        .. versionchanged:: 2.0.0
+
+            This method returns now a *proxy* object instead of *self*,
+            so that it can be used in a with statement without side effects.
+        """
+        # Use the default timeout, if no timeout is provided.
+        if timeout is None:
+            timeout = self.timeout
+
+        # Increment the number right at the beginning.
+        # We can still undo it, if something fails.
+        with self._thread_lock:
+            self._lock_counter += 1
+
+        try:
+            start_time = time.time()
+            while True:
+                with self._thread_lock:
+                    if not self.is_locked:
+                        self._acquire()
+
+                if self.is_locked:
+                    break
+                elif timeout >= 0 and time.time() - start_time > timeout:
+                    raise LockError(LOCKSTR.format(self._lock_file))
+                else:
+                    log.debug("Sleeping for %s seconds\n" % poll_intervall)
+                    time.sleep(poll_intervall)
+                    poll_intervall *= 2
+        except:
+            # Something did go wrong, so decrement the counter.
+            with self._thread_lock:
+                self._lock_counter = max(0, self._lock_counter - 1)
+
+            raise
+
+        # This class wraps the lock to make sure __enter__ is not called
+        # twiced when entering the with statement.
+        # If we would simply return *self*, the lock would be acquired again
+        # in the *__enter__* method of the BaseFileLock, but not released again
+        # automatically.
+        class ReturnProxy(object):
+
+            def __init__(self, lock):
+                self.lock = lock
+                return None
+
+            def __enter__(self):
+                return self.lock
+
+            def __exit__(self, exc_type, exc_value, traceback):
+                self.lock.release()
+                return None
+
+        return ReturnProxy(lock = self)
+
+    def release(self, force = False):
+        """
+        Releases the file lock.
+
+        Please note, that the lock is only completly released, if the lock
+        counter is 0.
+
+        Also note, that the lock file itself is not automatically deleted.
+
+        :arg bool force:
+            If true, the lock counter is ignored and the lock is released in
+            every case.
+        """
+        with self._thread_lock:
+
+            if self.is_locked:
+                self._lock_counter -= 1
+
+                if self._lock_counter == 0 or force:
+                    self._release()
+                    self._lock_counter = 0
+        return None
 
     def __enter__(self):
-        sleep_time = 1
-        self.lock_file_path = self.lock_file_path.format(os.getpid())
-        last_glob_match = None
-
-        for _ in range(self.retries + 1):
-
-            # search, whether there is process already locked on this file
-            glob_result = glob(self.lock_file_glob_str)
-            if glob_result:
-                log.debug(LOCKSTR.format(glob_result))
-                log.debug("Sleeping for %s seconds\n" % sleep_time)
-
-                time.sleep(sleep_time / 10)
-                sleep_time *= 2
-                last_glob_match = glob_result
-            else:
-                touch(self.lock_file_path)
-                return self
-
-        stdoutlog.error("Exceeded max retries, giving up")
-        raise LockError(LOCKSTR.format(last_glob_match))
+        self.acquire()
+        return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        from .install import rm_rf
-        rm_rf(self.lock_file_path)
+        self.release()
+        return None
+
+    def __del__(self):
+        self.release(force = True)
+        return None
 
 
-class DirectoryLock(FileLock):
-    """Lock a directory with the lock file sitting *within* the directory being locked.
+# Windows locking mechanism
+# ~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    Useful when, for example, locking the root prefix at ``/conda``, and ``/`` is not writable.
-
-    :param directory_path: the path to be locked
-    :param retries: max number of retries
+class WindowsFileLock(BaseFileLock):
     """
+    Uses the :func:`msvcrt.locking` function to hard lock the lock file on
+    windows systems.
+    """
+
+    def _acquire(self):
+        open_mode = os.O_RDWR | os.O_CREAT | os.O_TRUNC
+
+        try:
+            fd = os.open(self._lock_file, open_mode)
+        except OSError:
+            pass
+        else:
+            try:
+                msvcrt.locking(fd, msvcrt.LK_NBLCK, 1)
+            except (IOError, OSError):
+                os.close(fd)
+            else:
+                self._lock_file_fd = fd
+        return None
+
+    def _release(self):
+        fd = self._lock_file_fd
+        self._lock_file_fd = None
+        msvcrt.locking(fd, msvcrt.LK_UNLCK, 1)
+        os.close(fd)
+
+        try:
+            os.remove(self._lock_file)
+        # Probably another instance of the application
+        # that acquired the file lock.
+        except OSError:
+            pass
+        return None
+
+# Unix locking mechanism
+# ~~~~~~~~~~~~~~~~~~~~~~
+
+class UnixFileLock(BaseFileLock):
+    """
+    Uses the :func:`fcntl.flock` to hard lock the lock file on unix systems.
+    """
+
+    def _acquire(self):
+        open_mode = os.O_RDWR | os.O_CREAT | os.O_TRUNC
+        fd = os.open(self._lock_file, open_mode)
+
+        try:
+            fcntl.flock(fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        except (IOError, OSError):
+            os.close(fd)
+        else:
+            self._lock_file_fd = fd
+        return None
+
+    def _release(self):
+        fd = self._lock_file_fd
+        self._lock_file_fd = None
+        fcntl.flock(fd, fcntl.LOCK_UN)
+        os.close(fd)
+        return None
+
+# Soft lock
+# ~~~~~~~~~
+
+class SoftFileLock(BaseFileLock):
+    """
+    Simply watches the existence of the lock file.
+    """
+
+    def _acquire(self):
+        open_mode = os.O_WRONLY | os.O_CREAT | os.O_EXCL | os.O_TRUNC
+        try:
+            fd = os.open(self._lock_file, open_mode)
+        except (IOError, OSError):
+            pass
+        else:
+            self._lock_file_fd = fd
+        return None
+
+    def _release(self):
+        os.close(self._lock_file_fd)
+        self._lock_file_fd = None
+
+        try:
+            os.remove(self._lock_file)
+        # The file is already deleted and that's what we want.
+        except OSError:
+            pass
+        return None
+
 
     def __init__(self, directory_path, retries=10):
         self.directory_path = abspath(directory_path)
@@ -117,5 +413,18 @@ class DirectoryLock(FileLock):
             log.debug("forced to create %s", self.directory_path)
         assert os.access(self.directory_path, os.W_OK), "%s not writable" % self.directory_path
 
+# Platform filelock
+#: Alias for the lock, which should be used for the current platform. On
+#: Windows, this is an alias for :class:`WindowsFileLock`, on Unix for
+#: :class:`UnixFileLock` and otherwise for :class:`SoftFileLock`.
 
-Locked = DirectoryLock
+
+if msvcrt:
+    Locked = WindowsFileLock
+elif fcntl:
+    Locked = UnixFileLock
+else:
+    Locked = SoftFileLock
+
+    if warnings is not None:
+        warnings.warn("only soft file lock is available")
