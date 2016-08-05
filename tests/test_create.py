97ceb5622f8799d9b9e06d4062d3c3557b75a273
Kale Franz <kfranz@continuum.io>
2016-08-05 11:25:10 -0500

    xfail extend

--- tests/test_create.py
+++ tests/test_create.py
@@ -423,7 +423,7 @@ class IntegrationTests(TestCase):
                     assert_package_is_installed(clone_prefix, 'flask-0.10.1')
                     assert_package_is_installed(clone_prefix, 'python')
 
-    @pytest.mark.xfail(datetime.now() < datetime(2016, 8, 1), reason="configs are borked")
+    @pytest.mark.xfail(datetime.now() < datetime(2016, 8, 7), reason="configs are borked")
     @pytest.mark.skipif(on_win, reason="r packages aren't prime-time on windows just yet")
     @pytest.mark.timeout(600)
     def test_clone_offline_multichannel_with_untracked(self):
@@ -531,7 +531,7 @@ class IntegrationTests(TestCase):
                 os.remove(shortcut_file)
 
     @pytest.mark.skipif(not on_win, reason="shortcuts only relevant on Windows")
-    @pytest.mark.xfail(datetime.now() < datetime(2016, 8, 4), reason="deal with this later")
+    @pytest.mark.xfail(datetime.now() < datetime(2016, 8, 7), reason="deal with this later")
     def test_shortcut_absent_when_condarc_set(self):
         from menuinst.win32 import dirs as win_locations
         user_mode = 'user' if exists(join(sys.prefix, u'.nonadmin')) else 'system'
