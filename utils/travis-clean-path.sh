d9980ed6232ffb64e49605f3a5c44d402d3f97ec
Odegard, Ken (Flamingo Technologies) <ken.odegard@bp.com>
2016-08-07 23:25:24 -0500

    made path cleaning dynamic instead of static

---
+++ utils/travis-clean-path.sh
@@ -0,0 +1,18 @@
+export BACKUP_PATH="$PATH"
+
+if [ -n "$PATH" ]; then
+  old_PATH="$PATH:"
+  PATH=""
+  while [ -n "$old_PATH" ]; do
+    x=${old_PATH%%:*}       # the first remaining entry
+    case $PATH: in
+      *:"$x":*) ;;          # already there
+      *) PATH=$PATH:$x;;    # not there yet
+    esac
+    old_PATH=${old_PATH#*:}
+  done
+  PATH=${PATH#:}
+  unset old_PATH x
+fi
+
+export PATH="$PATH"
