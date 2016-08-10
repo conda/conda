1ad76cc67923c133d28dfb67bf7afc47d219d6e3
Kale Franz <kfranz@continuum.io>
2016-08-10 11:24:52 -0500

    conda-build install

--- utils/travis-run-script.sh
+++ utils/travis-run-script.sh
@@ -4,14 +4,20 @@ set -x
 main_test() {
     export PYTHONHASHSEED=$(python -c "import random as r; print(r.randint(0,4294967296))")
     echo $PYTHONHASHSEED
+
+    # basic unit tests
     python -m pytest --cov-report xml --shell=bash --shell=zsh -m "not installed" tests
     python setup.py --version
+
+    # activate tests
     python setup.py install
     hash -r
     python -m conda info
     python -m pytest --cov-report xml --cov-append --shell=bash --shell=zsh -m "installed" tests
-    python -m conda install -y -q conda-build
+
+    # conda-build smoke test
     python -m conda config --set auto_update_conda false
+    python -m conda install -y -q conda-build
     python -m conda build conda.recipe
 }
 
