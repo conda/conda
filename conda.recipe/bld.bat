e27a1ff56f7ab7dd2a32a543c77253a4ed442eb8
Kale Franz <kfranz@continuum.io>
2016-08-10 15:54:09 -0500

    round 11

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -3,6 +3,8 @@ set CONDA_DEFAULT_ENV=
 %PYTHON% setup.py install
 if errorlevel 1 exit 1
 
+%PYTHON% setup.py --version > __conda_version__.txt
+
 mkdir %PREFIX%\etc\fish\conf.d
 if errorlevel 1 exit 1
 
--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -5,6 +5,7 @@ unlink $PREFIX/bin/deactivate
 
 export CONDA_DEFAULT_ENV=''
 $PYTHON setup.py install
+CONDA_DEFAULT_ENV='' $PYTHON setup.py --version > __conda_version__.txt
 
 mkdir -p $PREFIX/etc/fish/conf.d/
 cp $SRC_DIR/shell/conda.fish $PREFIX/etc/fish/conf.d/
