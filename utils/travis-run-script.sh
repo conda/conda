29ade2b64b44d5848f331b99db3063afb7288591
Kale Franz <kfranz@continuum.io>
2016-08-10 11:50:16 -0500

    try pip-install conda-build

--- utils/travis-run-script.sh
+++ utils/travis-run-script.sh
@@ -17,7 +17,8 @@ main_test() {
 
     # conda-build smoke test
     python -m conda config --set auto_update_conda false
-    python -m conda install -y -q conda-build
+    python -m pip install git+https://github.com/conda/conda-build.git
+    # python -m conda install -y -q conda-build
     python -m conda build conda.recipe
 }
 
