45b13aedda2eaa6358c677482408e59983eb8605
tester <tester@test-machine.local>
2016-08-11 14:01:34 -0500

    fix more test

--- tests/conda_env/test_cli.py
+++ tests/conda_env/test_cli.py
@@ -2,8 +2,21 @@ import json
 import os
 import unittest
 import tempfile
-import subprocess
+import sys
+from shlex import split
+
 from conda_env.exceptions import SpecNotFound
+from conda_env.cli.main import create_parser
+
+from conda.base.context import context
+from conda.base.constants import ROOT_ENV_NAME
+from conda.common.io import captured
+from conda.install import rm_rf
+from conda.cli.main_create import configure_parser as conda_create_parser
+from conda.cli.main_list import configure_parser as list_parser
+from conda.cli.main_info import configure_parser as info_parser
+from conda.cli.main import generate_parser
+
 environment_1 = '''
 name: env-1
 dependencies:
@@ -21,18 +34,94 @@ channels:
   - malev
 '''
 
+test_env_name_1 = "env-1"
+test_env_name_2 = "snowflakes"
 
-def run(command):
-    process = subprocess.Popen(
-        command,
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        universal_newlines=True
-    )
-    stdout, stderr = process.communicate()
-    status = process.returncode
-    return (stdout, stderr, status)
+def escape_for_winpath(p):
+    if p:
+        return p.replace('\\', '\\\\')
+
+class Commands:
+    ENV_CREATE = "create"
+    ENV_REMOVE = "remove"
+    ENV_EXPORT = "export"
+    ENV_UPDATE = "update"
+    LIST = "list"
+    CREATE = "create"
+    INFO = "info"
+
+
+def run_env_command(command, prefix, *arguments):
+    """
+        Run conda env commands
+    Args:
+        command: The command, create, remove, export
+        prefix: The prefix, for remove and create
+        *arguments: The extra arguments
+    """
+    p = create_parser()
+    prefix = escape_for_winpath(prefix)
+
+    if arguments:
+        arguments = list(map(escape_for_winpath, arguments))
+
+    if command is Commands.ENV_EXPORT:
+        command_line = "{0} -n {1} {2}".format(command, prefix, " ".join(arguments))
+    elif command is Commands.ENV_CREATE: # CREATE
+        if prefix :
+            command_line = "{0} -f {1}  {2}".format(command, prefix, " ".join(arguments))
+        else:
+            command_line = "{0} ".format(command)
+    elif command is Commands.ENV_REMOVE:  # REMOVE
+        command_line = "{0} --yes -n {1} {2}".format(command, prefix, " ".join(arguments))
+    elif command is Commands.ENV_UPDATE:
+        command_line = "{0} -n {1} {2}".format(command, prefix, " ".join(arguments))
+    else:
+        command_line = " --help "
+
+    args = p.parse_args(split(command_line))
+    context._add_argparse_args(args)
+
+    with captured() as c:
+        args.func(args, p)
+
+    return c.stdout, c.stderr
+
+parser_config = {
+    Commands.CREATE: conda_create_parser,
+    Commands.LIST: list_parser,
+    Commands.INFO: info_parser,
+}
+
+
+def run_conda_command(command, prefix, *arguments):
+    """
+        Run conda command,
+    Args:
+        command: conda create , list, info
+        prefix: The prefix or the name of environment
+        *arguments: Extra arguments
+    """
+    p, sub_parsers = generate_parser()
+    assert command in parser_config, "Wrong command for conda {0}".format(command)
+    parser_config[command](sub_parsers)
+
+    prefix = escape_for_winpath(prefix)
+    if arguments:
+        arguments = list(map(escape_for_winpath, arguments))
+    if command is Commands.INFO:    # INFO
+        command_line = "{0} {1}".format(command, " ".join(arguments))
+    elif command is Commands.LIST:  # LIST
+        command_line = "{0} -n {1} {2}".format(command, prefix, " ".join(arguments))
+    else:  # CREATE
+        command_line = "{0} -y -q -n {1} {2}".format(command, prefix, " ".join(arguments))
+
+    args = p.parse_args(split(command_line))
+    context._add_argparse_args(args)
+    with captured() as c:
+        args.func(args, p)
+
+    return c.stdout, c.stderr
 
 
 def create_env(content, filename='environment.yml'):
@@ -45,54 +134,63 @@ def remove_env_file(filename='environment.yml'):
 
 
 class IntegrationTest(unittest.TestCase):
+
+    def setUp(self):
+        rm_rf("environment.yml")
+        if env_is_created(test_env_name_1):
+            run_env_command(Commands.ENV_REMOVE, test_env_name_1)
+
     def tearDown(self):
-        run('conda env remove -n env-1 -y')
-        run('conda env remove -n env-2 -y')
-        run('rm environment.yml')
+        rm_rf("environment.yml")
+        if env_is_created(test_env_name_1):
+            run_env_command(Commands.ENV_REMOVE, test_env_name_1)
 
     def test_conda_env_create_no_file(self):
         '''
         Test `conda env create` without an environment.yml file
         Should fail
         '''
-        o, e, s = run('conda env create')
-        self.assertRaises(SpecNotFound)
+        try:
+            run_env_command(Commands.ENV_CREATE, None)
+        except Exception as e:
+            self.assertIsInstance(e, SpecNotFound)
+
 
     def test_create_valid_env(self):
         '''
         Creates an environment.yml file and
         creates and environment with it
         '''
+
         create_env(environment_1)
+        run_env_command(Commands.ENV_CREATE, None)
+        self.assertTrue(env_is_created(test_env_name_1))
 
-        o, e, s = run('conda env create')
-        self.assertTrue(env_is_created("env-1"))
-
-        o, e, s = run('conda info --json')
+        o, e = run_conda_command(Commands.INFO, None, "--json")
         parsed = json.loads(o)
         self.assertNotEqual(
-            len([env for env in parsed['envs'] if env.endswith('env-1')]),
+            len([env for env in parsed['envs'] if env.endswith(test_env_name_1)]),
             0
         )
 
-        o, e, s = run('conda env remove -y -n env-1')
-        self.assertFalse(env_is_created("env-1"))
-
     def test_update(self):
         create_env(environment_1)
-        o, e, s = run('conda env create')
+        run_env_command(Commands.ENV_CREATE, None)
         create_env(environment_2)
-        o, e, s = run('conda env update -n env-1')
-        o, e, s = run('conda list flask -n env-1 --json')
+
+        run_env_command(Commands.ENV_UPDATE, test_env_name_1)
+
+        o, e = run_conda_command(Commands.LIST, test_env_name_1, "flask", "--json")
         parsed = json.loads(o)
         self.assertNotEqual(len(parsed), 0)
 
     def test_name(self):
         # smoke test for gh-254
         create_env(environment_1)
-        o, e, s = run('conda env create -n new-env create')
-
-        self.assertRaises(SpecNotFound)
+        try:
+            run_env_command(Commands.ENV_CREATE, test_env_name_1, "create")
+        except Exception as e:
+            self.assertIsInstance(e, SpecNotFound, str(e))
 
 
 def env_is_created(env_name):
@@ -103,14 +201,15 @@ def env_is_created(env_name):
     Returns: True if created
              False otherwise
     """
-    stdout, stderr, status = run("conda env list")
-    stdout_lines = stdout.split('\n')
-    for line in stdout_lines:
-        line = line.strip()
-        if line is None or line.startswith("#"):
-            continue
-        if env_name in line:
+    from conda import misc
+    from os.path import  basename
+
+    for prefix in misc.list_prefixes():
+        name = (ROOT_ENV_NAME if prefix == context.root_dir else
+                basename(prefix))
+        if name == env_name:
             return True
+
     return False
 
 
@@ -122,71 +221,59 @@ class NewIntegrationTest(unittest.TestCase):
         http://conda.pydata.org/docs/using/envs.html#export-the-environment-file
     """
 
-    def test_conda_env_help(self):
-        """
-            Test functionality of conda env --help
-        """
-        o, _, _ = run("conda env --help")
-        message = [ "create", "export", "list", "remove", "upload", "update"]
-        for m in message:
-            self.assertIn(m, o, o)
+    def setUp(self):
+        if env_is_created(test_env_name_2):
+            run_conda_command(Commands.ENV_REMOVE, test_env_name_2)
+            self.assertFalse(env_is_created(test_env_name_2))
+
+    def tearDown(self):
+        if env_is_created(test_env_name_2):
+            run_env_command(Commands.ENV_REMOVE, test_env_name_2)
+            self.assertFalse(env_is_created(test_env_name_2))
 
     def test_create_env(self):
         """
             Test conda create env and conda env remove env
         """
-        if env_is_created("snowflakes"):
-            run("conda env remove --yes --name snowflakes")
-            self.assertFalse(env_is_created("snowflakes"))
-
-        run("conda create --yes --name snowflakes")
-        self.assertTrue(env_is_created("snowflakes"))
-
-
-        run("conda env remove --yes --name snowflakes")
-        self.assertFalse(env_is_created("snowflakes"))
+        run_conda_command(Commands.CREATE, test_env_name_2)
+        self.assertTrue(env_is_created(test_env_name_2))
 
     def test_export(self):
         """
             Test conda env
         """
-        if not env_is_created("snowflakes"):
-            run("conda create --yes --name snowflakes python")
-            self.assertTrue(env_is_created("snowflakes"))
 
-        snowflake, e, s = run("conda env export -n snowflakes")
+        run_conda_command(Commands.CREATE, test_env_name_2, "flask")
+        self.assertTrue(env_is_created(test_env_name_2))
+
+        snowflake, e,  = run_env_command(Commands.ENV_EXPORT, test_env_name_2)
 
         with tempfile.NamedTemporaryFile(mode="w", suffix="yml") as env_yaml:
             env_yaml.write(snowflake)
             env_yaml.flush()
-            run("conda env remove --yes --name snowflakes")
-            self.assertFalse(env_is_created("snowflakes"))
-            o, e, s = run("conda env create -f " + env_yaml.name)
-            self.assertTrue(env_is_created("snowflakes"))
-
-        o, e, s = run("conda env remove --yes --name snowflakes")
-        self.assertFalse(env_is_created("snowflakes"))
+            run_env_command(Commands.ENV_REMOVE, test_env_name_2)
+            self.assertFalse(env_is_created(test_env_name_2))
+            run_env_command(Commands.ENV_CREATE, env_yaml.name)
+            self.assertTrue(env_is_created(test_env_name_2))
 
     def test_list(self):
         """
             Test conda env
         """
-        if not env_is_created("snowflakes"):
-            run("conda create --yes --name snowflakes")
-            self.assertTrue(env_is_created("snowflakes"))
 
-        snowflake, e, s = run("conda list -n snowflakes -e")
+        run_conda_command(Commands.CREATE, test_env_name_2)
+        self.assertTrue(env_is_created(test_env_name_2))
+
+        snowflake, e = run_conda_command(Commands.LIST, test_env_name_2, "-e")
 
         with tempfile.NamedTemporaryFile(mode="w", suffix="txt") as env_txt:
             env_txt.write(snowflake)
             env_txt.flush()
-            run("conda env remove --yes --name snowflakes")
-            self.assertFalse(env_is_created("snowflakes"))
-            o, e, s = run("conda create -n snowflakes --file " + env_txt.name)
-            self.assertTrue(env_is_created("snowflakes"))
+            run_env_command(Commands.ENV_REMOVE, test_env_name_2)
+            self.assertFalse(env_is_created(test_env_name_2))
+            run_conda_command(Commands.CREATE, test_env_name_2, "--file " + env_txt.name)
+            self.assertTrue(env_is_created(test_env_name_2))
 
-        o, e, s = run("conda env remove --yes --name snowflakes")
-        self.assertFalse(env_is_created("snowflakes"))
 
 if __name__ == '__main__':
     unittest.main()
