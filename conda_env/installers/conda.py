f7261105b4f20b79335f957e00e83d9b17906260
Zhang Tian <zhangtian1000@gmail.com>
2016-08-11 22:40:02 -0500

    tmp fix, still have bugs

--- conda_env/installers/conda.py
+++ conda_env/installers/conda.py
@@ -8,9 +8,19 @@ def install(prefix, specs, args, env, prune=False):
     # TODO: do we need this?
     check_specs(prefix, specs, json=args.json)
 
+    new_specs = []
+    channel_urls = set()
+    for elem in specs:
+        if "::" in elem:
+            channel_urls.add(elem.split("::")[0])
+            new_specs.append(elem.split("::")[-1])
+        else:
+            new_specs.append(elem)
+    specs = new_specs
+    channel_urls = list(channel_urls)
     # TODO: support all various ways this happens
     # Including 'nodefaults' in the channels list disables the defaults
-    index = get_index_trap(channel_urls=[chan for chan in env.channels
+    index = get_index_trap(channel_urls=channel_urls + [chan for chan in env.channels
                                                      if chan != 'nodefaults'],
                                   prepend='nodefaults' not in env.channels)
     actions = plan.install_actions(prefix, index, specs, prune=prune)
