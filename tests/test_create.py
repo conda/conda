9ce53dbf683627fc51aa93ea7e7b1c0780549918
Zhang Tian <zhangtian1000@gmail.com>
2016-08-17 23:55:57 -0500

    change to .condarc

--- tests/test_create.py
+++ tests/test_create.py
@@ -82,7 +82,7 @@ def run_command(command, prefix, *arguments):
     prefix = escape_for_winpath(prefix)
     arguments = list(map(escape_for_winpath, arguments))
     if command is Commands.CONFIG:
-        command_line = "{0} --file {1} {2}".format(command, join(prefix, '', 'condarc'), " ".join(arguments))
+        command_line = "{0} --file {1} {2}".format(command, join(prefix, '', '.condarc'), " ".join(arguments))
     elif command is Commands.SEARCH:
         command_line = "{0} {1}".format(command, " ".join(arguments))
     elif command is Commands.LIST:
@@ -109,7 +109,7 @@ def make_temp_env(*packages, **kwargs):
         with disable_logger('fetch'), disable_logger('dotupdate'):
             try:
                 # try to clear any config that's been set by other tests
-                reset_context([join(prefix, '', 'condarc')])
+                reset_context([join(prefix, '', '.condarc')])
                 run_command(Commands.CREATE, prefix, *packages)
                 yield prefix
             finally:
@@ -117,7 +117,7 @@ def make_temp_env(*packages, **kwargs):
 
 
 def reload_config(prefix):
-    prefix_condarc = join(prefix, '', 'condarc')
+    prefix_condarc = join(prefix, '', '.condarc')
     reset_context([prefix_condarc])
 
 
