b00cd592016caff49e6725f7fda9e34978112df1
Kale Franz <kfranz@continuum.io>
2016-09-02 13:13:40 -0500

    add argparse default

--- conda/cli/main.py
+++ conda/cli/main.py
@@ -40,12 +40,13 @@ from __future__ import absolute_import, division, print_function
 import importlib
 import sys
 from argparse import SUPPRESS
-from conda.gateways.logging import set_all_logger_level, set_verbosity
 from logging import CRITICAL, DEBUG, getLogger
 
 from .. import __version__
+from ..base.constants import NULL
 from ..base.context import context
 from ..exceptions import CommandNotFoundError, conda_exception_handler
+from ..gateways.logging import set_all_logger_level, set_verbosity
 from ..utils import on_win
 
 log = getLogger(__name__)
@@ -55,7 +56,8 @@ def generate_parser():
     from ..cli import conda_argparse
     p = conda_argparse.ArgumentParser(
         description='conda is a tool for managing and deploying applications,'
-                    ' environments and packages.'
+                    ' environments and packages.',
+        argument_default=NULL,
     )
     p.add_argument(
         '-V', '--version',
