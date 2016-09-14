17dd1efb58ab075aa6a4d35de220dc1415728c5c
Kale Franz <kfranz@continuum.io>
2016-09-14 15:52:21 -0500

    remove extraneous code

--- conda/compat.py
+++ conda/compat.py
@@ -115,52 +115,6 @@ else:
     # used elsewhere - do not remove
     from itertools import izip as zip
 
-    class TemporaryDirectory(object):
-        """Create and return a temporary directory.  This has the same
-        behavior as mkdtemp but can be used as a context manager.  For
-        example:
-
-            with TemporaryDirectory() as tmpdir:
-                ...
-
-        Upon exiting the context, the directory and everything contained
-        in it are removed.
-        """
-
-        # Handle mkdtemp raising an exception
-        name = None
-        _closed = False
-
-        def __init__(self, suffix="", prefix='tmp', dir=None):
-            self.name = mkdtemp(suffix, prefix, dir)
-
-        def __repr__(self):
-            return "<{} {!r}>".format(self.__class__.__name__, self.name)
-
-        def __enter__(self):
-            return self.name
-
-        def cleanup(self, _warn=False, _warnings=_warnings):
-            from .install import rm_rf as _rm_rf
-            if self.name and not self._closed:
-                try:
-                    _rm_rf(self.name)
-                except (TypeError, AttributeError) as ex:
-                    if "None" not in '%s' % (ex,):
-                        raise
-                    _rm_rf(self.name)
-                self._closed = True
-                if _warn and _warnings.warn:
-                    _warnings.warn("Implicitly cleaning up {!r}".format(self),
-                                   _warnings.ResourceWarning)
-
-        def __exit__(self, exc, value, tb):
-            self.cleanup()
-
-        def __del__(self):
-            # Issue a ResourceWarning if implicit cleanup needed
-            self.cleanup(_warn=True)
-
 
 if PY3:
     _iterkeys = "keys"
