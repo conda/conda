31459f21cd77e6f8c1f7aa843ccb54683a0486ec
Kale Franz <kfranz@continuum.io>
2016-09-15 09:57:21 -0500

    more tweaks to Record

--- conda/models/record.py
+++ conda/models/record.py
@@ -65,5 +65,5 @@ class Record(DictSafeMixin, Entity):
     url = StringField(required=False, nullable=True)
     auth = StringField(required=False, nullable=True)
 
-    files = ListField(string_types, required=False)
+    files = ListField(string_types, default=(), required=False)
     link = ComposableField(Link, required=False)
