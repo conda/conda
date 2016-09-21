fa8c7aa13c06782330ef6fbf10263fa0fb80451f
Kale Franz <kfranz@continuum.io>
2016-09-21 12:51:57 -0500

    flip test order

--- tests/test_create.py
+++ tests/test_create.py
@@ -653,16 +653,16 @@ class IntegrationTests(TestCase):
     def test_create_dry_run(self):
         # Regression test for #3453
         prefix = '/some/place'
-        with pytest.raises(DryRunExit):
-            run_command(Commands.CREATE, prefix, "--dry-run")
         stdout, stderr = run_command(Commands.CREATE, prefix, "--dry-run", use_exception_handler=True)
         assert "The following empty environments will be CREATED" in stdout
         assert prefix in stdout
+        with pytest.raises(DryRunExit):
+            run_command(Commands.CREATE, prefix, "--dry-run")
 
         prefix = '/another/place'
-        with pytest.raises(DryRunExit):
-            run_command(Commands.CREATE, prefix, "flask", "--dry-run")
         stdout, stderr = run_command(Commands.CREATE, prefix, "flask", "--dry-run", use_exception_handler=True)
         assert "flask:" in stdout
         assert "python:" in stdout
         assert prefix in stdout
+        with pytest.raises(DryRunExit):
+            run_command(Commands.CREATE, prefix, "flask", "--dry-run")
