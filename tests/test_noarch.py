38b5c9e1c86ecd71283b149a4746531b233627f5
sophia <scastellarin95@gmail.com>
2016-09-22 10:18:50 -0500

    Skeleton for tests

---
+++ tests/test_noarch.py
@@ -0,0 +1,37 @@
+import pytest
+import unittest
+
+from conda import noarch
+
+
+class TestHelpers(unittest.TestCase):
+
+    def test_get_python_version(self):
+        pass
+
+    def test_get_site_packages_dir(self):
+        pass
+
+    def test_get_bin_dir(self):
+        pass
+
+    def test_link_files(self):
+        pass
+
+    def test_compile_missing_pyc(self):
+        pass
+
+    def test_create_entry_points(self):
+        pass
+
+
+class TestNoArch(unittest.TestCase):
+
+    def test_link(self):
+        pass
+
+
+class TestNoArchPython(unittest.TestCase):
+
+    def test_link(self):
+        pass
