0229552d28d8a1555ea9284fef022550eefaf39a
sophia <scastellarin95@gmail.com>
2016-09-23 11:48:31 -0500

    Test getting site-packages dir

--- tests/test_noarch.py
+++ tests/test_noarch.py
@@ -1,7 +1,20 @@
 import pytest
 import unittest
+from unittest.mock import patch
+import sys
 
 from conda import noarch
+
+
+def stub_sys_platform(platform):
+    def sys_platform(func):
+        def func_wrapper(plat):
+            sys_plat = sys.platform
+            sys.platform = platform
+            func(plat)
+            sys.platform = sys_plat
+        return func_wrapper
+    return sys_platform
 
 
 class TestHelpers(unittest.TestCase):
@@ -12,13 +25,18 @@ class TestHelpers(unittest.TestCase):
         self.assertEquals(noarch.get_noarch_cls(True), noarch.NoArch)
         self.assertEquals(noarch.get_noarch_cls(None), None)
 
-    def test_get_python_version_for_prefix(self):
-        pass
+    @stub_sys_platform("win32")
+    def test_get_site_packages_dir_win(self):
+        site_packages_dir = noarch.get_site_packages_dir("")
+        self.assertEquals(site_packages_dir, "Lib")
 
-    def test_get_site_packages_dir(self):
-        pass
+    @stub_sys_platform("darwin")
+    def test_get_site_packages_dir_unix(self):
+        with patch.object(noarch, "get_python_version_for_prefix", return_value="3.5") as m:
+            site_packages_dir = noarch.get_site_packages_dir("")
+            self.assertEquals(site_packages_dir, 'lib/python3.5')
 
-    def test_get_bin_dir(self):
+    def test_get_bin_dir_win(self):
         pass
 
     def test_link_files(self):
