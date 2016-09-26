659ee38bcf4c84a6c2697584075a2a4f0bfa72b0
Kale Franz <kfranz@continuum.io>
2016-09-26 15:37:27 -0500

    fix #3467 AttributeError: 'str' object has no attribute 'items'

--- conda/common/configuration.py
+++ conda/common/configuration.py
@@ -637,9 +637,11 @@ class MapParameter(Parameter):
 
     def collect_errors(self, instance, value, source="<<merged>>"):
         errors = super(MapParameter, self).collect_errors(instance, value)
-        element_type = self._element_type
-        errors.extend(InvalidElementTypeError(self.name, val, source, type(val), element_type, key)
-                      for key, val in iteritems(value) if not isinstance(val, element_type))
+        if isinstance(value, Mapping):
+            element_type = self._element_type
+            errors.extend(InvalidElementTypeError(self.name, val, source, type(val),
+                                                  element_type, key)
+                          for key, val in iteritems(value) if not isinstance(val, element_type))
         return errors
 
     def _merge(self, matches):
