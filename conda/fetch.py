0499859637af67cd535add1b56290e42e08c6f8e
Michele Mastropietro <michele.mastropietro@gmail.com>
2016-09-27 16:59:03 +0200

    add auth also to non parallel call to fetch_repodata

--- conda/fetch.py
+++ conda/fetch.py
@@ -285,15 +285,14 @@ def fetch_index(channel_urls, use_cache=False, unknown=False, index=None, channe
     else:
         try:
             futures = tuple(executor.submit(fetch_repodata, url, use_cache=use_cache,
-                                            session=CondaSession(), auth=channel_auths[url])
-                                            for url in urls)
+                            session=CondaSession(), auth=channel_auths[url]) for url in urls)
             repodatas = [(u, f.result()) for u, f in zip(urls, futures)]
         except RuntimeError as e:
             # Cannot start new thread, then give up parallel execution
             log.debug(repr(e))
             session = CondaSession()
-            repodatas = [(url, fetch_repodata(url, use_cache=use_cache, session=session))
-                         for url in urls]
+            repodatas = [(url, fetch_repodata(url, use_cache=use_cache, session=session,
+                         auth=channel_auths[url])) for url in urls]
         finally:
             executor.shutdown(wait=True)
 
