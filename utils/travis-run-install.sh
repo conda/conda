fa3decb7f33d84789079d05a3bf577721b4fbbed
Odegard, Ken <ken.odegard@gmail.com>
2016-09-27 13:56:28 -0500

    added comments to travis testing
    working to better understand the test logic
    
    added additional echo statements such that it will be easier to follow
    the progress of the tests

--- utils/travis-run-install.sh
+++ utils/travis-run-install.sh
@@ -2,8 +2,10 @@ set -e
 set -x
 
 osx_setup() {
-    brew update || brew update
+    echo "OSX SETUP ($(pwd))"
 
+    # update Homebrew basics
+    brew update || brew update
     brew outdated openssl || brew upgrade openssl
 
     # test for shells before trying to install them
@@ -18,10 +20,12 @@ osx_setup() {
 
     # install pyenv
     git clone https://github.com/yyuu/pyenv.git ~/.pyenv
-    PYENV_ROOT="$HOME/.pyenv"
+    PYENV_ROOT="~/.pyenv"
     PATH="$PYENV_ROOT/bin:$PATH"
+    export PATH
     eval "$(pyenv init -)"
 
+    # install the specified python version
     case "$PYTHON_VERSION" in
         '2.7')
             curl -O https://bootstrap.pypa.io/get-pip.py
@@ -37,53 +41,94 @@ osx_setup() {
             ;;
     esac
     pyenv rehash
+
     export PYTHON_EXE="$(pyenv which python)"
+
+    echo "END OSX SETUP ($(pwd))"
 }
 
+linux_setup() {
+    echo "LINUX SETUP ($(pwd))"
 
-main_install() {
+    export PYTHON_EXE="$(which python)"
+
+    echo "END LINUX SETUP ($(pwd))"
+}
+
+python_install() {
+    echo "PYTHON INSTALL ($(pwd))"
+
     case "$(uname -s)" in
-        'Darwin') osx_setup ;;
-        'Linux') export PYTHON_EXE="$(which python)" ;;
-        *) ;;
+        'Darwin')
+            osx_setup
+            ;;
+        'Linux')
+            linux_setup
+            ;;
+        *)  ;;
     esac
 
-    python -m pip install psutil ruamel.yaml pycosat pycrypto
+    # install/upgrade basic dependencies
+    python -m pip install -U psutil ruamel.yaml pycosat pycrypto
     case "${TRAVIS_PYTHON_VERSION:-PYTHON_VERSION}" in
-      '2.7')
-          python -m pip install -U enum34 futures
-          ;;
-      *) ;;
+        '2.7')
+            python -m pip install -U enum34 futures;;
+        *)  ;;
     esac
+
+    echo "END PYTHON INSTALL ($(pwd))"
 }
 
 
 flake8_extras() {
+    echo "FLAKE8 EXTRAS ($(pwd))"
+
+    # install/update flake8 dependencies
     python -m pip install -U flake8
+
+    echo "END FLAKE8 EXTRAS ($(pwd))"
 }
 
 
 test_extras() {
+    echo "TEST EXTRAS ($(pwd))"
+
+    # install/upgrade unittest dependencies
     python -m pip install -U mock pytest pytest-cov pytest-timeout radon \
                              responses anaconda-client nbformat
+
+    echo "END TEST EXTRAS ($(pwd))"
 }
 
 
 miniconda_install() {
+    echo "MINICONDA INSTALL ($(pwd))"
+
+    # get, install, and verify miniconda
     curl http://repo.continuum.io/miniconda/Miniconda3-4.0.5-Linux-x86_64.sh -o ~/miniconda.sh
     bash ~/miniconda.sh -bfp ~/miniconda
     export PATH=~/miniconda/bin:$PATH
     hash -r
     which -a conda
     conda info
+
+    # install and verify pip
     conda install -y -q pip
     which -a pip
+
+    # verify python
     which -a python
+
+    # disable automatic updates
     conda config --set auto_update_conda false
+
+    echo "END MINICONDA INSTALL ($(pwd))"
 }
 
 
-conda_build_install() {
+conda_build_extras() {
+    echo "CONDA BUILD INSTALL ($(pwd))"
+
     # install conda
     python setup.py install
     conda info
@@ -106,16 +151,18 @@ conda_build_install() {
     popd
 
     git clone https://github.com/conda/conda_build_test_recipe.git
+
+    echo "END CONDA BUILD INSTALL ($(pwd))"
 }
 
 
 if [[ $FLAKE8 == true ]]; then
-    main_install
+    python_install
     flake8_extras
 elif [[ -n $CONDA_BUILD ]]; then
     miniconda_install
-    conda_build_install
+    conda_build_extras
 else
-    main_install
+    python_install
     test_extras
 fi
--- utils/travis-run-script.sh
+++ utils/travis-run-script.sh
@@ -2,49 +2,65 @@ set -e
 set +x
 
 main_test() {
+    echo "MAIN TEST ($(pwd))"
+
     export PYTHONHASHSEED=$(python -c "import random as r; print(r.randint(0,4294967296))")
     echo $PYTHONHASHSEED
 
-    case "$(uname -s)" in
-        'Darwin') shells="";;
-        'Linux') shells="--shell=posh";;
-        *) ;;
-    esac
+    # detect what shells are available to test with
     # don't bother testing for the default shell `sh`, generally speaking the default
     # shell will be supported if it's one of the supported shells
-    shells="$shells --shell=bash --shell=zsh --shell=dash --shell=csh --shell=tcsh"
+    shells=""
+    [[ $(which -s bash) ]] || shells="${shells} --shell=bash"
+    [[ $(which -s dash) ]] || shells="${shells} --shell=dash"
+    [[ $(which -s posh) ]] || shells="${shells} --shell=posh"
+    [[ $(which -s zsh) ]]  || shells="${shells} --shell=zsh"
+    [[ $(which -s csh) ]]  || shells="${shells} --shell=csh"
+    [[ $(which -s tcsh) ]] || shells="${shells} --shell=tcsh"
 
     echo "PRE-INSTALL CONDA TESTS"
     python -m pytest --cov-report xml $shells -m "not installed" tests
 
     echo "INSTALL CONDA"
     python setup.py --version
-
-    # activate tests
     python setup.py install
     hash -r
     python -m conda info
 
     echo "POST-INSTALL CONDA TESTS"
     python -m pytest --cov-report xml --cov-append $shells -m "installed" tests
+
+    echo "END MAIN TEST ($(pwd))"
 }
 
 flake8_test() {
+    echo "FLAKE8 TEST ($(pwd))"
+
     python -m flake8 --statistics
+
+    echo "END FLAKE8 TEST ($(pwd))"
 }
 
 conda_build_smoke_test() {
+    echo "CONDA BUILD SMOKE TEST ($(pwd))"
+
     conda config --add channels conda-canary
     conda build conda.recipe
+
+    echo "END CONDA BUILD SMOKE TEST ($(pwd))"
 }
 
 conda_build_unit_test() {
+    echo "CONDA BUILD UNIT TEST ($(pwd))"
+
     pushd conda-build
     echo
     echo ">>>>>>>>>>>> runnin conda-build unit tests >>>>>>>>>>>>>>>>>>>>>"
     echo
     python -m pytest -n 2 --basetemp /tmp/cb tests || echo -e "\n>>>>> conda-build tests exited with code" $? "\n\n\n"
     popd
+
+    echo "END CONDA BUILD UNIT TEST ($(pwd))"
 }
 
 if [[ $FLAKE8 == true ]]; then
@@ -52,8 +68,6 @@ if [[ $FLAKE8 == true ]]; then
 elif [[ -n $CONDA_BUILD ]]; then
     conda_build_smoke_test
     conda_build_unit_test
-    # if [[ $CONDA_BUILD == 1.21.11 || $CONDA_BUILD == master ]]; then
-    # fi
 else
     main_test
 fi
