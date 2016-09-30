73d704408bf38a5ad44c7a8f3f1e601071a9e6db
Odegard, Ken <ken.odegard@gmail.com>
2016-09-30 13:18:08 -0500

    somehow lost posh install via Homebrew on MacOSX, added back

--- utils/travis-run-install.sh
+++ utils/travis-run-install.sh
@@ -45,6 +45,11 @@ osx_setup() {
     else
         brew install ksh
     fi
+    if [[ $(which posh) ]]; then
+        brew outdated posh || brew upgrade posh
+    else
+        brew install posh
+    fi
     if [[ $(which tcsh) ]]; then
         brew outdated tcsh || brew upgrade tcsh
     else
