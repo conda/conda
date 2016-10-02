7f2418b90f9909a51df20ea1a9ee856bb0f15446
Michael C. Grant <mcg@cvxr.com>
2016-10-02 18:00:27 -0500

    Removed unused nspecs

--- conda/resolve.py
+++ conda/resolve.py
@@ -383,7 +383,6 @@ class Resolve(object):
         filter = {}
         touched = {}
         snames = set()
-        nspecs = set()
         unsat = set()
 
         def filter_group(matches, chains=None):
@@ -443,8 +442,6 @@ class Resolve(object):
             # not have a particular dependency, it must be ignored in this pass.
             if first:
                 snames.add(name)
-                if match1 not in specs:
-                    nspecs.add(MatchSpec(name))
             cdeps = defaultdict(list)
             for fkey in group:
                 if filter[fkey]:
