d18002702778517bcd8930c257bac6968a2b5ab2
Kale Franz <kfranz@continuum.io>
2016-10-14 04:41:58 -0500

    add notes for connection.py

--- conda/connection.py
+++ conda/connection.py
@@ -115,6 +115,7 @@ class BinstarAuth(AuthBase):
     def __call__(self, request):
         request.url = BinstarAuth.add_binstar_token(request.url)
         self._apply_basic_auth(request)
+        # request.register_hook('response', self.handle_407)
         return request
     # def __call__(self, request):
     #     # Initialize per-thread state, if needed
@@ -236,7 +237,42 @@ class BinstarAuth(AuthBase):
     #     session.proxies[scheme] = add_username_and_pass_to_url(
     #                             session.proxies[scheme], username, passwd)
 
-
+    # def handle_401(self, r, **kwargs):
+    #     """
+    #     Takes the given response and tries digest-auth, if needed.
+    #
+    #     :rtype: requests.Response
+    #     """
+    #
+    #     if self._thread_local.pos is not None:
+    #         # Rewind the file position indicator of the body to where
+    #         # it was to resend the request.
+    #         r.request.body.seek(self._thread_local.pos)
+    #     s_auth = r.headers.get('www-authenticate', '')
+    #
+    #     if 'digest' in s_auth.lower() and self._thread_local.num_401_calls < 2:
+    #         self._thread_local.num_401_calls += 1
+    #         pat = re.compile(r'digest ', flags=re.IGNORECASE)
+    #         self._thread_local.chal = parse_dict_header(pat.sub('', s_auth, count=1))
+    #
+    #         # Consume content and release the original connection
+    #         # to allow our new request to reuse the same one.
+    #         r.content
+    #         r.close()
+    #         prep = r.request.copy()
+    #         extract_cookies_to_jar(prep._cookies, r.request, r.raw)
+    #         prep.prepare_cookies(prep._cookies)
+    #
+    #         prep.headers['Authorization'] = self.build_digest_header(
+    #             prep.method, prep.url)
+    #         _r = r.connection.send(prep, **kwargs)
+    #         _r.history.append(r)
+    #         _r.request = prep
+    #
+    #         return _r
+    #
+    #     self._thread_local.num_401_calls = 1
+    #     return r
 
 
 
