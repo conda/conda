0e388decc8f0fe34831e7787a4a3bb39ebaf82b2
Kale Franz <kfranz@continuum.io>
2016-10-22 15:16:33 -0500

    only try once for windows trash removal

--- conda/common/disk.py
+++ conda/common/disk.py
@@ -20,6 +20,8 @@ __all__ = ["rm_rf", "exp_backoff_fn", "try_write"]
 log = getLogger(__name__)
 
 
+MAX_TRIES = 7
+
 def try_write(dir_path, heavy=False):
     """Test write access to a directory.
 
@@ -50,20 +52,21 @@ def try_write(dir_path, heavy=False):
         return access(dir_path, W_OK)
 
 
-def backoff_unlink(file_or_symlink_path):
+def backoff_unlink(file_or_symlink_path, max_tries=MAX_TRIES):
     def _unlink(path):
         make_writable(path)
         unlink(path)
 
     try:
-        exp_backoff_fn(lambda f: lexists(f) and _unlink(f), file_or_symlink_path)
+        exp_backoff_fn(lambda f: lexists(f) and _unlink(f), file_or_symlink_path,
+                       max_tries=max_tries)
     except (IOError, OSError) as e:
         if e.errno not in (ENOENT,):
             # errno.ENOENT File not found error / No such file or directory
             raise
 
 
-def backoff_rmdir(dirpath):
+def backoff_rmdir(dirpath, max_tries=MAX_TRIES):
     if not isdir(dirpath):
         return
 
@@ -75,13 +78,13 @@ def backoff_rmdir(dirpath):
     def retry(func, path, exc_info):
         if getattr(exc_info[1], 'errno', None) == ENOENT:
             return
-        recursive_make_writable(dirname(path))
+        recursive_make_writable(dirname(path), max_tries=max_tries)
         func(path)
 
     def _rmdir(path):
         try:
             recursive_make_writable(path)
-            exp_backoff_fn(rmtree, path, onerror=retry)
+            exp_backoff_fn(rmtree, path, onerror=retry, max_tries=max_tries)
         except (IOError, OSError) as e:
             if e.errno == ENOENT:
                 log.debug("no such file or directory: %s", path)
@@ -120,7 +123,7 @@ def make_writable(path):
             raise
 
 
-def recursive_make_writable(path):
+def recursive_make_writable(path, max_tries=MAX_TRIES):
     # The need for this function was pointed out at
     #   https://github.com/conda/conda/issues/3266#issuecomment-239241915
     # Especially on windows, file removal will often fail because it is marked read-only
@@ -128,25 +131,25 @@ def recursive_make_writable(path):
         for root, dirs, files in walk(path):
             for path in chain.from_iterable((files, dirs)):
                 try:
-                    exp_backoff_fn(make_writable, join(root, path))
+                    exp_backoff_fn(make_writable, join(root, path), max_tries=max_tries)
                 except (IOError, OSError) as e:
                     if e.errno == ENOENT:
                         log.debug("no such file or directory: %s", path)
                     else:
                         raise
     else:
-        exp_backoff_fn(make_writable, path)
+        exp_backoff_fn(make_writable, path, max_tries=max_tries)
 
 
 def exp_backoff_fn(fn, *args, **kwargs):
     """Mostly for retrying file operations that fail on Windows due to virus scanners"""
+    max_tries = kwargs.pop('max_tries', MAX_TRIES)
     if not on_win:
         return fn(*args, **kwargs)
 
     import random
     # with max_tries = 6, max total time ~= 3.2 sec
     # with max_tries = 7, max total time ~= 6.5 sec
-    max_tries = 7
     for n in range(max_tries):
         try:
             result = fn(*args, **kwargs)
@@ -228,9 +231,9 @@ def delete_trash(prefix=None):
             path = join(trash_dir, p)
             try:
                 if isdir(path):
-                    backoff_rmdir(path)
+                    backoff_rmdir(path, max_tries=1)
                 else:
-                    backoff_unlink(path)
+                    backoff_unlink(path, max_tries=1)
             except (IOError, OSError) as e:
                 log.info("Could not delete path in trash dir %s\n%r", path, e)
         if listdir(trash_dir):
