d6e16525f2f45c83bd02153014867a2db230cd51
Kale Franz <kfranz@continuum.io>
2016-10-23 10:33:32 -0500

    SAVEPOINT

--- conda/install.py
+++ conda/install.py
@@ -146,6 +146,13 @@ class FileMode(Enum):
         return "%s" % self.value
 
 
+class FileType(Enum):
+    regular = 'regular'
+    softlink = 'softlink'
+    hardlink = 'hardlink'
+    directory = 'directory'
+
+
 LINK_HARD = 1
 LINK_SOFT = 2
 LINK_COPY = 3
@@ -225,6 +232,22 @@ def read_has_prefix(path):
             raise RuntimeError("Invalid has_prefix file at path: %s" % path)
     parsed_lines = (parse_line(line) for line in yield_lines(path))
     return {pr.filepath: (pr.placeholder, pr.filemode) for pr in parsed_lines}
+
+
+def read_files(path):
+    ParseResult = namedtuple('ParseResult', ('filepath', 'hash', 'bytes', 'type'))
+
+    def parse_line(line):
+        # 'filepath', 'hash', 'bytes', 'type'
+        parts = line.split(',')
+        if len(parts) == 4:
+            return ParseResult(*parts)
+        elif len(parts) == 1:
+            return ParseResult(parts[0], None, None, None)
+        else:
+            raise RuntimeError("Invalid files at path: %s" % path)
+
+    return tuple(parse_line(line) for line in yield_lines(path))
 
 
 class _PaddingError(Exception):
