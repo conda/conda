f086369e0a9a58b8fcd9ea2dca18dd40a9a8a785
Kale Franz <kfranz@continuum.io>
2016-10-22 23:03:03 -0500

    move s3 adapter to its own module

--- conda/connection.py
+++ conda/connection.py
@@ -10,7 +10,6 @@ import email
 import mimetypes
 import os
 import platform
-import tempfile
 from logging import getLogger
 from requests import Response, Session, __version__ as REQUESTS_VERSION
 from requests.adapters import BaseAdapter, HTTPAdapter
@@ -22,9 +21,8 @@ from requests.utils import get_auth_from_url, get_netrc_auth
 from . import __version__ as VERSION
 from ._vendor.auxlib.ish import dals
 from .base.context import context
-from .common.disk import rm_rf
 from .common.url import (add_username_and_password, get_proxy_username_and_pass,
-                         split_anaconda_token, url_to_path, url_to_s3_info, urlparse)
+                         split_anaconda_token, url_to_path, urlparse)
 from .compat import iteritems
 from .exceptions import ProxyError
 from .gateways.adapters.ftp import FTPAdapter
@@ -34,7 +32,6 @@ from .utils import gnu_get_libc_version
 RETRIES = 3
 
 log = getLogger(__name__)
-stderrlog = getLogger('stderrlog')
 
 # Collect relevant info from OS for reporting purposes (present in User-Agent)
 _user_agent = ("conda/{conda_ver} "
@@ -193,67 +190,7 @@ class CondaHttpAuth(AuthBase):
         return _response
 
 
-class S3Adapter(BaseAdapter):
 
-    def __init__(self):
-        super(S3Adapter, self).__init__()
-        self._temp_file = None
-
-    def send(self, request, stream=None, timeout=None, verify=None, cert=None, proxies=None):
-
-        resp = Response()
-        resp.status_code = 200
-        resp.url = request.url
-
-        try:
-            import boto
-        except ImportError:
-            stderrlog.info('\nError: boto is required for S3 channels. '
-                           'Please install it with `conda install boto`\n'
-                           'Make sure to run `source deactivate` if you '
-                           'are in a conda environment.\n')
-            resp.status_code = 404
-            return resp
-
-        conn = boto.connect_s3()
-
-        bucket_name, key_string = url_to_s3_info(request.url)
-
-        # Get the bucket without validation that it exists and that we have
-        # permissions to list its contents.
-        bucket = conn.get_bucket(bucket_name, validate=False)
-
-        try:
-            key = bucket.get_key(key_string)
-        except boto.exception.S3ResponseError as exc:
-            # This exception will occur if the bucket does not exist or if the
-            # user does not have permission to list its contents.
-            resp.status_code = 404
-            resp.raw = exc
-            return resp
-
-        if key and key.exists:
-            modified = key.last_modified
-            content_type = key.content_type or "text/plain"
-            resp.headers = CaseInsensitiveDict({
-                "Content-Type": content_type,
-                "Content-Length": key.size,
-                "Last-Modified": modified,
-                })
-
-            _, self._temp_file = tempfile.mkstemp()
-            key.get_contents_to_filename(self._temp_file)
-            f = open(self._temp_file, 'rb')
-            resp.raw = f
-            resp.close = resp.raw.close
-        else:
-            resp.status_code = 404
-
-        return resp
-
-    def close(self):
-        if self._temp_file:
-            rm_rf(self._temp_file)
 
 
 class LocalFSAdapter(BaseAdapter):
