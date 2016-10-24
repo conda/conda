933f3b639180aed0a93da02de56a60ad639d84ef
Kale Franz <kfranz@continuum.io>
2016-10-24 10:14:41 -0500

    SAVEPOINT

--- conda/common/disk.py
+++ conda/common/disk.py
@@ -1,30 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import errno
-import shutil
-import sys
-from conda import CondaError
-from errno import EACCES, EEXIST, ENOENT, EPERM
-from itertools import chain
 from logging import getLogger
-from os import (W_OK, access, chmod, getpid, link as os_link, listdir, lstat, makedirs, readlink,
-                rename, symlink, unlink, walk, stat)
-from os.path import abspath, basename, dirname, isdir, isfile, islink, join, lexists
-from shutil import rmtree
-from stat import S_IEXEC, S_IMODE, S_ISDIR, S_ISLNK, S_ISREG, S_IWRITE
-from time import sleep
-from uuid import uuid4
-
-from ..compat import lchmod, text_type
-from ..exceptions import CondaOSError
-from ..utils import on_win
-
-__all__ = ["rm_rf", "exp_backoff_fn", "try_write"]
+from os import makedirs
+from os.path import isdir
 
 log = getLogger(__name__)
-
-
 
 
 def conda_bld_ensure_dir(path):
