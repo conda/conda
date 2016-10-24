290353ad908aab5e3765f4c07f93ec7d797e48e1
Kale Franz <kfranz@continuum.io>
2016-10-24 00:54:58 -0500

    SAVEPOINT

--- conda/core/install.py
+++ conda/core/install.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import re
+
 from conda.common.disk import yield_lines
 from logging import getLogger
-from os.path import join, isdir, dirname
+from os.path import join, isdir, dirname, islink
 
 log = getLogger(__name__)
 
@@ -144,8 +146,83 @@ def unlink(prefix, dist):
             rm_rf(alt_files_path)
 
 
+def read_soft_links(extracted_package_directory, files):
+    return tuple(f for f in files if islink(join(extracted_package_directory, f)))
 
 
+def collect_all_info_for_package(extracted_package_directory):
+    info_dir = join(extracted_package_directory, 'info')
+
+    # collect information from info directory
+    from ..install import read_has_prefix, read_no_link
+    files = tuple(yield_lines(join(extracted_package_directory, 'info', 'files')))
+
+    # file system calls
+    has_prefix_files = read_has_prefix(join(info_dir, 'has_prefix'))
+    no_link = read_no_link(info_dir)
+    soft_links = read_soft_links(extracted_package_directory, files)
+
+    # simple processing
+    MENU_RE = re.compile(r'^menu/.*\.json$', re.IGNORECASE)
+    menu_files = tuple(f for f in files if MENU_RE.match(f))
+
+    return files, has_prefix_files, no_link, soft_links, menu_files
+
+
+
+def make_op_details(files, has_prefix_files, no_link, soft_links, menu_files, requested_link_type):
+    file_operations = []
+    for filepath in files:
+        if filepath in has_prefix_files:
+            link_type = LINK_COPY
+            prefix_placehoder, file_mode = has_prefix_files[filepath]
+        elif filepath in no_link or filepath in soft_links:
+            link_type = LINK_COPY
+            prefix_placehoder, file_mode = None, None
+        else:
+            link_type = requested_link_type
+            prefix_placehoder, file_mode = None, None
+        is_menu_file = filepath in menu_files
+        file_operations.append((filepath, link_type, prefix_placehoder, file_mode, is_menu_file))
+
+    # file_paths, link_types, prefix_placeholders, file_modes, is_menu_file
+    return file_operations
+
+
+def execute_file_operations(extracted_package_directory, prefix, leaf_directories, file_operations):
+
+        # Step 1. Make all directories
+        for d in leaf_directories:
+            makedirs(join(prefix, d))
+
+        # Step 2. Do the actual file linking
+        # Step 3. Replace prefix placeholder within all necessary files
+        # Step 4. Make shortcuts on Windows
+        for op in file_operations:
+            file_path, link_type, prefix_placeholder, file_mode, is_menu_file = op
+            source_path = join(extracted_package_directory, file_path)
+            destination_path = join(prefix, file_path)
+            try:
+                _link(source_path, destination_path, link_type)
+            except OSError as e:
+                raise CondaOSError('failed to link (src=%r, dst=%r, type=%r, error=%r)' %
+                                   (src, dst, lt, e))
+            if prefix_placeholder:
+                try:
+                    update_prefix(destination_path, prefix, prefix_placeholder, file_mode)
+                except _PaddingError:
+                    raise PaddingError(dist, prefix_placeholder, len(prefix_placeholder))
+
+            if on_win and is_menu_file and context.shortcuts:
+                make_menu(source_path, remove=False)
+
+        if on_win:
+            # make sure that the child environment behaves like the parent,
+            #    wrt user/system install on win
+            # This is critical for doing shortcuts correctly
+            nonadmin = join(sys.prefix, ".nonadmin")
+            if isfile(nonadmin):
+                open(join(prefix, ".nonadmin"), 'w').close()
 
 
 
@@ -162,11 +239,6 @@ class PackageInstaller(object):
 
 
 
-
-
-
-
-
         self.prefix = prefix
         self.extracted_package_directory = extracted_package_directory
 
@@ -179,17 +251,21 @@ class PackageInstaller(object):
 
         source_dir = self.extracted_package_directory
 
+        files, has_prefix_files, no_link, soft_links, menu_files = collect_all_info_for_package(self.extracted_package_directory)
+        leaf_directories = get_leaf_directories(files)
+        file_operations = make_op_details(files, has_prefix_files, no_link, soft_links, menu_files, link_type)
+
+
+
+
+
+
         # TODO: discuss with @mcg1969
         # run pre-link script
         # if not run_script(source_dir, dist, 'pre-link', prefix):
         #     raise LinkError('Error: pre-link failed: %s' % dist)
 
-        # collect information from info directory
-        from ..install import read_has_prefix, read_no_link
-        info_dir = join(source_dir, 'info')
-        files = list(yield_lines(join(info_dir, 'files')))
-        has_prefix_files = read_has_prefix(join(info_dir, 'has_prefix'))
-        no_link = read_no_link(info_dir)
+
 
         # # for the lock issue
         # # may run into lock if prefix not exist
@@ -197,40 +273,14 @@ class PackageInstaller(object):
         #     os.makedirs(prefix)
         assert isdir(self.prefix)
 
-        with DirectoryLock(self.prefix), FileLock(source_dir):
 
-            # Step 1. Make all directories
-            leaf_directories = get_leaf_directories(files)
-            for d in leaf_directories:
-                os.makedirs(d)
-
-            # Step 2. Do the actual file linking
-            for filepath in files:
-                src = join(source_dir, filepath)
-                dst = join(prefix, filepath)
-                if os.path.exists(dst):
-                    log.info("file exists, but clobbering: %r" % dst)
-                    rm_rf(dst)
-                lt = linktype
-                if filepath in has_prefix_files or filepath in no_link or islink(src):
-                    lt = LINK_COPY
-
-                try:
-                    _link(src, dst, lt)
-                except OSError as e:
-                    raise CondaOSError('failed to link (src=%r, dst=%r, type=%r, error=%r)' %
-                                       (src, dst, lt, e))
-
-            # Step 3. Replace prefix placeholder within all necessary files
-
-            # Step 4. Make shortcuts on Windows
 
             # Step 5. Run post-link script
             if not run_script(prefix, dist, 'post-link'):
                 raise LinkError("Error: post-link failed for: %s" % dist)
 
             # Step 6. Create package's prefix/conda-meta file
-
+            create_meta(prefix, dist, info_dir, meta_dict)
 
 
 
@@ -268,13 +318,6 @@ class PackageInstaller(object):
                     raise PaddingError(dist, placeholder, len(placeholder))
 
             # make shortcuts on Windows
-            if on_win:
-                # make sure that the child environment behaves like the parent,
-                #    wrt user/system install on win
-                # This is critical for doing shortcuts correctly
-                nonadmin = join(sys.prefix, ".nonadmin")
-                if isfile(nonadmin):
-                    open(join(prefix, ".nonadmin"), 'w').close()
 
                 if context.shortcuts:
                     mk_menus(prefix, files, remove=False)
