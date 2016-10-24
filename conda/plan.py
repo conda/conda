9860f724fe2af433b6e7f4f9d5cc48c15f19ee3b
Kale Franz <kfranz@continuum.io>
2016-10-24 14:10:59 -0500

    how'd set_trace sneak in?

--- conda/plan.py
+++ conda/plan.py
@@ -47,7 +47,6 @@ def print_dists(dists_extras):
 
 
 def display_actions(actions, index, show_channel_urls=None):
-    import pdb; pdb.set_trace()
     if show_channel_urls is None:
         show_channel_urls = context.show_channel_urls
 
