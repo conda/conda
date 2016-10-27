5a01114f34c842282491f877f33789d13429add0
Kale Franz <kfranz@continuum.io>
2016-10-26 19:06:16 -0500

    fix #3738 move conda-env to ruamel_yaml

--- conda_env/yaml.py
+++ conda_env/yaml.py
@@ -5,21 +5,21 @@ This is based on the answer at http://stackoverflow.com/a/16782282
 """
 from __future__ import absolute_import, print_function
 from collections import OrderedDict
-import yaml
+import ruamel_yaml as yaml
 
 
-def represent_ordereddict(dumper, data):
-    value = []
-
-    for item_key, item_value in data.items():
-        node_key = dumper.represent_data(item_key)
-        node_value = dumper.represent_data(item_value)
-
-        value.append((node_key, node_value))
-
-    return yaml.nodes.MappingNode(u'tag:yaml.org,2002:map', value)
-
-yaml.add_representer(OrderedDict, represent_ordereddict)
+# def represent_ordereddict(dumper, data):
+#     value = []
+#
+#     for item_key, item_value in data.items():
+#         node_key = dumper.represent_data(item_key)
+#         node_value = dumper.represent_data(item_value)
+#
+#         value.append((node_key, node_value))
+#
+#     return yaml.nodes.MappingNode(u'tag:yaml.org,2002:map', value)
+#
+# yaml.add_representer(OrderedDict, represent_ordereddict)
 
 dump = yaml.dump
 load = yaml.load
