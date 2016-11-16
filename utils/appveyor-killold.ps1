a1825c02d1468582cbd4ed1ce38456bb857d5be7
Odegard, Ken <ken.odegard@gmail.com>
2016-11-16 11:30:56 -0700

    Added trailing back tick to AppVeyor fast-fail

--- utils/appveyor-killold.ps1
+++ utils/appveyor-killold.ps1
@@ -13,8 +13,8 @@
 # reference:
 #   JuliaLang developers
 #   (https://github.com/JuliaLang/Example.jl/commit/8a4959122ce843b3664337c8b5119ce3a968c408)
-if ($env:APPVEYOR_PULL_REQUEST_NUMBER -and $env:APPVEYOR_BUILD_NUMBER -ne ((Invoke-RestMethod
-    https://ci.appveyor.com/api/projects/$env:APPVEYOR_ACCOUNT_NAME/$env:APPVEYOR_PROJECT_SLUG/history?recordsNumber=50).builds |
+if ($env:APPVEYOR_PULL_REQUEST_NUMBER -and $env:APPVEYOR_BUILD_NUMBER -ne ((Invoke-RestMethod `
+    https://ci.appveyor.com/api/projects/$env:APPVEYOR_ACCOUNT_NAME/$env:APPVEYOR_PROJECT_SLUG/history?recordsNumber=50).builds | `
     Where-Object pullRequestId -eq $env:APPVEYOR_PULL_REQUEST_NUMBER)[0].buildNumber) {
     throw "There are newer queued builds for this pull request, failing early."
 }
