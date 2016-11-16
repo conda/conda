e19619430bb5333416ce46fae8833c44f09d468a
Kale Franz <kfranz@continuum.io>
2016-11-15 18:22:48 -0600

    another try when file doesn't exist

--- conda/gateways/adapters/localfs.py
+++ conda/gateways/adapters/localfs.py
@@ -5,6 +5,7 @@ from email.utils import formatdate
 from logging import getLogger
 from mimetypes import guess_type
 from os import lstat
+from os.path import isfile
 from requests.adapters import BaseAdapter
 from requests.models import Response
 from requests.structures import CaseInsensitiveDict
@@ -29,17 +30,20 @@ class LocalFSAdapter(BaseAdapter):
             resp.status_code = 404
             resp.raw = exc
         else:
-            modified = formatdate(stats.st_mtime, usegmt=True)
-            content_type = guess_type(pathname)[0] or "text/plain"
-            resp.headers = CaseInsensitiveDict({
-                "Content-Type": content_type,
-                "Content-Length": stats.st_size,
-                "Last-Modified": modified,
-            })
+            if isfile(pathname):
+                modified = formatdate(stats.st_mtime, usegmt=True)
+                content_type = guess_type(pathname)[0] or "text/plain"
+                resp.headers = CaseInsensitiveDict({
+                    "Content-Type": content_type,
+                    "Content-Length": stats.st_size,
+                    "Last-Modified": modified,
+                })
 
-            resp.raw = open(pathname, "rb")
-            resp.close = resp.raw.close
-
+                resp.raw = open(pathname, "rb")
+                resp.close = resp.raw.close
+            else:
+                resp.status_code = 404
+                resp.raw = b'{"error": "file does not exist", "path": "%s"}' % pathname
         return resp
 
     def close(self):
