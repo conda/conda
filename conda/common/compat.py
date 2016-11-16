dc2afa7935385c92232966db161a2d4e873dcfb4
Kale Franz <kfranz@continuum.io>
2016-11-16 14:33:20 -0600

    fix utf8 import

--- conda/common/compat.py
+++ conda/common/compat.py
@@ -6,15 +6,14 @@ from logging import getLogger
 from .._vendor.auxlib.compat import (iteritems, with_metaclass, itervalues,  # NOQA
                                      string_types, primitive_types, text_type, odict,  # NOQA
                                      StringIO, isiterable)  # NOQA
-from ..base.constants import UTF8
 from ..compat import *  # NOQA
 
 log = getLogger(__name__)
 
 
 def ensure_binary(value):
-    return value.encode(UTF8) if hasattr(value, 'encode') else value
+    return value.encode('utf-8') if hasattr(value, 'encode') else value
 
 
 def ensure_text_type(value):
-    return value.decode(UTF8) if hasattr(value, 'decode') else value
+    return value.decode('utf-8') if hasattr(value, 'decode') else value
