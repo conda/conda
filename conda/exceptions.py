95a85744f68b85920147b55072c7fc246efc81e4
Kale Franz <kfranz@continuum.io>
2016-11-23 20:31:35 -0600

    make reason upper case

--- conda/exceptions.py
+++ conda/exceptions.py
@@ -11,7 +11,7 @@ from traceback import format_exc
 from . import CondaError, text_type, CondaExitZero
 from ._vendor.auxlib.entity import EntityEncoder
 from ._vendor.auxlib.ish import dals
-from .compat import iteritems, iterkeys
+from .common.compat import iteritems, iterkeys, string_types
 
 log = logging.getLogger(__name__)
 
@@ -247,6 +247,8 @@ class CondaHTTPError(CondaError):
         """) + message
         if isinstance(elapsed_time, timedelta):
             elapsed_time = text_type(elapsed_time).split(':', 1)[-1]
+        if isinstance(reason, string_types):
+            reason = reason.upper()
         super(CondaHTTPError, self).__init__(message, url=url, status_code=status_code,
                                              reason=reason, elapsed_time=elapsed_time)
 
