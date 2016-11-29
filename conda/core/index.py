dfce753c7dd8cbd70df684ad022103421e4921e1
Kale Franz <kfranz@continuum.io>
2016-11-28 23:35:14 -0600

    beginning work to make Record immutable

--- conda/core/index.py
+++ conda/core/index.py
@@ -346,6 +346,7 @@ def add_http_value_to_dict(resp, http_key, d, dict_key):
 
 
 def add_unknown(index, priorities):
+    # TODO: discuss with @mcg1969 and document
     priorities = {p[0]: p[1] for p in itervalues(priorities)}
     maxp = max(itervalues(priorities)) + 1 if priorities else 1
     for dist, info in iteritems(package_cache()):
@@ -356,7 +357,7 @@ def add_unknown(index, priorities):
             continue
         try:
             with open(join(info['dirs'][0], 'info', 'index.json')) as fi:
-                meta = Record(**json.load(fi))
+                meta = json.load(fi)
         except IOError:
             continue
         if info['urls']:
@@ -383,13 +384,14 @@ def add_unknown(index, priorities):
                      })
         meta.setdefault('depends', [])
         log.debug("adding cached pkg to index: %s" % dist)
-        index[dist] = meta
+        index[dist] = Record(**meta)
 
 
 def add_pip_dependency(index):
-    for info in itervalues(index):
+    # TODO: discuss with @mcg1969 and document
+    for dist, info in iteritems(index):
         if info['name'] == 'python' and info['version'].startswith(('2.', '3.')):
-            info['depends'] = info['depends'] + ('pip',)
+            index[dist] = Record.from_objects(info, depends=info['depends'] + ('pip',))
 
 
 def create_cache_dir():
