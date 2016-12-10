0d77b59ea77c8939a5bff2b6f30fcfe675a3750f
Kale Franz <kfranz@continuum.io>
2016-12-10 13:16:46 -0600

    just some tweaks

--- conda/cli/python_api.py
+++ conda/cli/python_api.py
@@ -5,10 +5,11 @@ from importlib import import_module
 from logging import getLogger
 from shlex import split
 
-from ..base.context import context, reset_context
+from ..base.constants import APP_NAME, SEARCH_PATH
+from ..base.context import context
 from ..cli.main import generate_parser
-from ..common.compat import on_win
 from ..common.io import captured, replace_log_streams
+from ..common.path import win_path_double_escape
 from ..exceptions import conda_exception_handler
 from ..gateways import initialize_logging
 
@@ -33,10 +34,6 @@ def get_configure_parser_function(command):
     return import_module(module).configure_parser
 
 
-def escape_for_winpath(p):
-    return p.replace('\\', '\\\\') if on_win else p
-
-
 def run_command(command, *arguments, **kwargs):
     """
 
@@ -45,30 +42,36 @@ def run_command(command, *arguments, **kwargs):
         *arguments: instructions you would normally pass to the conda comamnd on the command line
                     see below for examples
         **kwargs: special instructions for programmatic overrides
-          - use_exception_handler: defaults to False.  False will let the code calling
+          use_exception_handler: defaults to False.  False will let the code calling
               `run_command` handle all exceptions.  True won't raise when an exception
               has occured, and instead give a non-zero return code
-                  currently only `use_exception_handler`, which defaults to False
+          search_path: an optional non-standard search path for configuration information
+              that overrides the default SEARCH_PATH
 
     Returns: a tuple of stdout, stderr, and return_code
 
     Examples:
-        >>  run_command(Commands.CREATE, "-n newenv python=3 flask")
+        >>  run_command(Commands.CREATE, "-n newenv python=3 flask", use_exception_handler=True)
         >>  run_command(Commands.CREATE, "-n newenv", "python=3", "flask")
-        >>  run_command(Commands.CREATE, ["-n newenv", "python=3", "flask"])
+        >>  run_command(Commands.CREATE, ["-n newenv", "python=3", "flask"], search_path=())
 
 
     """
     use_exception_handler = kwargs.get('use_exception_handler', False)
+    configuration_search_path = kwargs.get('search_path', SEARCH_PATH)
     p, sub_parsers = generate_parser()
     get_configure_parser_function(command)(sub_parsers)
 
-    arguments = map(escape_for_winpath, arguments)
+    arguments = map(win_path_double_escape, arguments)
     command_line = "%s %s" % (command, " ".join(arguments))
     split_command_line = split(command_line)
 
     args = p.parse_args(split_command_line)
-    context._add_argparse_args(args)
+    context.__init__(
+        search_path=configuration_search_path,
+        app_name=APP_NAME,
+        argparse_args=args,
+    )
     log.debug("executing command >>> %s", command_line)
     with captured() as c, replace_log_streams():
         if use_exception_handler:
@@ -76,6 +79,4 @@ def run_command(command, *arguments, **kwargs):
         else:
             return_code = args.func(args, p)
     log.debug("\n  stdout: %s\n  stderr: %s\n  return_code: %s", c.stdout, c.stderr, return_code)
-    if command == Commands.CONFIG:
-        reset_context()
     return c.stdout, c.stderr, return_code
