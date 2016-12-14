b54d441862f5df75ef039e9e28b0abc21fbedf3b
Kale Franz <kfranz@continuum.io>
2016-12-13 21:45:24 -0600

    remove -O entry point for now, because it's not being preserved somewhere

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -6,14 +6,14 @@ unlink $PREFIX/bin/deactivate
 $PYTHON conda.recipe/setup.py install
 $PYTHON conda.recipe/setup.py --version > __conda_version__.txt
 
-cat <<- EOF > $PREFIX/bin/conda
-#!$PYTHON -O
-if __name__ == '__main__':
-   import sys
-   import conda.cli.main
-   sys.exit(conda.cli.main.main())
-EOF
-
+#cat <<- EOF > $PREFIX/bin/conda
+##!$PYTHON -O
+#if __name__ == '__main__':
+#   import sys
+#   import conda.cli.main
+#   sys.exit(conda.cli.main.main())
+#EOF
+#
 
 mkdir -p $PREFIX/etc/fish/conf.d/
 cp $SRC_DIR/shell/conda.fish $PREFIX/etc/fish/conf.d/
