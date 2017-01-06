a30c591c5e179389b22a491c1918876b331e3219
Kale Franz <kfranz@continuum.io>
2017-01-05 18:20:13 -0600

    setup-testing.py from 4.3.x

--- utils/setup-testing.py
+++ utils/setup-testing.py
@@ -3,6 +3,7 @@
 #
 # conda is distributed under the terms of the BSD 3-clause license.
 # Consult LICENSE.txt or http://opensource.org/licenses/BSD-3-Clause.
+from __future__ import absolute_import, division, print_function, unicode_literals
 import os
 import sys
 if 'develop' in sys.argv:
@@ -14,24 +15,16 @@ if not (sys.version_info[:2] == (2, 7) or sys.version_info[:2] >= (3, 3)):
     sys.exit("conda is only meant for Python 2.7 or 3.3 and up.  "
              "current version: %d.%d" % sys.version_info[:2])
 
-if os.environ.get('CONDA_DEFAULT_ENV'):
-    # Try to prevent accidentally installing conda into a non-root conda environment
-    sys.exit("""
-You appear to be in a non-root conda environment. Conda is only supported in
-the root environment. Deactivate and try again.  If you believe this message
-is in error, run CONDA_DEFAULT_ENV='' python setup.py.
-""")
-
-# When executing the setup.py, we need to be able to import ourselves, this
+# When executing setup.py, we need to be able to import ourselves, this
 # means that we need to add the src directory to the sys.path.
 here = os.path.abspath(os.path.dirname(__file__))
-src_dir = os.path.join(here, "conda")
+src_dir = os.path.join(here, "..")
 sys.path.insert(0, src_dir)
 
 import conda  # NOQA
 from conda._vendor.auxlib import packaging  # NOQA
 
-with open(os.path.join(here, "README.rst")) as f:
+with open(os.path.join(src_dir, "README.rst")) as f:
     long_description = f.read()
 
 scripts = ['shell/activate',
@@ -79,10 +72,10 @@ setup(
         'build_py': packaging.BuildPyCommand,
         'sdist': packaging.SDistCommand,
     },
-    install_requires=[],
+    install_requires=install_requires,
     entry_points={
         'console_scripts': [
-            "conda = conda.cli.main:main",
+            "conda = conda.cli:main",
             "conda-env = conda_env.cli.main:main"
         ],
     },
