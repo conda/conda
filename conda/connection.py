5a4457f2342e9dd67688c1dc39b18c27b631af4d
Kale Franz <kfranz@continuum.io>
2017-01-05 20:16:56 -0600

    fix flake8

--- conda/connection.py
+++ conda/connection.py
@@ -315,33 +315,6 @@ class LocalFSAdapter(requests.adapters.BaseAdapter):
             resp.close = resp.raw.close
         return resp
 
-
-    # def send(self, request, stream=None, timeout=None, verify=None, cert=None, proxies=None):
-    #     pathname = url_to_path(request.url)
-    #
-    #     resp = requests.models.Response()
-    #     resp.status_code = 200
-    #     resp.url = request.url
-    #
-    #     try:
-    #         stats = os.stat(pathname)
-    #     except OSError as exc:
-    #         resp.status_code = 404
-    #         resp.raw = exc
-    #     else:
-    #         modified = email.utils.formatdate(stats.st_mtime, usegmt=True)
-    #         content_type = mimetypes.guess_type(pathname)[0] or "text/plain"
-    #         resp.headers = requests.structures.CaseInsensitiveDict({
-    #             "Content-Type": content_type,
-    #             "Content-Length": stats.st_size,
-    #             "Last-Modified": modified,
-    #         })
-    #
-    #         resp.raw = open(pathname, "rb")
-    #         resp.close = resp.raw.close
-    #
-    #     return resp
-
     def close(self):
         pass
 
