42ea0e0dee4dfd4b93749668f76be77232959687
Chris Tomkins-Tinch <tomkinsc@gmail.com>
2017-01-17 17:15:00 -0500

    workaround for symlink race conditions
    See: https://github.com/conda/conda/issues/3001
    This should fix a bug related to symlink race conditions on cluster shared filesystems.

--- conda/install.py
+++ conda/install.py
@@ -183,13 +183,17 @@ def symlink_conda_hlp(prefix, root_dir, where, symlink_fn):
             # try to kill stale links if they exist
             if os.path.lexists(prefix_file):
                 rm_rf(prefix_file)
-
-            # if they're in use, they won't be killed, skip making new symlink
+            # if they're in use, they won't be killed.  Skip making new symlink.
             if not os.path.lexists(prefix_file):
                 symlink_fn(root_file, prefix_file)
         except (IOError, OSError) as e:
-            if (os.path.lexists(prefix_file) and (e.errno in [EPERM, EACCES, EROFS, EEXIST])):
+            if (os.path.lexists(prefix_file) and
+                    (e.errno in (errno.EPERM, errno.EACCES, errno.EROFS, errno.EEXIST))):
                 log.debug("Cannot symlink {0} to {1}. Ignoring since link already exists."
                           .format(root_file, prefix_file))
+            elif (e.errno == errno.ENOENT):
+                log.debug("Problem with symlink management {0} {1}. File may have been removed by another concurrent process." .format(root_file, prefix_file))
+            elif (e.errno == errno.EEXIST)):
+                log.debug("Problem with symlink management {0} {1}. File may have been created by another concurrent process." .format(root_file, prefix_file))
             else:
                 raise
