7740a8c1c98b1ff64dc9503caef3ddcd221d2f2d
Kale Franz <kfranz@continuum.io>
2017-01-20 12:08:59 -0600

    switch LinkError to UpgradeError

--- conda/exceptions.py
+++ conda/exceptions.py
@@ -381,6 +381,12 @@ class CondaSignatureError(CondaError):
         super(CondaSignatureError, self).__init__(msg)
 
 
+class CondaUpgradeError(CondaError):
+    def __init__(self, message):
+        msg = "Conda upgrade error: %s" % message
+        super(CondaUpgradeError, self).__init__(msg)
+
+
 def print_conda_exception(exception):
     from conda.base.context import context
 
