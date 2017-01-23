149712ed5838d58955365e7da69bde5347f8b1e0
Kale Franz <kfranz@continuum.io>
2017-01-23 15:51:49 -0600

    fix #4398 apparently 'hard' was used for link type at one point

--- conda/models/index_record.py
+++ conda/models/index_record.py
@@ -12,6 +12,10 @@ class LinkTypeField(EnumField):
     def box(self, instance, val):
         if isinstance(val, string_types):
             val = val.replace('-', '').replace('_', '').lower()
+            if val == 'hard':
+                val = LinkType.hardlink
+            elif val == 'soft':
+                val = LinkType.softlink
         return super(LinkTypeField, self).box(instance, val)
 
 
