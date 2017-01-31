74c0055a119634b265980ca7fe2bd5b5b907ade9
Kale Franz <kfranz@continuum.io>
2017-01-31 07:48:08 -0600

    add durations to conda-build tests

--- utils/travis-run-script.sh
+++ utils/travis-run-script.sh
@@ -56,7 +56,7 @@ conda_build_unit_test() {
     echo ">>>>>>>>>>>> running conda-build unit tests >>>>>>>>>>>>>>>>>>>>>"
     echo
     ~/miniconda/bin/python -m conda info
-    ~/miniconda/bin/python -m pytest --basetemp /tmp/cb -v tests
+    ~/miniconda/bin/python -m pytest --basetemp /tmp/cb -v --durations=20 tests
     popd
 }
 
