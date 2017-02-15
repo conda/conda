476f7b9b06d5c45d63a860aaec0e47029d51293b
Kale Franz <kfranz@continuum.io>
2017-02-15 13:09:25 -0600

    fix #4649 incorrect assert statement concerning package cache directory

--- conda/core/index.py
+++ conda/core/index.py
@@ -591,8 +591,6 @@ def add_http_value_to_dict(resp, http_key, d, dict_key):
 
 
 def create_cache_dir():
-    pkgs_dir = PackageCache.first_writable(context.pkgs_dirs).pkgs_dir
-    assert pkgs_dir == context.pkgs_dirs[0], (pkgs_dir, context.pkgs_dirs)
     cache_dir = join(PackageCache.first_writable(context.pkgs_dirs).pkgs_dir, 'cache')
     try:
         makedirs(cache_dir)
