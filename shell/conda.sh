bd6890948f4c3b3bdfc73101ff3a45b47fc0aec9
Kale Franz <kfranz@continuum.io>
2017-04-11 16:49:30 -0500

    conda.sh is dash compatible

--- shell/conda.sh
+++ shell/conda.sh
@@ -1,20 +1,19 @@
+#!/bin/dash
 # This file should be sourced by bash or zsh.  It should not itself be executed.
 
 # This file is WIP
 # For now, to test, try something like `source shell/conda.sh && conda activate /my/env`
 
 _conda_hashr() {
-    [[ -z $BASH_VERSION ]] || hash -r
-    [[ -z $ZSH_VERSION ]] || rehash
+    [ -z "$BASH_VERSION" ] || hash -r
+    [ -z "$ZSH_VERSION" ] || rehash
 }
 
 
 _conda_activate() {
-    while read -r line; do
-        eval "$line"
-    done < <(python -m conda.activate activate posix "$@")
+    eval "$(python -m conda.activate activate posix "$@")"
 
-    if [ "${PS1:0:22}" != '$CONDA_PROMPT_MODIFIER' ]; then
+    if [ "$(echo "$PS1" | awk '{ string=substr($0, 1, 22); print string; }')" != '$CONDA_PROMPT_MODIFIER' ]; then
         PS1='$CONDA_PROMPT_MODIFIER'"$PS1"
     fi
 
@@ -22,21 +21,18 @@ _conda_activate() {
 }
 
 _conda_deactivate() {
-    while read -r line; do
-        eval "$line"
-    done < <(python -m conda.activate deactivate posix "$@")
+    eval "$(python -m conda.activate deactivate posix "$@")"
 
     if [ -z "$CONDA_PREFIX" ]; then
-        PS1=${PS1:22}
+        # PS1=${PS1:22}
+        PS1=$(echo "$PS1" | awk '{ string=substr($0, 23); print string; }')
     fi
 
     _conda_hashr
 }
 
 _conda_reactivate() {
-    while read -r line; do
-        eval "$line"
-    done < <(python -m conda.activate reactivate posix "$@")
+    eval "$(python -m conda.activate reactivate posix)"
 
     _conda_hashr
 }
@@ -49,7 +45,7 @@ conda() {
             _conda_activate "$@"
             ;;
         deactivate)
-            _conda_deactivate
+            _conda_deactivate "$@"
             ;;
         install | update | uninstall | remove)
             CONDA="$(which conda)"
