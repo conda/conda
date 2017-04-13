6e9a1ee51b38cc89e0f38ec1a00b6b81d202fdf9
Kale Franz <kfranz@continuum.io>
2017-04-13 00:25:24 -0500

    fix build.sh recipe

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -1,7 +1,7 @@
 # necessary because conda symlinks
-unlink $PREFIX/bin/conda
-unlink $PREFIX/bin/activate
-unlink $PREFIX/bin/deactivate
+unlink $PREFIX/bin/conda || true
+unlink $PREFIX/bin/activate || true
+unlink $PREFIX/bin/deactivate || true
 
 $PYTHON conda.recipe/setup.py install
 $PYTHON conda.recipe/setup.py --version > __conda_version__.txt
@@ -13,6 +13,4 @@ mkdir -p $PREFIX/etc/profile.d
 cp $SRC_DIR/shell/conda.sh $PREFIX/etc/profile.d/
 
 mkdir -p $PREFIX/etc/conda/activate.d
-pushd $PREFIX/etc/profile.d/
-ln -s conda.sh ../conda/activate.d/conda.sh
-popd
+ln -s $PREFIX/etc/profile.d/conda.sh $PREFIX/etc/conda/activate.d/
