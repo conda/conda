79e9a7c2dfb06c42f6d432c20e3ab30eabb7c04e
Kale Franz <kfranz@continuum.io>
2017-04-22 15:14:53 -0500

    backward compatibilty stub

---
+++ conda/connection.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+from .gateways.connection import CondaSession
+CondaSession = CondaSession
