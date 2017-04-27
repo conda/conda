79d5e266de633c7c9fa2d3c4366eb10174a33ef3
Kale Franz <kfranz@continuum.io>
2017-04-27 17:09:02 -0500

    cmd.exe is actually good about knowing a script file's own location

--- shell/Library/bin/conda.bat
+++ shell/Library/bin/conda.bat
@@ -1,13 +1,14 @@
+@SET "_CONDA_EXE=%~dp0\..\..\Scripts\conda.exe"
 
-@IF NOT "%CONDA_EXE%" == "" GOTO skip_set_conda_exe
-@SET "CONDA_EXE=python -m conda"
-:skip_set_conda_exe
+@IF NOT "%_CONDA_EXE%" == "" GOTO skip_set__CONDA_EXE
+@SET "_CONDA_EXE=python -m conda"
+:skip_set__CONDA_EXE
 
 
 @IF "%1"=="activate" GOTO :DO_ACTIVATE
 @IF "%1"=="deactivate" GOTO :DO_DEACTIVATE
 
-@CALL %CONDA_EXE% %*
+@CALL %_CONDA_EXE% %*
 
 @IF "%1"=="install" GOTO :DO_DEACTIVATE
 @IF "%1"=="update" GOTO :DO_DEACTIVATE
@@ -21,7 +22,7 @@
 @IF "%CONDA_PROMPT_MODIFIER%" == "" GOTO skip_prompt_set_activate
 @CALL SET PROMPT=%%PROMPT:%CONDA_PROMPT_MODIFIER%=%replacement%%%
 :skip_prompt_set_activate
-@FOR /F "delims=" %%i IN ('@call %CONDA_EXE% shell.activate cmd.exe %2') DO @SET "TEMP_SCRIPT=%%i"
+@FOR /F "delims=" %%i IN ('@call %_CONDA_EXE% shell.activate cmd.exe %2') DO @SET "TEMP_SCRIPT=%%i"
 @CALL "%TEMP_SCRIPT%"
 @DEL /F /Q "%TEMP_SCRIPT%"
 @SET TEMP_SCRIPT=
@@ -32,7 +33,7 @@
 @IF "%CONDA_PROMPT_MODIFIER%" == "" GOTO skip_prompt_set_deactivate
 @CALL SET PROMPT=%%PROMPT:%CONDA_PROMPT_MODIFIER%=%replacement%%%
 :skip_prompt_set_deactivate
-@FOR /F "delims=" %%i IN ('@call %CONDA_EXE% shell.deactivate cmd.exe') DO @SET "TEMP_SCRIPT=%%i"
+@FOR /F "delims=" %%i IN ('@call %_CONDA_EXE% shell.deactivate cmd.exe') DO @SET "TEMP_SCRIPT=%%i"
 @CALL "%TEMP_SCRIPT%"
 @DEL /F /Q "%TEMP_SCRIPT%"
 @SET TEMP_SCRIPT=
@@ -40,12 +41,12 @@
 @GOTO :End
 
 :DO_REACTIVATE
-@FOR /F "delims=" %%i IN ('@call %CONDA_EXE% shell.reactivate cmd.exe') DO @SET "TEMP_SCRIPT=%%i"
+@FOR /F "delims=" %%i IN ('@call %_CONDA_EXE% shell.reactivate cmd.exe') DO @SET "TEMP_SCRIPT=%%i"
 @CALL "%TEMP_SCRIPT%"
 @DEL /F /Q "%TEMP_SCRIPT%"
 @SET TEMP_SCRIPT=
 @GOTO :End
 
 :End
-@SET CONDA_EXE=
+@SET _CONDA_EXE=
 @GOTO :eof
