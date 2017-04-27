76bb8dd74fe1dd98648f712b318ff6af07d93fd9
Kale Franz <kfranz@continuum.io>
2017-04-27 01:44:05 -0500

    fix flake8

--- conda/resolve.py
+++ conda/resolve.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import logging
 
-from .base.constants import DEFAULTS_CHANNEL_NAME, MAX_CHANNEL_PRIORITY, CONDA_TARBALL_EXTENSION
+from .base.constants import DEFAULTS_CHANNEL_NAME, MAX_CHANNEL_PRIORITY
 from .base.context import context
 from .common.compat import iteritems, iterkeys, itervalues, string_types
 from .common.toposort import toposort
