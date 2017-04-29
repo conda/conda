3198bbf80f7fe9ecd978cb5c68cbcb76f9ddf19f
Kale Franz <kfranz@continuum.io>
2017-04-28 19:24:27 -0500

    put back old memoized function in exports

--- conda/exports.py
+++ conda/exports.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+from collections import Hashable
 from functools import partial
 from logging import getLogger
+import threading
 from warnings import warn
 
 log = getLogger(__name__)
@@ -65,10 +67,10 @@ from .signature import KEYS, KEYS_DIR, hash_file, verify  # NOQA
 KEYS, KEYS_DIR = KEYS, KEYS_DIR
 hash_file, verify = hash_file, verify
 
-from .utils import hashsum_file, human_bytes, memoized, unix_path_to_win, url_path  # NOQA
+from .utils import hashsum_file, human_bytes, unix_path_to_win, url_path  # NOQA
 from .common.path import win_path_to_unix  # NOQA
 hashsum_file, human_bytes = hashsum_file, human_bytes
-memoized, unix_path_to_win = memoized, unix_path_to_win
+unix_path_to_win = unix_path_to_win
 win_path_to_unix, url_path = win_path_to_unix, url_path
 
 from .gateways.disk.read import compute_md5sum  # NOQA
@@ -144,4 +146,38 @@ class Completer(object):  # pragma: no cover
     def __iter__(self):
         return iter(self.get_items())
 
-class InstalledPackages(object): pass  # NOQA
+
+class InstalledPackages(object):
+    pass
+
+
+class memoized(object):
+    """Decorator. Caches a function's return value each time it is called.
+    If called later with the same arguments, the cached value is returned
+    (not reevaluated).
+    """
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+        self.lock = threading.Lock()
+
+    def __call__(self, *args, **kw):
+        newargs = []
+        for arg in args:
+            if isinstance(arg, list):
+                newargs.append(tuple(arg))
+            elif not isinstance(arg, Hashable):
+                # uncacheable. a list, for instance.
+                # better to not cache than blow up.
+                return self.func(*args, **kw)
+            else:
+                newargs.append(arg)
+        newargs = tuple(newargs)
+        key = (newargs, frozenset(sorted(kw.items())))
+        with self.lock:
+            if key in self.cache:
+                return self.cache[key]
+            else:
+                value = self.func(*args, **kw)
+                self.cache[key] = value
+                return value
