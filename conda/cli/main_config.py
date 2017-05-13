47777af27fdbeee8bb36284ee2ae5b28aec402ff
Kale Franz <kfranz@continuum.io>
2017-05-12 20:25:01 -0500

    fix #5256 enable conda config --get for all primitive parameters

--- conda/cli/main_config.py
+++ conda/cli/main_config.py
@@ -21,8 +21,7 @@ from ..common.compat import isiterable, iteritems, string_types, text_type
 from ..common.configuration import pretty_list, pretty_map
 from ..common.constants import NULL
 from ..common.yaml import yaml_dump, yaml_load
-from ..config import (rc_bool_keys, rc_list_keys, rc_other, rc_string_keys, sys_rc_path,
-                      user_rc_path)
+from ..config import rc_other, sys_rc_path, user_rc_path
 
 descr = """
 Modify configuration values in .condarc.  This is modeled after the git
@@ -228,6 +227,10 @@ def format_dict(d):
 
 
 def execute_config(args, parser):
+    try:
+        from cytoolz.itertoolz import groupby
+    except ImportError:  # pragma: no cover
+        from .._vendor.toolz.itertoolz import groupby  # NOQA
     from .._vendor.auxlib.entity import EntityEncoder
     json_warnings = []
     json_get = {}
@@ -317,13 +320,19 @@ def execute_config(args, parser):
     else:
         rc_config = {}
 
+    grouped_paramaters = groupby(lambda p: context.describe_parameter(p)['parameter_type'],
+                                 context.list_parameters())
+    primitive_parameters = grouped_paramaters['primitive']
+    sequence_parameters = grouped_paramaters['sequence']
+    map_parameters = grouped_paramaters['map']
+
     # Get
     if args.get is not None:
         context.validate_all()
         if args.get == []:
             args.get = sorted(rc_config.keys())
         for key in args.get:
-            if key not in rc_list_keys + rc_bool_keys + rc_string_keys:
+            if key not in primitive_parameters + sequence_parameters:
                 if key not in rc_other:
                     message = "unknown key %s" % key
                     if not context.json:
@@ -356,8 +365,6 @@ def execute_config(args, parser):
 
     # prepend, append, add
     for arg, prepend in zip((args.prepend, args.append), (True, False)):
-        sequence_parameters = [p for p in context.list_parameters()
-                               if context.describe_parameter(p)['parameter_type'] == 'sequence']
         for key, item in arg:
             if key == 'channels' and key not in rc_config:
                 rc_config[key] = ['defaults']
@@ -382,15 +389,6 @@ def execute_config(args, parser):
                 else:
                     json_warnings.append(message)
             arglist.insert(0 if prepend else len(arglist), item)
-
-    primitive_parameters = frozenset(
-        p for p in context.list_parameters()
-        if context.describe_parameter(p)['parameter_type'] == 'primitive'
-    )
-    map_parameters = frozenset(
-        p for p in context.list_parameters()
-        if context.describe_parameter(p)['parameter_type'] == 'map'
-    )
 
     # Set
     for key, item in args.set:
