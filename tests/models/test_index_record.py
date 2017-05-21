61a730d412ba22595352e1416353c64efd3dbfd0
Kale Franz <kfranz@continuum.io>
2017-05-20 23:19:56 -0500

    add channel, subdir, and fn proxy to url

--- tests/models/test_index_record.py
+++ tests/models/test_index_record.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from logging import getLogger
 from unittest import TestCase
 
+from conda.models.channel import Channel
 from conda.models.index_record import PrefixRecord
 
 log = getLogger(__name__)
@@ -23,3 +24,22 @@ class PrefixRecordTests(TestCase):
         assert pr.channel.canonical_name == 'defaults'
         assert pr.subdir == "win-32"
         assert pr.fn == "austin-1.2.3-py34_2.tar.bz2"
+        assert dict(pr.dump()) == dict(
+            name='austin',
+            version='1.2.3',
+            build='py34_2',
+            build_number=2,
+            url="https://repo.continuum.io/pkgs/free/win-32/austin-1.2.3-py34_2.tar.bz2",
+            md5='0123456789',
+            files=(),
+            channel=Channel("https://repo.continuum.io/pkgs/free/win-32/austin-1.2.3-py34_2.tar.bz2").dump(),
+            subdir="win-32",
+            fn="austin-1.2.3-py34_2.tar.bz2",
+
+            auth=None,
+            constrains=(),
+            depends=(),
+            leased_paths=None,
+            noarch=None,
+            preferred_env=None,
+        )
