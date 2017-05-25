e3798633464ab586cc736d97a49fc5ab36cdf502
Kale Franz <kfranz@continuum.io>
2017-05-25 13:55:41 -0500

    fix merge error

--- conda_env/cli/main_update.py
+++ conda_env/cli/main_update.py
@@ -3,8 +3,8 @@ import os
 import sys
 import textwrap
 
-from conda.cli import common, install as cli_install
-from conda.cli.conda_argparse import add_parser_json
+from conda.cli import install as cli_install
+from conda.cli.conda_argparse import add_parser_json, add_parser_prefix
 from conda.misc import touch_nonadmin
 from .common import get_prefix
 from .. import exceptions, specs as install_specs
@@ -33,7 +33,7 @@ def configure_parser(sub_parsers):
         help=description,
         epilog=example,
     )
-    common.add_parser_prefix(p)
+    add_parser_prefix(p)
     p.add_argument(
         '-f', '--file',
         action='store',
