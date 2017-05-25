36b440b8d7957c57977812632d68c99254888324
Kale Franz <kfranz@continuum.io>
2017-05-25 15:39:30 -0500

    suppress requests unclosed socket warning

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,9 @@
 import sys
+import warnings
+
+import pytest
+
+from conda.common.compat import PY3
 
 win_default_shells = ["cmd.exe", "powershell", "git_bash", "cygwin"]
 shells = ["bash", "zsh"]
@@ -10,6 +15,15 @@ def pytest_addoption(parser):
     parser.addoption("--shell", action="append", default=[],
         help="list of shells to run shell tests on")
 
+
 def pytest_generate_tests(metafunc):
     if 'shell' in metafunc.fixturenames:
         metafunc.parametrize("shell", metafunc.config.option.shell)
+
+
+@pytest.fixture()
+def suppress_resource_warning(request):
+    # suppress unclosed socket warning
+    # https://github.com/kennethreitz/requests/issues/1882
+    if PY3 and 'integration' in request.keywords:
+        warnings.filterwarnings("ignore", category=ResourceWarning)
