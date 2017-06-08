90da864e55bfac2bda428f0b119c255fc5960655
Kale Franz <kfranz@continuum.io>
2017-06-08 05:54:55 -0500

    clean up in-code documentation and comments

--- conda/core/solve.py
+++ conda/core/solve.py
@@ -28,9 +28,7 @@ try:
 except ImportError:
     from .._vendor.toolz.itertoolz import concat, concatv, groupby  # NOQA
 
-
 log = getLogger(__name__)
-
 
 # """
 # conda update -h
@@ -135,10 +133,6 @@ class Solver(object):
                 the solved state of the environment.
 
         """
-        # NOTE: when working with MatchSpec objects,
-        #  - to minimize the version change, set MatchSpec(name=name, target=dist.full_name)
-        #  - to freeze the package, set all the components of MatchSpec individually
-
         index, r = self._prepare()
         prune = context.prune if prune is NULL else prune
         ignore_pinned = context.ignore_pinned if ignore_pinned is NULL else ignore_pinned
@@ -147,15 +141,21 @@ class Solver(object):
         specs_to_remove = self.specs_to_remove
         specs_to_add = self.specs_to_add
 
+        # force_remove is a special case where we return early
+        if specs_to_remove and force_remove:
+            solution = tuple(Dist(rec) for rec in PrefixData(self.prefix).iter_records()
+                             if not any(spec.match(rec) for spec in specs_to_remove))
+            return IndexedSet(index[d] for d in r.dependency_sort({d.name: d for d in solution}))
+
         log.debug("solving prefix %s\n"
                   "  specs_to_remove: %s\n"
                   "  specs_to_add: %s\n"
                   "  prune: %s", self.prefix, specs_to_remove, specs_to_add, prune)
 
         # declare starting point, the initial state of the environment
+        # `solution` and `specs_map` are mutated throughout this method
         prefix_data = PrefixData(self.prefix)
         solution = tuple(Dist(d) for d in prefix_data.iter_records())
-
         if prune or deps_modifier == DepsModifier.UPDATE_ALL:
             # start with empty specs map for UPDATE_ALL because we're optimizing the update
             # only for specs the user has requested; it's ok to remove dependencies
@@ -163,88 +163,82 @@ class Solver(object):
         else:
             specs_map = {d.name: MatchSpec(d.name) for d in solution}
 
-        # get specs from history, and replace any historically-requested specs with
-        # the current specs_to_add
+        # add in historically-requested specs
         specs_from_history_map = History(self.prefix).get_requested_specs_map()
         specs_map.update(specs_from_history_map)
 
         if specs_to_remove:
-            if force_remove:
-                solution = tuple(dist for dist in solution
-                                 if not any(spec.match(dist) for spec in specs_to_remove))
-                solution = IndexedSet(r.dependency_sort({d.name: d for d in solution}))
-                return IndexedSet(index[d] for d in solution)
+            # Rather than invoking SAT for removal, we can use the DAG and simple tree traversal
+            # if we're careful about how we handle features.
+            _track_features_specs = (spec for spec in specs_to_remove if 'track_features' in spec)
+            feature_names = set(concat(spec.get_raw_value('track_features')
+                                       for spec in _track_features_specs))
+            dag = SimpleDag((index[dist] for dist in solution), itervalues(specs_map))
 
-            # If the specs we're removing involve features, then we need to invoke the SAT
-            # solver.  Otherwise, we can remove using simple tree traversal.
-            track_features_specs = tuple(spec for spec in specs_to_remove if 'track_features' in spec)
-            if track_features_specs:
-                dag = SimpleDag((index[dist] for dist in solution), itervalues(specs_map))
+            removed_records = []
+            for spec in specs_to_remove:
+                # If the spec was is track_features spec, then we need to also remove every package
+                # with a feature that matches the track_feature.  The `dag.remove_spec()` method
+                # handles that for us.
+                removed_records.extend(dag.remove_spec(spec))
 
-                removed_records = []
-                for spec in specs_to_remove:
-                    removed_records.extend(dag.remove_spec(spec))
-
-                # if the spec was a track_features spec, then we need to also remove every package
-                # with a feature that matches the track_feature
-                feature_names = set(concat(spec.get_raw_value('track_features')
-                                           for spec in track_features_specs))
-                for rec in removed_records:
-                    # We keep specs (minus the feature part) for the non track_features packages
-                    # if they're in the history specs
-                    if set(rec.features or ()) & set(feature_names) and rec.name in specs_from_history_map:
-                        spec = specs_map.get(rec.name, MatchSpec(rec.name))
-                        spec._match_components.pop('features', None)
-                        specs_map[spec.name] = spec
-                    else:
-                        specs_map.pop(rec.name, None)
-
-                solution = tuple(Dist(rec) for rec in dag.records)
-            else:
-                dag = SimpleDag((index[dist] for dist in solution), itervalues(specs_map))
-
-                removed_records = []
-                for spec in specs_to_remove:
-                    removed_records.extend(dag.remove_spec(spec))
-
-                for rec in removed_records:
+            for rec in removed_records:
+                # We keep specs (minus the feature part) for the non track_features packages
+                # if they're in the history specs.  Otherwise, we pop them from the specs_map.
+                if set(rec.features or ()) & feature_names and rec.name in specs_from_history_map:
+                    spec = specs_map.get(rec.name, MatchSpec(rec.name))
+                    spec._match_components.pop('features', None)
+                    specs_map[spec.name] = spec
+                else:
                     specs_map.pop(rec.name, None)
 
-                solution = tuple(Dist(rec) for rec in dag.records)
+            solution = tuple(Dist(rec) for rec in dag.records)
+
             if not removed_records and not prune:
                 raise PackageNotFoundError("No packages found to remove from environment.")
 
-        # keep packages matching specs from history that are causing inconsistencies
+        # We handle as best as possible environments in inconsistent states. To do this,
+        # we remove now from consideration the set of packages causing inconsistencies,
+        # and then we add them back in following the main SAT call.
         _, inconsistent_dists = r.bad_installed(solution, ())
         add_back_map = {}  # name: (dist, spec)
         if inconsistent_dists:
             for dist in inconsistent_dists:
                 # pop and save matching spec in specs_map
-                # pop and save matching dist from solution
                 add_back_map[dist.name] = (dist, specs_map.pop(dist.name, None))
             solution = tuple(dist for dist in solution if dist not in inconsistent_dists)
 
-        # for the remaining specs in specs_map, add target to each spec
-        # only if spec not in specs_to_add
+        # For the remaining specs in specs_map, add target to each spec. `target` is a reference
+        # to the package currently existing in the environment. Setting target instructs the
+        # solver to not disturb that package if it's not necessary.
+        # If the spec.name is being modified by inclusion in specs_to_add, we don't set `target`,
+        # since we *want* the solver to modify/update that package.
+        #
+        # TLDR: when working with MatchSpec objects,
+        #  - to minimize the version change, set MatchSpec(name=name, target=dist.full_name)
+        #  - to freeze the package, set all the components of MatchSpec individually
         for pkg_name, spec in iteritems(specs_map):
             matches_for_spec = tuple(rec for rec in solution if spec.match(rec))
             if matches_for_spec:
-                if len(matches_for_spec) > 1:
-                    raise NotImplementedError()  # IDontKnowWhatToDoYetError
-                else:
-                    target = Dist(matches_for_spec[0]).full_name
-                    specs_map[pkg_name] = MatchSpec(spec, target=target)
+                assert len(matches_for_spec) == 1
+                target = Dist(matches_for_spec[0]).full_name
+                specs_map[pkg_name] = MatchSpec(spec, target=target)
 
+        # If we're in UPDATE_ALL mode, we need to drop all the constraints attached to specs,
+        # so they can all float and the solver can find the most up-to-date solution. In the case
+        # of UPDATE_ALL, `specs_map` wasn't initialized with packages from the current environment,
+        # but *only* historically-requested specs.  This let's UPDATE_ALL drop dependencies if
+        # they're no longer needed, and their presence would otherwise prevent the updated solution
+        # the user most likely wants.
         if deps_modifier == DepsModifier.UPDATE_ALL:
-            specs_map = {pkg_name: MatchSpec(spec.name)
+            specs_map = {pkg_name: MatchSpec(spec.name, optional=spec.optional)
                          for pkg_name, spec in iteritems(specs_map)}
 
         # add in explicitly requested specs from specs_to_add
         # this overrides any name-matching spec already in the spec map
         specs_map.update((s.name, s) for s in specs_to_add)
 
-        # collect "optional"-type specs, which represent pinned specs and
-        # aggressive update specs
+        # collect "optional"-type specs, which represent pinned specs and aggressive update specs
         optional_specs = set()
         if not ignore_pinned:
             optional_specs.update(get_pinned_specs(self.prefix))
@@ -262,8 +256,13 @@ class Solver(object):
             track_features_specs,
         ))
 
-        # look for conflicts we can avoid by neutering specs that have a target
-        # (e.g. removing version constraint) and also making them optional
+        # We've previously checked `solution` for consistency (which at that point was the
+        # pre-solve state of the environment). Now we check our compiled set of
+        # `final_environment_specs` for the possibility of a solution.  If there are conflicts,
+        # we can often avoid them by neutering specs that have a target (e.g. removing version
+        # constraint) and also making them optional. The result here will be less cases of
+        # `UnsatisfiableError` handed to users, at the cost of more packages being modified
+        # or removed from the environment.
         conflicting_specs = r.get_conflicting_specs(final_environment_specs)
         for spec in conflicting_specs:
             if spec.target:
@@ -271,23 +270,24 @@ class Solver(object):
                 neutered_spec = MatchSpec(spec.name, target=spec.target, optional=True)
                 final_environment_specs.add(neutered_spec)
 
+        # Finally! We get to call SAT.
         log.debug("final specs to add:\n    %s\n",
                   "\n    ".join(text_type(s) for s in final_environment_specs))
         pre_solution = solution
-        solution = r.solve(final_environment_specs)
+        solution = r.solve(final_environment_specs)  # return value is List[dist]
 
-        # add back to solution inconsistent packages
+        # add back inconsistent packages to solution
         if add_back_map:
-            add_dists, add_specs = [], set()
             for name, (dist, spec) in iteritems(add_back_map):
                 if not any(d.name == name for d in solution):
-                    add_dists.append(dist)
+                    solution.append(dist)
                     if spec:
-                        add_specs.add(spec)
-            solution += add_dists
-            final_environment_specs |= add_specs
+                        final_environment_specs.add(spec)
 
+        # Special case handling for various DepsModifer flags. Maybe this block could be pulled
+        # out into its own method?
         if deps_modifier == DepsModifier.NO_DEPS:
+            # In the NO_DEPS case we're just filtering out packages from the solution.
             dont_add_packages = []
             new_packages = set(solution) - set(pre_solution)
             for record in new_packages:
@@ -295,12 +295,16 @@ class Solver(object):
                     dont_add_packages.append(record)
             solution = tuple(rec for rec in solution if rec not in dont_add_packages)
         elif deps_modifier == DepsModifier.ONLY_DEPS:
+            # Using a special instance of the DAG to remove leaf nodes that match the original
+            # specs_to_add.  It's important to only remove leaf nodes, because a typical use
+            # might be `conda install --only-deps python=2 flask`, and in that case we'd want
+            # to keep python.
             dag = SimpleDag((index[d] for d in solution), specs_to_add)
             dag.remove_leaf_nodes_with_specs()
             solution = tuple(Dist(rec) for rec in dag.records)
         elif deps_modifier in (DepsModifier.UPDATE_DEPS, DepsModifier.UPDATE_DEPS_ONLY_DEPS):
-            # we basically have to solve again, because it's only now we know the dependency
-            # chain of specs_to_add
+            # Here we have to SAT solve again :(  It's only now that we know the dependency
+            # chain of specs_to_add.
             specs_to_add_names = set(spec.name for spec in specs_to_add)
             update_names = set()
             dag = SimpleDag((index[d] for d in solution), final_environment_specs)
@@ -338,9 +342,7 @@ class Solver(object):
                   "  solved_linked_dists:\n"
                   "    %s\n",
                   self.prefix, "\n    ".join(text_type(d) for d in solution))
-        solution = IndexedSet(index[d] for d in solution)
-
-        return solution
+        return IndexedSet(index[d] for d in solution)
 
     def solve_for_diff(self, deps_modifier=None, prune=NULL, ignore_pinned=NULL,
                           force_remove=NULL, force_reinstall=NULL):
