ce4d17c7f598e5c088373c36e1faf847196f0bf3
Kale Franz <kfranz@continuum.io>
2017-06-17 23:36:56 -0500

    note about changing language in display_actions

--- tests/test_priority.py
+++ tests/test_priority.py
@@ -2,7 +2,8 @@ from unittest import TestCase
 
 import pytest
 
-from conda.base.context import context
+from conda.base.context import context, reset_context
+from conda.common.io import env_var
 from .test_create import (make_temp_env, assert_package_is_installed,
                           run_command, Commands, get_conda_list_tuple)
 
@@ -11,28 +12,40 @@ from .test_create import (make_temp_env, assert_package_is_installed,
 class PriorityIntegrationTests(TestCase):
 
     def test_channel_order_channel_priority_true(self):
-        with make_temp_env("python=3.5 pycosat==0.6.1") as prefix:
-            assert_package_is_installed(prefix, 'python')
-            assert_package_is_installed(prefix, 'pycosat')
+        with env_var("CONDA_PINNED_PACKAGES", "python=3.5", reset_context):
+            with make_temp_env("pycosat==0.6.1") as prefix:
+                assert_package_is_installed(prefix, 'python-3.5')
+                assert_package_is_installed(prefix, 'pycosat')
 
-            # add conda-forge channel
-            o, e = run_command(Commands.CONFIG, prefix, "--prepend channels conda-forge", '--json')
+                # add conda-forge channel
+                o, e = run_command(Commands.CONFIG, prefix, "--prepend channels conda-forge", '--json')
 
-            assert context.channels == ("conda-forge", "defaults"), o + e
-            # update --all
-            update_stdout, _ = run_command(Commands.UPDATE, prefix, '--all')
+                assert context.channels == ("conda-forge", "defaults"), o + e
+                # update --all
+                update_stdout, _ = run_command(Commands.UPDATE, prefix, '--all')
 
-            # pycosat should be in the SUPERSEDED list
-            superceded_split = update_stdout.split('SUPERSEDED')
-            assert len(superceded_split) == 2
-            assert 'pycosat' in superceded_split[1]
+                # this assertion works with the pinned_packages config to make sure
+                # conda update --all still respects the pinned python version
+                assert_package_is_installed(prefix, 'python-3.5')
 
-            # python sys.version should show conda-forge python
-            python_tuple = get_conda_list_tuple(prefix, "python")
-            assert python_tuple[3] == 'conda-forge'
-            # conda list should show pycosat coming from conda-forge
-            pycosat_tuple = get_conda_list_tuple(prefix, "pycosat")
-            assert pycosat_tuple[3] == 'conda-forge'
+                # pycosat should be in the SUPERSEDED list
+                # after the 4.4 solver work, looks like it's in the DOWNGRADED list
+                # This language needs changed anyway here.
+                # For packages that CHANGE because they're being moved to a higher-priority channel
+                # the message should be
+                #
+                # The following packages will be UPDATED to a higher-priority channel:
+                #
+                superceded_split = update_stdout.split('DOWNGRADED')
+                assert len(superceded_split) == 2
+                assert 'pycosat' in superceded_split[1]
+
+                # python sys.version should show conda-forge python
+                python_tuple = get_conda_list_tuple(prefix, "python")
+                assert python_tuple[3] == 'conda-forge'
+                # conda list should show pycosat coming from conda-forge
+                pycosat_tuple = get_conda_list_tuple(prefix, "pycosat")
+                assert pycosat_tuple[3] == 'conda-forge'
 
     def test_channel_priority_update(self):
         """
