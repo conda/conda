f0924aa03908942009932d5453390847aca8ccec
Kale Franz <kfranz@continuum.io>
2017-06-19 01:04:36 -0500

    add conda install filelock for conda-build

--- utils/functions.sh
+++ utils/functions.sh
@@ -280,6 +280,7 @@ install_conda_build() {
         anaconda-client numpy \
         filelock jinja2 conda-verify contextlib2 pkginfo
     conda config --remove channels conda-forge
+    conda install -y -q filelock
     if ! [ -n "$ON_WIN" ]; then
         $prefix/$BIN_DIR/conda install -y -q patchelf
     fi
