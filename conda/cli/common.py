ffbf5b89a876f9727827cb62a368eb2608499dda
Tyler Kim <tkim139@illinois.edu>
2017-06-30 16:49:25 -0500

    json encode fix

--- conda/cli/common.py
+++ conda/cli/common.py
@@ -178,7 +178,7 @@ def stdout_json(d):
     import json
     from .._vendor.auxlib.entity import EntityEncoder
     json.dump(d, sys.stdout, indent=2, sort_keys=True, cls=EntityEncoder)
-    sys.stdout.write('\n\0')
+    sys.stdout.write('\n')
 
 
 def stdout_json_success(success=True, **kwargs):
