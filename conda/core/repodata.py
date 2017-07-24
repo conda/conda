e383095333c2777f032e682be29c1cd3615ed837
jusjusjus <jschwabedal@gmail.com>
2017-07-24 15:40:51 -0400

    Caught OSError upon missing device.
    On CentOS line `closing(mmap(f.fileno(), 0, access=ACCESS_READ)) as m:`
    caused an OSError.

--- conda/core/repodata.py
+++ conda/core/repodata.py
@@ -82,10 +82,11 @@ def read_mod_and_etag(path):
                 match_objects = take(3, re.finditer(REPODATA_HEADER_RE, m))
                 result = dict(map(ensure_unicode, mo.groups()) for mo in match_objects)
                 return result
-        except (BufferError, ValueError):
+        except (BufferError, ValueError, OSError):
             # BufferError: cannot close exported pointers exist
             #   https://github.com/conda/conda/issues/4592
             # ValueError: cannot mmap an empty file
+            # OSError: [Errno 19] No such device
             return {}
 
 
