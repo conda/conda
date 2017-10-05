f186f170ce72039953d522cc5f9298cb2a8eb55f
Kale Franz <kfranz@continuum.io>
2017-10-05 12:50:30 -0500

    extend timebombs

--- tests/test_create.py
+++ tests/test_create.py
@@ -552,7 +552,7 @@ class IntegrationTests(TestCase):
             assert not exists(prefix)
 
     @pytest.mark.skipif(on_win, reason="nomkl not present on windows")
-    @pytest.mark.xfail(conda.__version__.startswith('4.3'), reason='currently broken in 4.3')
+    @pytest.mark.xfail(conda.__version__.startswith('4.3') and datetime.now() < datetime(2017, 11, 1), reason='currently broken in 4.3')
     def test_remove_features(self):
         with make_temp_env("python=2 numpy nomkl") as prefix:
             assert exists(join(prefix, PYTHON_BINARY))
@@ -701,7 +701,7 @@ class IntegrationTests(TestCase):
             assert package_is_installed(prefix, 'itsdangerous-0.23')
             assert package_is_installed(prefix, 'flask')
 
-    @pytest.mark.xfail(datetime.now() < datetime(2017, 10, 1), reason="#5263", strict=True)
+    @pytest.mark.xfail(datetime.now() < datetime(2017, 11, 1), reason="#5263", strict=True)
     def test_update_deps_flag_present(self):
         with make_temp_env("python=2 itsdangerous=0.23") as prefix:
             assert package_is_installed(prefix, 'python-2')
