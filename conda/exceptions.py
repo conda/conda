02b6384ba277f2ef7c77f3fbf1c1096db26bac4d
Kale Franz <kfranz@continuum.io>
2017-10-20 16:06:24 -0500

    fix flake8

--- conda/exceptions.py
+++ conda/exceptions.py
@@ -270,7 +270,7 @@ class ProxyError(CondaError):
     def __init__(self):
         message = dals("""
         Conda cannot proceed due to an error in your proxy configuration.
-        Check for typos and other configuration errors in any '.netrc' file in your home directory, 
+        Check for typos and other configuration errors in any '.netrc' file in your home directory,
         any environment variables ending in '_PROXY', and any other system-wide proxy
         configuration settings.
         """)
