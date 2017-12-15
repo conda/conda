3c4048b278289e8d7f81a892449b5947522283d0
Kale Franz <kfranz@continuum.io>
2017-12-15 13:01:58 -0600

    save dists during subdir_data caching

--- conda/core/repodata.py
+++ conda/core/repodata.py
@@ -53,7 +53,7 @@ except ImportError:  # pragma: no cover
 log = getLogger(__name__)
 stderrlog = getLogger('conda.stderrlog')
 
-REPODATA_PICKLE_VERSION = 17
+REPODATA_PICKLE_VERSION = 18
 REPODATA_HEADER_RE = b'"(_etag|_mod|_cache_control)":[ ]?"(.*?[^\\\\])"[,\}\s]'
 
 
@@ -157,6 +157,7 @@ class SubdirData(object):
         _internal_state = self._load()
         self._internal_state = _internal_state
         self._package_records = _internal_state['_package_records']
+        self._package_dists = _internal_state['_package_dists']  # only needed as an optimization for conda-build  # NOQA
         self._names_index = _internal_state['_names_index']
         self._track_features_index = _internal_state['_track_features_index']
         self._loaded = True
@@ -166,6 +167,11 @@ class SubdirData(object):
         if not self._loaded:
             self.load()
         return iter(self._package_records)
+
+    def iter_dists_records(self):
+        if not self._loaded:
+            self.load()
+        return zip(self._package_dists, self._package_records)
 
     def _load(self):
         try:
@@ -178,6 +184,7 @@ class SubdirData(object):
                           self.url_w_subdir, self.cache_path_json)
                 return {
                     '_package_records': (),
+                    '_package_dists': (),
                     '_names_index': defaultdict(list),
                     '_track_features_index': defaultdict(list),
                 }
@@ -307,6 +314,7 @@ class SubdirData(object):
         schannel = self.channel.canonical_name
 
         self._package_records = _package_records = []
+        self._package_dists = _package_dists = []  # creating and caching these here is an optimization for conda-build  # NOQA
         self._names_index = _names_index = defaultdict(list)
         self._track_features_index = _track_features_index = defaultdict(list)
 
@@ -317,6 +325,7 @@ class SubdirData(object):
             'cache_path_base': self.cache_path_base,
 
             '_package_records': _package_records,
+            '_package_dists': _package_dists,
             '_names_index': _names_index,
             '_track_features_index': _track_features_index,
 
@@ -347,6 +356,7 @@ class SubdirData(object):
             package_record = PackageRecord(**info)
 
             _package_records.append(package_record)
+            _package_dists.append(Dist(package_record))
             _names_index[package_record.name].append(package_record)
             for ftr_name in package_record.track_features:
                 _track_features_index[ftr_name].append(package_record)
@@ -618,7 +628,7 @@ def collect_all_repodata_as_index(use_cache, channel_urls):
     index = {}
     for url in channel_urls:
         sd = SubdirData(Channel(url))
-        index.update((Dist(prec), prec) for prec in sd.iter_records())
+        index.update(sd.iter_dists_records())
     return index
 
 
