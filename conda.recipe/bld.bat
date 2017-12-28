98d7e30f387c2cf38098140115c26c75aeeeffb8
Kale Franz <kfranz@continuum.io>
2017-12-28 14:48:59 -0600

    tested on Windows

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,6 +1,7 @@
 :: Delegate to the Unixy script. We need to translate the key path variables
 :: to be Unix-y rather than Windows-y, though.
 set "saved_recipe_dir=%RECIPE_DIR%"
+set "WIN_PREFIX=%PREFIX%"
 FOR /F "delims=" %%i IN ('cygpath.exe -u -p "%PATH%"') DO set "PATH_OVERRIDE=%%i"
 FOR /F "delims=" %%i IN ('cygpath.exe -u "%LIBRARY_PREFIX%"') DO set "LIBRARY_PREFIX=%%i"
 FOR /F "delims=" %%i IN ('cygpath.exe -u "%PREFIX%"') DO set "PREFIX=%%i"
