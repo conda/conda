313373c087dc7968216a09a198409a71e778f69f
Kale Franz <kfranz@continuum.io>
2018-01-10 13:50:26 -0600

    resolve #6700 add message for no space left on device

--- conda/exceptions.py
+++ conda/exceptions.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from datetime import timedelta
+from errno import ENOSPC
 import json
 from logging import getLogger
 import os
@@ -675,6 +676,13 @@ class EncodingError(CondaError):
         super(EncodingError, self).__init__(message, caused_by=caused_by, **kwargs)
 
 
+class NoSpaceLeftError(CondaError):
+
+    def __init__(self, caused_by, **kwargs):
+        message = "No space left on devices."
+        super(NoSpaceLeftError, self).__init__(message, caused_by=caused_by, **kwargs)
+
+
 def maybe_raise(error, context):
     if isinstance(error, CondaMultiError):
         groups = groupby(lambda e: isinstance(e, ClobberError), error.errors)
@@ -769,17 +777,19 @@ class ExceptionHandler(object):
         return_code = getattr(exc_val, 'return_code', None)
         if return_code == 0:
             return 0
-        elif isinstance(exc_val, CondaError):
+        if isinstance(exc_val, CondaError):
             return self.handle_application_exception(exc_val, exc_tb)
-        elif isinstance(exc_val, UnicodeError) and PY2:
+        if isinstance(exc_val, UnicodeError) and PY2:
             return self.handle_application_exception(EncodingError(exc_val), exc_tb)
-        elif isinstance(exc_val, KeyboardInterrupt):
+        if isinstance(exc_val, EnvironmentError):
+            if hasattr(exc_val, 'errno') and exc_val.errno == ENOSPC:
+                return self.handle_application_exception(NoSpaceLeftError(exc_val), exc_tb)
+        if isinstance(exc_val, KeyboardInterrupt):
             self._print_conda_exception(CondaError("KeyboardInterrupt"), _format_exc())
             return 1
-        elif isinstance(exc_val, SystemExit):
+        if isinstance(exc_val, SystemExit):
             return exc_val.code
-        else:
-            return self.handle_unexpected_exception(exc_val, exc_tb)
+        return self.handle_unexpected_exception(exc_val, exc_tb)
 
     def handle_application_exception(self, exc_val, exc_tb):
         self._print_conda_exception(exc_val, exc_tb)
