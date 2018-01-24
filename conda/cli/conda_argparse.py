af2f7842d6716e747a45f2c73490ba6a66ad8329
Kale Franz <kfranz@continuum.io>
2018-01-24 12:32:46 -0600

    fix #5884 allow --insecure CLI flag without contradicting meaning of ssl_verify

--- conda/cli/conda_argparse.py
+++ conda/cli/conda_argparse.py
@@ -1265,6 +1265,7 @@ def add_parser_insecure(p):
     p.add_argument(
         "-k", "--insecure",
         action="store_false",
+        dest="ssl_verify",
         default=NULL,
         help="Allow conda to perform \"insecure\" SSL connections and transfers. "
              "Equivalent to setting 'ssl_verify' to 'false'."
