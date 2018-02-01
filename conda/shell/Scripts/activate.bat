949b6e599c85cbb3cf7ad5d231305c954e0b1fa0
Kale Franz <kfranz@continuum.io>
2018-02-01 14:06:08 -0600

    code review comments

--- conda/shell/Scripts/activate.bat
+++ conda/shell/Scripts/activate.bat
@@ -1,15 +1,17 @@
-:: Test first character and last character of %1 to see if first character is a "
-::   but the last character isn't.
-:: This was a bug as described in https://github.com/ContinuumIO/menuinst/issues/60
-:: When Anaconda Prompt has the form
-::   %windir%\system32\cmd.exe "/K" "C:\Users\builder\Miniconda3\Scripts\activate.bat" "C:\Users\builder\Miniconda3"
-:: Rather than the correct
-::    %windir%\system32\cmd.exe /K ""C:\Users\builder\Miniconda3\Scripts\activate.bat" "C:\Users\builder\Miniconda3""
+@REM Test first character and last character of %1 to see if first character is a "
+@REM   but the last character isn't.
+@REM This was a bug as described in https://github.com/ContinuumIO/menuinst/issues/60
+@REM When Anaconda Prompt has the form
+@REM   %windir%\system32\cmd.exe "/K" "C:\Users\builder\Miniconda3\Scripts\activate.bat" "C:\Users\builder\Miniconda3"
+@REM Rather than the correct
+@REM    %windir%\system32\cmd.exe /K ""C:\Users\builder\Miniconda3\Scripts\activate.bat" "C:\Users\builder\Miniconda3""
+@REM this solution taken from https://stackoverflow.com/a/31359867
 @set "_c1=%1"
 @set _c1f=%_c1:~0,1%
 @set _c1l=%_c1:~-1%
 @set _c1f=%_c1f:"=+%
 @set _c1l=%_c1l:"=+%
+@set _c1=
 
 @if "%_c1f%"=="+" if NOT "%_c1l%"=="+" (
     @CALL "%~dp0..\Library\bin\conda.bat" activate
@@ -19,6 +21,5 @@
 @CALL "%~dp0..\Library\bin\conda.bat" activate %*
 
 :End
-@set _c1=
 @set _c1f=
 @set _c1l=
