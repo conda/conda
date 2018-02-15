a67856854aea3a8b3a7fdd18bacedb48ff80eaa1
Kale Franz <kfranz@continuum.io>
2018-02-15 12:00:18 -0600

    add .lgtm.yml

--- conda/core/package_cache.py
+++ conda/core/package_cache.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+# for conda-build
 from .package_cache_data import ProgressiveFetchExtract
 ProgressiveFetchExtract = ProgressiveFetchExtract
