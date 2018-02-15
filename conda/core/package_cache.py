2f2103df4d5a9812204dfd055cc5f193d68dfd45
Kale Franz <kfranz@continuum.io>
2018-02-15 11:57:09 -0600

    package_cache.py stub for conda-build

---
+++ conda/core/package_cache.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from .package_cache_data import ProgressiveFetchExtract
+ProgressiveFetchExtract = ProgressiveFetchExtract
