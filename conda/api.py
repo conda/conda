73063d51e4ba77b9d4c99e351cd798cb3a60d43c
Kale Franz <kfranz@continuum.io>
2018-02-22 15:44:14 -0600

    resolve #5610 add PrefixData, SubdirData, and PackageCacheData to conda/api.py

--- conda/api.py
+++ conda/api.py
@@ -1,5 +1,105 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from .core.solve import Solver  # NOQA
-Solver = Solver  # lgtm [py/redundant-assignment]
+from conda.models.channel import Channel
+from .common.constants import NULL
+from .core.solve import Solver as _Solver
+from .core.package_cache_data import PackageCacheData as _PackageCacheData
+from .core.subdir_data import SubdirData as _SubdirData
+from .core.prefix_data import PrefixData as _PrefixData
+
+
+class Solver(object):
+
+    def __init__(self, prefix, channels, subdirs=(), specs_to_add=(), specs_to_remove=()):
+        self._internal = _Solver(prefix, channels, subdirs, specs_to_add, specs_to_remove)
+
+    def solve_final_state(self, deps_modifier=NULL, prune=NULL, ignore_pinned=NULL,
+                          force_remove=NULL):
+        return self._internal.solve_final_state(deps_modifier, prune, ignore_pinned,
+                                                force_remove)
+
+    def solve_for_diff(self, deps_modifier=NULL, prune=NULL, ignore_pinned=NULL,
+                       force_remove=NULL, force_reinstall=False):
+        return self._internal.solve_for_diff(deps_modifier, prune, ignore_pinned,
+                                             force_remove, force_reinstall)
+
+    def solve_for_transaction(self, deps_modifier=NULL, prune=NULL, ignore_pinned=NULL,
+                              force_remove=NULL, force_reinstall=False):
+        return self._internal.solve_for_transaction(deps_modifier, prune, ignore_pinned,
+                                                    force_remove, force_reinstall)
+
+
+class SubdirData(object):
+
+    def __init__(self, channel):
+        assert isinstance(channel, Channel)
+        assert channel.subdir
+        assert not channel.package_filename
+        self._internal = _SubdirData(channel)
+
+    def query(self, package_ref_or_match_spec):
+        pass
+
+    @staticmethod
+    def query_all(channels, subdirs, package_ref_or_match_spec):
+        pass
+
+    def iter_records(self):
+        return self._internal.iter_records()
+
+    def reload(self):
+        raise NotImplementedError()
+
+
+class PackageCacheData(object):
+
+    def __init__(self, pkgs_dir):
+        self._internal = _PackageCacheData(pkgs_dir)
+
+    def get(self, package_ref, default=NULL):
+        return self._internal.get(package_ref, default)
+
+    def query(self, package_ref_or_match_spec):
+        return self._internal.query(package_ref_or_match_spec)
+
+    @staticmethod
+    def query_all(package_ref_or_match_spec, pkgs_dirs=None):
+        return _PackageCacheData.query_all(package_ref_or_match_spec, pkgs_dirs)
+
+    def iter_records(self):
+        raise NotImplementedError()
+
+    @property
+    def is_writable(self):
+        return self._internal.is_writable
+
+    @staticmethod
+    def first_writable(pkgs_dirs=None):
+        return _PackageCacheData.first_writable(pkgs_dirs)
+
+    def reload(self):
+        raise NotImplementedError()
+
+
+class PrefixData(object):
+
+    def __init__(self, prefix_path):
+        self._internal = _PrefixData(prefix_path)
+
+    def get(self, package_ref, default=NULL):
+        return self._internal.get(package_ref.name, default)
+
+    def query(self, package_ref_or_match_spec):
+        pass
+
+    def iter_records(self):
+        pass
+
+    @property
+    def is_writable(self):
+        # return self._internal.is_writable
+        raise NotImplementedError()
+
+    def reload(self):
+        raise NotImplementedError()
--- tests/test_api.py
+++ tests/test_api.py
@@ -3,16 +3,21 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import inspect
 
-from conda.api import Solver
+from conda.api import Solver, PackageCacheData, SubdirData, PrefixData
 from conda.common.compat import odict
 from conda.common.constants import NULL
+
+
+class PositionalArgument:
+    pass
 
 
 def inspect_arguments(f, arguments):
     result = inspect.getargspec(f)
     arg_names = result[0]
-    default_val_first_idx = len(arg_names) - len(result.defaults)
-    arg_values = [NULL] * default_val_first_idx + list(result.defaults)
+    defaults = result.defaults or ()
+    default_val_first_idx = len(arg_names) - len(defaults)
+    arg_values = [PositionalArgument] * default_val_first_idx + list(defaults)
     for (recorded_name, recorded_value), (arg_name, arg_value) in zip(arguments.items(), zip(arg_names, arg_values)):
         print(recorded_name, arg_name)
         assert recorded_name == arg_name
@@ -21,9 +26,9 @@ def inspect_arguments(f, arguments):
 
 def test_Solver_contract():
     init_args = odict((
-        ('self', NULL),
-        ('prefix', NULL),
-        ('channels', NULL),
+        ('self', PositionalArgument),
+        ('prefix', PositionalArgument),
+        ('channels', PositionalArgument),
         ('subdirs', ()),
         ('specs_to_add', ()),
         ('specs_to_remove', ()),
@@ -31,7 +36,7 @@ def test_Solver_contract():
     inspect_arguments(Solver.__init__, init_args)
 
     solve_final_state_args = odict((
-        ('self', NULL),
+        ('self', PositionalArgument),
         ('deps_modifier', NULL),
         ('prune', NULL),
         ('ignore_pinned', NULL),
@@ -40,7 +45,7 @@ def test_Solver_contract():
     inspect_arguments(Solver.solve_final_state, solve_final_state_args)
 
     solve_for_diff_args = odict((
-        ('self', NULL),
+        ('self', PositionalArgument),
         ('deps_modifier', NULL),
         ('prune', NULL),
         ('ignore_pinned', NULL),
@@ -50,7 +55,7 @@ def test_Solver_contract():
     inspect_arguments(Solver.solve_for_diff, solve_for_diff_args)
 
     solve_for_transaction_args = odict((
-        ('self', NULL),
+        ('self', PositionalArgument),
         ('deps_modifier', NULL),
         ('prune', NULL),
         ('ignore_pinned', NULL),
@@ -58,3 +63,111 @@ def test_Solver_contract():
         ('force_reinstall', False),
     ))
     inspect_arguments(Solver.solve_for_transaction, solve_for_transaction_args)
+
+
+def test_SubdirData_contract():
+    init_args = odict((
+        ('self', PositionalArgument),
+        ('channel', PositionalArgument),
+    ))
+    inspect_arguments(SubdirData.__init__, init_args)
+
+    query_args = odict((
+        ('self', PositionalArgument),
+        ('package_ref_or_match_spec', PositionalArgument),
+    ))
+    inspect_arguments(SubdirData.query, query_args)
+
+    query_all_args = odict((
+        ('channels', PositionalArgument),
+        ('subdirs', PositionalArgument),
+        ('package_ref_or_match_spec', PositionalArgument),
+    ))
+    inspect_arguments(SubdirData.query_all, query_all_args)
+
+    iter_records_args = odict((
+        ('self', PositionalArgument),
+    ))
+    inspect_arguments(SubdirData.iter_records, iter_records_args)
+
+    reload_args = odict((
+        ('self', PositionalArgument),
+    ))
+    inspect_arguments(SubdirData.reload, reload_args)
+
+
+def test_PackageCacheData_contract():
+    init_args = odict((
+        ('self', PositionalArgument),
+        ('pkgs_dir', PositionalArgument),
+    ))
+    inspect_arguments(PackageCacheData.__init__, init_args)
+
+    get_args = odict((
+        ('self', PositionalArgument),
+        ('package_ref', PositionalArgument),
+        ('default', NULL),
+    ))
+    inspect_arguments(PackageCacheData.get, get_args)
+
+    query_args = odict((
+        ('self', PositionalArgument),
+        ('package_ref_or_match_spec', PositionalArgument),
+    ))
+    inspect_arguments(PackageCacheData.query, query_args)
+
+    query_all_args = odict((
+        ('package_ref_or_match_spec', PositionalArgument),
+        ('pkgs_dirs', None),
+    ))
+    inspect_arguments(PackageCacheData.query_all, query_all_args)
+
+    iter_records_args = odict((
+        ('self', PositionalArgument),
+    ))
+    inspect_arguments(PackageCacheData.iter_records, iter_records_args)
+
+    isinstance(PackageCacheData.is_writable, property)
+
+    first_writable_args = odict((
+        ('pkgs_dirs', None),
+    ))
+    inspect_arguments(PackageCacheData.first_writable, first_writable_args)
+
+    reload_args = odict((
+        ('self', PositionalArgument),
+    ))
+    inspect_arguments(PackageCacheData.reload, reload_args)
+
+
+def test_PrefixData_contract():
+    init_args = odict((
+        ('self', PositionalArgument),
+        ('prefix_path', PositionalArgument),
+    ))
+    inspect_arguments(PrefixData.__init__, init_args)
+
+    get_args = odict((
+        ('self', PositionalArgument),
+        ('package_ref', PositionalArgument),
+        ('default', NULL),
+    ))
+    inspect_arguments(PrefixData.get, get_args)
+
+    query_args = odict((
+        ('self', PositionalArgument),
+        ('package_ref_or_match_spec', PositionalArgument),
+    ))
+    inspect_arguments(PrefixData.query, query_args)
+
+    iter_records_args = odict((
+        ('self', PositionalArgument),
+    ))
+    inspect_arguments(PrefixData.iter_records, iter_records_args)
+
+    isinstance(PrefixData.is_writable, property)
+
+    reload_args = odict((
+        ('self', PositionalArgument),
+    ))
+    inspect_arguments(PrefixData.reload, reload_args)
