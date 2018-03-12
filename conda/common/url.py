71d76a538d4346f42581a6ce0725777b894ab14d
Marcel Bargull <marcel.bargull@udo.edu>
2018-03-12 20:30:12 +0100

    common.url: avoid urllib.request import

--- conda/common/url.py
+++ conda/common/url.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from getpass import getpass
 from logging import getLogger
+import os
 from os.path import abspath, expanduser
 import re
 import socket
@@ -17,7 +18,13 @@ try:  # pragma: py2 no cover
     # Python 3
     from urllib.parse import (quote, quote_plus, unquote, unquote_plus,  # NOQA
                               urlunparse as stdlib_urlparse, urljoin)  # NOQA
-    from urllib.request import pathname2url  # NOQA
+    # Importing urllib.request is exceptionally slow in Python 3.
+    # Copy pathname2url's implementation directly instead:
+    if os.name == 'nt':
+        from nturl2path import pathname2url  # NOQA
+    else:
+        def pathname2url(pathname):
+            return quote(pathname)
 except ImportError:  # pragma: py3 no cover
     # Python 2
     from urllib import quote, quote_plus, unquote, unquote_plus, pathname2url  # NOQA
