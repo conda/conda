a7ee34a046ba2079788ab6a7af652e85c7961029
Kale Franz <kfranz@continuum.io>
2018-03-25 00:54:31 -0500

    back to ctypes
    Signed-off-by: Kale Franz <kfranz@continuum.io>

--- conda/gateways/disk/delete.py
+++ conda/gateways/disk/delete.py
@@ -5,13 +5,14 @@ from errno import ENOENT
 from logging import getLogger
 from os import listdir, removedirs, rename, rmdir, unlink
 from os.path import abspath, isdir, join
+import sys
 from uuid import uuid4
 
 from . import MAX_TRIES, exp_backoff_fn, mkdir_p
 from .link import islink, lexists
 from .permissions import make_writable
 from ...base.context import context
-from ...common.compat import ensure_fs_path_encoding, on_win, text_type
+from ...common.compat import ensure_fs_path_encoding, on_win, text_type, PY3
 from ...common.io import Spinner, ThreadLimitedThreadPoolExecutor
 
 try:
@@ -20,11 +21,103 @@ except ImportError:  # pragma: no cover
     from ..._vendor.toolz.itertoolz import concatv  # NOQA
 
 if on_win:
+    import ctypes
     from ctypes import FormatError
     from pywintypes import error as PyWinTypeError
     from win32api import FindFiles
     from win32file import (DeleteFileW, FILE_ATTRIBUTE_DIRECTORY, FILE_ATTRIBUTE_NORMAL,
                            GetFileAttributesW, RemoveDirectory, SetFileAttributesW)
+
+    if PY3:
+        import builtins
+    else:
+        import __builtin__ as builtins
+
+    SetFileAttributes = ctypes.windll.kernel32.SetFileAttributesW
+    SetFileAttributes.argtypes = ctypes.wintypes.LPWSTR, ctypes.wintypes.DWORD
+    SetFileAttributes.restype = ctypes.wintypes.BOOL
+
+    DeleteFile = ctypes.windll.kernel32.DeleteFileW
+    DeleteFile.argtypes = ctypes.wintypes.LPWSTR,
+    DeleteFile.restype = ctypes.wintypes.BOOL
+
+    RemoveDirectory = ctypes.windll.kernel32.RemoveDirectoryW
+    RemoveDirectory.argtypes = ctypes.wintypes.LPWSTR,
+    RemoveDirectory.restype = ctypes.wintypes.BOOL
+
+    FILE_ATTRIBUTE_NORMAL = 0x80
+
+
+    class WindowsError(builtins.WindowsError):
+        """
+        More info about errors at
+        http://msdn.microsoft.com/en-us/library/ms681381(VS.85).aspx
+        """
+
+        def __init__(self, value=None):
+            if value is None:
+                value = ctypes.windll.kernel32.GetLastError()
+            strerror = format_system_message(value)
+            if sys.version_info > (3, 3):
+                args = 0, strerror, None, value
+            else:
+                args = value, strerror
+            super(WindowsError, self).__init__(*args)
+
+        @property
+        def message(self):
+            return self.strerror
+
+        @property
+        def code(self):
+            return self.winerror
+
+        def __str__(self):
+            return self.message
+
+        def __repr__(self):
+            return '{self.__class__.__name__}({self.winerror})'.format(**vars())
+
+
+    def format_system_message(errno):
+        """
+        Call FormatMessage with a system error number to retrieve
+        the descriptive error message.
+        """
+        # first some flags used by FormatMessageW
+        ALLOCATE_BUFFER = 0x100
+        FROM_SYSTEM = 0x1000
+
+        # Let FormatMessageW allocate the buffer (we'll free it below)
+        # Also, let it know we want a system error message.
+        flags = ALLOCATE_BUFFER | FROM_SYSTEM
+        source = None
+        message_id = errno
+        language_id = 0
+        result_buffer = ctypes.wintypes.LPWSTR()
+        buffer_size = 0
+        arguments = None
+        bytes = ctypes.windll.kernel32.FormatMessageW(
+            flags,
+            source,
+            message_id,
+            language_id,
+            ctypes.byref(result_buffer),
+            buffer_size,
+            arguments,
+        )
+        # note the following will cause an infinite loop if GetLastError
+        #  repeatedly returns an error that cannot be formatted, although
+        #  this should not happen.
+        handle_nonzero_success(bytes)
+        message = result_buffer.value
+        ctypes.windll.kernel32.LocalFree(result_buffer)
+        return message
+
+
+    def handle_nonzero_success(result):
+        if result == 0:
+            raise WindowsError()
 
 
 log = getLogger(__name__)
@@ -125,8 +218,12 @@ def _win_fs_syscall(func, *args):
 def _do_unlink(path):
     if on_win:
         win_path = _make_win_path(path)
-        _win_fs_syscall(SetFileAttributesW, win_path, FILE_ATTRIBUTE_NORMAL)
-        _win_fs_syscall(DeleteFileW, win_path)
+
+        handle_nonzero_success(SetFileAttributes(win_path, FILE_ATTRIBUTE_NORMAL))
+        handle_nonzero_success(DeleteFile(win_path))
+
+        # _win_fs_syscall(SetFileAttributesW, win_path, FILE_ATTRIBUTE_NORMAL)
+        # _win_fs_syscall(DeleteFileW, win_path)
         if lexists(win_path):
             try:
                 make_writable(win_path)
@@ -150,8 +247,11 @@ def _do_unlink(path):
 def _do_rmdir(path):
     if on_win:
         win_path = _make_win_path(path)
-        _win_fs_syscall(SetFileAttributesW, win_path, FILE_ATTRIBUTE_NORMAL)
-        _win_fs_syscall(RemoveDirectory, win_path)
+        handle_nonzero_success(SetFileAttributes(win_path, FILE_ATTRIBUTE_NORMAL))
+        handle_nonzero_success(RemoveDirectory(win_path))
+
+        # _win_fs_syscall(SetFileAttributesW, win_path, FILE_ATTRIBUTE_NORMAL)
+        # _win_fs_syscall(RemoveDirectory, win_path)
     else:
         try:
             make_writable(path)
