598fef853869eccbd6bcf3b2dda1b4667498679d
Kale Franz <kfranz@continuum.io>
2018-04-20 18:25:12 -0500

    try cmd.exe macro
    Signed-off-by: Kale Franz <kfranz@continuum.io>

--- conda/shell/condacmd/conda-hook.bat
+++ conda/shell/condacmd/conda-hook.bat
@@ -1,8 +1,13 @@
 @IF DEFINED CONDA_SHLVL @GOTO :EOF
 
-@FOR %%F in ("%~dp0") do @SET __conda_exe_dir=%%~dpF
-@SET "PATH=%__conda_exe_dir%;%PATH%"
-@SET __conda_exe_dir=
+@FOR %%F in ("%~dp0") do @SET __condacmd_dir=%%~dpF
+REM @SET "PATH=%__condacmd_dir%;%PATH%"
+@FOR %%F in ("%__condacmd_dir%") do @SET __prefix=%%~dpF
+@SET CONDA_EXE="%__prefix%\Scripts\conda.exe"
+@DOSKEY conda="%__prefix%\condacmd\conda.bat"
+@SET __condacmd_dir=
+@SET __prefix=
+
 @SET CONDA_SHLVL=0
 @FOR /F "delims=" %%i IN ('@call conda config --show auto_activate_base') DO @SET "__conda_auto_activate_base=%%i"
 @IF NOT "x%__conda_auto_activate_base:True=%"=="x%__conda_auto_activate_base%" @CALL conda activate base
--- conda/shell/condacmd/conda.bat
+++ conda/shell/condacmd/conda.bat
@@ -1,1 +1,64 @@
-@CALL "%~dp0..\Library\bin\conda.bat" %*
+@IF NOT DEFINED CONDA_EXE @SET CONDA_EXE="%~dp0..\..\Scripts\conda.exe"
+
+@IF "%1"=="activate" GOTO :DO_ACTIVATE
+@IF "%1"=="deactivate" GOTO :DO_DEACTIVATE
+
+@CALL %CONDA_EXE% %*
+
+@REM This block should really be the equivalent of
+@REM   if "install" in %* GOTO :DO_REACTIVATE
+@IF "%1"=="install" GOTO :DO_REACTIVATE
+@IF "%1"=="update" GOTO :DO_REACTIVATE
+@IF "%1"=="remove" GOTO :DO_REACTIVATE
+@IF "%1"=="uninstall" GOTO :DO_REACTIVATE
+
+@GOTO :End
+
+
+:DO_ACTIVATE
+@IF "%CONDA_PS1_BACKUP%"=="" GOTO FIXUP43
+    @REM Handle transition from shell activated with conda 4.3 to a subsequent activation
+    @REM after conda updated to 4.4. See issue #6173.
+    @SET "PROMPT=%CONDA_PS1_BACKUP%"
+    @SET CONDA_PS1_BACKUP=
+:FIXUP43
+
+@FOR /F "delims=" %%i IN ('@CALL %CONDA_EXE% shell.cmd.exe activate %*') DO @SET "_TEMP_SCRIPT_PATH=%%i"
+@IF "%_TEMP_SCRIPT_PATH%"=="" GOTO :ErrorEnd
+@IF "%CONDA_PROMPT_MODIFIER%" == "" GOTO skip_prompt_set_activate
+    @CALL SET "PROMPT=%%PROMPT:%CONDA_PROMPT_MODIFIER%=%_empty_not_set_%%%"
+:skip_prompt_set_activate
+@CALL "%_TEMP_SCRIPT_PATH%"
+@DEL /F /Q "%_TEMP_SCRIPT_PATH%"
+@SET _TEMP_SCRIPT_PATH=
+@SET "PROMPT=%CONDA_PROMPT_MODIFIER%%PROMPT%"
+
+@IF DEFINED PYTHONIOENCODING chcp %PYTHONIOENCODING% > NUL
+
+@GOTO :End
+
+:DO_DEACTIVATE
+@FOR /F "delims=" %%i IN ('@CALL %CONDA_EXE% shell.cmd.exe deactivate %*') DO @SET "_TEMP_SCRIPT_PATH=%%i"
+@IF "%_TEMP_SCRIPT_PATH%"=="" GOTO :ErrorEnd
+@IF "%CONDA_PROMPT_MODIFIER%" == "" GOTO skip_prompt_set_deactivate
+    @CALL SET "PROMPT=%%PROMPT:%CONDA_PROMPT_MODIFIER%=%_empty_not_set_%%%"
+:skip_prompt_set_deactivate
+@CALL "%_TEMP_SCRIPT_PATH%"
+@DEL /F /Q "%_TEMP_SCRIPT_PATH%"
+@SET _TEMP_SCRIPT_PATH=
+@SET "PROMPT=%CONDA_PROMPT_MODIFIER%%PROMPT%"
+@GOTO :End
+
+:DO_REACTIVATE
+@FOR /F "delims=" %%i IN ('@CALL %CONDA_EXE% shell.cmd.exe reactivate') DO @SET "_TEMP_SCRIPT_PATH=%%i"
+@IF "%_TEMP_SCRIPT_PATH%"=="" GOTO :ErrorEnd
+@CALL "%_TEMP_SCRIPT_PATH%"
+@DEL /F /Q "%_TEMP_SCRIPT_PATH%"
+@SET _TEMP_SCRIPT_PATH=
+@GOTO :End
+
+:End
+@GOTO :EOF
+
+:ErrorEnd
+@EXIT /B 1
