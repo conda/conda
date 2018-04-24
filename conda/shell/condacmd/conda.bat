ff61a92b007d9f56738ed66a7d07c0b78df332bd
Kale Franz <kfranz@continuum.io>
2018-04-24 10:01:06 -0500

    cmd.exe activate debug
    Signed-off-by: Kale Franz <kfranz@continuum.io>

--- conda/shell/condacmd/conda.bat
+++ conda/shell/condacmd/conda.bat
@@ -1,10 +1,10 @@
 @IF NOT DEFINED CONDA_EXE @SET CONDA_EXE="%~dp0..\Scripts\conda.exe"
 
-@IF "%1"=="activate" conda_activate %*
-@IF "%1"=="deactivate" conda_activate %*
+IF "%1"=="activate" conda_activate %*
+IF "%1"=="deactivate" conda_activate %*
 
-@CALL %CONDA_EXE% %*
-@IF %errorlevel% NEQ 0 exit /b %errorlevel%
+CALL %CONDA_EXE% %*
+IF %errorlevel% NEQ 0 exit /b %errorlevel%
 
 @IF "%1"=="install" conda_activate reactivate
 @IF "%1"=="update" conda_activate reactivate
