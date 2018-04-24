cbe1570ad4df36a91aa6c3e44de18d5b1af83955
Kale Franz <kfranz@continuum.io>
2018-04-24 00:18:06 -0500

    conda_activate
    Signed-off-by: Kale Franz <kfranz@continuum.io>

--- conda/shell/condacmd/conda.bat
+++ conda/shell/condacmd/conda.bat
@@ -1,12 +1,12 @@
 @IF NOT DEFINED CONDA_EXE @SET CONDA_EXE="%~dp0..\Scripts\conda.exe"
 
-@IF "%1"=="activate" _conda-activate %*
-@IF "%1"=="deactivate" _conda-activate %*
+@IF "%1"=="activate" conda_activate %*
+@IF "%1"=="deactivate" conda_activate %*
 
 @CALL %CONDA_EXE% %*
 @IF %errorlevel% NEQ 0 exit /b %errorlevel%
 
-@IF "%1"=="install" _conda-activate reactivate
-@IF "%1"=="update" _conda-activate reactivate
-@IF "%1"=="remove" _conda-activate reactivate
-@IF "%1"=="uninstall" _conda-activate reactivate
+@IF "%1"=="install" conda_activate reactivate
+@IF "%1"=="update" conda_activate reactivate
+@IF "%1"=="remove" conda_activate reactivate
+@IF "%1"=="uninstall" conda_activate reactivate
