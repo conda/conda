2963cbcdae8219f1a2d8ce1b67f410a128a3a08d
Gonzalo Peña-Castellanos <goanpeca@gmail.com>
2018-06-05 16:16:45 -0500

    Reverse command removal and instead remove help

--- conda/cli/conda_argparse.py
+++ conda/cli/conda_argparse.py
@@ -285,6 +285,10 @@ def configure_parser_info(sub_parsers):
         action="store_true",
         help="List all known conda environments.",
     )
+    p.add_argument(	
+        '-l', "--license",	
+        action="store_true",	
+        help=SUPPRESS,
     p.add_argument(
         '-s', "--system",
         action="store_true",
