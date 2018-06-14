1bb23a96a095cedf80bf3960dc895d2c211445f4
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2018-06-14 09:46:12 -0500

    Refactor import logic

--- conda/common/os/__init__.py
+++ conda/common/os/__init__.py
@@ -5,32 +5,15 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from logging import getLogger
 
-from ..os.linux import linux_get_libc_version
-from ..os.unix import get_free_space_on_unix, is_admin_on_unix
-from ..os.windows import get_free_space_on_windows, is_admin_on_windows
 from ..compat import on_win
 
 
+if on_win:
+    from .windows import get_free_space_on_windows as get_free_space
+    from .windows import is_admin_on_windows as is_admin
+else:
+    from .unix import get_free_space_on_unix as get_free_space  # noqa
+    from .unix import is_admin_on_unix as is_admin  # noqa
+
+
 log = getLogger(__name__)
-
-
-linux_get_libc_version
-
-
-def is_admin():
-    func = is_admin_on_windows if on_win else is_admin_on_unix
-    return func()
-
-
-def get_free_space(dir_name):
-    """Return folder/drive free space (in bytes).
-    :param dir_name: the dir name need to check
-    :return: amount of free space
-
-    Examples:
-        >>> import os
-        >>> get_free_space(os.getcwd()) > 0
-        True
-    """
-    func = get_free_space_on_windows if on_win else get_free_space_on_unix
-    return func(dir_name)
