4d21fd79ed66a9028bec0446abbd4ec07eab8ddb
Kale Franz <kfranz@continuum.io>
2018-07-09 11:51:08 -0500

    fix #7506 requests monkeypatch fallback for old requests versions
    Signed-off-by: Kale Franz <kfranz@continuum.io>

--- conda/gateways/connection/__init__.py
+++ conda/gateways/connection/__init__.py
@@ -6,7 +6,13 @@ def should_bypass_proxies_patched(should_bypass_proxies_func, url, no_proxy):
     # Monkey patch requests, per https://github.com/requests/requests/pull/4723
     if url.startswith("file://"):
         return True
-    return should_bypass_proxies_func(url, no_proxy)
+    try:
+        return should_bypass_proxies_func(url, no_proxy)
+    except TypeError:
+        # For versions of requests we shouldn't have to deal with.
+        # https://github.com/conda/conda/issues/7503
+        # https://github.com/conda/conda/issues/7506
+        return should_bypass_proxies_func(url)
 
 
 try:
