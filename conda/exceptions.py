834d7d7c449ff7dce837e26f851760d4a89453a3
Nehal J Wani <nehaljw.kkd1@gmail.com>
2018-07-14 08:41:37 +0000

    backport NotWritableError to 4.3.x

--- conda/exceptions.py
+++ conda/exceptions.py
@@ -642,3 +642,36 @@ def conda_exception_handler(func, *args, **kwargs):
             return return_value
     except Exception as e:
         return handle_exception(e)
+
+class NotWritableError(CondaError, OSError):
+
+    def __init__(self, path, errno, **kwargs):
+        kwargs.update({
+            'path': path,
+            'errno': errno,
+        })
+        if on_win:
+            message = dals("""
+            The current user does not have write permissions to a required path.
+              path: %(path)s
+            """)
+        else:
+            message = dals("""
+            The current user does not have write permissions to a required path.
+              path: %(path)s
+              uid: %(uid)s
+              gid: %(gid)s
+
+            If you feel that permissions on this path are set incorrectly, you can manually
+            change them by executing
+
+              $ sudo chown %(uid)s:%(gid)s %(path)s
+
+            In general, it's not advisable to use 'sudo conda'.
+            """)
+            import os
+            kwargs.update({
+                'uid': os.geteuid(),
+                'gid': os.getegid(),
+            })
+        super(NotWritableError, self).__init__(message, **kwargs)
