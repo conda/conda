a4ad300635a56310e790a2a01b7e68830a61746d
Marcel Bargull <marcel.bargull@udo.edu>
2018-08-06 19:43:22 +0200

    core.index.get_reduced_index: only parallelize I/O

--- conda/core/index.py
+++ conda/core/index.py
@@ -155,92 +155,93 @@ def get_reduced_index(prefix, channels, subdirs, specs):
     #                 keep_specs.append(spec)
     #         consolidated_specs.update(keep_specs)
 
+    channel_urls = all_channel_urls(channels, subdirs=subdirs)
+    check_whitelist(channel_urls)
+
+    if context.offline:
+        grouped_urls = groupby(lambda url: url.startswith('file://'), channel_urls)
+        ignored_urls = grouped_urls.get(False, ())
+        if ignored_urls:
+            log.info("Ignoring the following channel urls because mode is offline.%s",
+                     dashlist(ignored_urls))
+        channel_urls = IndexedSet(grouped_urls.get(True, ()))
+    subdir_datas = tuple(SubdirData(Channel(url)) for url in channel_urls)
+
     with ThreadLimitedThreadPoolExecutor() as executor:
+        for _ in as_completed(executor.submit(sd.load) for sd in subdir_datas):
+            pass
 
-        channel_urls = all_channel_urls(channels, subdirs=subdirs)
-        check_whitelist(channel_urls)
+    records = IndexedSet()
+    collected_names = set()
+    collected_track_features = set()
+    pending_names = set()
+    pending_track_features = set()
 
-        if context.offline:
-            grouped_urls = groupby(lambda url: url.startswith('file://'), channel_urls)
-            ignored_urls = grouped_urls.get(False, ())
-            if ignored_urls:
-                log.info("Ignoring the following channel urls because mode is offline.%s",
-                         dashlist(ignored_urls))
-            channel_urls = IndexedSet(grouped_urls.get(True, ()))
-        subdir_datas = tuple(SubdirData(Channel(url)) for url in channel_urls)
+    def query_all(spec):
+        return tuple(concat(sd.query(spec) for sd in subdir_datas))
 
-        records = IndexedSet()
-        collected_names = set()
-        collected_track_features = set()
-        pending_names = set()
-        pending_track_features = set()
+    def push_spec(spec):
+        name = spec.get_raw_value('name')
+        if name and name not in collected_names:
+            pending_names.add(name)
+        track_features = spec.get_raw_value('track_features')
+        if track_features:
+            for ftr_name in track_features:
+                if ftr_name not in collected_track_features:
+                    pending_track_features.add(ftr_name)
 
-        def query_all(spec):
-            futures = tuple(executor.submit(sd.query, spec) for sd in subdir_datas)
-            return tuple(concat(future.result() for future in as_completed(futures)))
+    def push_record(record):
+        push_spec(MatchSpec(record.name))
+        for _spec in record.combined_depends:
+            push_spec(_spec)
+        if record.track_features:
+            for ftr_name in record.track_features:
+                push_spec(MatchSpec(track_features=ftr_name))
 
-        def push_spec(spec):
-            name = spec.get_raw_value('name')
-            if name and name not in collected_names:
-                pending_names.add(name)
-            track_features = spec.get_raw_value('track_features')
-            if track_features:
-                for ftr_name in track_features:
-                    if ftr_name not in collected_track_features:
-                        pending_track_features.add(ftr_name)
+    if prefix:
+        for prefix_rec in PrefixData(prefix).iter_records():
+            push_record(prefix_rec)
+    for spec in specs:
+        push_spec(spec)
 
-        def push_record(record):
-            push_spec(MatchSpec(record.name))
-            for _spec in record.combined_depends:
-                push_spec(_spec)
-            if record.track_features:
-                for ftr_name in record.track_features:
-                    push_spec(MatchSpec(track_features=ftr_name))
+    while pending_names or pending_track_features:
+        while pending_names:
+            name = pending_names.pop()
+            collected_names.add(name)
+            spec = MatchSpec(name)
+            new_records = query_all(spec)
+            for record in new_records:
+                push_record(record)
+            records.update(new_records)
 
-        if prefix:
-            for prefix_rec in PrefixData(prefix).iter_records():
-                push_record(prefix_rec)
-        for spec in specs:
-            push_spec(spec)
+        while pending_track_features:
+            feature_name = pending_track_features.pop()
+            collected_track_features.add(feature_name)
+            spec = MatchSpec(track_features=feature_name)
+            new_records = query_all(spec)
+            for record in new_records:
+                push_record(record)
+            records.update(new_records)
 
-        while pending_names or pending_track_features:
-            while pending_names:
-                name = pending_names.pop()
-                collected_names.add(name)
-                spec = MatchSpec(name)
-                new_records = query_all(spec)
-                for record in new_records:
-                    push_record(record)
-                records.update(new_records)
+    reduced_index = {rec: rec for rec in records}
 
-            while pending_track_features:
-                feature_name = pending_track_features.pop()
-                collected_track_features.add(feature_name)
-                spec = MatchSpec(track_features=feature_name)
-                new_records = query_all(spec)
-                for record in new_records:
-                    push_record(record)
-                records.update(new_records)
+    if prefix is not None:
+        _supplement_index_with_prefix(reduced_index, prefix)
 
-        reduced_index = {rec: rec for rec in records}
+    if context.offline or ('unknown' in context._argparse_args
+                           and context._argparse_args.unknown):
+        # This is really messed up right now.  Dates all the way back to
+        # https://github.com/conda/conda/commit/f761f65a82b739562a0d997a2570e2b8a0bdc783
+        # TODO: revisit this later
+        _supplement_index_with_cache(reduced_index)
 
-        if prefix is not None:
-            _supplement_index_with_prefix(reduced_index, prefix)
+    # add feature records for the solver
+    known_features = set()
+    for rec in itervalues(reduced_index):
+        known_features.update(concatv(rec.track_features, rec.features))
+    known_features.update(context.track_features)
+    for ftr_str in known_features:
+        rec = make_feature_record(ftr_str)
+        reduced_index[rec] = rec
 
-        if context.offline or ('unknown' in context._argparse_args
-                               and context._argparse_args.unknown):
-            # This is really messed up right now.  Dates all the way back to
-            # https://github.com/conda/conda/commit/f761f65a82b739562a0d997a2570e2b8a0bdc783
-            # TODO: revisit this later
-            _supplement_index_with_cache(reduced_index)
-
-        # add feature records for the solver
-        known_features = set()
-        for rec in itervalues(reduced_index):
-            known_features.update(concatv(rec.track_features, rec.features))
-        known_features.update(context.track_features)
-        for ftr_str in known_features:
-            rec = make_feature_record(ftr_str)
-            reduced_index[rec] = rec
-
-        return reduced_index
+    return reduced_index
