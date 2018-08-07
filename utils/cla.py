9fcd171c22fee7472edfce114bada31cd22396ae
Kale Franz <kfranz@continuum.io>
2018-08-07 03:32:36 -0500

    flake8
    Signed-off-by: Kale Franz <kfranz@continuum.io>

--- utils/cla.py
+++ utils/cla.py
@@ -26,13 +26,11 @@ with open('.github-map') as fh:
         (line.split("|") for line in fh.read().strip().split('\n') if '|' in line)
     )
 
-
 # git log --format="%aN <%aE>" | awk '{ ++c[$0]; } END { for(cc in c) printf "%5d %s\n",c[cc],cc; }' | sort -r
 output = check_output('git log --format="%aN <%aE>" | awk \'{ ++c[$0]; } END { for(cc in c) printf "%5d %s\\n",c[cc],cc; }\' | sort -r', shell=True)
 output = output.decode("utf-8").strip().split('\n')
 contributions = tuple((git_id, commits) for commits, git_id in (line.strip().split(' ', 1) for line in output) if git_id in missing_signatures)
 contributions_map = dict((git_id, int(commits)) for commits, git_id in (line.strip().split(' ', 1) for line in output) if git_id in missing_signatures)
-
 
 missing_signatures_stats = {}
 for git_id in missing_signatures:
@@ -56,14 +54,11 @@ for git_id in sorted(missing_signatures_stats, key=lambda x: (missing_signatures
     stats = missing_signatures_stats[git_id]
     print("%-18s%-63s%4s%4s%4s" % (github_map[git_id], git_id, stats[0], stats[1], stats[2]))
 
-
-
 print()
 print("missing signatures", len(missing_signatures_stats))
 print("total commits", sum(x[0] for x in missing_signatures_stats.values()))
 print("total additions", sum(x[1] for x in missing_signatures_stats.values()))
 print()
-
 
 for git_id in sorted(missing_signatures_stats, key=lambda x: (missing_signatures_stats[x][0], missing_signatures_stats[x][1]), reverse=True):
     print("\n===============================================================")
