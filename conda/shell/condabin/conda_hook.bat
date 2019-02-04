21fd25a2925bf0d401d46124384d32142aeb8ae9
Murray Read <read@ccdc.cam.ac.uk>
2019-02-04 16:23:31 +0000

    CONDA_EXE and CONDA_BAT quoting changed so that they are quoted on use rather than on set. Tested by installing miniconda 4.5 in a directory with spaces on windows, then updating conda. At this point the problems show themselves. Replace the contents of condabin with these modified files and conda works again.

--- conda/shell/condabin/conda_hook.bat
+++ conda/shell/condabin/conda_hook.bat
@@ -7,10 +7,10 @@
 
 @FOR %%F in ("%~dp0") do @SET __condabin_dir=%%~dpF
 @SET "PATH=%__condabin_dir%;%PATH%"
-@SET CONDA_BAT="%__condabin_dir%conda.bat"
+@SET "CONDA_BAT=%__condabin_dir%conda.bat"
 @SET __condabin_dir=%__condabin_dir:~0,-1%
 @FOR %%F in ("%__condabin_dir%") do @SET __conda_root=%%~dpF
-@SET CONDA_EXE="%__conda_root%Scripts\conda.exe"
+@SET "CONDA_EXE=%__conda_root%Scripts\conda.exe"
 @SET __condabin_dir=
 @SET __conda_root=
 
