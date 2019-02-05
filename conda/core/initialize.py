47e45197745f23f1393fb07a4be2b6542a4dfff6
Nehal J Wani <nehaljw.kkd1@gmail.com>
2019-02-05 16:14:02 +0000

    [powershell] make sure \path\to\conda.exe is always quoted
    Fixes #8221

--- conda/core/initialize.py
+++ conda/core/initialize.py
@@ -1246,7 +1246,7 @@ def _powershell_profile_content(conda_prefix):
     conda_powershell_module = dals("""
     #region conda initialize
     # !! Contents within this block are managed by 'conda init' !!
-    (& {conda_exe} "shell.powershell" "hook") | Out-String | Invoke-Expression
+    (& "{conda_exe}" "shell.powershell" "hook") | Out-String | Invoke-Expression
     #endregion
     """.format(conda_exe=conda_exe))
 
