7e91eb57fda3b0f0d6b15e96c6f785a7ee237cb7
Nehal J Wani <nehaljw.kkd1@gmail.com>
2019-02-05 19:17:21 +0000

    Never assume CONDA_EXE to be a string containing quotes
    Fixes #8224

--- conda/shell/condabin/conda.bat
+++ conda/shell/condabin/conda.bat
@@ -1,11 +1,11 @@
 @REM Copyright (C) 2012 Anaconda, Inc
 @REM SPDX-License-Identifier: BSD-3-Clause
-@IF NOT DEFINED CONDA_EXE @SET CONDA_EXE="%~dp0..\Scripts\conda.exe"
+@IF NOT DEFINED CONDA_EXE @SET "CONDA_EXE=%~dp0..\Scripts\conda.exe"
 
 @IF [%1]==[activate]   "%~dp0_conda_activate" %*
 @IF [%1]==[deactivate] "%~dp0_conda_activate" %*
 
-@CALL %CONDA_EXE% %*
+@CALL "%CONDA_EXE%" %*
 @IF %errorlevel% NEQ 0 EXIT /B %errorlevel%
 
 @IF [%1]==[install]   "%~dp0_conda_activate" reactivate
