224655ede530fd28fa444015cb319e1a8b7e486b
Jonathan J. Helmus <jjhelmus@gmail.com>
2019-02-06 17:23:22 -0600

    fixup escape, add .bat suffix

--- conda/core/link.py
+++ conda/core/link.py
@@ -1002,9 +1002,9 @@ def run_script(prefix, prec, action='post-link', env_prefix=None, activate=False
             return False
         if activate:
             conda_bat = env.get("CONDA_BAT", abspath(join(context.root_prefix, 'bin', 'conda')))
-            with tempfile.NamedTemporaryFile(mode='w', prefix=prefix, delete=False) as fh:
-                fh.write('@CALL {0} activate \"{1}\""\n'.format(conda_bat, prefix))
-                fh.write('@CALL "\{0}\"\n'.format(path))
+            with tempfile.NamedTemporaryFile(mode='w', prefix=prefix, suffix='.bat', delete=False) as fh:
+                fh.write('@CALL \"{0}\" activate \"{1}\"\n'.format(conda_bat, prefix))
+                fh.write('@CALL \"{0}\"\n'.format(path))
                 script_caller = fh.name
             command_args = [comspec, '/d', '/c', script_caller]
         else:
