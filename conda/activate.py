c224db25433829fe15a6f8a4f3264965d59dc27a
Morten Enemark Lund <melund@gmail.com>
2019-02-09 13:43:21 +0100

    Make xonsh CONDA_EXE a raw string
    This fixes problem with hook preamble inserted into the xonsh activation script. On windows paths contain backslashes which must be escaped.

--- conda/activate.py
+++ conda/activate.py
@@ -708,7 +708,7 @@ class XonshActivator(_Activator):
         super(XonshActivator, self).__init__(arguments)
 
     def _hook_preamble(self):
-        return 'CONDA_EXE = "%s"' % context.conda_exe
+        return 'CONDA_EXE = r"%s"' % context.conda_exe
 
 
 class CmdExeActivator(_Activator):
