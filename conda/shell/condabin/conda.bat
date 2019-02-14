42c86513bfd9aa845881a7e62d72df1de1cc5a51
Ray Donnelly <mingw.android@gmail.com>
2019-02-14 20:20:25 +0100

    Set the necessary PATH entries in conda.bat

--- conda/shell/condabin/conda.bat
+++ conda/shell/condabin/conda.bat
@@ -5,7 +5,11 @@
 @IF [%1]==[activate]   "%~dp0_conda_activate" %*
 @IF [%1]==[deactivate] "%~dp0_conda_activate" %*
 
+@setlocal
+@for %%B in (%~dp0.) do @set PATH=%%~dpB;%%~dpBLibrary\mingw-w64\bin;%%~dpBLibrary\usr\bin;%%~dpBLibrary\bin;%%~dpBScripts;%%~dpBbin;%PATH%
 @CALL "%CONDA_EXE%" %*
+@endlocal
+
 @IF %errorlevel% NEQ 0 EXIT /B %errorlevel%
 
 @IF [%1]==[install]   "%~dp0_conda_activate" reactivate
