3acb47b8bc863a983a5d6304c63990e89682c59f
Mike Sarahan <msarahan@gmail.com>
2019-02-19 16:16:40 -0600

    windows dev setup: add path spec to startup script commands

--- dev/start.bat
+++ dev/start.bat
@@ -15,10 +15,10 @@
 @powershell.exe -NoProfile -Command (new-object System.Net.WebClient).DownloadFile('https://repo.continuum.io/miniconda/Miniconda%pyver%-latest-Windows-x86_64.exe','miniconda.exe')
 @ECHO Installing miniconda to: %devenv%
 @start /wait "" miniconda.exe /InstallationType=JustMe /RegisterPython=0 /AddToPath=0 /S /D=%CD%\%devenv%
-@ECHO ^>^> conda update -yq --all
-@CALL "%devenv%\Scripts\conda" update -yq --all
-@ECHO ^>^> conda install -yq --file dev/test-requirements.txt -c defaults -c conda-forge
-@CALL "%devenv%\Scripts\conda" install -yq --file dev/test-requirements.txt -c defaults -c conda-forge
+@ECHO ^>^> conda update -p %CD%\%devenv% -yq --all
+@CALL "%devenv%\Scripts\conda" update -p %CD%\%devenv% -yq --all
+@ECHO ^>^> conda install -yq -p %CD%\%devenv% --file dev/test-requirements.txt -c defaults -c conda-forge
+@CALL "%devenv%\Scripts\conda" install -p %CD%\%devenv% -yq --file dev/test-requirements.txt -c defaults -c conda-forge
 
 :ALREADY_INSTALLED
 @CALL "%devenv%\python" -m conda init --dev cmd.exe > NUL
