8b6ff4e9e54d58fafaa39a9a2804abe97e23bb81
Michael Sarahan <msarahan@gmail.com>
2019-02-21 14:29:45 -0600

    fix conda run behavior when no prefix arg is specified

--- conda/cli/main_run.py
+++ conda/cli/main_run.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from logging import getLogger
+import os
 from subprocess import PIPE, Popen
 import sys
 
@@ -14,8 +15,8 @@ def execute(args, parser):
     on_win = sys.platform == "win32"
 
     call = " ".join(args.executable_call)
-    script_caller, command_args = wrap_subprocess_call(on_win, context.root_prefix,
-                                                       args.prefix, call)
+    prefix = args.prefix or os.getenv("CONDA_PREFIX") or context.root_prefix
+    script_caller, command_args = wrap_subprocess_call(on_win, context.root_prefix, prefix, call)
     process = Popen(command_args, universal_newlines=True, stdout=PIPE, stderr=PIPE)
     for line in process.stdout:
         sys.stdout.write(line)
