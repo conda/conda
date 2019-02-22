15413fd7e14d52d873742d86613bf19d7befa567
Ray Donnelly <mingw.android@gmail.com>
2019-02-22 20:30:07 +0100

    More quietly

--- conda/shell/condabin/conda.bat
+++ conda/shell/condabin/conda.bat
@@ -5,12 +5,12 @@ IF NOT DEFINED CONDA_EXE @SET "CONDA_EXE=%~dp0..\Scripts\conda.exe"
 IF [%1]==[activate]   "%~dp0_conda_activate" %*
 IF [%1]==[deactivate] "%~dp0_conda_activate" %*
 
-setlocal
-for %%A in ("%~dp0\.") do set _sysp=%%~dpA
-SET _sysp=%_sysp:~0,-1%
-for %%B in (%~dp0.) do @set PATH=%_sysp%;%_sysp%\Library\mingw-w64\bin;%_sysp%\Library\usr\bin;%_sysp%\Library\bin;%_sysp%\Scripts;%_sysp%\bin;%PATH%
+@SETLOCAL
+@FOR %%A IN ("%~dp0\.") DO @SET _sysp=%%~dpA
+@SET _sysp=%_sysp:~0,-1%
+FOR %%B in (%~dp0.) DO @SET PATH=%_sysp%;%_sysp%\Library\mingw-w64\bin;%_sysp%\Library\usr\bin;%_sysp%\Library\bin;%_sysp%\Scripts;%_sysp%\bin;%PATH%
 CALL "%CONDA_EXE%" %*
-endlocal
+@ENDLOCAL
 
 @IF %errorlevel% NEQ 0 EXIT /B %errorlevel%
 
