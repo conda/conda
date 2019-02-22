6aa6b36efb80f5d96338305d9c5a6a70ff32c1d8
Ray Donnelly <mingw.android@gmail.com>
2019-02-22 23:09:03 +0100

    More quietly part 2

--- conda/shell/condabin/conda.bat
+++ conda/shell/condabin/conda.bat
@@ -1,14 +1,14 @@
-REM Copyright (C) 2012 Anaconda, Inc
-REM SPDX-License-Identifier: BSD-3-Clause
-IF NOT DEFINED CONDA_EXE @SET "CONDA_EXE=%~dp0..\Scripts\conda.exe"
+@REM Copyright (C) 2012 Anaconda, Inc
+@REM SPDX-License-Identifier: BSD-3-Clause
+@IF NOT DEFINED CONDA_EXE @SET "CONDA_EXE=%~dp0..\Scripts\conda.exe"
 
-IF [%1]==[activate]   "%~dp0_conda_activate" %*
-IF [%1]==[deactivate] "%~dp0_conda_activate" %*
+@IF [%1]==[activate]   "%~dp0_conda_activate" %*
+@IF [%1]==[deactivate] "%~dp0_conda_activate" %*
 
 @SETLOCAL
 @FOR %%A IN ("%~dp0\.") DO @SET _sysp=%%~dpA
 @SET _sysp=%_sysp:~0,-1%
-FOR %%B in (%~dp0.) DO @SET PATH=%_sysp%;%_sysp%\Library\mingw-w64\bin;%_sysp%\Library\usr\bin;%_sysp%\Library\bin;%_sysp%\Scripts;%_sysp%\bin;%PATH%
+@FOR %%B in (%~dp0.) DO @SET PATH=%_sysp%;%_sysp%\Library\mingw-w64\bin;%_sysp%\Library\usr\bin;%_sysp%\Library\bin;%_sysp%\Scripts;%_sysp%\bin;%PATH%
 CALL "%CONDA_EXE%" %*
 @ENDLOCAL
 
