155dbf5085148e074b939c8dd3bab21404d37bbf
Mike Sarahan <msarahan@gmail.com>
2019-02-22 17:32:19 -0600

    always set CONDA_EXE relative to calling conda.bat

--- conda/shell/condabin/conda.bat
+++ conda/shell/condabin/conda.bat
@@ -1,6 +1,6 @@
 @REM Copyright (C) 2012 Anaconda, Inc
 @REM SPDX-License-Identifier: BSD-3-Clause
-@IF NOT DEFINED CONDA_EXE @SET "CONDA_EXE=%~dp0..\Scripts\conda.exe"
+@SET "CONDA_EXE=%~dp0..\Scripts\conda.exe"
 
 @IF [%1]==[activate]   "%~dp0_conda_activate" %*
 @IF [%1]==[deactivate] "%~dp0_conda_activate" %*
