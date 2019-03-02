22dcbad2b947890610f4df59d1d1db37f1c077e7
Ray Donnelly <mingw.android@gmail.com>
2019-03-02 18:55:25 +0000

    Unset some problematic env vars in dev\start.bat

--- conda/shell/condabin/conda.bat
+++ conda/shell/condabin/conda.bat
@@ -13,7 +13,7 @@
 @FOR %%A IN ("%~dp0\.") DO @SET _sysp=%%~dpA
 @SET _sysp=%_sysp:~0,-1%
 @FOR %%B in (%~dp0.) DO @SET PATH=%_sysp%;%_sysp%\Library\mingw-w64\bin;%_sysp%\Library\usr\bin;%_sysp%\Library\bin;%_sysp%\Scripts;%_sysp%\bin;%PATH%
-CALL "%CONDA_EXE%" %*
+@CALL "%CONDA_EXE%" %*
 @ENDLOCAL
 
 @IF %errorlevel% NEQ 0 EXIT /B %errorlevel%
