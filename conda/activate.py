a18410275512f376d21de08de42d0ee59d792d61
Ray Donnelly <mingw.android@gmail.com>
2019-03-04 15:54:13 +0000

    Stop using dirsep of / on Windows + PowerShell
    We should assume that 3rd party software may naively handle
    only \, for example if were to process PATH.

--- conda/activate.py
+++ conda/activate.py
@@ -765,7 +765,7 @@ class PowerShellActivator(_Activator):
 
     def __init__(self, arguments=None):
         self.pathsep_join = ';'.join if on_win else ':'.join
-        self.sep = '/'  # Even on Windows, PowerShell can handle Unix-style separators.
+        self.sep = '\\'
         self.path_conversion = path_identity
         self.script_extension = '.ps1'
         self.tempfile_extension = None  # write instructions to stdout rather than a temp file
