8361c6e3c03b8b003f6c7088746d401342b70dfc
Sophia Castellarin <scastellarin@anaconda.com>
2019-03-06 14:26:08 -0600

    Move packages to conda-test channel

--- tests/test_link_order.py
+++ tests/test_link_order.py
@@ -19,32 +19,19 @@ except ImportError:
 
 log = getLogger(__name__)
 
-def build_package(package_path):
-    output = api.build(package_path, need_source_download=False)
-    return output
-
 class TestLinkOrder(unittest.TestCase):
     def setUp(self):
-        thisdir = os.path.dirname(os.path.realpath(__file__))
-        self.recipe_dir = os.path.join(thisdir, 'test-recipes')
         self.prefix = tempfile.mkdtemp()
 
     def tearDown(self):
         shutil.rmtree(self.prefix)
 
-
     @pytest.mark.integration
     def test_link_order_post_link_actions(self):
-        recipes = tuple(["b_post_link_package", "c_post_link_package"])
-        for recipe in recipes:
-            build_package(os.path.join(self.recipe_dir, recipe))
-        stdout, stderr = run_command(Commands.CREATE, self.prefix, "c_post_link_package --use-local")
+        stdout, stderr = run_command(Commands.CREATE, self.prefix, "c_post_link_package -c conda-test")
         assert(stderr == '')
 
     @pytest.mark.integration
     def test_link_order_post_link_depend(self):
-        recipes = tuple(["d_post_link_package", "e_post_link_package"])
-        for recipe in recipes:
-            build_package(os.path.join(self.recipe_dir, recipe))
-        stdout, stderr = run_command(Commands.CREATE, self.prefix, "e_post_link_package --use-local")
+        stdout, stderr = run_command(Commands.CREATE, self.prefix, "e_post_link_package -c conda-test")
         assert(stderr == '')
