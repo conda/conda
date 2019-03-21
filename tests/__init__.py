6e3cdbcd36c987a04d58ffa5f6185777e2546a0f
Ray Donnelly <mingw.android@gmail.com>
2019-03-21 16:22:09 +0100

    tests (init): Ensure that if CONDA_PREFIX is set, its at front of PATH
    Otherwise the wrong programs (e.g. system ones) will get run in some
    situations, run_command(Commands.RUN, "python") being a particularly
    fun one.

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,3 +2,29 @@
 # imports work.
 from conda.gateways.logging import initialize_logging
 initialize_logging()
+
+# Attempt to move any conda entries in PATH to the front of it.
+# IDEs have their own ideas about how PATH should be managed and
+# they do dumb stuff like add /usr/bin to the front of it
+# meaning conda takes a submissve role and the wrong stuff
+# runs (when other conda prefixes get activated they replace
+# the wrongly placed entries with newer wrongly placed entries).
+#
+# Note, there's still condabin to worry about here, and also should
+# we not remove all traces of conda instead of just this fixup?
+# Ideally we'd have two modes, 'removed' and 'fixed'. I have seen
+# condabin come from an entirely different installation than
+# CONDA_PREFIX too in some instances and that really needs fixing.
+
+import os
+if 'CONDA_PREFIX' in os.environ:
+    from conda.activate import (PosixActivator, CmdExeActivator)
+    activator = PosixActivator()
+    # But why not just use _replace_prefix_in_path? => because moving
+    # the entries to the front of PATH is the goal here, not swapping
+    # x for x (which would be pointless anyway).
+    p = activator._remove_prefix_from_path(os.environ['CONDA_PREFIX'])
+    os.environ['PATH'] = os.pathsep.join(p)
+    activator = PosixActivator()
+    p = activator._add_prefix_to_path(os.environ['CONDA_PREFIX'])
+    os.environ['PATH'] = os.pathsep.join(p)
