5f8be5e701cf285aeba7880b027ecdf855dd5bdf
Jonathan Helmus <jjhelmus@gmail.com>
2019-04-09 16:36:23 -0500

    add PATH management to shell script

--- conda/shell/etc/profile.d/conda.sh
+++ conda/shell/etc/profile.d/conda.sh
@@ -1,6 +1,29 @@
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
 
+__add_sys_prefix_to_path() {
+    # In dev-mode CONDA_EXE is python.exe and on Windows
+    # it is in a different relative location to condabin.
+    if [ -n "${_CE_CONDA+x}" ] && [ -n "${WINDIR+x}" ]; then
+        SYSP=$(\dirname "${CONDA_EXE}")
+    else
+        SYSP=$(\dirname "${CONDA_EXE}")
+        SYSP=$(\dirname "${SYSP}")
+    fi
+
+    if [ -n "${WINDIR+x}" ]; then
+        PATH="${SYSP}/bin:${PATH}"
+        PATH="${SYSP}/Library/Scripts:${PATH}"
+        PATH="${SYSP}/Library/bin:${PATH}"
+        PATH="${SYSP}/Library/usr/bin:${PATH}"
+        PATH="${SYSP}/Library/mingw-w64/bin:${PATH}"
+        PATH="${SYSP}:${PATH}"
+    else
+        PATH="${SYSP}/bin:${PATH}"
+    fi
+    \export PATH
+}
+
 __conda_hashr() {
     if [ -n "${ZSH_VERSION:+x}" ]; then
         \rehash
@@ -22,7 +45,10 @@ __conda_activate() {
     \local cmd="$1"
     shift
     \local ask_conda
+    OLDPATH="${PATH}"
+    __add_sys_prefix_to_path
     ask_conda="$(PS1="$PS1" "$CONDA_EXE" $_CE_M $_CE_CONDA shell.posix "$cmd" "$@")" || \return $?
+    PATH="${OLDPATH}"
     \eval "$ask_conda"
     __conda_hashr
 }
