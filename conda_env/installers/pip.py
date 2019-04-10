810dd45a0aba2ee96a3afe5830b83abe1a672ec2
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2019-04-10 15:50:29 -0400

    Add upgrade flag to pip installer
    The "-U" flag ensures that pip-managed packages are always upgraded (within the requirements) when "conda env update" is used.
    
    Adding the flag here causes it to always be used, even for "conda env create", but in that case the "-U" has not effect because the package is not already installed.

--- conda_env/installers/pip.py
+++ conda_env/installers/pip.py
@@ -42,7 +42,7 @@ def _pip_install_via_requirements(prefix, specs, args, *_, **kwargs):
         requirements.write('\n'.join(specs))
         requirements.close()
         # pip command line...
-        pip_cmd = ['install', '-r', requirements.name]
+        pip_cmd = ['install', '-U', '-r', requirements.name]
         pip_subprocess(pip_cmd, prefix, cwd=pip_workdir)
     finally:
         # Win/Appveyor does not like it if we use context manager + delete=True.
