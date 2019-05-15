5c891fe15b8520de412b42b20e913406e0f795c2
Michael Sarahan <msarahan@gmail.com>
2019-05-15 13:56:52 -0500

    use our TemporaryDirectory impl always

--- conda/gateways/disk/create.py
+++ conda/gateways/disk/create.py
@@ -31,51 +31,51 @@ from ...common.serialize import json_dump
 from ...exceptions import BasicClobberError, CondaOSError, maybe_raise
 from ...models.enums import FileMode, LinkType
 
-try:
-    from tempfile import TemporaryDirectory
-except ImportError:
-    class TemporaryDirectory(object):
-        """Create and return a temporary directory.  This has the same
-        behavior as mkdtemp but can be used as a context manager.  For
-        example:
 
-            with TemporaryDirectory() as tmpdir:
-                ...
+# we have our own TemporaryDirectory implementation both for historical reasons and because
+#     using our rm_rf function is more robust than the shutil equivalent
+class TemporaryDirectory(object):
+    """Create and return a temporary directory.  This has the same
+    behavior as mkdtemp but can be used as a context manager.  For
+    example:
 
-        Upon exiting the context, the directory and everything contained
-        in it are removed.
-        """
+        with TemporaryDirectory() as tmpdir:
+            ...
 
-        # Handle mkdtemp raising an exception
-        name = None
-        _closed = False
+    Upon exiting the context, the directory and everything contained
+    in it are removed.
+    """
 
-        def __init__(self, suffix="", prefix='tmp', dir=None):
-            self.name = tempfile.mkdtemp(suffix, prefix, dir)
+    # Handle mkdtemp raising an exception
+    name = None
+    _closed = False
 
-        def __repr__(self):
-            return "<{} {!r}>".format(self.__class__.__name__, self.name)
+    def __init__(self, suffix="", prefix='tmp', dir=None):
+        self.name = tempfile.mkdtemp(suffix, prefix, dir)
 
-        def __enter__(self):
-            return self.name
+    def __repr__(self):
+        return "<{} {!r}>".format(self.__class__.__name__, self.name)
 
-        def cleanup(self, _warn=False, _warnings=_warnings):
-            from .delete import rm_rf as _rm_rf
-            if self.name and not self._closed:
-                try:
-                    _rm_rf(self.name)
-                except (TypeError, AttributeError) as ex:
-                    if "None" not in '%s' % (ex,):
-                        raise
-                    _rm_rf(self.name)
-                self._closed = True
+    def __enter__(self):
+        return self.name
 
-        def __exit__(self, exc, value, tb):
-            self.cleanup()
+    def cleanup(self, _warn=False, _warnings=_warnings):
+        from .delete import rm_rf as _rm_rf
+        if self.name and not self._closed:
+            try:
+                _rm_rf(self.name)
+            except (TypeError, AttributeError) as ex:
+                if "None" not in '%s' % (ex,):
+                    raise
+                _rm_rf(self.name)
+            self._closed = True
 
-        def __del__(self):
-            # Issue a ResourceWarning if implicit cleanup needed
-            self.cleanup(_warn=True)
+    def __exit__(self, exc, value, tb):
+        self.cleanup()
+
+    def __del__(self):
+        # Issue a ResourceWarning if implicit cleanup needed
+        self.cleanup(_warn=True)
 
 
 log = getLogger(__name__)
