396974fb0b73f99487992bd8497cb85f0a055b69
Rami Chowdhury <rami.chowdhury@gmail.com>
2019-05-27 13:20:20 -0400

    Add some (failing) tests
    Basically a copy of the XonshActivator tests, something to start from

--- tests/test_activate.py
+++ tests/test_activate.py
@@ -16,7 +16,8 @@ from conda import CONDA_PACKAGE_ROOT
 from conda._vendor.auxlib.ish import dals
 from conda._vendor.toolz.itertoolz import concatv
 from conda.activate import CmdExeActivator, CshActivator, FishActivator, PosixActivator, \
-    PowerShellActivator, XonshActivator, activator_map, main as activate_main, native_path_to_unix
+    PowerShellActivator, XonshActivator, JSONActivator, activator_map, \
+    main as activate_main, native_path_to_unix
 from conda.base.constants import ROOT_ENV_NAME, PREFIX_STATE_FILE, PACKAGE_ENV_VARS_DIR, \
     CONDA_ENV_VARS_UNSET_VAR
 from conda.base.context import context, conda_tests_ctxt_mgmt_def_pol
@@ -1669,6 +1670,85 @@ class ShellWrapperUnitTests(TestCase):
                 with open(join(td, "stdout"), "wt") as stdout:
                     with captured(stdout=stdout) as c:
                         rc = activate_main(['', shell] + activate_args + [self.prefix])
+
+    def test_json_basic(self):
+        activator = JSONActivator()
+
+        with captured() as c:
+            rc = activate_main(['', 'shell.__json__'] + activate_args + [self.prefix])
+        assert not c.stderr
+        assert rc == 0
+        activate_data = c.stdout
+
+        new_path_parts = activator._add_prefix_to_path(self.prefix)
+        conda_exe_export, conda_exe_unset = activator.get_scripts_export_unset_vars()
+        e_activate_data = dals("""
+        $PATH = '%(new_path)s'
+        $CONDA_PREFIX = '%(native_prefix)s'
+        $CONDA_SHLVL = '1'
+        $CONDA_DEFAULT_ENV = '%(native_prefix)s'
+        $CONDA_PROMPT_MODIFIER = '(%(native_prefix)s) '
+        %(conda_exe_export)s
+        source "%(activate1)s"
+        """) % {
+            'converted_prefix': activator.path_conversion(self.prefix),
+            'native_prefix': self.prefix,
+            'new_path': activator.pathsep_join(new_path_parts),
+            'sys_executable': activator.path_conversion(sys.executable),
+            'activate1': activator.path_conversion(join(self.prefix, 'etc', 'conda', 'activate.d', 'activate1.xsh')),
+            'conda_exe_export': conda_exe_export,
+        }
+        assert activate_data == e_activate_data
+
+        with env_vars({
+            'CONDA_PREFIX': self.prefix,
+            'CONDA_SHLVL': '1',
+            'PATH': os.pathsep.join(concatv(new_path_parts, (os.environ['PATH'],))),
+        }):
+            activator = JSONActivator()
+            with captured() as c:
+                rc = activate_main(['', 'shell.__json__'] + reactivate_args)
+            assert not c.stderr
+            assert rc == 0
+            reactivate_data = c.stdout
+
+            new_path_parts = activator._replace_prefix_in_path(self.prefix, self.prefix)
+            e_reactivate_data = dals("""
+            source "%(deactivate1)s"
+            $PATH = '%(new_path)s'
+            $CONDA_SHLVL = '1'
+            $CONDA_PROMPT_MODIFIER = '(%(native_prefix)s) '
+            source "%(activate1)s"
+            """) % {
+                'new_path': activator.pathsep_join(new_path_parts),
+                'activate1': activator.path_conversion(join(self.prefix, 'etc', 'conda', 'activate.d', 'activate1.xsh')),
+                'deactivate1': activator.path_conversion(join(self.prefix, 'etc', 'conda', 'deactivate.d', 'deactivate1.xsh')),
+                'native_prefix': self.prefix,
+            }
+            assert reactivate_data == e_reactivate_data
+
+            with captured() as c:
+                rc = activate_main(['', 'shell.xonsh'] + deactivate_args)
+            assert not c.stderr
+            assert rc == 0
+            deactivate_data = c.stdout
+
+            new_path = activator.pathsep_join(activator._remove_prefix_from_path(self.prefix))
+            conda_exe_export, conda_exe_unset = activator.get_scripts_export_unset_vars()
+            e_deactivate_data = dals("""
+            $PATH = '%(new_path)s'
+            source "%(deactivate1)s"
+            del $CONDA_PREFIX
+            del $CONDA_DEFAULT_ENV
+            del $CONDA_PROMPT_MODIFIER
+            $CONDA_SHLVL = '0'
+            %(conda_exe_export)s
+            """) % {
+                'new_path': new_path,
+                'deactivate1': activator.path_conversion(join(self.prefix, 'etc', 'conda', 'deactivate.d', 'deactivate1.xsh')),
+                'conda_exe_export': conda_exe_export,
+            }
+            assert deactivate_data == e_deactivate_data
 
 
 class InteractiveShell(object):
