7520a8bc1c5f7ba1694fea9c2bd416390fc94e10
Michael Sarahan <msarahan@gmail.com>
2019-05-29 21:32:21 -0500

    add more debugging output for py27 win subprocess failing test

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -14,8 +14,13 @@ def test_conda_subprocess():
     from subprocess import Popen, PIPE
     import conda
 
-    p = Popen(['echo', '"%s"' % conda.__version__], env=os.environ, stdout=PIPE, stderr=PIPE, 
-              shell=on_win)
+    try:
+        p = Popen(['echo', '"%s"' % conda.__version__], env=os.environ, stdout=PIPE, stderr=PIPE,
+                  shell=on_win)
+    except TypeError:
+        for k, v in os.environ.items():
+            print("%s (%s): %s (%s)" % (k, type(k), v, type(v)))
+        raise
     stdout, stderr = p.communicate()
     rc = p.returncode
     if rc != 0:
