b434bcbb5290f7db874974d7cbd70346476d4c14
Johannes Köster <johannes.koester@tu-dortmund.de>
2019-06-13 20:45:17 +0200

    missing parentheses

--- conda/cli/main_lock.py
+++ conda/cli/main_lock.py
@@ -4,7 +4,7 @@ import os
 from flufl.lock import Lock
 
 def execute(args, parser):
-    lock = Lock(os.path.join(gettempdir(), "conda", "locks", args.lockid)
+    lock = Lock(os.path.join(gettempdir(), "conda", "locks", args.lockid))
     # Set PID to the parent process, in order to allow lock and unlock to be
     # called as separate commands.
     lock._set_claimfile(os.getppid())
