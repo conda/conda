fa84b9a9d8aabaa1131b4407ab1f3eba44fba9c0
Johannes Köster <johannes.koester@tu-dortmund.de>
2019-06-14 08:59:39 +0200

    Document lifetime meaning.

--- conda/cli/main_lock.py
+++ conda/cli/main_lock.py
@@ -4,6 +4,15 @@ import os
 
 from flufl.lock import Lock
 
+# From the docs:
+# Locks have a lifetime, which is the maximum length of time the process expects
+# to retain the lock. It is important to pick a good number here because other
+# processes will not break an existing lock until the expected lifetime has
+# expired. Too long and other processes will hang; too short and you’ll end up
+# trampling on existing process locks – and possibly corrupting data. In a
+# distributed (NFS) environment, you also need to make sure that your clocks are
+# properly synchronized.
+#
 # TODO find out what a good lifetime is
 LIFETIME = 30
 
