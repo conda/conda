1d3fb3c870bdafb06d9a6b31ecbb668e4243647d
Johannes Köster <johannes.koester@tu-dortmund.de>
2019-06-14 08:57:39 +0200

    set default lifetime

--- conda/cli/main_lock.py
+++ conda/cli/main_lock.py
@@ -1,10 +1,15 @@
 from tempfile import gettempdir
+from datetime import timedelta
 import os
 
 from flufl.lock import Lock
 
+# TODO find out what a good lifetime is
+LIFETIME = 30
+
 def execute(args, parser):
     lock = Lock(os.path.join(gettempdir(), "conda", "locks", args.lockid))
+    lock.lifetime = timedelta(seconds=LIFETIME)
     # Set PID to the parent process, in order to allow lock and unlock to be
     # called as separate commands.
     lock._set_claimfile(os.getppid())
