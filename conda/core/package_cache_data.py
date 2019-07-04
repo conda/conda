c909d2ec6b4dc35b9b075413c5c93764bc96ec1e
mcopes73 <mcopes73@gmail.com>
2019-07-04 14:37:46 +0100

    indent

--- conda/core/package_cache_data.py
+++ conda/core/package_cache_data.py
@@ -524,13 +524,13 @@ class ProgressiveFetchExtract(object):
         if pcrec_from_writable_cache and pcrec_matches(pcrec_from_writable_cache) and pcrec_from_writable_cache.get('url'):
             # extract in place
             extract_axn = ExtractPackageAction(
-              source_full_path=pcrec_from_writable_cache.package_tarball_full_path,
-              target_pkgs_dir=dirname(pcrec_from_writable_cache.package_tarball_full_path),
-              target_extracted_dirname=basename(pcrec_from_writable_cache.extracted_package_dir),
-              record_or_spec=pcrec_from_writable_cache,
-              sha256=pcrec_from_writable_cache.sha256 or sha256,
-              size=pcrec_from_writable_cache.size or size,
-              md5=pcrec_from_writable_cache.md5 or md5,
+                source_full_path=pcrec_from_writable_cache.package_tarball_full_path,
+                target_pkgs_dir=dirname(pcrec_from_writable_cache.package_tarball_full_path),
+                target_extracted_dirname=basename(pcrec_from_writable_cache.extracted_package_dir),
+                record_or_spec=pcrec_from_writable_cache,
+                sha256=pcrec_from_writable_cache.sha256 or sha256,
+                size=pcrec_from_writable_cache.size or size,
+                md5=pcrec_from_writable_cache.md5 or md5,
             )
             return None, extract_axn
 
