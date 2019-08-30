5ae858fb5040628f7a62618404b85b84a26e547b
Kahlil Hodgson <unknown@email.com>
2019-08-30 11:30:29 +1000

    Bugfix paths in bash activation script

--- conda/shell/etc/conda_bash.sh
+++ conda/shell/etc/conda_bash.sh
@@ -1,5 +1,5 @@
 # Load the POSIX setup
-. $_CONDA_ROOT/etc/profile.d/conda.sh
+. $_CONDA_ROOT/shell/etc/profile.d/conda.sh
 
 # Check for interactive bash and that we haven't already been sourced.
 if [ -n "${BASH_VERSION-}" -a -n "${PS1-}" -a -z "${BASH_COMPLETION_VERSINFO-}" ]; then
@@ -8,6 +8,6 @@ if [ -n "${BASH_VERSION-}" -a -n "${PS1-}" -a -z "${BASH_COMPLETION_VERSINFO-}" 
     if [ ${BASH_VERSINFO[0]} -gt 4 ] || \
        [ ${BASH_VERSINFO[0]} -eq 4 -a ${BASH_VERSINFO[1]} -ge 1 ]; then
 
-        . $_CONDA_ROOT/etc/bash_completion.d/conda
+        . $_CONDA_ROOT/shell/etc/bash_completion.d/conda
     fi
 fi
