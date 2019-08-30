d74bfce2ec5e5cedf4c54d7770bdbdda1ce9914a
Kahlil Hodgson <unknown@email.com>
2019-08-30 11:38:56 +1000

    Fix bash completion loading guard

--- conda/shell/etc/conda_bash.sh
+++ conda/shell/etc/conda_bash.sh
@@ -1,8 +1,8 @@
 # Load the POSIX setup
 . $_CONDA_ROOT/shell/etc/profile.d/conda.sh
 
-# Check for interactive bash and that we haven't already been sourced.
-if [ -n "${BASH_VERSION-}" -a -n "${PS1-}" -a -z "${BASH_COMPLETION_VERSINFO-}" ]; then
+# Check for interactive bash with bash_completion setup
+if [ -n "${BASH_VERSION-}" -a -n "${PS1-}" -a -n "${BASH_COMPLETION_VERSINFO-}" ]; then
 
     # Check for recent enough version of bash.
     if [ ${BASH_VERSINFO[0]} -gt 4 ] || \
