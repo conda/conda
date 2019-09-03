e7b4237ea68e517a8252a9f254f2db3e35b381cf
Sophia Castellarin <scastellarin@anaconda.com>
2019-09-03 10:29:17 -0400

    Make env vars ordered

--- conda/activate.py
+++ conda/activate.py
@@ -669,7 +669,7 @@ class _Activator(object):
         )), reverse=True))
 
     def _get_environment_env_vars(self, prefix):
-        env_vars = {}
+        env_vars = OrderedDict()
         env_vars_file = join(prefix, 'conda-meta', 'env_vars')
         if exists(env_vars_file):
             with open(env_vars_file, 'r') as f:
