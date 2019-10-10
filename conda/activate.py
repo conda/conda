faa66a76cc0248f72c2267e6bea1b6d9f7ca9a0a
Anthony Scopatz <scopatz@gmail.com>
2019-10-10 11:10:26 -0400

    dollar fix

--- conda/activate.py
+++ conda/activate.py
@@ -954,7 +954,7 @@ class XonshActivator(_Activator):
         super(XonshActivator, self).__init__(arguments)
 
     def _hook_preamble(self):
-        return 'CONDA_EXE = "%s"' % self.path_conversion(context.conda_exe)
+        return '$CONDA_EXE = "%s"' % self.path_conversion(context.conda_exe)
 
 
 class CmdExeActivator(_Activator):
