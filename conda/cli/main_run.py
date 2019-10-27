b39b1a6efa453842173cc4846ab84853fde21489
Giles Cope <gilescope@gmail.com>
2019-10-27 21:24:47 +0000

    Performance improvements for conda run - avoiding Popen.communicate().

--- conda/cli/main_run.py
+++ conda/cli/main_run.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from logging import getLogger
 import os
-from subprocess import PIPE, Popen
+from subprocess import Popen
 import sys
 
 from ..base.context import context
@@ -22,21 +22,13 @@ def execute(args, parser):
     script_caller, command_args = wrap_subprocess_call(on_win, context.root_prefix, prefix,
                                                        args.dev, args.debug_wrapper_scripts, call)
     env = encode_environment(os.environ.copy())
-    process = Popen(command_args, universal_newlines=False, stdout=PIPE, stderr=PIPE,
+    process = Popen(command_args, universal_newlines=False,
                     env=env, cwd=cwd)
-    stdout, stderr = process.communicate()
-    if hasattr(stdout, "decode"):
-        stdout = stdout.decode('utf-8', errors='replace')
-    if hasattr(stderr, "decode"):
-        stderr = stderr.decode('utf-8', errors='replace')
-    if stdout:
-        sys.stdout.write(stdout)
-    if stderr:
-        sys.stderr.write(stderr)
+    process.wait()
     if process.returncode != 0:
         log = getLogger(__name__)
-        log.error("Subprocess for 'conda run {}' command failed.  Stderr was:\n{}"
-                  .format(call, stderr))
+        log.error("Subprocess for 'conda run {}' command failed.  (See above for error)"
+                  .format(call))
     if script_caller is not None:
         if 'CONDA_TEST_SAVE_TEMPS' not in os.environ:
             rm_rf(script_caller)
