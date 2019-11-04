f2919973790ad6ee94a583e5982fd5f8c9e8834a
Kahlil Hodgson <tartansandal@users.noreply.github.com>
2019-11-04 18:21:13 +1100

    Ensure conda installed bash completions are loaded.

--- conda/shell/etc/conda_bash.sh
+++ conda/shell/etc/conda_bash.sh
@@ -10,13 +10,28 @@ fi
 # Load the POSIX setup
 . $CONDA_ETC/profile.d/conda.sh
 
-# Check for interactive bash with bash_completion setup
+# Load bash-completion library if installed in the base environment by conda.
+# (Note: This is a no-op if the system level library has already been loaded).
+if [ -r $CONDA_ETC/profile.d/bash_completion.sh -o true ]; then
+    . $CONDA_ETC/profile.d/bash_completion.sh
+
+    # Ensure conda share dir is in XDG_DATA_DIRS so completions can get dynamically
+    # loaded
+    _conda_share_dir=$(\dirname "${CONDA_ETC}")/share
+    if ! expr match ":${XDG_DATA_DIRS-}:" ".*:$_conda_share_dir:.*" >/dev/null; then
+        XDG_DATA_DIRS=${XDG_DATA_DIRS-}:$_conda_share_dir
+    fi
+    unset _conda_share_dir
+fi
+
+# Check for interactive bash with the bash-completion library loaded
 if [ -n "${BASH_VERSION-}" -a -n "${PS1-}" -a -n "${BASH_COMPLETION_VERSINFO-}" ]; then
 
     # Check for recent enough version of bash.
     if [ ${BASH_VERSINFO[0]} -gt 4 ] || \
        [ ${BASH_VERSINFO[0]} -eq 4 -a ${BASH_VERSINFO[1]} -ge 1 ]; then
-
-        . $CONDA_ETC/bash_completion.d/conda
+        if shopt -q progcomp; then
+            . $CONDA_ETC/bash_completion.d/conda
+        fi
     fi
 fi
