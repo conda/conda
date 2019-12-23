4a313f7bcb9bc0746a36b90913bc69f46ff72157
Sophia Castellarin <scastellarin@anaconda.com>
2019-12-23 13:41:57 -0600

    Conda env create to create an empty env if no specs specified
    addresses: https://github.com/conda/conda/issues/9487

--- conda_env/cli/main_create.py
+++ conda_env/cli/main_create.py
@@ -99,21 +99,27 @@ def execute(args, parser):
     # channel_urls = args.channel or ()
 
     result = {"conda": None, "pip": None}
-    for installer_type, pkg_specs in env.dependencies.items():
-        try:
-            installer = get_installer(installer_type)
-            result[installer_type] = installer.install(prefix, pkg_specs, args, env)
-        except InvalidInstaller:
-            sys.stderr.write(textwrap.dedent("""
-                Unable to install package for {0}.
-
-                Please double check and ensure your dependencies file has
-                the correct spelling.  You might also try installing the
-                conda-env-{0} package to see if provides the required
-                installer.
-                """).lstrip().format(installer_type)
-            )
-            return -1
+    if len(env.dependencies.items()) == 0:
+        installer_type = "conda"
+        pkg_specs = []
+        installer = get_installer(installer_type)
+        result[installer_type] = installer.install(prefix, pkg_specs, args, env)
+    else:
+        for installer_type, pkg_specs in env.dependencies.items():
+            try:
+                installer = get_installer(installer_type)
+                result[installer_type] = installer.install(prefix, pkg_specs, args, env)
+            except InvalidInstaller:
+                sys.stderr.write(textwrap.dedent("""
+                    Unable to install package for {0}.
+    
+                    Please double check and ensure your dependencies file has
+                    the correct spelling.  You might also try installing the
+                    conda-env-{0} package to see if provides the required
+                    installer.
+                    """).lstrip().format(installer_type)
+                )
+                return -1
 
     touch_nonadmin(prefix)
     print_result(args, prefix, result)
