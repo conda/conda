31bc146cc8ff59d7ed724a46b3bd769fd35add37
Sophia Castellarin <scastellarin@anaconda.com>
2019-12-23 13:48:02 -0600

    Add test for creating empty env

--- conda_env/cli/main_create.py
+++ conda_env/cli/main_create.py
@@ -112,7 +112,7 @@ def execute(args, parser):
             except InvalidInstaller:
                 sys.stderr.write(textwrap.dedent("""
                     Unable to install package for {0}.
-    
+
                     Please double check and ensure your dependencies file has
                     the correct spelling.  You might also try installing the
                     conda-env-{0} package to see if provides the required
