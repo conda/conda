5dc04f2ef9b49bc46f0d868fb90b035b6c97a3fa
Niklas Holm <niklas.holm.271@gmail.com>
2021-01-15 10:05:30 +0100

    Fix profile script PATH setup on MSYS2
    PosixActivator#_hook_preamble sets _CE_CONDA to the empty string rather
    than unsetting it, so [ -n "${_CE_CONDA+x}" ] will always return true,
    resulting in the wrong branch being executed and PATH set incorrectly.
    
    This commit changes '+x' to ':+x' to only return true if _CE_CONDA is
    set and non-empty.
    
    Partially fixes #8693
    
    Signed-off-by: Niklas Holm <niklas.holm.271@gmail.com>

--- conda/shell/etc/profile.d/conda.sh
+++ conda/shell/etc/profile.d/conda.sh
@@ -105,7 +105,7 @@ if [ -z "${CONDA_SHLVL+x}" ]; then
     \export CONDA_SHLVL=0
     # In dev-mode CONDA_EXE is python.exe and on Windows
     # it is in a different relative location to condabin.
-    if [ -n "${_CE_CONDA+x}" ] && [ -n "${WINDIR+x}" ]; then
+    if [ -n "${_CE_CONDA:+x}" ] && [ -n "${WINDIR+x}" ]; then
         PATH="$(\dirname "$CONDA_EXE")/condabin${PATH:+":${PATH}"}"
     else
         PATH="$(\dirname "$(\dirname "$CONDA_EXE")")/condabin${PATH:+":${PATH}"}"
