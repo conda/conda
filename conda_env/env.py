9035949f471026cc0ae2a56eb0a30c91929d1df3
Cheng H. Lee <clee@anaconda.com>
2021-03-24 12:09:09 -0500

    Minor improvements to non-defaults pip PR
    - Split using "::" as the channel/package name separator.
    - Use `isinstance` to check for dep specification being dict, rather
      than relying on the presence of the `keys` attribute.

--- conda_env/env.py
+++ conda_env/env.py
@@ -53,8 +53,8 @@ def validate_keys(data, kwargs):
 
     deps = data.get('dependencies', [])
     depsplit = re.compile(r"[<>~\s=]")
-    is_pip = lambda dep: 'pip' in depsplit.split(dep)[0].split(':')
-    lists_pip = any(is_pip(_) for _ in deps if not hasattr(_, 'keys'))
+    is_pip = lambda dep: 'pip' in depsplit.split(dep)[0].split('::')
+    lists_pip = any(is_pip(_) for _ in deps if not isinstance(_, dict))
     for dep in deps:
         if (isinstance(dep, dict) and 'pip' in dep and not lists_pip):
             print("Warning: you have pip-installed dependencies in your environment file, "
