c24fea7bfcae10568cdd78bb7bf46f01ec63be58
Daniel Bast <2790401+dbast@users.noreply.github.com>
2021-08-11 17:22:14 +0200

    Set pytest parameters centrally in setup.cfg

--- ci/integration_tests.sh
+++ ci/integration_tests.sh
@@ -5,5 +5,5 @@ set -o errtrace -o pipefail -o errexit
 eval "$(sudo /opt/conda/bin/python -m conda init --dev bash)"
 sudo su root -c "/opt/conda/bin/conda install -yq conda-build"
 conda-build tests/test-recipes/activate_deactivate_package
-pytest $ADD_COV --cov-append -m "integration and not installed" -v
+pytest -m "integration and not installed" -v
 python -m conda.common.io
