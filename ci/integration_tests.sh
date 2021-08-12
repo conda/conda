4baee532f02874225b7a7f73972d4591f4bdbf58
Daniel Bast <2790401+dbast@users.noreply.github.com>
2021-08-12 20:17:27 +0200

    Use pytest-test-groups for more parallelisation

--- ci/integration_tests.sh
+++ ci/integration_tests.sh
@@ -2,8 +2,12 @@
 
 set -o errtrace -o pipefail -o errexit
 
+GROUP_COUNT="${GROUP_COUNT:-1}"
+TEST_GROUP="${TEST_GROUP:-1}"
+
 eval "$(sudo /opt/conda/bin/python -m conda init --dev bash)"
 sudo su root -c "/opt/conda/bin/conda install -yq conda-build"
+sudo su root -c "/opt/conda/bin/conda run --name base pip install pytest-test-groups"
 conda-build tests/test-recipes/activate_deactivate_package
-pytest -m "integration and not installed" -v
+pytest -m "integration and not installed" -v --test-group-count ${GROUP_COUNT} --test-group=${TEST_GROUP} --test-group-random-seed=31415
 python -m conda.common.io
