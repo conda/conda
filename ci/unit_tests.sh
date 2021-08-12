1c7564873bf3bb15c4b4d7f5d9baf5980cb6bd4c
Daniel Bast <2790401+dbast@users.noreply.github.com>
2021-08-12 20:17:27 +0200

    Use pytest-test-groups for more parallelisation

--- ci/unit_tests.sh
+++ ci/unit_tests.sh
@@ -2,8 +2,12 @@
 
 set -o errtrace -o pipefail -o errexit
 
+GROUP_COUNT="${GROUP_COUNT:-1}"
+TEST_GROUP="${TEST_GROUP:-1}"
+
 eval "$(sudo /opt/conda/bin/python -m conda init --dev bash)"
 conda info
+sudo su root -c "/opt/conda/bin/conda run --name base pip install pytest-test-groups"
 # remove the pkg cache.  We can't hardlink from here anyway.  Having it around causes log problems.
 sudo rm -rf /opt/conda/pkgs/*-*-*
-pytest -m "not integration and not installed" -v
+pytest -m "not integration and not installed" -v --test-group-count ${GROUP_COUNT} --test-group=${TEST_GROUP} --test-group-random-seed=31415
