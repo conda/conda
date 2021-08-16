bb10a956e52d7ac77a953fe4ee0254a6da077a3d
Jannis Leidel <jannis@leidel.info>
2021-08-16 22:35:05 +0200

    Move "CI" files into tests directory for later use in local dev environment.

---
+++ tests/integration_tests.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -o errtrace -o pipefail -o errexit
+
+GROUP_COUNT="${GROUP_COUNT:-1}"
+TEST_GROUP="${TEST_GROUP:-1}"
+
+eval "$(sudo /opt/conda/bin/python -m conda init --dev bash)"
+sudo su root -c "/opt/conda/bin/conda install -yq conda-build conda-forge::pytest-split"
+conda-build tests/test-recipes/activate_deactivate_package
+pytest -m "integration and not installed" -v --splits ${GROUP_COUNT} --group=${TEST_GROUP}
+python -m conda.common.io
