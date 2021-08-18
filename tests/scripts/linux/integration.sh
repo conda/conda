47e9526f4eeadfecfa6e109e6ee11114e45357a8
Jannis Leidel <jannis@leidel.info>
2021-08-18 23:18:13 +0200

    Eval dev setup

--- tests/scripts/linux/integration.sh
+++ tests/scripts/linux/integration.sh
@@ -5,6 +5,8 @@ set -o errtrace -o pipefail -o errexit
 TEST_SPLITS="${TEST_SPLITS:-1}"
 TEST_GROUP="${TEST_GROUP:-1}"
 
+eval "$(sudo /usr/share/miniconda/bin/python -m conda init --dev bash)"
+sudo su root -c "/usr/share/miniconda/bin/conda install -yq conda-build"
 conda-build tests/test-recipes/activate_deactivate_package
 pytest -m "integration and not installed" -v --splits ${TEST_SPLITS} --group=${TEST_GROUP}
 python -m conda.common.io
--- tests/scripts/linux/unit.sh
+++ tests/scripts/linux/unit.sh
@@ -5,5 +5,6 @@ set -o errtrace -o pipefail -o errexit
 TEST_SPLITS="${TEST_SPLITS:-1}"
 TEST_GROUP="${TEST_GROUP:-1}"
 
+eval "$(sudo /usr/share/miniconda/bin/python -m conda init --dev bash)"
 conda info
 pytest -m "not integration and not installed" -vv --splits ${TEST_SPLITS} --group=${TEST_GROUP}
