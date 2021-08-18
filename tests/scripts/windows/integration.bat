ba1c1a2a73c5b6d98e9e3847d0948c9e6e5fd5b1
Jannis Leidel <jannis@leidel.info>
2021-08-18 22:03:02 +0200

    Less links

--- tests/scripts/windows/integration.bat
+++ tests/scripts/windows/integration.bat
@@ -1,6 +1,6 @@
 @ECHO ON
 pushd %TEMP%
-cd \conda_src
+cd %GITHUB_WORKSPACE%
 CALL dev-init.bat
 CALL conda-build tests\test-recipes\activate_deactivate_package
 CALL pytest -m "integration and not installed" --basetemp=C:\tmp -v --splits=%GROUP_COUNT% --group=%TEST_GROUP%
