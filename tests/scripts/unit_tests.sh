cf78bfc2ffda65022dc4048e51d6b141a2d4c5a1
Jannis Leidel <jannis@leidel.info>
2021-08-18 20:23:02 +0200

    Install pytest-split in base env.

--- tests/scripts/unit_tests.sh
+++ tests/scripts/unit_tests.sh
@@ -7,7 +7,6 @@ TEST_GROUP="${TEST_GROUP:-1}"
 
 eval "$(sudo /opt/conda/bin/python -m conda init --dev bash)"
 conda info
-sudo su root -c "/opt/conda/bin/conda install -yq conda-forge::pytest-split"
 # remove the pkg cache.  We can't hardlink from here anyway.  Having it around causes log problems.
 sudo rm -rf /opt/conda/pkgs/*-*-*
 pytest -m "not integration and not installed" -vv --splits ${GROUP_COUNT} --group=${TEST_GROUP}
