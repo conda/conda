3fe6b5be66a038b42dc30d35586d1c27ff595bbd
Jannis Leidel <jannis@leidel.info>
2021-08-18 18:43:11 +0200

    Install pytest-split

--- tests/scripts/setup.bat
+++ tests/scripts/setup.bat
@@ -7,7 +7,7 @@ mklink /J \conda_src %GITHUB_WORKSPACE%
 
 cd \conda_src
 CALL \conda_bin\scripts\activate.bat
-CALL conda create -n ci_base -y python=%PYTHON% pycosat conda requests ruamel_yaml pytest pytest-cov pytest-timeout mock responses urllib3 pexpect pywin32 anaconda-client conda-package-handling %SCANDIR%
+CALL conda create -n ci_base -y python=%PYTHON% pycosat conda requests ruamel_yaml pytest pytest-cov pytest-timeout mock responses urllib3 pexpect pywin32 anaconda-client conda-package-handling conda-forge::pytest-split %SCANDIR%
 CALL conda activate ci_base
 CALL conda install -yq pip conda-build conda-verify
 CALL conda update openssl ca-certificates certifi
