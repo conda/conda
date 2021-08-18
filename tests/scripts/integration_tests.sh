d7c0c426864f78384f68e6a64450c44bf8ede04a
Jannis Leidel <jannis@leidel.info>
2021-08-18 20:23:02 +0200

    Install pytest-split in base env.

--- tests/scripts/integration_tests.sh
+++ tests/scripts/integration_tests.sh
@@ -6,7 +6,7 @@ GROUP_COUNT="${GROUP_COUNT:-1}"
 TEST_GROUP="${TEST_GROUP:-1}"
 
 eval "$(sudo /opt/conda/bin/python -m conda init --dev bash)"
-sudo su root -c "/opt/conda/bin/conda install -yq conda-build conda-forge::pytest-split"
+sudo su root -c "/opt/conda/bin/conda install -yq conda-build"
 conda-build tests/test-recipes/activate_deactivate_package
 pytest -m "integration and not installed" -v --splits ${GROUP_COUNT} --group=${TEST_GROUP}
 python -m conda.common.io
