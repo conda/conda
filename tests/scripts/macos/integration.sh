9c0b5f356415cc8b8f76bca175ef2c661b5b859a
Jannis Leidel <jannis@leidel.info>
2021-08-19 13:28:04 +0200

    Don't use test groups on macos
    It doesn't make sense to use them on macos due to the small number of runners there.

--- tests/scripts/macos/integration.sh
+++ tests/scripts/macos/integration.sh
@@ -2,9 +2,6 @@
 
 set -o errtrace -o pipefail -o errexit
 
-TEST_SPLITS="${TEST_SPLITS:-1}"
-TEST_GROUP="${TEST_GROUP:-1}"
-
 conda-build tests/test-recipes/activate_deactivate_package
-pytest -m "integration" -v --splits ${TEST_SPLITS} --group=${TEST_GROUP}
+pytest -m "integration" -v
 python -m conda.common.io
--- tests/scripts/macos/unit.sh
+++ tests/scripts/macos/unit.sh
@@ -2,8 +2,5 @@
 
 set -o errtrace -o pipefail -o errexit
 
-TEST_SPLITS="${TEST_SPLITS:-1}"
-TEST_GROUP="${TEST_GROUP:-1}"
-
 conda info
-pytest -m "not integration" -vv --splits ${TEST_SPLITS} --group=${TEST_GROUP}
+pytest -m "not integration" -vv
