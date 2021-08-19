858e8ac67a1431bf35ccd5fd6606f324e9fa2868
Jannis Leidel <jannis@leidel.info>
2021-08-19 12:39:57 +0200

    Try without compiling.

--- tests/scripts/macos/integration.sh
+++ tests/scripts/macos/integration.sh
@@ -5,7 +5,6 @@ set -o errtrace -o pipefail -o errexit
 TEST_SPLITS="${TEST_SPLITS:-1}"
 TEST_GROUP="${TEST_GROUP:-1}"
 
-sudo "${CONDA}/bin/conda install -yq conda-build"
 conda-build tests/test-recipes/activate_deactivate_package
 pytest -m "integration" -v --splits ${TEST_SPLITS} --group=${TEST_GROUP}
 python -m conda.common.io
