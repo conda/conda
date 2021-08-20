1f75917ea8a909b4c8fbce283abbbc2b7ef61f20
Jannis Leidel <jannis@leidel.info>
2021-08-20 13:43:42 +0200

    Call devenv's init --dev for macos.

--- ci/scripts/macos/integration.sh
+++ ci/scripts/macos/integration.sh
@@ -2,6 +2,7 @@
 
 set -o errtrace -o pipefail -o errexit
 
+eval "$(sudo ./devenv/bin/python -m conda init --dev bash)"
 conda-build tests/test-recipes/activate_deactivate_package
 pytest -m "integration" -v
 python -m conda.common.io
--- ci/scripts/macos/unit.sh
+++ ci/scripts/macos/unit.sh
@@ -2,5 +2,6 @@
 
 set -o errtrace -o pipefail -o errexit
 
+eval "$(sudo ./devenv/bin/python -m conda init --dev bash)"
 conda info
 pytest -m "not integration" -v
