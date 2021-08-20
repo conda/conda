7a3dc35ae9fec4a743dee738e12cfa22321d3da2
Jannis Leidel <jannis@leidel.info>
2021-08-20 14:18:34 +0200

    Use 2 test groups on macos to use the 5 runners

--- ci/scripts/macos/integration.sh
+++ ci/scripts/macos/integration.sh
@@ -4,5 +4,5 @@ set -o errtrace -o pipefail -o errexit
 
 eval "$(sudo ./devenv/bin/python -m conda init --dev bash)"
 conda-build tests/test-recipes/activate_deactivate_package
-pytest -m "integration" -v
+pytest -m "integration" -v --splits ${TEST_SPLITS} --group=${TEST_GROUP}
 python -m conda.common.io
--- ci/scripts/macos/unit.sh
+++ ci/scripts/macos/unit.sh
@@ -4,4 +4,4 @@ set -o errtrace -o pipefail -o errexit
 
 eval "$(sudo ./devenv/bin/python -m conda init --dev bash)"
 conda info
-pytest -m "not integration" -v
+pytest -m "not integration" -v --splits ${TEST_SPLITS} --group=${TEST_GROUP}
