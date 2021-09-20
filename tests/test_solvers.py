4b92c8d333399350a59eda4e8e440ea92f8e7f94
Filipe Laíns <lains@riseup.net>
2021-09-20 19:02:24 +0100

    tests: solvers: refactor helpers
    This patch extracts the environment/solver logic from SolverTests and
    puts it in EnvironmentTester. The helpers in SolverTests now should only
    be simple-ish asserts doing mostly data translation.
    
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- tests/test_solvers.py
+++ tests/test_solvers.py
@@ -2,6 +2,7 @@
 
 import collections
 import contextlib
+import functools
 import json
 import os
 import pathlib
@@ -18,9 +19,88 @@ import conda.core.solve
 from conda.base.context import context
 from conda.exceptions import ResolvePackageNotFound, UnsatisfiableError
 from conda.models.channel import Channel
+from conda.models.records import PackageRecord
 from conda.resolve import MatchSpec
 
 from . import helpers
+
+
+@functools.lru_cache()
+def index_packages(num):
+    """Get the index data of the ``helpers.get_index_r_*`` helpers."""
+    # XXX: get_index_r_X should probably be refactored to avoid loading the environment like this.
+    get_index = getattr(helpers, f'get_index_r_{num}')
+    index, _ = get_index(context.subdir)
+    return index.values()
+
+
+class TestEnvironment:
+    REPO_DATA_KEYS = (
+        'build',
+        'build_number',
+        'depends',
+        'license',
+        'md5',
+        'name',
+        'sha256',
+        'size',
+        'subdir',
+        'timestamp',
+        'version',
+        'track_features',
+        'features',
+    )
+
+    def __init__(self, path, solver_class, subdirs=context.subdirs):
+        self._path = pathlib.Path(path)
+        self._solver_class = solver_class
+        self.subdirs = subdirs
+        self.repo_packages = []
+
+    def solver(self, add, remove):
+        self._write_packages()
+        channels = tuple(
+            Channel(f'file://{self._path / subdir}')
+            for subdir in self.subdirs
+        )
+        return self._solver_class(
+            prefix='dummy - does not exist',
+            subdirs=self.subdirs,
+            channels=channels,
+            specs_to_add=add,
+            specs_to_remove=remove,
+        )
+
+    def solver_transaction(self, add=(), remove=()):
+        return self.solver(add=add, remove=remove).solve_final_state()
+
+    def install(self, *specs):
+        return self.solver_transaction(add=specs)
+
+    def remove(self, *specs):
+        return self.solver_transaction(remove=specs)
+
+    def _write_packages(self):
+        """Write packages to the channel path."""
+        # build package data
+        package_data = collections.defaultdict(dict)
+        for record in self.repo_packages:
+            package_data[record.subdir][record.fn] = {
+                key: value
+                for key, value in vars(record).items()
+                if key in self.REPO_DATA_KEYS
+            }
+        # write repodata
+        assert set(self.subdirs).issuperset(set(package_data.keys()))
+        for subdir in self.subdirs:
+            subdir_path = self._path / subdir
+            subdir_path.mkdir(parents=True, exist_ok=True)
+            subdir_path.joinpath('repodata.json').write_text(json.dumps({
+                'info': {
+                    'subdir': subdir,
+                },
+                'packages': package_data.get(subdir, {}),
+            }))
 
 
 class SolverTests:
@@ -31,76 +111,10 @@ class SolverTests:
         """Class under test."""
         raise NotImplementedError
 
-    def _write_channel_packages(self, channel_path: pathlib.Path, subdirs, packages):
-        """Create a channel containing a set of packages."""
-        # build package data
-        package_data = collections.defaultdict(dict)
-        for record in packages:
-            package_data[record.subdir][record.fn] = {
-                key: value
-                for key, value in vars(record).items()
-                if key in (
-                    'build',
-                    'build_number',
-                    'depends',
-                    'license',
-                    'md5',
-                    'name',
-                    'sha256',
-                    'size',
-                    'subdir',
-                    'timestamp',
-                    'version',
-                    'track_features',
-                    'features',
-                )
-            }
-        # write repodata
-        for subdir in subdirs:
-            subdir_path = channel_path / subdir
-            subdir_path.mkdir(parents=True, exist_ok=True)
-            subdir_path.joinpath('repodata.json').write_text(json.dumps({
-                'info': {
-                    'subdir': subdir,
-                },
-                'packages': package_data.get(subdir, {}),
-            }))
-
-    @contextlib.contextmanager
-    def solver(self, *, add=(), remove=(), packages=()):
-        """Create a new solver.
-
-        Roughly equivalent to ``Solver(specs_to_add=..., specs_to_remove=...)``
-        with a custom channel containing the data specified in `packages`.
-        """
-        with tempfile.TemporaryDirectory(prefix='conda-test-repo-') as tmpdir_path:
-            tmpdir = pathlib.Path(tmpdir_path)
-            channels = tuple(
-                Channel(f'file://{tmpdir / subdir}')
-                for subdir in context.subdirs
-            )
-            self._write_channel_packages(tmpdir, context.subdirs, list(packages))
-            yield self.solver_class(
-                prefix='dummy - does not exist',
-                subdirs=context.subdirs,
-                channels=channels,
-                specs_to_add=add,
-                specs_to_remove=remove,
-            )
-
-    def install(self, *specs, packages=None):
-        """Try a install transaction in channel with a set of specified packages."""
-        if not packages:
-            packages = self.index(1)
-        with self.solver(add=specs, packages=packages) as solver:
-            return solver.solve_final_state()
-
-    def index(self, num):
-        """Get the index data of the ``helpers.get_index_r_*`` helpers."""
-        # XXX: get_index_r_X should probably be refactored to avoid loading the environment like this.
-        get_index = getattr(helpers, f'get_index_r_{num}')
-        index, _ = get_index(context.subdir)
-        return index.values()
+    @pytest.fixture()
+    def env(self):
+        with tempfile.TemporaryDirectory(prefix='conda-test-repo-') as tmpdir:
+            yield TestEnvironment(tmpdir, self.solver_class)
 
     def package_string_set(self, packages):
         """Transforms package container in package string set."""
@@ -109,10 +123,10 @@ class SolverTests:
             for record in packages
         }
 
-    def assert_installed(self, specs, expecting):
+    def assert_installs_expected(self, environment, specs, expecting):
         """Helper to assert that a transaction result contains the packages
         specified by the set of specification string."""
-        installed = self.install(*specs)
+        installed = environment.install(*specs)
         assert installed, f'no installed specs ({installed})'
         assert self.package_string_set(installed) == set(expecting) | {
             # XXX: injected
@@ -120,12 +134,8 @@ class SolverTests:
             'pip-1.3.1-py27_1',
         }
 
-    def assert_same_installed(self, specs1, specs2):
-        assert self.package_string_set(
-            self.install(*specs1),
-        ) == self.package_string_set(
-            self.install(*specs2),
-        )
+    def assert_same_packages(self, packages1, packages2):
+        assert self.package_string_set(packages1) == self.package_string_set(packages2)
 
     def assert_record_in(self, record_str, records):
         """Helper to assert that a record list contains a record matching the
@@ -144,14 +154,18 @@ class SolverTests:
                 for entries in exc_info.value.unsatisfiable
             ) == entries
 
-    def test_empty(self):
-        assert self.install() == []
+    def test_empty(self, env):
+        env.repo_packages = index_packages(1)
+        assert env.install() == []
 
-    def test_iopro_nomkl(self):
+    def test_iopro_nomkl(self, env):
         if self.solver_class is conda.core.solve.LibSolvSolver:
             pytest.xfail('LibSolvSolver does not support track-features/features')
-        self.assert_installed(
-            ['iopro 1.4*', 'python 2.7*', 'numpy 1.7*'], [
+        env.repo_packages = index_packages(1)
+        self.assert_installs_expected(
+            env,
+            ['iopro 1.4*', 'python 2.7*', 'numpy 1.7*'],
+            [
                 'iopro-1.4.3-np17py27_p0',
                 'numpy-1.7.1-py27_0',
                 'openssl-1.0.1c-0',
@@ -165,24 +179,30 @@ class SolverTests:
             ],
         )
 
-    def test_mkl(self):
+    def test_mkl(self, env):
         if self.solver_class is conda.core.solve.LibSolvSolver:
             pytest.xfail('LibSolvSolver does not support track-features/features')
-        self.assert_same_installed(['mkl'], [
-            'mkl 11*', MatchSpec(track_features='mkl')
-        ])
+        env.repo_packages = index_packages(1)
+        self.assert_same_packages(
+            env.install('mkl'),
+            env.install('mkl 11*', MatchSpec(track_features='mkl')),
+        )
 
-    def test_accelerate(self):
+    def test_accelerate(self, env):
         if self.solver_class is conda.core.solve.LibSolvSolver:
             pytest.xfail('LibSolvSolver does not support track-features/features')
-        self.assert_same_installed(['accelerate'], [
-            'accelerate', MatchSpec(track_features='mkl')
-        ])
+        env.repo_packages = index_packages(1)
+        self.assert_same_packages(
+            env.install('accelerate'),
+            env.install('accelerate', MatchSpec(track_features='mkl')),
+        )
 
-    def test_scipy_mkl(self):
+    def test_scipy_mkl(self, env):
         if self.solver_class is conda.core.solve.LibSolvSolver:
             pytest.xfail('LibSolvSolver does not support track-features/features')
-        records = self.install('scipy', 'python 2.7*', 'numpy 1.7*', MatchSpec(track_features='mkl'))
+
+        env.repo_packages = index_packages(1)
+        records = env.install('scipy', 'python 2.7*', 'numpy 1.7*', MatchSpec(track_features='mkl'))
 
         for record in records:
             if record.name in ('numpy', 'scipy'):
@@ -191,17 +211,22 @@ class SolverTests:
         self.assert_record_in('numpy-1.7.1-py27_p0', records)
         self.assert_record_in('scipy-0.12.0-np17py27_p0', records)
 
-    def test_anaconda_nomkl(self):
-        records = self.install('anaconda 1.5.0', 'python 2.7*', 'numpy 1.7*')
+    def test_anaconda_nomkl(self, env):
+        env.repo_packages = index_packages(1)
+        records = env.install('anaconda 1.5.0', 'python 2.7*', 'numpy 1.7*')
         assert len(records) == 107
         self.assert_record_in('scipy-0.12.0-np17py27_0', records)
 
-    def test_pseudo_boolean(self):
+    def test_pseudo_boolean(self, env):
         if self.solver_class is conda.core.solve.LibSolvSolver:
             pytest.xfail('LibSolvSolver does not support track-features/features')
+
+        env.repo_packages = index_packages(1)
         # The latest version of iopro, 1.5.0, was not built against numpy 1.5
-        self.assert_installed(
-            ['iopro', 'python 2.7*', 'numpy 1.5*'], [
+        self.assert_installs_expected(
+            env,
+            ['iopro', 'python 2.7*', 'numpy 1.5*'],
+            [
                 'iopro-1.4.3-np15py27_p0',
                 'numpy-1.5.1-py27_4',
                 'openssl-1.0.1c-0',
@@ -214,8 +239,10 @@ class SolverTests:
                 'zlib-1.2.7-0',
             ],
         )
-        self.assert_installed(
-            ['iopro', 'python 2.7*', 'numpy 1.5*', MatchSpec(track_features='mkl')], [
+        self.assert_installs_expected(
+            env,
+            ['iopro', 'python 2.7*', 'numpy 1.5*', MatchSpec(track_features='mkl')],
+            [
                 'iopro-1.4.3-np15py27_p0',
                 'mkl-rt-11.0-p0',
                 'numpy-1.5.1-py27_p4',
@@ -230,16 +257,18 @@ class SolverTests:
             ],
         )
 
-    def test_unsat_from_r1(self):
+    def test_unsat_from_r1(self, env):
+        env.repo_packages = index_packages(1)
+
         with pytest.raises(UnsatisfiableError) as exc_info:
-            self.install('numpy 1.5*', 'scipy 0.12.0b1')
+            env.install('numpy 1.5*', 'scipy 0.12.0b1')
         self.assert_unsatisfiable(exc_info, [
             ('numpy=1.5',),
             ('scipy==0.12.0b1', "numpy[version='1.6.*|1.7.*']"),
         ])
 
         with pytest.raises(UnsatisfiableError) as exc_info:
-            self.install('numpy 1.5*', 'python 3*')
+            env.install('numpy 1.5*', 'python 3*')
         self.assert_unsatisfiable(exc_info, [
             ('numpy=1.5', 'nose', 'python=3.3'),
             ('numpy=1.5', "python[version='2.6.*|2.7.*']"),
@@ -247,23 +276,21 @@ class SolverTests:
         ])
 
         with pytest.raises((ResolvePackageNotFound, UnsatisfiableError)) as exc_info:
-            self.install('numpy 1.5*', 'numpy 1.6*')
+            env.install('numpy 1.5*', 'numpy 1.6*')
         if exc_info.type is ResolvePackageNotFound:
             assert sorted(map(str, exc_info.value.bad_deps)) == [
                 "numpy[version='1.5.*,1.6.*']",
             ]
 
-    def test_unsat_simple(self):
+    def test_unsat_simple(self, env):
+        env.repo_packages = [
+            helpers.record(name='a', depends=['c >=1,<2']),
+            helpers.record(name='b', depends=['c >=2,<3']),
+            helpers.record(name='c', version='1.0'),
+            helpers.record(name='c', version='2.0'),
+        ]
         with pytest.raises(UnsatisfiableError) as exc_info:
-            self.install(
-                'a', 'b',
-                packages=(
-                    helpers.record(name='a', depends=['c >=1,<2']),
-                    helpers.record(name='b', depends=['c >=2,<3']),
-                    helpers.record(name='c', version='1.0'),
-                    helpers.record(name='c', version='2.0'),
-                )
-            )
+            env.install('a', 'b')
         self.assert_unsatisfiable(exc_info, [
             ('a', "c[version='>=1,<2']"),
             ('b', "c[version='>=2,<3']"),
