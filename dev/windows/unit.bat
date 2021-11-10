064be2cbd7fa7e15fe271a751a7b41cd07131b51
Jaime RGP <jaimergp@users.noreply.github.com>
2021-11-10 17:41:57 +0100

    fix restrains

--- dev/windows/unit.bat
+++ dev/windows/unit.bat
@@ -4,7 +4,7 @@ cd \conda_src || goto :error
 CALL dev-init.bat || goto :error
 CALL conda info || goto :error
 REM TODO:  Remove before merge, temporary:
-CALL conda install -c napari/label/bundle_tools -c conda-forge "menuinst>=2" || goto :error
+CALL conda install -c napari/label/bundle_tools -c conda-forge "menuinst=2" || goto :error
 CALL pytest -m "not integration" -v --splits=%TEST_SPLITS% --group=%TEST_GROUP% || goto :error
 goto :EOF
 
