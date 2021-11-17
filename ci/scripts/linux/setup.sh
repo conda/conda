d3090e15b317fcfa56b06dbc071ef18c146ed8d2
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2021-11-17 19:22:49 +0100

    Enable one-click gitpod and GitHub Codespaces setup for Linux development (#11040)

--- ci/scripts/linux/setup.sh
+++ ci/scripts/linux/setup.sh
@@ -3,7 +3,10 @@
 set -o errtrace -o pipefail -o errexit
 
 apt-get update --fix-missing
-apt-get install -y --no-install-recommends tini wget build-essential bzip2 ca-certificates libglib2.0-0 libxext6 libsm6 libxrender1 git mercurial subversion sudo
+apt-get install -y --no-install-recommends \
+    tini wget build-essential bzip2  ca-certificates \
+    libglib2.0-0 libxext6 libsm6 libxrender1 git mercurial subversion \
+    sudo htop less nano man
 apt-get clean
 rm -rf /var/lib/apt/lists/*
 
@@ -11,3 +14,7 @@ useradd -m -s /bin/bash test_user
 usermod -u 1001 test_user
 groupmod -g 1001 test_user
 echo "test_user ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+### Gitpod user ###
+useradd -l -u 33333 -G sudo -md /home/gitpod -s /bin/bash -p gitpod gitpod
+echo "gitpod ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
