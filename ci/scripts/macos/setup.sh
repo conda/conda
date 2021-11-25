f279c5ad991decb2274a156fbf3f51b484ce6038
Jannis Leidel <jannis@leidel.info>
2021-11-25 10:38:14 +0100

    Eval the dev call.

--- ci/scripts/macos/setup.sh
+++ ci/scripts/macos/setup.sh
@@ -2,6 +2,11 @@
 
 set -o errtrace -o pipefail -o errexit
 
+echo "Initializing conda in dev mode..."
+echo "Factory config is:"
+grep -e "conda location" -e "conda version" -e "python version" <(conda info -a) | sed 's/^\s*/  /'
 conda install -yq pip conda-build conda-verify pytest
 conda update openssl ca-certificates certifi
-python -m conda init bash --dev
+eval "$(python -m conda init bash --dev)"
+echo "Done! Now running:"
+grep -e "conda location" -e "conda version" -e "python version" <(conda info -a) | sed 's/^\s*/  /'
