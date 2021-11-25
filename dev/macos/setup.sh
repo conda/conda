6ebfe202e05742849fd0802e96b8b8ed3fa06697
Jannis Leidel <jannis@leidel.info>
2021-11-25 11:14:29 +0100

    Just checking..

--- dev/macos/setup.sh
+++ dev/macos/setup.sh
@@ -2,11 +2,6 @@
 
 set -o errtrace -o pipefail -o errexit
 
-echo "Initializing conda in dev mode..."
-echo "Factory config is:"
-grep -e "conda location" -e "conda version" -e "python version" <(conda info -a) | sed 's/^\s*/  /'
 conda install -yq pip conda-build conda-verify pytest
 conda update openssl ca-certificates certifi
 eval "$(python -m conda init bash --dev)"
-echo "Done! Now running:"
-grep -e "conda location" -e "conda version" -e "python version" <(conda info -a) | sed 's/^\s*/  /'
