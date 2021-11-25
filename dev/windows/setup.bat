9627f35f23e33a13f9c7b6214fe18301da6a7e5a
Jannis Leidel <jannis@leidel.info>
2021-11-25 12:49:20 +0100

    Move things back into setup.sh. Fix changeps1.

--- dev/windows/setup.bat
+++ dev/windows/setup.bat
@@ -9,7 +9,6 @@ cd \conda_src || goto :error
 CALL \conda_bin\scripts\activate.bat || goto :error
 CALL conda create -n conda-test-env -y python=%PYTHON% pywin32 --file=tests\requirements.txt || goto :error
 CALL conda activate conda-test-env || goto :error
-CALL conda install -yq pip conda-build conda-verify || goto :error
 CALL conda update openssl ca-certificates certifi || goto :error
 python -m conda init cmd.exe --dev || goto :error
 goto :EOF
