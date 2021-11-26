366dd3fea72b0df55ebca8830fa40b451949edf5
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2021-11-26 17:58:03 +0100

    test unit tests for libsolv in experimental-resolver better

--- dev/linux/experimental-resolver.sh
+++ dev/linux/experimental-resolver.sh
@@ -10,4 +10,4 @@ conda info
 # remove the pkg cache.  We can't hardlink from here anyway.  Having it around causes log problems.
 sudo rm -rf /opt/conda/pkgs/*-*-*
 mamba --version
-CONDA_SOLVER_LOGIC=libsolv pytest -m "not integration" -v --splits ${TEST_SPLITS} --group=${TEST_GROUP} tests/core/test_solve.py
+CONDA_SOLVER_LOGIC=libsolv pytest -m "not integration" -k "not TestLegacySolver" -v --splits ${TEST_SPLITS} --group=${TEST_GROUP} tests/core/test_solve.py tests/test_solvers.py
