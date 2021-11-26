1e8014e3967749846204ea5056f30fb52898faf7
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2021-11-26 18:29:52 +0100

    also for win

--- dev/windows/unit.bat
+++ dev/windows/unit.bat
@@ -2,7 +2,7 @@
 pushd %TEMP% || goto :error
 cd \conda_src || goto :error
 CALL dev-init.bat || goto :error
-CALL pytest -m "not integration" -v --splits=%TEST_SPLITS% --group=%TEST_GROUP% || goto :error
+CALL pytest -m "not integration" -k "not TestLibSolvSolver" -v --splits=%TEST_SPLITS% --group=%TEST_GROUP% || goto :error
 goto :EOF
 
 :error
