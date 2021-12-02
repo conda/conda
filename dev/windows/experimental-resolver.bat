7a0b5b28ecab358aa98946d893c1bf7bbde3df9d
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2021-12-02 12:56:13 +0100

    more info on windows

--- dev/windows/experimental-resolver.bat
+++ dev/windows/experimental-resolver.bat
@@ -2,6 +2,8 @@
 pushd %TEMP% || goto :error
 cd \conda_src || goto :error
 CALL dev-init.bat || goto :error
+CALL conda info || goto :error
+CALL mamba --version || goto :error
 set CONDA_SOLVER_LOGIC=libmamba
 CALL pytest -m "not integration" -k "not TestClassicSolver" -v tests/core/test_solve.py tests/test_solvers.py || goto :error
 goto :EOF
