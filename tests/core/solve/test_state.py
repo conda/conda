027cd07162f4513f3810202e8d9560168c09515c
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2021-12-27 11:59:21 +0100

    more tests

--- tests/core/solve/test_state.py
+++ tests/core/solve/test_state.py
@@ -9,6 +9,8 @@ behavioral units that, eventually, will allow us to further split the
 logic into discrete, composable parts.
 """
 from tempfile import TemporaryDirectory
+from itertools import chain
+from typing import Match
 
 import pytest
 
@@ -18,7 +20,8 @@ from conda.core.solve.classic import Solver
 from conda.core.solve.libmamba2 import LibMambaIndexHelper
 from conda.core.solve.state import SolverInputState, SolverOutputState
 from conda.models.match_spec import MatchSpec
-from ...test_solvers import SimpleEnvironment
+from conda.models.version import VersionSpec
+from ...test_solvers import SimpleEnvironment, index_packages
 
 
 def empty_prefix():
@@ -26,7 +29,7 @@ def empty_prefix():
 
 
 @pytest.fixture()
-def env(solver_class=Solver):
+def env(solver_class=Solver) -> SimpleEnvironment:
     with empty_prefix() as prefix:
         yield SimpleEnvironment(prefix, solver_class)
 
@@ -83,5 +86,33 @@ def test_create_requested_and_pinned():
             assert sos.real_specs == {"python": MatchSpec(pinned)}
 
 
-def test_update_major_python(env):
-    pass
+def test_python_updates(env: SimpleEnvironment):
+    """
+    Usually, python is implicitly pinned to its installed major.minor version.
+    That can only be overridden if the user explicitly requests python in the
+    CLI specs. Ideally, the explicit spec contains a version constrain too.
+    """
+    # Setup the prefix
+    env.repo_packages = index_packages(1)
+    env.installed_packages = env.install("python 2.*", as_specs=True)
+    env._write_installed_packages()
+
+    index = LibMambaIndexHelper(installed_records=env.installed_packages)
+
+    # If no explicit python is passed, we should see python 2.7 in the specs
+    sis = SolverInputState(prefix=env._prefix_path, requested=["numpy"])
+    sos = SolverOutputState(solver_input_state=sis)
+    sos.prepare_specs(index)
+    # First attempt, it will be pinned to the installed version
+    assert sos.specs["python"] == sis.installed["python"].to_match_spec()
+
+    # However, if we mark it as conflicting, it will be relaxed to major.minor only
+    sos = SolverOutputState(solver_input_state=sis, conflicts={"python": sos.specs["python"]})
+    sos.prepare_specs(index)
+    assert sos.specs["python"] == MatchSpec("python=2.7")
+
+    # If we do explicitly request it, then it won't have any version constrains
+    sis = SolverInputState(prefix=env._prefix_path, requested=["python"])
+    sos = SolverOutputState(solver_input_state=sis)
+    sos.prepare_specs(index)
+    assert ("python", MatchSpec("python")) in sos.real_specs.items()
