5bb5e56c74fa8b6c89694b17c768fd610745b558
Ken Odegard <kodegard@anaconda.com>
2022-01-28 13:49:58 -0600

    Cleanup `conda.cli.main_run.execute` (#11173)

--- conda/cli/main_run.py
+++ conda/cli/main_run.py
@@ -8,44 +8,46 @@ import sys
 from ..base.context import context
 from ..utils import wrap_subprocess_call
 from ..gateways.disk.delete import rm_rf
-from ..common.compat import encode_environment
+from ..common.compat import on_win, encode_environment
 from ..gateways.subprocess import subprocess_call
 from .common import validate_prefix
 
 
 def execute(args, parser):
-    on_win = sys.platform == "win32"
-
-    call = args.executable_call
-    cwd = args.cwd
-    no_capture_output = args.no_capture_output
-
-    script_caller, command_args = wrap_subprocess_call(
+    # create run script
+    script, command = wrap_subprocess_call(
         on_win,
         context.root_prefix,
         validate_prefix(context.target_prefix or os.getenv("CONDA_PREFIX") or context.root_prefix),
         args.dev,
         args.debug_wrapper_scripts,
-        call,
+        args.executable_call,
     )
-    env = encode_environment(os.environ.copy())
 
-    response = subprocess_call(command_args, env=env, path=cwd, raise_on_error=False,
-                               capture_output=not no_capture_output, live_stream=args.live_stream)
+    # run script
+    response = subprocess_call(
+        command,
+        env=encode_environment(os.environ.copy()),
+        path=args.cwd,
+        raise_on_error=False,
+        capture_output=not args.no_capture_output,
+        live_stream=args.live_stream,
+    )
     if response.rc != 0:
         log = getLogger(__name__)
-        log.error("Subprocess for 'conda run {}' command failed.  (See above for error)"
-                  .format(call))
-    if script_caller is not None:
-        if 'CONDA_TEST_SAVE_TEMPS' not in os.environ:
-            rm_rf(script_caller)
-        else:
-            log = getLogger(__name__)
-            log.warning('CONDA_TEST_SAVE_TEMPS :: retaining main_run script_caller {}'.format(
-                script_caller))
+        log.error(f"`conda run {' '.join(args.executable_call)}` failed. (See above for error)")
+
+    # remove script
+    if "CONDA_TEST_SAVE_TEMPS" not in os.environ:
+        rm_rf(script)
+    else:
+        log = getLogger(__name__)
+        log.warning(f"CONDA_TEST_SAVE_TEMPS :: retaining main_run script {script}")
+
     if not args.live_stream:
         if response.stdout:
             print(response.stdout, file=sys.stdout)
         if response.stderr:
             print(response.stderr, file=sys.stderr)
+
     return response
