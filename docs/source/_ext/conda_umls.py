28b45314f9b958034a4e4cceff0f2b2365813335
Ken Odegard <kodegard@anaconda.com>
2022-01-31 16:47:44 -0600

    Remove old license blurbs

--- docs/source/_ext/conda_umls.py
+++ docs/source/_ext/conda_umls.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
-# (c) 2021 Anaconda, Inc.
-# All Rights Reserved
-#
-# conda is distributed under the terms of the BSD 3-clause license.
-# Consult LICENSE.txt or http://opensource.org/licenses/BSD-3-Clause.
+# -*- coding: utf-8 -*-
+# Copyright (C) 2012 Anaconda, Inc
+# SPDX-License-Identifier: BSD-3-Clause
+
 import fileinput
 import os
 import requests
