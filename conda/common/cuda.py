35cd47952fe694b49375a72bdbaf7834570fdc2d
Ken Odegard <kodegard@anaconda.com>
2022-02-01 09:32:52 -0600

    Enforce copyright/license disclaimer & encoding pragma (#11183)
    * Enforce copyright/license disclaimer
    
    * Include encoding pragma hook
    
    * Remove old license blurbs
    
    * Ignore conda/auxlib/_vendor
    
    * Apply
    
    * Move disclaimer.txt to .github/

--- conda/common/cuda.py
+++ conda/common/cuda.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2019 Anaconda, Inc
+# Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
 
 def cuda_detect():
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2012 Anaconda, Inc
+# SPDX-License-Identifier: BSD-3-Clause
+
 # This is just here so that tests is a package, so that dotted relative
 # imports work.
 from __future__ import print_function
