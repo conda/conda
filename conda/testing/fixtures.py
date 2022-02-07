895258b601735787716b4c5ae870d58f1ddb93cd
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-02-07 16:08:18 +0100

    make fixtures importable downstream

---
+++ conda/testing/fixtures.py
@@ -0,0 +1,32 @@
+import warnings
+import py
+import pytest
+
+from conda.common.compat import PY3
+from conda.gateways.disk.create import TemporaryDirectory
+from conda.core.subdir_data import SubdirData
+
+
+@pytest.fixture(autouse=True)
+def suppress_resource_warning():
+    '''
+    Suppress `Unclosed Socket Warning`
+
+    It seems urllib3 keeps a socket open to avoid costly recreation costs.
+
+    xref: https://github.com/kennethreitz/requests/issues/1882
+    '''
+    if PY3:
+        warnings.filterwarnings("ignore", category=ResourceWarning)
+
+
+@pytest.fixture(scope='function')
+def tmpdir(tmpdir, request):
+    tmpdir = TemporaryDirectory(dir=str(tmpdir))
+    request.addfinalizer(tmpdir.cleanup)
+    return py.path.local(tmpdir.name)
+
+
+@pytest.fixture(autouse=True)
+def clear_subdir_cache():
+    SubdirData.clear_cached_local_channel_data()
