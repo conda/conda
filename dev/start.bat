f5191cba8837edd0654fb7141c36a13171957986
Ken Odegard <kodegard@anaconda.com>
2022-02-10 17:47:14 -0600

    Rework to allow multiple envs
    Makes environments more descriptive (no longer `base` prefix, uses `devenv-$PYTHON` prefix) and allow side-by-side installs of multiple different Python versions.

--- dev/start.bat
+++ dev/start.bat
@@ -1,47 +1,181 @@
-@IF "%~1"=="" (
-    @SET DEVENV=%CD%\devenv
-) ELSE (
-    @SET "DEVENV=%~1"
+@REM parse args
+:ARGS_LOOP
+@IF "%~1"=="" @GOTO :ARGS_END
+
+@REM convert to uppercase
+@FOR /F "usebackq delims=" %%I IN (`powershell.exe "'%~1'.toUpper()"`) DO @SET "_ARG=%%~I"
+
+@IF "%_ARG%"=="/P" (
+    @SET "_PYTHON=%~2"
+    @SHIFT
+    @SHIFT
+    @GOTO :ARGS_LOOP
 )
-@IF "%PYTHON%"=="" (
-    @SET "PYTHON=3.8"
+
+@IF "%_ARG%"=="/U" (
+    @SET "_UPDATE=0"
+    @SHIFT
+    @GOTO :ARGS_LOOP
 )
 
-@REM Unset some variables that get in the way
-set CONDA_BAT=
-set CONDA_EXE=
-set CONDA_SHLVL=
-set PYTHONPATH=
-set PYTHONHOME=
+@IF "%_ARG%"=="/D" (
+    @SET "_DEVENV=%~2"
+    @SHIFT
+    @SHIFT
+    @GOTO :ARGS_LOOP
+)
 
-@IF EXIST "dev-init.bat" @GOTO :INIT_BUILD
-@IF EXIST "%DEVENV%\conda-meta\history" @GOTO :ALREADY
-@ECHO Downloading Miniconda3-latest-Windows-x86_64.exe as miniconda.exe
-@powershell.exe -NoProfile -Command (new-object System.Net.WebClient).DownloadFile('https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe','miniconda.exe')
-@ECHO Installing miniconda to: %DEVENV%
-@start /wait "" miniconda.exe /InstallationType=JustMe /RegisterPython=0 /AddToPath=0 /S /D=%DEVENV%
-call "%DEVENV%"\Scripts\activate.bat
-conda install -y defaults::git
-@ECHO exit at this point.
+@IF "%_ARG%"=="/?" (
+    @ECHO Usage: %~0 [/P VERSION] [/U] [/D PATH] [/?]
+    @ECHO.
+    @ECHO Options:
+    @ECHO   /P  PYTHON  Specify the Python version for the devenv ^(default: 3.8^)
+    @ECHO   /U          Force update packages ^(default: update every 24 hours^)
+    @ECHO   /D  PATH    Provide the desired devenv path ^(default: devenv\Windows^)
+    @ECHO.  /?          Display this
+    @EXIT /B 0
+)
 
-:ALREADY
-call "%DEVENV%"\Scripts\activate.bat
+@ECHO Error: unknown option %~1 1>&2
+@EXIT /B 1
+:ARGS_END
+@IF "%_DEVENV%"=="" @SET "_DEVENV=%CD%\devenv\Windows"
+@IF NOT EXIST "%_DEVENV%" @MKDIR "%_DEVENV%"
+@IF "%_PYTHON%"=="" @SET "_PYTHON=3.8"
+@IF "%_UPDATE%"=="" @SET "_UPDATE=1"
 
-@REM Unset some variables that get in the way
-set CONDA_BAT=
-set CONDA_EXE=
-set CONDA_SHLVL=
-set PYTHONPATH=
-set PYTHONHOME=
+@REM deactivate any prior envs
+@IF "%CONDA_SHLVL%"=="" @GOTO DEACTIVATED
+@ECHO Deactivating %CONDA_SHLVL% environments...
+:DEACTIVATING
+@IF "%CONDA_SHLVL%"=="0" @GOTO DEACTIVATED
+@CALL conda deactivate
+@IF NOT %ErrorLevel%==0 (
+    @ECHO Error: failed to deactivate environment 1>&2
+    @EXIT /B 1
+)
+@GOTO DEACTIVATING
+:DEACTIVATED
 
-@ECHO               ^>^> conda update -p "%DEVENV%" -yq --all
-@CALL "%DEVENV%\Scripts\conda" update -p "%DEVENV%" -yq --all
-@ECHO               ^>^> conda install -yp "%DEVENV%" defaults::git
-@CALL "%DEVENV%\Scripts\conda" install -yp "%DEVENV%" defaults::git
-@ECHO               ^>^> conda install -yq -p "%DEVENV%" python="%PYTHON%" pywin32 --file tests\requirements.txt -c defaults
-@CALL "%DEVENV%\Scripts\conda" install -yq -p "%DEVENV%" python="%PYTHON%" pywin32 --file tests\requirements.txt -c defaults
+@REM get/set variables
+@SET "_STUB=devenv-%_PYTHON%"
+@SET "_ENV=%_DEVENV%\envs\%_STUB%"
+@SET "_UPDATED=%_ENV%\.devenv-updated"
+@SET "_CONDA_EXE=%_DEVENV%\Scripts\conda.exe"
+@SET "_ENV_EXE=%_ENV%\Scripts\conda.exe"
+@SET "_CONDA_BAT=%_ENV%\condabin\conda.bat"
 
-@CALL "%DEVENV%\python" -m conda init --dev cmd.exe > NUL
+@REM does conda install exist?
+@IF EXIST "%_DEVENV%\conda-meta\history" @GOTO INSTALLED
 
-:INIT_BUILD
-@CALL dev-init.bat
+@REM downloading conda
+@IF EXIST "%_DEVENV%\miniconda.exe" @GOTO DOWNLOADED
+@ECHO Downloading conda...
+@powershell.exe -Command "Invoke-WebRequest -Uri 'https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe' -OutFile '%_DEVENV%\miniconda.exe' | Out-Null"
+@IF NOT %ErrorLevel%==0 (
+    @ECHO Error: failed to download conda 1>&2
+    @EXIT /B 1
+)
+:DOWNLOADED
+
+@REM installing conda
+@ECHO Installing conda...
+@START /wait "" "%_DEVENV%\miniconda.exe" /InstallationType=JustMe /RegisterPython=0 /AddToPath=0 /S /D=%_DEVENV% > NUL
+@IF NOT %ErrorLevel%==0 (
+    @ECHO Error: failed to install conda 1>&2
+    @EXIT /B 1
+)
+@REM Windows doesn't ship with git so ensure installed into base
+@CALL :CONDA install -yq --name base defaults::git > NUL
+:INSTALLED
+
+@REM create empty env if it doesn't exist
+@IF EXIST "%_ENV%" @GOTO ENVEXISTS
+@ECHO Creating %_STUB%...
+
+@CALL :CONDA create -yq --prefix "%_ENV%" > NUL
+@IF NOT %ErrorLevel%==0 (
+    @ECHO Error: failed to create %_STUB% 1>&2
+    @EXIT /B 1
+)
+:ENVEXISTS
+
+@REM check if explicitly updating or if 24 hrs since last update
+@IF %_UPDATE%==1 (
+    @IF EXIST "%_UPDATED%" (
+        @powershell.exe -Command "exit (Get-Item "%_UPDATED%").LastWriteTime -lt (Get-Date).AddHours(-24)"
+        @IF %ErrorLevel%==0 @GOTO UPTODATE
+    )
+)
+@ECHO Updating %_STUB%...
+
+@CALL :CONDA update -yq --all > NUL
+@IF NOT %ErrorLevel%==0 (
+    @ECHO Error: failed to update conda 1>&2
+    @EXIT /B 1
+)
+
+@CALL :CONDA install ^
+    -yq ^
+    --prefix "%_ENV%" ^
+    --override-channels ^
+    -c defaults ^
+    --file tests\requirements.txt ^
+    pywin32 ^
+    "python=%_PYTHON%" > NUL
+@IF NOT %ErrorLevel%==0 (
+    @ECHO Error: failed to update %_STUB% 1>&2
+    @EXIT /B 1
+)
+
+@REM update timestamp
+@IF EXIST "%_UPDATED%" @DEL "%_UPDATED%"
+@ECHO > "%_UPDATED%"
+:UPTODATE
+
+@REM initialize conda command
+@ECHO Initializing conda...
+@CALL :CONDA init --dev cmd.exe > NUL
+@CALL dev-init.bat > NUL
+@IF NOT %ErrorLevel%==0 (
+    @ECHO Error: failed to initialize conda 1>&2
+    @EXIT /B 1
+)
+@DEL /Q dev-init.bat
+
+@REM activate dev env
+@ECHO Activating %_STUB%...
+@CALL conda activate "%_ENV%" > NUL
+@IF NOT %ErrorLevel%==0 (
+    @ECHO Error: failed to activate %_STUB% 1>&2
+    @EXIT /B 1
+)
+@SET "CONDA_EXE=%_ENV_EXE%"
+@SET "CONDA_BAT=%_CONDA_BAT%"
+@DOSKEY conda="%CONDA_BAT%" $*
+
+@REM cleanup
+@SET _ARG=
+@SET _DEVENV=
+@SET _PYTHON=
+@SET _STUB=
+@SET _ENV=
+@SET _UPDATED=
+@SET _ENV_EXE=
+@SET _CONDA_BAT=
+
+@GOTO :EOF
+
+:CONDA *args
+@REM include OpenSSL & git on %PATH%
+@SET "_PATH=%PATH%"
+@SET "PATH=%_DEVENV%\Library\bin;%PATH%"
+
+@CALL "%_CONDA_EXE%" %*
+@IF NOT %ErrorLevel%==0 @EXIT /B %ErrorLevel%
+
+@REM restore %PATH%
+@SET "PATH=%_PATH%"
+@SET _PATH=
+
+@GOTO :EOF
