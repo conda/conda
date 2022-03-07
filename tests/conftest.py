65398af02e18357c8c59e4bb0cb78cc983e34e51
Jannis Leidel <jannis@leidel.info>
2022-03-08 00:04:44 +0100

    Minor cleanup.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
-
 from pathlib import Path
 import subprocess
 import sys
-import warnings
+
+import pytest
 
 from conda.testing.fixtures import (
     suppress_resource_warning,
@@ -27,30 +27,6 @@ def pytest_addoption(parser):
 def pytest_generate_tests(metafunc):
     if 'shell' in metafunc.fixturenames:
         metafunc.parametrize("shell", metafunc.config.option.shell)
-
-
-@pytest.fixture(autouse=True)
-def suppress_resource_warning():
-    """
-    Suppress `Unclosed Socket Warning`
-
-    It seems urllib3 keeps a socket open to avoid costly recreation costs.
-
-    xref: https://github.com/kennethreitz/requests/issues/1882
-    """
-    warnings.filterwarnings("ignore", category=ResourceWarning)
-
-
-@pytest.fixture(scope='function')
-def tmpdir(tmpdir, request):
-    tmpdir = TemporaryDirectory(dir=str(tmpdir))
-    request.addfinalizer(tmpdir.cleanup)
-    return py.path.local(tmpdir.name)
-
-
-@pytest.fixture(autouse=True)
-def clear_subdir_cache():
-    SubdirData.clear_cached_local_channel_data()
 
 
 @pytest.fixture(scope="session", autouse=True)
