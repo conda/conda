412357a686a8fe8fcaf5f4cefb9902c49bba1eda
Ken Odegard <kodegard@anaconda.com>
2022-03-08 14:55:12 -0600

    Move auxlib's six vendoring

--- conda/_vendor/six.py
+++ conda/_vendor/six.py
@@ -1,3 +1,5 @@
+"""Utilities for writing code that runs on Python 2 and 3"""
+
 # Copyright (c) 2010-2015 Benjamin Peterson
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -17,8 +19,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
-"""Utilities for writing code that runs on Python 2 and 3"""
 
 from __future__ import absolute_import
 
