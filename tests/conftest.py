daae92cd636fdff53c7c58447fd447c173d63754
Ken Odegard <kodegard@anaconda.com>
2022-03-23 19:19:47 -0500

    Remove unused pytest --shell arg

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,30 +3,8 @@
 # SPDX-License-Identifier: BSD-3-Clause
 from pathlib import Path
 import subprocess
-import sys
 
 import pytest
-
-from conda.testing.fixtures import (
-    suppress_resource_warning,
-    tmpdir,
-    clear_subdir_cache,
-)
-
-win_default_shells = ["cmd.exe", "powershell", "git_bash", "cygwin"]
-shells = ["bash", "zsh"]
-if sys.platform == "win32":
-    shells = win_default_shells
-
-
-def pytest_addoption(parser):
-    parser.addoption("--shell", action="append", default=[],
-                     help="list of shells to run shell tests on")
-
-
-def pytest_generate_tests(metafunc):
-    if 'shell' in metafunc.fixturenames:
-        metafunc.parametrize("shell", metafunc.config.option.shell)
 
 
 def _conda_build_recipe(pkg):
