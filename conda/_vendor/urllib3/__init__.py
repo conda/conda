9a2eb40e6f408171c935385503f77c0fffd0d368
Travis Hathaway <travis.j.hathaway@gmail.com>
2022-03-28 13:48:15 +0200

    removing urllib3

--- conda/_vendor/urllib3/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-urllib3 - Thread-safe connection pooling and re-using.
-"""
-
-from __future__ import absolute_import
-
-__author__ = 'Andrey Petrov (andrey.petrov@shazow.net)'
-__license__ = 'MIT'
-__version__ = '1.19.1'
