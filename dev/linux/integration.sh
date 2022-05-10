560b43274c38e90f45dbfc1c25aba9291d097b18
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-05-10 14:16:57 +0200

    pin conda-build

--- dev/linux/integration.sh
+++ dev/linux/integration.sh
@@ -6,7 +6,7 @@ TEST_SPLITS="${TEST_SPLITS:-1}"
 TEST_GROUP="${TEST_GROUP:-1}"
 
 eval "$(sudo /opt/conda/bin/python -m conda init --dev bash)"
-sudo su root -c "/opt/conda/bin/conda install -yq conda-build"
+sudo su root -c "/opt/conda/bin/conda install -yq 'conda-build>=3.21.8'"
 conda-build tests/test-recipes/activate_deactivate_package tests/test-recipes/pre_link_messages_package
 conda info
 pytest -m "integration" -v --splits ${TEST_SPLITS} --group=${TEST_GROUP}
