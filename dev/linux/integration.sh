bc548e8210e1f2298ca98c16ac5548453e69901c
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-05-10 14:22:57 +0200

    list env

--- dev/linux/integration.sh
+++ dev/linux/integration.sh
@@ -7,6 +7,8 @@ TEST_GROUP="${TEST_GROUP:-1}"
 
 eval "$(sudo /opt/conda/bin/python -m conda init --dev bash)"
 sudo su root -c "/opt/conda/bin/conda install -yq 'conda-build>=3.21.8'"
+conda info -a
+conda list
 conda-build tests/test-recipes/activate_deactivate_package tests/test-recipes/pre_link_messages_package
 conda info
 pytest -m "integration" -v --splits ${TEST_SPLITS} --group=${TEST_GROUP}
