f072c34da40e3534bdf04bef4c13f0180901605c
Ken Odegard <kodegard@anaconda.com>
2022-05-31 14:18:52 -0500

    Remove `conda.cli.common.ensure_name_or_prefix` (#11490)
    Co-authored-by: Jannis Leidel <jannis@leidel.info>

--- conda/cli/common.py
+++ conda/cli/common.py
@@ -7,6 +7,7 @@ from logging import getLogger
 from os.path import basename, dirname, isdir, isfile, join
 import re
 import sys
+from warnings import warn
 
 from ..auxlib.ish import dals
 from ..base.constants import ROOT_ENV_NAME
@@ -67,6 +68,10 @@ def confirm_yn(message="Proceed", default='yes', dry_run=NULL):
 
 
 def ensure_name_or_prefix(args, command):
+    warn(
+        "conda.cli.common.ensure_name_or_prefix is pending deprecation in a future release.",
+        PendingDeprecationWarning,
+    )
     if not (args.name or args.prefix):
         from ..exceptions import CondaValueError
         raise CondaValueError('either -n NAME or -p PREFIX option required,\n'
