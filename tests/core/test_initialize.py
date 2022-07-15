7a4b3d381fbbf0c7382eb4a3df491bac4120050c
Wolfgang Ulmer <wulmer@web.de>
2022-07-16 00:42:49 +0200

    Adapt test_init_sh_user_windows

--- tests/core/test_initialize.py
+++ tests/core/test_initialize.py
@@ -860,7 +860,9 @@ class InitializeTests(TestCase):
 
             # >>> conda initialize >>>
             # !! Contents within this block are managed by 'conda init' !!
-            eval "$('%(cygpath_conda_prefix)s/Scripts/conda.exe' 'shell.bash' 'hook')"
+            if [ -f %(cygpath_conda_prefix)s ]; then
+                eval "$('%(cygpath_conda_prefix)s/Scripts/conda.exe' 'shell.bash' 'hook')"
+            fi
             # <<< conda initialize <<<
 
             # . etc/profile.d/conda.sh  # commented out by conda initialize
