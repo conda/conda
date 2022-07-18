3472b5b76382cb22aa4fc9fb0d79a7dfa40364ec
Orion Poplawski <orion@nwra.com>
2022-07-17 21:05:34 -0600

    Use updated ruamel.yaml API
    stringio

--- conda/common/serialize.py
+++ conda/common/serialize.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+from io import StringIO
 import json
 from logging import getLogger
 
@@ -34,7 +35,8 @@ yaml.representer.SafeRepresenter.add_representer(odict, represent_ordereddict)
 
 
 def yaml_round_trip_load(string):
-    return yaml.round_trip_load(string, version="1.2")
+    yinst = yaml.YAML(typ="rt")
+    return yinst.load(string)
 
 
 def yaml_safe_load(string):
@@ -44,22 +46,35 @@ def yaml_safe_load(string):
         {'key': 'value'}
 
     """
-    return yaml.safe_load(string, version="1.2")
+    yinst = yaml.YAML(typ="safe", pure=True)
+    return yinst.load(string)
 
 
-def yaml_round_trip_dump(object):
-    """dump object to string"""
-    return yaml.round_trip_dump(
-        object, block_seq_indent=2, default_flow_style=False, indent=2
-    )
+def yaml_round_trip_dump(object, stream=None):
+    """dump object to string or stream"""
+    yinst = yaml.YAML(typ="rt")
+    yinst.indent(mapping=2, offset=2, sequence=4)
+    inefficient = False
+    if stream is None:
+        inefficient = True
+        stream = StringIO()
+    yinst.dump(object, stream)
+    if inefficient:
+        return stream.getvalue()
 
 
-def yaml_safe_dump(object):
-    """dump object to string"""
-    return yaml.safe_dump(
-        object, block_seq_indent=2, default_flow_style=False, indent=2
-    )
-
+def yaml_safe_dump(object, stream=None):
+    """dump object to string or stream"""
+    yinst = yaml.YAML(typ="safe", pure=True)
+    yinst.indent(mapping=2, offset=2, sequence=4)
+    yinst.default_flow_style = False
+    inefficient = False
+    if stream is None:
+        inefficient = True
+        stream = StringIO()
+    yinst.dump(object, stream)
+    if inefficient:
+        return stream.getvalue()
 
 def json_load(string):
     return json.loads(string)
