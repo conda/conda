1edb43180ebcccd074520bd6e9a9535d51181c74
Ken Odegard <kodegard@anaconda.com>
2022-07-21 15:26:37 -0500

    Move no ssl back into main test suite

--- dev/linux/no_ssl.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errtrace -o pipefail -o errexit
-
-eval "$(sudo /opt/conda/bin/python -m conda init --dev bash)"
-conda info
-pytest -v tests_no_ssl
--- dev/macos/no_ssl.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errtrace -o pipefail -o errexit
-
-eval "$(sudo python -m conda init bash --dev)"
-conda info
-pytest -v tests_no_ssl
