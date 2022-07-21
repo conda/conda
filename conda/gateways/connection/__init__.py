60e2b388a7c08b2ec7fe74393ac91f9d61a2ed86
Jannis Leidel <jannis@leidel.info>
2022-07-21 14:32:58 +0200

    Remove unneeded import.

--- conda/gateways/connection/__init__.py
+++ conda/gateways/connection/__init__.py
@@ -2,7 +2,6 @@
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
 from __future__ import absolute_import, division, print_function, unicode_literals
-from functools import partial
 
 
 try:
