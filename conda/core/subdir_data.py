7bd0dddad07c480a6dd29da1f32971f8ef2da17e
Christopher Howard <christopher.howard@gtri.gatech.edu>
2022-07-27 09:33:18 -0400

    Add TODO for FIPS MD5 refactor when Python 3.9+

--- conda/core/subdir_data.py
+++ conda/core/subdir_data.py
@@ -905,6 +905,9 @@ def cache_fn_url(url, repodata_fn=REPODATA_FN):
     #    are looking for the cache under keys without this.
     if repodata_fn != REPODATA_FN:
         url += repodata_fn
+
+    # TODO: remove try-except when conda only supports Python 3.9+, as
+    # `usedforsecurity=False` was added in 3.9.
     try:
         md5 = hashlib.md5(ensure_binary(url))
     except ValueError:
