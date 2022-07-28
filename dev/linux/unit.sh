84eef626e8c76a441a5a431c35a6de0707bec4e6
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-07-28 19:32:05 +0200

    remove temporary blocks [skip ci]

--- dev/linux/unit.sh
+++ dev/linux/unit.sh
@@ -5,9 +5,6 @@ set -o errtrace -o pipefail -o errexit
 TEST_SPLITS="${TEST_SPLITS:-1}"
 TEST_GROUP="${TEST_GROUP:-1}"
 
-# REMOVE BEFORE MERGE
-sudo su root -c "/opt/conda/bin/conda install -yq conda-forge::pytest-xprocess boto3"
-# /REMOVE
 eval "$(sudo /opt/conda/bin/python -m conda init --dev bash)"
 conda info
 # remove the pkg cache.  We can't hardlink from here anyway.  Having it around causes log problems.
