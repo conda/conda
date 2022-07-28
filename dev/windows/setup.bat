bcd125d3d4488c18d2becf687eb843882b8a43fc
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-07-28 22:44:03 +0200

    Add tests for S3 channels using minio locally (#11644)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Jannis Leidel <jannis@leidel.info>

--- dev/windows/setup.bat
+++ dev/windows/setup.bat
@@ -19,6 +19,11 @@ CALL conda create -n conda-test-env -y python=%PYTHON% pywin32 --file=tests\requ
 CALL conda activate conda-test-env || goto :error
 CALL conda update openssl ca-certificates certifi || goto :error
 python -m conda init cmd.exe --dev || goto :error
+
+:: Download minio server needed for S3 tests and place it in our conda environment so is in PATH
+:: certutil somehow is able to download arbitrary files; don't aske me why: https://superuser.com/a/1545689
+certutil -urlcache -split -f "https://dl.minio.io/server/minio/release/windows-amd64/minio.exe" "%CONDA_PREFIX%\minio.exe" || goto :error
+
 goto :EOF
 
 :error
