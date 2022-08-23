83860699c6e0b1137cf271f5a0fac9685f46d74c
Daniel Holth <dholth@anaconda.com>
2022-08-23 17:43:42 -0400

    basetemp in ~/.conda

--- dev/linux/integration.sh
+++ dev/linux/integration.sh
@@ -9,5 +9,5 @@ eval "$(sudo /opt/conda/bin/python -m conda init --dev bash)"
 sudo su root -c "/opt/conda/bin/conda install -yq conda-build"
 conda-build tests/test-recipes/activate_deactivate_package tests/test-recipes/pre_link_messages_package
 conda info
-pytest -m "integration" --basetemp=/opt/conda/$(date -Iseconds) -v --splits ${TEST_SPLITS} --group=${TEST_GROUP}
+pytest -m "integration" --basetemp=$HOME/.conda/tmp-$(date -Iseconds) -v --splits ${TEST_SPLITS} --group=${TEST_GROUP}
 python -m conda.common.io
