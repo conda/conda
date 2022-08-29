4f3d9015f8d99310c9158096377605ea4cea199a
Daniel Holth <dholth@anaconda.com>
2022-08-29 09:38:42 -0400

    tests: remove dead code

--- conda/testing/integration.py
+++ conda/testing/integration.py
@@ -356,39 +356,6 @@ def make_temp_package_cache():
             del PackageCacheData._cache_[pkgs_dir]
 
 
-def fixurl(url):
-    # turn string into unicode
-    if not isinstance(url, str):
-        url = url.decode("utf8")
-
-    # parse it
-    parsed = urlparse.urlsplit(url)
-
-    # divide the netloc further
-    userpass, at, hostport = parsed.netloc.rpartition("@")
-    user, colon1, pass_ = userpass.partition(":")
-    host, colon2, port = hostport.partition(":")
-
-    # encode each component
-    scheme = parsed.scheme.encode("utf8")
-    user = urllib.quote(user.encode("utf8"))
-    colon1 = colon1.encode("utf8")
-    pass_ = urllib.quote(pass_.encode("utf8"))
-    at = at.encode("utf8")
-    host = host.encode("idna")
-    colon2 = colon2.encode("utf8")
-    port = port.encode("utf8")
-    path = "/".join(  # could be encoded slashes!
-        urllib.quote(urllib.unquote(pce).encode("utf8"), "") for pce in parsed.path.split("/")
-    )
-    query = urllib.quote(urllib.unquote(parsed.query).encode("utf8"), "=&?/")
-    fragment = urllib.quote(urllib.unquote(parsed.fragment).encode("utf8"))
-
-    # put it back together
-    netloc = "".join((user, colon1, pass_, at, host, colon2, port))
-    return urlparse.urlunsplit((scheme, netloc, path, query, fragment))
-
-
 @contextmanager
 def make_temp_channel(packages):
     package_reqs = [pkg.replace("-", "=") for pkg in packages]
