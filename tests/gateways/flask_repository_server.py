8cb3a63adb5ea99ccd82ecd1d6f6001296b0725b
Daniel Holth <dholth@anaconda.com>
2022-08-30 11:08:25 -0400

    shell of flask test server

---
+++ tests/gateways/flask_repository_server.py
@@ -0,0 +1,18 @@
+"""
+Flask-based conda repository server for testing.
+
+Change contents to simulate an updating repository.
+"""
+
+import flask
+
+app = flask.Flask(__name__)
+
+# flask.send_from_directory(directory, path, **kwargs)
+# Send a file from within a directory using send_file().
+@app.route("/uploads/<path:name>")
+def download_file(name):
+    # conditional=True is the default
+    return flask.send_from_directory(
+        app.config['UPLOAD_FOLDER'], name
+    )
