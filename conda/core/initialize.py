f8aa5b2d7d3ec65c9ae4b1cec862fd585abf3a85
Wolfgang Ulmer <wolfgang.ulmer@de.bosch.com>
2022-09-13 12:06:40 +0200

    Make pwsh profile script more robust

--- conda/core/initialize.py
+++ conda/core/initialize.py
@@ -1510,7 +1510,7 @@ def _powershell_profile_content(conda_prefix):
     #region conda initialize
     # !! Contents within this block are managed by 'conda init' !!
     If (Test-Path "{conda_exe}") {{
-        (& "{conda_exe}" "shell.powershell" "hook") | Out-String | Invoke-Expression
+        (& "{conda_exe}" "shell.powershell" "hook") | Out-String | ?{{$_}} | Invoke-Expression
     }}
     #endregion
     """.format(conda_exe=conda_exe))
