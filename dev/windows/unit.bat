3c768f403fa4d38b1e46d053b4f8461b9873cac5
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-09-22 16:24:36 +0200

    same for windows

--- dev/windows/unit.bat
+++ dev/windows/unit.bat
@@ -1,10 +1,10 @@
 @ECHO ON
 pushd %TEMP% || goto :error
 cd \conda_src || goto :error
+REM TODO:  Remove before merge, temporary:
+CALL conda install "jaimergp/label/menuinst_dev::menuinst=2" --no-deps || goto :error
 CALL dev-init.bat || goto :error
 CALL conda info || goto :error
-REM TODO:  Remove before merge, temporary:
-CALL conda install -c jaimergp/label/menuinst_dev -c conda-forge "menuinst=2" || goto :error
 CALL pytest -m "not integration" -v --splits=%TEST_SPLITS% --group=%TEST_GROUP% || goto :error
 goto :EOF
 
