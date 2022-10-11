9fb8d75e79be039dd15b2da680f152f1f4a80aa6
Daniel Holth <dholth@anaconda.com>
2022-10-11 14:54:33 -0400

    absolute path

--- dev/linux/integration.sh
+++ dev/linux/integration.sh
@@ -7,7 +7,7 @@ TEST_GROUP="${TEST_GROUP:-1}"
 
 sudo su root -c "/opt/conda/bin/conda install -yq conda-build"
 # make sure all test requirements are installed
-sudo conda install -y -c defaults --file tests/requirements.txt
+sudo /opt/conda/bin/conda install -y -c defaults --file tests/requirements.txt
 conda info
 eval "$(sudo /opt/conda/bin/python -m conda init --dev bash)"
 conda-build tests/test-recipes/activate_deactivate_package tests/test-recipes/pre_link_messages_package
