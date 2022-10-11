2e9d00454d346a0100e8fc0e8d1c4cd853b5b801
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-10-11 17:22:22 +0200

    argparse.Action base class implements __call__ abstractly - do not super()!

--- conda/cli/conda_argparse.py
+++ conda/cli/conda_argparse.py
@@ -250,13 +250,11 @@ class ExtendConstAction(Action):
 class PendingDeprecationAction(Action):
     def __call__(self, parser, namespace, values, option_string=None):
         warnings.warn(f"Option {self.option_strings} is pending deprecation.", PendingDeprecationWarning)
-        super().__call__(parser, namespace, values, option_string)
 
 
 class DeprecatedAction(Action):
     def __call__(self, parser, namespace, values, option_string=None):
         warnings.warn(f"Option {self.option_strings} is deprecated!", DeprecationWarning)
-        super().__call__(parser, namespace, values, option_string)
 
 
 # #############################################################################################
