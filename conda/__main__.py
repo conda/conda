7e298071417bb1a8b0b9dc9ed446ed8f9e8fc4ba
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-10-13 16:35:14 +0200

    fix sys.path in __main__ too

--- conda/__main__.py
+++ conda/__main__.py
@@ -2,6 +2,13 @@
 # SPDX-License-Identifier: BSD-3-Clause
 import sys
 
+# Before any more imports, leave cwd out of sys.path for internal 'conda shell.*' commands.
+# see https://github.com/conda/conda/issues/6549
+if len(sys.argv) > 1 and sys.argv[1].startswith('shell.') and sys.path and sys.path[0] == '':
+    # The standard first entry in sys.path is an empty string,
+    # and os.path.abspath('') expands to os.getcwd().
+    del sys.path[0]
+
 from .cli import main
 
 sys.exit(main())
