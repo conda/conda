c7e6a64f12feaed7cf79f49277378b7e515d95e2
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-10-19 10:47:26 +0200

    and yet more quotes?

--- tests/core/test_initialize.py
+++ tests/core/test_initialize.py
@@ -766,7 +766,7 @@ class InitializeTests(TestCase):
 
             # >>> conda initialize >>>
             # !! Contents within this block are managed by 'conda init' !!
-            __conda_setup="$('%(prefix)s/bin/python' '-I' '-m' 'conda' 'shell.bash' 'hook' 2> /dev/null)"
+            __conda_setup="$('%(prefix)s/bin/python' -I -m conda 'shell.bash' hook 2> /dev/null)"
             if [ $? -eq 0 ]; then
                 eval "$__conda_setup"
             else
