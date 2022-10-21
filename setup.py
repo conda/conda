70bb1b160ea855cf9f54e029e3d94715aef86f25
Mahe Iram Khan <maheiramkhan@Mahes-MacBook-Pro.fritz.box>
2022-10-21 13:05:19 +0200

    add tqdm to setup.py

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ install_requires = [
     "requests >=2.20.1",
     "ruamel.yaml >=0.11.14",
     "menuinst ; platform_system=='Windows'",
+    "tqdm >=4.64.1",
 ]
 
 def package_files(*root_directories):
