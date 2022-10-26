306e4a93433267374a77cb369e13fc4440e2ce57
Bianca Henderson <bhenderson@anaconda.com>
2022-10-26 13:14:27 -0400

    Change location of cache clearing in virtual package cuda tests

--- tests/plugins/test_virtual_packages.py
+++ tests/plugins/test_virtual_packages.py
@@ -74,14 +74,14 @@ def test_cuda_detection(request):
 
 
 def test_cuda_override():
+    cuda.cached_cuda_version.cache_clear()  # prevents test from receiving stale value
     with env_var('CONDA_OVERRIDE_CUDA', '4.5'):
         version = cuda.cached_cuda_version()
         assert version == "4.5"
-        cuda.cached_cuda_version.cache_clear()  # prevents test from receiving stale value
 
 
 def test_cuda_override_none():
+    cuda.cached_cuda_version.cache_clear()  # prevents test from receiving stale value
     with env_var('CONDA_OVERRIDE_CUDA', ''):
         version = cuda.cuda_version()
         assert version is None
-        cuda.cached_cuda_version.cache_clear()  # prevents test from receiving stale value
