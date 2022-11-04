b6c3992e33de28d4bf084023e663b77abdb74081
Ken Odegard <kodegard@anaconda.com>
2022-11-04 09:56:15 -0500

    PluginManager.load_default

--- conda/plugins/manager.py
+++ conda/plugins/manager.py
@@ -8,10 +8,16 @@ from . import virtual_packages
 from .hookspec import CondaSpecs, spec_name
 
 
+class PluginManager(pluggy.PluginManager):
+    def load_default(self):
+        for plugin in virtual_packages.plugins:
+            self.register(plugin)
+
+
 @functools.lru_cache(maxsize=None)  # FUTURE: Python 3.9+, replace w/ functools.cache
 def get_plugin_manager() -> pluggy.PluginManager:
-    pm = pluggy.PluginManager(spec_name)
+    pm = PluginManager(spec_name)
     pm.add_hookspecs(CondaSpecs)
-    virtual_packages.register(pm)
+    pm.load_default()
     pm.load_setuptools_entrypoints(spec_name)
     return pm
--- conda/plugins/virtual_packages/__init__.py
+++ conda/plugins/virtual_packages/__init__.py
@@ -2,17 +2,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 from __future__ import annotations
 
-import pluggy
-
 from . import archspec, cuda, linux, osx, windows
 
 
-def register(pm: pluggy.PluginManager) -> None:
-    """
-    Please add a nice doc string too explaining why this function exists!
-    """
-    pm.register(archspec)
-    pm.register(cuda)
-    pm.register(linux)
-    pm.register(osx)
-    pm.register(windows)
+plugins = [archspec, cuda, linux, osx, windows]
