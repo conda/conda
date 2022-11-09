27e33cc551a28c4de45f729d871f6138496b3da5
Jannis Leidel <jannis@leidel.info>
2022-11-09 14:49:11 +0100

    Add plugins loader.

--- conda/plugins/manager.py
+++ conda/plugins/manager.py
@@ -1,23 +1,42 @@
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
+from __future__ import annotations
+
 import functools
 
 import pluggy
 
 from . import virtual_packages
 from .hookspec import CondaSpecs, spec_name
+from ..exceptions import PluginError
 
 
-class PluginManager(pluggy.PluginManager):
-    def registers(self, *plugins):
+class CondaPluginManager(pluggy.PluginManager):
+    """
+    The conda plugin manager to implement behavior additional to
+    pluggy's default plugin manager.
+    """
+    def __init__(self, project_name: str | None = None, *args, **kwargs):
+        if project_name is None:
+            project_name = spec_name
+        super().__init__(project_name, *args, **kwargs)
+
+    def load_plugins(self, *plugins) -> list[str]:
+        plugin_names = []
         for plugin in plugins:
-            self.register(plugin)
+            try:
+                plugin_name = self.register(plugin)
+            except ValueError as err:
+                raise PluginError(f"Error while registering conda plugins: {err}")
+            else:
+                plugin_names.append(plugin_name)
+        return plugin_names
 
 
 @functools.lru_cache(maxsize=None)  # FUTURE: Python 3.9+, replace w/ functools.cache
-def get_plugin_manager() -> pluggy.PluginManager:
-    pm = PluginManager(spec_name)
+def get_plugin_manager() -> CondaPluginManager:
+    pm = CondaPluginManager()
     pm.add_hookspecs(CondaSpecs)
-    pm.registers(*virtual_packages.plugins)
+    pm.load_plugins(*virtual_packages.plugins)
     pm.load_setuptools_entrypoints(spec_name)
     return pm
--- conda/plugins/virtual_packages/__init__.py
+++ conda/plugins/virtual_packages/__init__.py
@@ -4,5 +4,5 @@ from __future__ import annotations
 
 from . import archspec, cuda, linux, osx, windows
 
-
+#: The list of virtual package plugins for easier registration with pluggy
 plugins = [archspec, cuda, linux, osx, windows]
