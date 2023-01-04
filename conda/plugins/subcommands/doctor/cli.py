9bd20fe524b0fd8ece09c979f825e08f9f1c502d
Mahe Iram Khan <forgotten.programme@gmail.com>
2023-01-04 16:46:01 +0100

    add a detailed_health_checks func

--- conda/plugins/subcommands/doctor/cli.py
+++ conda/plugins/subcommands/doctor/cli.py
@@ -3,19 +3,28 @@
 import argparse
 from . import health_checks
 
-parser = argparse.ArgumentParser(
-    "conda doctor", description="Display a health report for your environment."
-)
-parser.add_argument(
-    "-v", "--verbose", action="store_true", help="generate a detailed environment health report"
-)
-args = parser.parse_args()
+# from ... import CondaSubcommand, hookimpl
+from ....base.context import context
 
-if args.verbose:
-    print("A detailed health report is given below:")
-    packages = health_checks.find_packages_with_missing_files
-    print(health_checks.format_error_message(packages))
 
-def display_health_check(health_check: health_checks.HealthCheck) -> None:
-    print(health_check.title)
-    print(health_check.description)
+def get_parsed_args(argv: list[str]) -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        "conda doctor", description="Display a health report for your environment."
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="generate a detailed environment health report",
+    )
+    args = parser.parse_args(argv)
+
+    return args
+
+
+def display_health_checks(args: argparse.Namespace) -> None:
+    if args.verbose:
+        print("A more detailed environment health report is given below:")
+        health_checks.run_health_checks(context)
+    else:
+        health_checks.run_detailed_health_checks(context)
--- conda/plugins/subcommands/doctor/health_checks.py
+++ conda/plugins/subcommands/doctor/health_checks.py
@@ -4,9 +4,10 @@ import json
 
 import conda.plugins
 
-from pathlib import Path
+from pathlib import Path, PurePath
 from rich.console import Console
 from rich.table import Table
+from rich import print
 
 from conda.base.context import context
 
@@ -14,9 +15,11 @@ from conda.base.context import context
 REPORT_TITLE = "\nENVIRONMENT HEALTH REPORT\n"
 
 active_prefix = context.active_prefix
+environment = PurePath(active_prefix)
+environment_name = environment.name
 
 
-def condensed_missing_files(prefix: str):
+def get_number_of_missing_files(prefix: str):
     """Print number of missing files for each package"""
     packages_with_missing_files = find_packages_with_missing_files(prefix)
 
@@ -35,7 +38,24 @@ def condensed_missing_files(prefix: str):
         console.print(table)
 
     else:
-        print("There are no packages with missing files.")
+        print(":white_check_mark: There are no packages with missing files.\n")
+
+
+def get_names_of_missing_files(prefix: str):
+    """Print the names of missing files in each package"""
+    packages_with_missing_files = find_packages_with_missing_files(prefix)
+
+    if packages_with_missing_files:
+        table = Table(title="Packages With Missing Files")
+
+        table.add_column("Package Name", justify="right", style="cyan", no_wrap=True)
+        table.add_column("Missing Files", style="magenta")
+
+        for k in packages_with_missing_files:
+            table.add_row(str(k), str(packages_with_missing_files[k]))
+
+        console = Console()
+        console.print(table)
 
 
 def find_packages_with_missing_files(prefix: str):
@@ -63,8 +83,14 @@ def find_packages_with_missing_files(prefix: str):
 
 def run_health_checks(prefix: str):
     print(REPORT_TITLE)
-    condensed_missing_files(active_prefix)
+    print(f"Name: {environment_name}\n")
+    get_number_of_missing_files(active_prefix)
 
+
+def run_detailed_health_checks(prefix: str):
+    print(REPORT_TITLE)
+    print(f"Name: {environment_name}\n")
+    get_names_of_missing_files(active_prefix)
 
 @conda.plugins.hookimpl
 def conda_subcommands():
