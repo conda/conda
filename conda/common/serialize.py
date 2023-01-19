0cb34d98267336926ec60e70fd873d2a6649c264
Ken Odegard <kodegard@anaconda.com>
2023-01-18 23:05:38 -0600

    Don't sort mapping keys

--- conda/common/serialize.py
+++ conda/common/serialize.py
@@ -34,6 +34,7 @@ def _yaml_safe():
     parser = yaml.YAML(typ="safe", pure=True)
     parser.indent(mapping=2, offset=2, sequence=4)
     parser.default_flow_style = False
+    parser.sort_base_mapping_type_on_output = False
     return parser
 
 
