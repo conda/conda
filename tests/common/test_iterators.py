10c424dea242bedeedd118b39b7851db122c565d
Ken Odegard <kodegard@anaconda.com>
2023-01-20 16:03:00 -0600

    Add missing import

--- tests/common/test_iterators.py
+++ tests/common/test_iterators.py
@@ -1,5 +1,8 @@
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
+import warnings
+
+
 def test_unpacking_for_merge():
     warnings.warn(
         "`toolz` is pending deprecation and will be removed in a future release.",
