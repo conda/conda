94c95d3142011f5b655af47eec61542bfe989b83
Daniel Holth <dholth@anaconda.com>
2023-01-31 21:58:37 -0500

    try integer division on nanoseconds

--- conda/gateways/repodata/__init__.py
+++ conda/gateways/repodata/__init__.py
@@ -632,7 +632,7 @@ class RepodataCache:
         max_age *= 10**9  # nanoseconds
         now = time.time_ns()
         refresh = self.state.get("refresh_ns", 0)
-        return ((now - refresh) + max_age) / 10**9
+        return ((now - refresh) + max_age) // 10**9
 
 
 try:
