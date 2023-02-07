11035e094c8b81d463c468d39d62fab85f983203
Daniel Holth <dholth@anaconda.com>
2023-02-07 08:58:06 -0500

    skip no-same-owner hack for conda-package-handling 2.x

--- conda/common/compat.py
+++ conda/common/compat.py
@@ -95,7 +95,7 @@ def ensure_binary(value):
         return value
 
 
-def ensure_text_type(value):
+def ensure_text_type(value) -> str:
     try:
         return value.decode('utf-8')
     except AttributeError:  # pragma: no cover
