bfeec628eba399515df5b6f81f0662c70adcddb6
Travis Hathaway <travis.j.hathaway@gmail.com>
2023-02-07 15:06:41 +0100

    Channel Notices doesn't handle i/o errors (#12312)
    * adding a quick fix to a permissions issue encountered while trying to update a cache file
    
    * updating notices to be more forgiving of errors when reading/writing cache files
    
    * check the exit code for new test

--- conda/cli/main_notices.py
+++ conda/cli/main_notices.py
@@ -3,6 +3,7 @@
 
 from argparse import Namespace, ArgumentParser
 
+from ..exceptions import CondaError
 from ..notices import core as notices
 
 
@@ -10,6 +11,9 @@ def execute(args: Namespace, _: ArgumentParser):
     """
     Command that retrieves channel notifications, caches them and displays them.
     """
-    channel_notice_set = notices.retrieve_notices()
+    try:
+        channel_notice_set = notices.retrieve_notices()
+    except OSError as exc:
+        raise CondaError(f"Unable to retrieve notices: {exc}")
 
     notices.display_notices(channel_notice_set)
