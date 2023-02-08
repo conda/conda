2ef71edf9398f9b3d9520b83ad6916585b10530c
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2023-02-08 16:12:13 +0100

    git fixes

--- dev/linux/bashrc.sh
+++ dev/linux/bashrc.sh
@@ -6,6 +6,10 @@ print_conda_info() {
 }
 
 echo "Initializing conda in dev mode..."
+
+### Prevent git safety errors when mounting directories ###
+git config --global --add safe.directory /opt/conda-src
+
 echo "Factory config is:"
 print_conda_info
 eval "$(sudo /opt/conda/bin/python -m conda init --dev bash)"
--- dev/linux/unit.sh
+++ dev/linux/unit.sh
@@ -2,6 +2,9 @@
 
 set -o errtrace -o pipefail -o errexit
 
+### Prevent git safety errors when mounting directories ###
+git config --global --add safe.directory /opt/conda-src
+
 TEST_SPLITS="${TEST_SPLITS:-1}"
 TEST_GROUP="${TEST_GROUP:-1}"
 
