7614d14a5a307052d037129485777d4aa1f930bf
Daniel Holth <dholth@anaconda.com>
2023-02-09 12:02:21 -0500

    add display_health_checks tests

--- tests/plugins/subcommands/doctor/test_health_checks.py
+++ tests/plugins/subcommands/doctor/test_health_checks.py
@@ -1,9 +1,13 @@
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
-from pathlib import Path
-from conda.plugins.subcommands.doctor import health_checks
 import json
+from pathlib import Path
+
 import pytest
+
+from conda.base.context import conda_tests_ctxt_mgmt_def_pol
+from conda.common.io import env_vars
+from conda.plugins.subcommands.doctor import health_checks
 
 BIN_TEST_EXE = "bin/test-exe"
 LIB_TEST_PACKAGE = "lib/test-package.py"
@@ -38,7 +42,7 @@ def conda_mock_dir_missing_files(conda_mock_dir):
     return conda_mock_dir
 
 
-def test_find_packages_with_no_missing_files(conda_mock_dir):
+def test_find_packages_with_no_missing_files(conda_mock_dir):  # XXX
     """Test that runs for the case with no missing files"""
     result = health_checks.find_packages_with_missing_files(conda_mock_dir)
     assert result == {}
@@ -71,3 +75,33 @@ def test_get_names_of_missing_files_when_no_missing_files(conda_mock_dir):
     result = health_checks.find_packages_with_missing_files(conda_mock_dir)
     TEST_PACKAGE_JSON = "test-package"
     assert result == {}
+
+
+def test_display_health_checks(conda_mock_dir):
+    """
+    Run display_health_checks without and with missing files.
+    """
+    with env_vars(
+        {
+            "CONDA_PREFIX": conda_mock_dir,
+        },
+        stack_callback=conda_tests_ctxt_mgmt_def_pol,
+    ):
+        health_checks.display_health_checks()
+        Path(conda_mock_dir, BIN_TEST_EXE).unlink()
+        health_checks.display_health_checks()
+
+
+def test_display_detailed_health_checks(conda_mock_dir):
+    """
+    Run display_detailed_health_checks without and with missing files.
+    """
+    with env_vars(
+        {
+            "CONDA_PREFIX": conda_mock_dir,
+        },
+        stack_callback=conda_tests_ctxt_mgmt_def_pol,
+    ):
+        health_checks.display_detailed_health_checks()
+        Path(conda_mock_dir, BIN_TEST_EXE).unlink()
+        health_checks.display_detailed_health_checks()
