ff435b10265b56a7948f00761b0b1e35b7e666a7
Jannis Leidel <jannis@leidel.info>
2023-03-01 22:46:15 +0100

    Build UML files on RTD separately.

--- docs/source/_ext/conda_umls.py
+++ docs/source/_ext/conda_umls.py
@@ -61,7 +61,7 @@ def post_process(files, output_path):
                 sys.stdout.write(line)
 
 
-def generate_pumls(app, config):
+def generate_pumls(app=None, config=None):
     """
     Generates PlantUML files for the given packages and writes
     the files to the components directory in the documentation source.
@@ -124,6 +124,7 @@ def setup(app):
     if "AUTOBUILD" not in os.environ:
         app.add_config_value("plantuml_jarfile_path", None, rebuild="")
         app.connect("config-inited", download_plantuml)
+    if "READTHEDOCS" not in os.environ:
         app.connect("config-inited", generate_pumls)
 
     return {
@@ -131,3 +132,7 @@ def setup(app):
         "parallel_read_safe": False,
         "parallel_write_safe": False,
     }
+
+
+if __name__ == "__main__":
+    generate_pumls()
