1a39ca992331fa99558598b09302d9611c06a188
Ken Odegard <kodegard@anaconda.com>
2023-03-21 09:18:49 -0500

    Rely on setuptools_scm instead of custom git implementation

--- conda/__version__.py
+++ conda/__version__.py
@@ -4,58 +4,8 @@
 
 The logic here is used during development installs only so keep it simple. Since conda
 abides by CEP-8, which outlines using CalVer, our development version is simply:
-    YY.MM.0.devN+HASH[.dirty]
+    YY.MM.0.devN[+dirty]
 """
-from contextlib import suppress
-from datetime import date
-from pathlib import Path
-from subprocess import run, CalledProcessError
+from setuptools_scm import get_version
 
-from .common.compat import on_win
-
-release = f"{date.today():%y.%#m}.0" if on_win else f"{date.today():%y.%-m}.0"
-dev = "0"
-commit = "nogit"
-dirty = ""
-with suppress(FileNotFoundError, CalledProcessError, ValueError):
-    # FileNotFoundError: git isn't installed
-    # CalledProcessError: got an error while running git
-    # ValueError: unable to split describe string
-    path = Path(__file__).parent
-
-    response = run(
-        ["git", "-C", path, "rev-parse", "--short", "HEAD"],
-        check=True,
-        capture_output=True,
-        text=True,
-    )
-    if response.stdout and not response.stderr:
-        commit = response.stdout.strip()
-
-    response = run(
-        [
-            "git",
-            "-C",
-            path,
-            "describe",
-            "--tags",
-            "--always",
-            "--long",
-            "--first-parent",
-            "--dirty",
-        ],
-        check=True,
-        capture_output=True,
-        text=True,
-    )
-    if response.stdout and not response.stderr:
-        older, dev, _ = response.stdout.strip().split("-", 2)
-
-        if older.startswith(release[:-1]):
-            release = f"{release[:-1]}{int(older.rsplit('.', 1)[1]) + 1}"
-
-        if response.stdout.strip().endswith("-dirty"):
-            dirty = ".dirty"
-
-
-__version__ = f"{release}.dev{dev}+{commit}{dirty}"
+__version__ = get_version(root="..", relative_to=__file__)
