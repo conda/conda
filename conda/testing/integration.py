953393fa48d667f48eee0bc0e767ce89cf7bd3f9
Ken Odegard <kodegard@anaconda.com>
2023-04-13 16:11:20 -0500

    Revert run_command changes

--- conda/testing/integration.py
+++ conda/testing/integration.py
@@ -196,7 +196,6 @@ class Commands:
     SEARCH = "search"
     UPDATE = "update"
     RUN = "run"
-    DOCTOR = "doctor"
 
 
 @contextmanager
@@ -249,7 +248,6 @@ def run_command(command, prefix, *arguments, **kwargs):
         Commands.REMOVE,
         Commands.UPDATE,
         Commands.RUN,
-        Commands.DOCTOR,
     ):
         arguments.insert(0, "-p")
         arguments.insert(1, prefix)
