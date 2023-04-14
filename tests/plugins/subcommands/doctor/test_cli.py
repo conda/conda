05d363f60d1dcb12abae9d114764ccd6a1473b99
Mahe Iram Khan <forgotten.programme@gmail.com>
2023-04-14 14:20:04 -0700

    remove commented test

--- tests/plugins/subcommands/doctor/test_cli.py
+++ tests/plugins/subcommands/doctor/test_cli.py
@@ -57,9 +57,3 @@ def test_conda_doctor_with_test_environment(env_one):
     assert MISSING_FILES_SUCCESS_MESSAGE in out
     assert err == ""  # no error message
     assert exit_code is None
-
-
-# def test_conda_doctor_prints_desired_detail_report(env_one):
-#    """
-#    Prints a detailed report in exactly the format we expect.
-#    """
