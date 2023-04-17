03f3ea782708b31c871a1bd0f59a2e6086faa9fe
Preston Moore <pkmoore@anaconda.com>
2023-04-17 12:28:45 -0400

    Remove conditional import for debugging purposes

--- conda/trust/signature_verification.py
+++ conda/trust/signature_verification.py
@@ -14,17 +14,17 @@ from ..gateways.connection import HTTPError, InsecureRequestWarning
 from ..gateways.connection.session import CondaSession
 from .constants import INITIAL_TRUST_ROOT, KEY_MGR_FILE
 
-try:
-    from conda_content_trust.authentication import verify_delegation, verify_root
-    from conda_content_trust.common import (
-        SignatureError,
-        load_metadata_from_file,
-        write_metadata_to_file,
-    )
-    from conda_content_trust.signing import wrap_as_signable
-except ImportError:
-    # SignatureStatus.enabled handles this
-    pass
+#try:
+from conda_content_trust.authentication import verify_delegation, verify_root
+from conda_content_trust.common import (
+    SignatureError,
+    load_metadata_from_file,
+    write_metadata_to_file,
+)
+#    from conda_content_trust.signing import wrap_as_signable
+#except ImportError:
+#    # SignatureStatus.enabled handles this
+#    pass
 
 
 log = getLogger(__name__)
