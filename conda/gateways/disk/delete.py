68646ce11708d0604fb36945d397679096f07d14
Jerry Ding <jubilantjerry@gmail.com>
2023-04-30 02:35:43 +0800

    Fix rsync deletion and its error handling path
    See issue #12606

--- conda/gateways/disk/delete.py
+++ conda/gateways/disk/delete.py
@@ -115,7 +115,7 @@ def rmtree(path, *args, **kwargs):
                     out = check_output(
                         [
                             rsync,
-                            "-a",
+                            "-rlD",
                             "--force",
                             "--delete",
                             join(getcwd(), ".empty") + "/",
@@ -124,9 +124,7 @@ def rmtree(path, *args, **kwargs):
                         stderr=STDOUT,
                     )
                 except CalledProcessError:
-                    log.debug(
-                        f"removing dir contents the fast way failed.  Output was: {out}"
-                    )
+                    log.debug("removing dir contents the fast way failed.")
 
             shutil.rmtree(".empty")
     shutil.rmtree(path)
