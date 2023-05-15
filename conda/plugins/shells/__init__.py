de52ea078eb9ec40a63fc79ddec5d12420f76bf6
Katherine Abrikian <kalawac@users.noreply.github.com>
2023-05-15 13:01:33 -0400

    updated plugin list

--- conda/plugins/shells/__init__.py
+++ conda/plugins/shells/__init__.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 from __future__ import annotations
 
-from . import posix_os_exec, posix_os_exec_shell
+from . import posix_os_exec, posix_os_exec_shell, posix_activate
 
 #: The list of virtual package plugins for easier registration with pluggy
-plugins = [posix_os_exec, posix_os_exec_shell]
+plugins = [posix_os_exec, posix_os_exec_shell, posix_activate]
