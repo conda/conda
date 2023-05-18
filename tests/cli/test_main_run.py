fddb0b472e2c2bdd24f3083effa13d3537ba5651
Ken Odegard <kodegard@anaconda.com>
2023-05-18 18:12:25 -0500

    Fix imports

--- tests/cli/test_main_run.py
+++ tests/cli/test_main_run.py
@@ -8,6 +8,7 @@ from pathlib import Path
 
 import pytest
 
+from conda.common.compat import on_win
 from conda.testing import CondaCLIFixture, TmpEnvFixture
 
 
--- tests/cli/test_main_search.py
+++ tests/cli/test_main_search.py
@@ -1,26 +1,11 @@
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
 import json
-import os
 import re
-import stat
-import unittest
-import uuid
-from unittest.mock import patch
 
 import pytest
 
-from conda.base.constants import on_win
-from conda.base.context import context
-from conda.gateways.disk.delete import rm_rf
-from conda.testing import CondaCLIFixture, TmpEnvFixture
-from conda.testing.helpers import capture_json_with_argv, run_inprocess_conda_command
-from conda.testing.integration import (
-    Commands,
-    make_temp_env,
-    make_temp_prefix,
-    run_command,
-)
+from conda.testing import CondaCLIFixture
 
 
 @pytest.mark.integration
