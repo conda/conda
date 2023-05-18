8eae2cce609553caff32a0e8b823896a11098182
Ken Odegard <kodegard@anaconda.com>
2023-05-18 16:45:25 -0500

    Extract main test to test_main.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -258,8 +258,3 @@ def test_run_readonly_env(request):
         reset_permissions()
 
         assert result == 0
-
-
-def test_main():
-    with pytest.raises(SystemExit):
-        __import__("conda.__main__")
