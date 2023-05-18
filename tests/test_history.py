50fc8cf846b801cd5b97a24c3ea3dcfc9c7eee1b
Ken Odegard <kodegard@anaconda.com>
2023-05-17 23:08:11 -0500

    Group fixtures at the top of file

--- tests/test_history.py
+++ tests/test_history.py
@@ -16,6 +16,12 @@ def tmp_history(tmp_path: Path) -> History:
     return History(tmp_path)
 
 
+@pytest.fixture
+def user_requests() -> list[dict]:
+    # see tests/conda-meta/history
+    return History(dirname(__file__)).get_user_requests()
+
+
 def test_works_as_context_manager(tmp_history: History):
     assert getattr(tmp_history, "__enter__")
     assert getattr(tmp_history, "__exit__")
@@ -52,11 +58,6 @@ def test_parse_on_empty_env(tmp_history: History, mocker: MockerFixture):
         assert mock_parse.call_count == 0
         assert len(tmp_history.parse()) == 0
     assert len(tmp_history.parse()) == 1
-
-
-@pytest.fixture
-def user_requests() -> list[dict]:
-    return History(dirname(__file__)).get_user_requests()
 
 
 def test_len(user_requests: list[dict]):
