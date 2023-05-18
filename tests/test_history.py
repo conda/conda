b7cbf3068cf40100290ad4add5bd980d55541684
Ken Odegard <kodegard@anaconda.com>
2023-05-17 20:29:29 -0500

    Use mocker + pytest.mark.parametrize

--- tests/test_history.py
+++ tests/test_history.py
@@ -4,9 +4,9 @@ import unittest
 from os.path import dirname
 from pathlib import Path
 from pprint import pprint
-from unittest import mock
 
 import pytest
+from pytest_mock import MockerFixture
 
 from conda.history import History
 from conda.testing.cases import BaseTestCase
@@ -19,41 +19,39 @@ def test_works_as_context_manager():
     assert getattr(h, "__exit__")
 
 
-def test_calls_update_on_exit():
-    h = History("/path/to/prefix")
-    with mock.patch.object(h, "init_log_file") as init_log_file:
-        init_log_file.return_value = None
-        with mock.patch.object(h, "update") as update:
-            with h:
-                assert update.call_count == 0
-                pass
-        assert update.call_count == 1
+def test_calls_update_on_exit(mocker: MockerFixture, tmp_path: Path):
+    h = History(str(tmp_path))
+    update = mocker.spy(h, "update")
+
+    with h:
+        assert update.call_count == 0
+    assert update.call_count == 1
 
 
-def test_returns_history_object_as_context_object():
-    h = History("/path/to/prefix")
-    with mock.patch.object(h, "init_log_file") as init_log_file:
-        init_log_file.return_value = None
-        with mock.patch.object(h, "update"):
-            with h as h2:
-                assert h == h2
+def test_returns_history_object_as_context_object(
+    mocker: MockerFixture,
+    tmp_path: Path,
+):
+    h = History(str(tmp_path))
+
+    with h as h2:
+        assert h == h2
 
 
-def test_empty_history_check_on_empty_env():
-    with mock.patch.object(History, "file_is_empty") as mock_file_is_empty:
-        with History(make_temp_prefix()) as h:
-            assert mock_file_is_empty.call_count == 0
+def test_empty_history_check_on_empty_env(mocker: MockerFixture, tmp_path: Path):
+    mock_file_is_empty = mocker.spy(History, "file_is_empty")
+    with History(str(tmp_path)) as h:
         assert mock_file_is_empty.call_count == 0
         assert h.file_is_empty()
     assert mock_file_is_empty.call_count == 1
     assert not h.file_is_empty()
 
 
-def test_parse_on_empty_env(tmp_path: Path):
-    with mock.patch.object(History, "parse") as mock_parse:
-        with History(make_temp_prefix(name=str(tmp_path))) as h:
-            assert mock_parse.call_count == 0
-            assert len(h.parse()) == 0
+def test_parse_on_empty_env(mocker: MockerFixture, tmp_path: Path):
+    mock_parse = mocker.spy(History, "parse")
+    with History(str(tmp_path)) as h:
+        assert mock_parse.call_count == 0
+        assert len(h.parse()) == 0
     assert len(h.parse()) == 1
 
 
@@ -88,115 +86,100 @@ def test_last(user_requests):
     }
 
 
-def test_conda_comment_version_parsing():
-    assert History._parse_comment_line("# conda version: 4.5.1") == {
-        "conda_version": "4.5.1"
-    }
-    assert History._parse_comment_line("# conda version: 4.5.1rc1") == {
-        "conda_version": "4.5.1rc1"
-    }
-    assert History._parse_comment_line("# conda version: 4.5.1dev0") == {
-        "conda_version": "4.5.1dev0"
-    }
-
-
-def test_specs_line_parsing_44():
-    # New format (>=4.4)
-    item = History._parse_comment_line(
-        "# update specs: [\"param[version='>=1.5.1,<2.0']\"]"
-    )
-    pprint(item)
-    assert item == {
-        "action": "update",
-        "specs": [
-            "param[version='>=1.5.1,<2.0']",
-        ],
-        "update_specs": [
-            "param[version='>=1.5.1,<2.0']",
-        ],
-    }
-
-
-def test_specs_line_parsing_43():
-    # Old format (<4.4)
-    item = History._parse_comment_line("# install specs: param >=1.5.1,<2.0")
-    pprint(item)
-    assert item == {
-        "action": "install",
-        "specs": [
-            "param >=1.5.1,<2.0",
-        ],
-        "update_specs": [
-            "param >=1.5.1,<2.0",
-        ],
-    }
-
-    item = History._parse_comment_line(
-        "# install specs: param >=1.5.1,<2.0,0packagename >=1.0.0,<2.0"
-    )
-    pprint(item)
-    assert item == {
-        "action": "install",
-        "specs": [
-            "param >=1.5.1,<2.0",
-            "0packagename >=1.0.0,<2.0",
-        ],
-        "update_specs": [
-            "param >=1.5.1,<2.0",
-            "0packagename >=1.0.0,<2.0",
-        ],
-    }
-
-    item = History._parse_comment_line(
-        "# install specs: python>=3.5.1,jupyter >=1.0.0,<2.0,matplotlib >=1.5.1,<2.0,numpy >=1.11.0,<2.0,pandas >=0.19.2,<1.0,psycopg2 >=2.6.1,<3.0,pyyaml >=3.12,<4.0,scipy >=0.17.0,<1.0"
-    )
-    pprint(item)
-    assert item == {
-        "action": "install",
-        "specs": [
-            "python>=3.5.1",
-            "jupyter >=1.0.0,<2.0",
-            "matplotlib >=1.5.1,<2.0",
-            "numpy >=1.11.0,<2.0",
-            "pandas >=0.19.2,<1.0",
-            "psycopg2 >=2.6.1,<3.0",
-            "pyyaml >=3.12,<4.0",
-            "scipy >=0.17.0,<1.0",
-        ],
-        "update_specs": [
-            "python>=3.5.1",
-            "jupyter >=1.0.0,<2.0",
-            "matplotlib >=1.5.1,<2.0",
-            "numpy >=1.11.0,<2.0",
-            "pandas >=0.19.2,<1.0",
-            "psycopg2 >=2.6.1,<3.0",
-            "pyyaml >=3.12,<4.0",
-            "scipy >=0.17.0,<1.0",
-        ],
-    }
-
-    item = History._parse_comment_line("# install specs: _license >=1.0.0,<2.0")
-    pprint(item)
-    assert item == {
-        "action": "install",
-        "specs": [
-            "_license >=1.0.0,<2.0",
-        ],
-        "update_specs": [
-            "_license >=1.0.0,<2.0",
-        ],
-    }
-
-    item = History._parse_comment_line("# install specs: pandas,_license >=1.0.0,<2.0")
-    pprint(item)
-    assert item == {
-        "action": "install",
-        "specs": [
-            "pandas",
-            "_license >=1.0.0,<2.0",
-        ],
-        "update_specs": [
-            "pandas",
-            "_license >=1.0.0,<2.0",
-        ],
-    }
+@pytest.mark.parametrize(
+    "comment,spec",
+    [
+        # conda version parsing
+        pytest.param(
+            "# conda version: 4.5.1",
+            {"conda_version": "4.5.1"},
+            id="conda 4.5.1",
+        ),
+        pytest.param(
+            "# conda version: 4.5.1rc1",
+            {"conda_version": "4.5.1rc1"},
+            id="conda 4.5.1rc1",
+        ),
+        pytest.param(
+            "# conda version: 4.5.1dev0",
+            {"conda_version": "4.5.1dev0"},
+            id="conda 4.5.1dev0",
+        ),
+        # pre 4.4
+        pytest.param(
+            "# install specs: param >=1.5.1,<2.0",
+            {
+                "action": "install",
+                "specs": ["param >=1.5.1,<2.0"],
+                "update_specs": ["param >=1.5.1,<2.0"],
+            },
+            id="pre 4.4",
+        ),
+        pytest.param(
+            "# install specs: param >=1.5.1,<2.0,0packagename >=1.0.0,<2.0",
+            {
+                "action": "install",
+                "specs": ["param >=1.5.1,<2.0", "0packagename >=1.0.0,<2.0"],
+                "update_specs": ["param >=1.5.1,<2.0", "0packagename >=1.0.0,<2.0"],
+            },
+            id="pre 4.4",
+        ),
+        pytest.param(
+            "# install specs: python>=3.5.1,jupyter >=1.0.0,<2.0,matplotlib >=1.5.1,<2.0,numpy >=1.11.0,<2.0,pandas >=0.19.2,<1.0,psycopg2 >=2.6.1,<3.0,pyyaml >=3.12,<4.0,scipy >=0.17.0,<1.0",
+            {
+                "action": "install",
+                "specs": [
+                    "python>=3.5.1",
+                    "jupyter >=1.0.0,<2.0",
+                    "matplotlib >=1.5.1,<2.0",
+                    "numpy >=1.11.0,<2.0",
+                    "pandas >=0.19.2,<1.0",
+                    "psycopg2 >=2.6.1,<3.0",
+                    "pyyaml >=3.12,<4.0",
+                    "scipy >=0.17.0,<1.0",
+                ],
+                "update_specs": [
+                    "python>=3.5.1",
+                    "jupyter >=1.0.0,<2.0",
+                    "matplotlib >=1.5.1,<2.0",
+                    "numpy >=1.11.0,<2.0",
+                    "pandas >=0.19.2,<1.0",
+                    "psycopg2 >=2.6.1,<3.0",
+                    "pyyaml >=3.12,<4.0",
+                    "scipy >=0.17.0,<1.0",
+                ],
+            },
+            id="pre 4.4",
+        ),
+        pytest.param(
+            "# install specs: _license >=1.0.0,<2.0",
+            {
+                "action": "install",
+                "specs": ["_license >=1.0.0,<2.0"],
+                "update_specs": ["_license >=1.0.0,<2.0"],
+            },
+            id="pre 4.4",
+        ),
+        pytest.param(
+            "# install specs: pandas,_license >=1.0.0,<2.0",
+            {
+                "action": "install",
+                "specs": ["pandas", "_license >=1.0.0,<2.0"],
+                "update_specs": ["pandas", "_license >=1.0.0,<2.0"],
+            },
+            id="pre 4.4",
+        ),
+        # post 4.4
+        pytest.param(
+            """# update specs: ["param[version='>=1.5.1,<2.0']"]""",
+            {
+                "action": "update",
+                "specs": ["param[version='>=1.5.1,<2.0']"],
+                "update_specs": ["param[version='>=1.5.1,<2.0']"],
+            },
+            id="post 4.4",
+        ),
+    ],
+)
+def test_comment_parsing(comment: str, spec: dict):
+    assert History._parse_comment_line(comment) == spec
