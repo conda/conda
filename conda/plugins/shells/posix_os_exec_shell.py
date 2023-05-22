c31bbd20bbf8c8e2126f7230438f2ea88cb7d355
Katherine Abrikian <kalawac@users.noreply.github.com>
2023-05-22 07:38:05 -0400

    PosixActivator class now in plugin

--- conda/plugins/shells/posix_os_exec_shell.py
+++ conda/plugins/shells/posix_os_exec_shell.py
@@ -2,12 +2,68 @@
 # SPDX-License-Identifier: BSD-3-Clause
 import argparse
 import os
+import re
 
-from conda.activate import PosixActivator
+from conda import CONDA_PACKAGE_ROOT
+from conda.activate import _Activator, native_path_to_unix
 from conda.base.context import context
 from conda.cli.main import init_loggers
+from conda.common.compat import on_win
 from conda.exceptions import ArgumentError
 from conda.plugins import CondaShellPlugins, hookimpl
+
+
+class PosixPluginActivator(_Activator):
+    pathsep_join = ":".join
+    sep = "/"
+    path_conversion = staticmethod(native_path_to_unix)
+    script_extension = ".sh"
+    tempfile_extension = None  # output to stdout
+    command_join = "\n"
+
+    unset_var_tmpl = "unset %s"
+    export_var_tmpl = "export %s='%s'"
+    set_var_tmpl = "%s='%s'"
+    run_script_tmpl = '. "%s"'
+
+    hook_source_path = os.path.join(
+        CONDA_PACKAGE_ROOT,
+        "shell",
+        "etc",
+        "profile.d",
+        "conda.sh",
+    )
+
+    def _update_prompt(self, set_vars, conda_prompt_modifier):
+        ps1 = self.environ.get("PS1", "")
+        if "POWERLINE_COMMAND" in ps1:
+            # Defer to powerline (https://github.com/powerline/powerline) if it's in use.
+            return
+        current_prompt_modifier = self.environ.get("CONDA_PROMPT_MODIFIER")
+        if current_prompt_modifier:
+            ps1 = re.sub(re.escape(current_prompt_modifier), r"", ps1)
+        # Because we're using single-quotes to set shell variables, we need to handle the
+        # proper escaping of single quotes that are already part of the string.
+        # Best solution appears to be https://stackoverflow.com/a/1250279
+        ps1 = ps1.replace("'", "'\"'\"'")
+        set_vars.update(
+            {
+                "PS1": conda_prompt_modifier + ps1,
+            }
+        )
+
+    def _hook_preamble(self) -> str:
+        result = []
+        for key, value in context.conda_exe_vars_dict.items():
+            if value is None:
+                # Using `unset_var_tmpl` would cause issues for people running
+                # with shell flag -u set (error on unset).
+                result.append(self.export_var_tmpl % (key, ""))
+            elif on_win and ("/" in value or "\\" in value):
+                result.append(f'''export {key}="$(cygpath '{value}')"''')
+            else:
+                result.append(self.export_var_tmpl % (key, value))
+        return "\n".join(result) + "\n"
 
 
 def get_activate_builder(activator):
@@ -152,7 +208,7 @@ def posix_plugin_with_shell(*args, **kwargs):
 
     env_args = (command, env) if env else (command,)
 
-    activator = PosixActivator(env_args)
+    activator = PosixPluginActivator(env_args)
 
     # call the methods leading up to the command-specific builds
     activator._parse_and_set_args(env_args)
