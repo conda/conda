f89567001d1c7efbb31c628c5a7b2eff2473b5bf
Ken Odegard <kodegard@anaconda.com>
2023-05-25 12:49:04 -0500

    Drop TEST_ENV_NAME_1 in favor of env_one fixture

--- tests/cli/test_main_rename.py
+++ tests/cli/test_main_rename.py
@@ -15,20 +15,20 @@ from conda.core.envs_manager import list_all_known_prefixes
 from conda.exceptions import CondaError, EnvironmentNameNotFound
 from conda.testing.helpers import set_active_prefix
 
-TEST_ENV_NAME_1 = "env-1"
 TEST_ENV_NAME_2 = "env-2"
 TEST_ENV_NAME_RENAME = "renamed-env"
 
 
 @pytest.fixture
-def env_one():
+def env_one() -> Iterable[str]:
     # Setup
-    conda_cli("create", "-n", TEST_ENV_NAME_1, "-y")
+    name = uuid.uuid4().hex
+    conda_cli("create", "-n", name, "-y")
 
-    yield
+    yield name
 
     # Teardown
-    conda_cli("remove", "--all", "-y", "-n", TEST_ENV_NAME_1)
+    conda_cli("remove", "--all", "-y", "-n", name)
     conda_cli("remove", "--all", "-y", "-n", TEST_ENV_NAME_RENAME)
 
 
@@ -56,54 +56,51 @@ def env_prefix_one():
     conda_cli("remove", "--all", "-y", "-p", tmpdir)
 
 
-def test_rename_by_name_success(env_one):
-    conda_cli("rename", "-n", TEST_ENV_NAME_1, TEST_ENV_NAME_RENAME)
+def test_rename_by_name_success(env_one: str):
+    conda_cli("rename", "-n", env_one, TEST_ENV_NAME_RENAME)
 
     assert locate_prefix_by_name(TEST_ENV_NAME_RENAME)
     with pytest.raises(EnvironmentNameNotFound):
-        locate_prefix_by_name(TEST_ENV_NAME_1)
+        locate_prefix_by_name(env_one)
 
 
-def test_rename_by_path_success(env_one):
+def test_rename_by_path_success(env_one: str):
     with tempfile.TemporaryDirectory() as temp_dir:
         new_name = str(pathlib.Path(temp_dir).joinpath("new-env"))
-        conda_cli("rename", "-n", TEST_ENV_NAME_1, new_name)
+        conda_cli("rename", "-n", env_one, new_name)
 
         result = list_all_known_prefixes()
 
         path_appears_in_env_list = any(new_name == path for path in result)
-        original_name_in_envs = any(path.endswith(TEST_ENV_NAME_1) for path in result)
+        original_name_in_envs = any(path.endswith(env_one) for path in result)
 
         assert path_appears_in_env_list
         assert not original_name_in_envs
 
 
-def test_rename_by_name_name_already_exists_error(env_one):
+def test_rename_by_name_name_already_exists_error(env_one: str):
     """Test to ensure that we do not rename if the name already exists"""
-    out, err, exit_code = conda_cli("rename", "-n", TEST_ENV_NAME_1, TEST_ENV_NAME_1)
-    assert (
-        f"The environment '{TEST_ENV_NAME_1}' already exists. Override with --force"
-        in err
-    )
+    out, err, exit_code = conda_cli("rename", "-n", env_one, env_one)
+    assert f"The environment '{env_one}' already exists. Override with --force" in err
 
 
-def test_rename_by_path_path_already_exists_error(env_one):
+def test_rename_by_path_path_already_exists_error(env_one: str):
     """Test to ensure that we do not rename if the path already exists"""
     with tempfile.TemporaryDirectory() as tempdir:
-        out, err, exit_code = conda_cli("rename", "-n", TEST_ENV_NAME_1, tempdir)
+        out, err, exit_code = conda_cli("rename", "-n", env_one, tempdir)
         assert (
             f"The environment '{os.path.basename(os.path.normpath(tempdir))}' already exists. Override with --force"
             in err
         )
 
 
-def test_cannot_rename_base_env_by_name(env_one):
+def test_cannot_rename_base_env_by_name():
     """Test to ensure that we cannot rename the base env invoked by name"""
     out, err, exit_code = conda_cli("rename", "-n", "base", TEST_ENV_NAME_RENAME)
     assert "The 'base' environment cannot be renamed" in err
 
 
-def test_cannot_rename_base_env_by_path(env_one):
+def test_cannot_rename_base_env_by_path():
     """Test to ensure that we cannot rename the base env invoked by path"""
     out, err, exit_code = conda_cli(
         "rename", "-p", context.root_prefix, TEST_ENV_NAME_RENAME
@@ -111,37 +108,35 @@ def test_cannot_rename_base_env_by_path(env_one):
     assert "The 'base' environment cannot be renamed" in err
 
 
-def test_cannot_rename_active_env_by_name(env_one):
+def test_cannot_rename_active_env_by_name(env_one: str):
     """Makes sure that we cannot rename our active environment."""
     result = list_all_known_prefixes()
 
-    prefix_list = [res for res in result if res.endswith(TEST_ENV_NAME_1)]
+    prefix_list = [res for res in result if res.endswith(env_one)]
 
     assert len(prefix_list) > 0
 
     prefix = prefix_list[0]
 
     with set_active_prefix(prefix):
-        out, err, exit_code = conda_cli(
-            "rename", "-n", TEST_ENV_NAME_1, TEST_ENV_NAME_RENAME
-        )
+        out, err, exit_code = conda_cli("rename", "-n", env_one, TEST_ENV_NAME_RENAME)
         assert "Cannot rename the active environment" in err
 
 
-def test_rename_with_force(env_one, env_two):
+def test_rename_with_force(env_one: str, env_two):
     """
     Runs a test where we specify the --force flag to remove an existing directory.
     Without this flag, it would return with an error message.
     """
     # Do a force rename
-    conda_cli("rename", "-n", TEST_ENV_NAME_1, TEST_ENV_NAME_2, "--force")
+    conda_cli("rename", "-n", env_one, TEST_ENV_NAME_2, "--force")
 
     assert locate_prefix_by_name(TEST_ENV_NAME_2)
     with pytest.raises(EnvironmentNameNotFound):
-        locate_prefix_by_name(TEST_ENV_NAME_1)
+        locate_prefix_by_name(env_one)
 
 
-def test_rename_with_force_with_errors(env_one, env_two):
+def test_rename_with_force_with_errors(env_one: str, env_two):
     """
     Runs a test where we specify the --force flag to remove an existing directory.
     Additionally, in this test, we mock an exception to recreate a failure condition.
@@ -152,14 +147,14 @@ def test_rename_with_force_with_errors(env_one, env_two):
     with mock.patch("conda.cli.main_rename.install.clone") as clone_mock:
         clone_mock.side_effect = [CondaError(error_message)]
         _, err, exit_code = conda_cli(
-            "rename", "-n", TEST_ENV_NAME_1, TEST_ENV_NAME_2, "--force"
+            "rename", "-n", env_one, TEST_ENV_NAME_2, "--force"
         )
         assert error_message in err
         assert exit_code == 1
 
     # Make sure both environments still exist
     assert locate_prefix_by_name(TEST_ENV_NAME_2)
-    assert locate_prefix_by_name(TEST_ENV_NAME_1)
+    assert locate_prefix_by_name(env_one)
 
 
 def test_rename_with_force_with_errors_prefix(env_prefix_one):
@@ -184,16 +179,16 @@ def test_rename_with_force_with_errors_prefix(env_prefix_one):
         assert os.path.isdir(env_prefix_one)
 
 
-def test_rename_with_dry_run(env_one):
+def test_rename_with_dry_run(env_one: str):
     """
     Runs a test where we specify the --dry-run flag to remove an existing directory.
     Without this flag, it would actually execute all the actions.
     """
     (rename_out, rename_err, rename_exit_code) = conda_cli(
-        "rename", "-n", TEST_ENV_NAME_1, TEST_ENV_NAME_RENAME, "--dry-run"
+        "rename", "-n", env_one, TEST_ENV_NAME_RENAME, "--dry-run"
     )
 
-    assert locate_prefix_by_name(TEST_ENV_NAME_1)
+    assert locate_prefix_by_name(env_one)
     with pytest.raises(EnvironmentNameNotFound):
         locate_prefix_by_name(TEST_ENV_NAME_RENAME)
 
@@ -202,17 +197,17 @@ def test_rename_with_dry_run(env_one):
     assert "Dry run action: rm_rf" in rename_stdout
 
 
-def test_rename_with_force_and_dry_run(env_one, env_prefix_one):
+def test_rename_with_force_and_dry_run(env_one: str, env_prefix_one):
     """
     Runs a test where we specify the --force and --dry-run flags to forcefully rename
     an existing directory. We need to ensure that --dry-run is effective and that no
     changes occur.
     """
     (rename_out, rename_err, rename_exit_code) = conda_cli(
-        "rename", "-n", TEST_ENV_NAME_1, TEST_ENV_NAME_RENAME, "--force", "--dry-run"
+        "rename", "-n", env_one, TEST_ENV_NAME_RENAME, "--force", "--dry-run"
     )
 
-    assert locate_prefix_by_name(TEST_ENV_NAME_1)
+    assert locate_prefix_by_name(env_one)
     with pytest.raises(EnvironmentNameNotFound):
         locate_prefix_by_name(TEST_ENV_NAME_RENAME)
 
