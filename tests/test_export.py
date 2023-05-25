1eedcc5079a4e4b1c9f2b3157ef4723b286147a9
Ken Odegard <kodegard@anaconda.com>
2023-05-25 13:46:12 -0500

    Convert test_export.py to pytest style

--- tests/test_export.py
+++ tests/test_export.py
@@ -18,91 +18,92 @@ from conda.testing.integration import (
 
 
 @pytest.mark.integration
-class ExportIntegrationTests(TestCase):
-    def test_basic(self, tmp_env: TmpEnvFixture):
-        with tmp_env("python=3.5") as prefix:
-            assert exists(join(prefix, PYTHON_BINARY))
-            assert package_is_installed(prefix, "python=3")
+def test_basic(tmp_env: TmpEnvFixture):
+    with tmp_env("python=3.5") as prefix:
+        assert exists(join(prefix, PYTHON_BINARY))
+        assert package_is_installed(prefix, "python=3")
 
-            output, error, _ = run_command(Commands.LIST, prefix, "-e")
+        output, error, _ = run_command(Commands.LIST, prefix, "-e")
 
+        with Utf8NamedTemporaryFile(mode="w", suffix="txt", delete=False) as env_txt:
+            env_txt.write(output)
+            env_txt.flush()
+            env_txt.close()
+            prefix2 = make_temp_prefix()
+            run_command(Commands.CREATE, prefix2, "--file", env_txt.name)
+
+            assert package_is_installed(prefix2, "python")
+
+        output2, error, _ = run_command(Commands.LIST, prefix2, "-e")
+        assert output == output2
+
+
+@pytest.mark.integration
+@pytest.mark.skipif(True, reason="Bring back `conda list --export` #3445")
+def test_multi_channel_export(tmp_env: TmpEnvFixture):
+    """
+    When try to import from txt
+    every package should come from same channel
+    """
+    with tmp_env("python=3.5") as prefix:
+        assert exists(join(prefix, PYTHON_BINARY))
+        assert package_is_installed(prefix, "python=3")
+
+        run_command(Commands.INSTALL, prefix, "six", "-c", "conda-forge")
+        assert package_is_installed(prefix, "six")
+
+        output, error, _ = run_command(Commands.LIST, prefix, "-e")
+        assert "conda-forge" in output
+
+        try:
             with Utf8NamedTemporaryFile(
                 mode="w", suffix="txt", delete=False
             ) as env_txt:
                 env_txt.write(output)
-                env_txt.flush()
                 env_txt.close()
                 prefix2 = make_temp_prefix()
                 run_command(Commands.CREATE, prefix2, "--file", env_txt.name)
 
                 assert package_is_installed(prefix2, "python")
-
             output2, error, _ = run_command(Commands.LIST, prefix2, "-e")
-            self.assertEqual(output, output2)
+            assert output == output2
+        finally:
+            rm_rf(env_txt.name)
 
-    @pytest.mark.skipif(True, reason="Bring back `conda list --export` #3445")
-    def test_multi_channel_export(self, tmp_env: TmpEnvFixture):
-        """
-        When try to import from txt
-        every package should come from same channel
-        """
-        with tmp_env("python=3.5") as prefix:
-            assert exists(join(prefix, PYTHON_BINARY))
-            assert package_is_installed(prefix, "python=3")
 
-            run_command(Commands.INSTALL, prefix, "six", "-c", "conda-forge")
-            assert package_is_installed(prefix, "six")
+@pytest.mark.integration
+def test_multi_channel_explicit(tmp_env: TmpEnvFixture):
+    """
+    When try to import from txt
+    every package should come from same channel
+    """
+    with tmp_env("python=3.5") as prefix:
+        assert exists(join(prefix, PYTHON_BINARY))
+        assert package_is_installed(prefix, "python=3")
 
-            output, error, _ = run_command(Commands.LIST, prefix, "-e")
-            self.assertIn("conda-forge", output)
+        run_command(Commands.INSTALL, prefix, "six", "-c", "conda-forge")
+        assert package_is_installed(prefix, "conda-forge::six")
 
-            try:
-                with Utf8NamedTemporaryFile(
-                    mode="w", suffix="txt", delete=False
-                ) as env_txt:
-                    env_txt.write(output)
-                    env_txt.close()
-                    prefix2 = make_temp_prefix()
-                    run_command(Commands.CREATE, prefix2, "--file", env_txt.name)
+        output, error, _ = run_command(Commands.LIST, prefix, "--explicit")
+        assert not error
+        assert "conda-forge" in output
 
-                    assert package_is_installed(prefix2, "python")
-                output2, error, _ = run_command(Commands.LIST, prefix2, "-e")
-                self.assertEqual(output, output2)
-            finally:
-                rm_rf(env_txt.name)
+        urls1 = {url for url in output.split() if url.startswith("http")}
 
-    def test_multi_channel_explicit(self, tmp_env: TmpEnvFixture):
-        """
-        When try to import from txt
-        every package should come from same channel
-        """
-        with tmp_env("python=3.5") as prefix:
-            assert exists(join(prefix, PYTHON_BINARY))
-            assert package_is_installed(prefix, "python=3")
+        try:
+            with Utf8NamedTemporaryFile(
+                mode="w", suffix="txt", delete=False
+            ) as env_txt:
+                env_txt.write(output)
+                env_txt.close()
+                prefix2 = make_temp_prefix()
+                run_command(Commands.CREATE, prefix2, "--file", env_txt.name)
 
-            run_command(Commands.INSTALL, prefix, "six", "-c", "conda-forge")
-            assert package_is_installed(prefix, "conda-forge::six")
-
-            output, error, _ = run_command(Commands.LIST, prefix, "--explicit")
-            assert not error
-            assert "conda-forge" in output
-
-            urls1 = {url for url in output.split() if url.startswith("http")}
-
-            try:
-                with Utf8NamedTemporaryFile(
-                    mode="w", suffix="txt", delete=False
-                ) as env_txt:
-                    env_txt.write(output)
-                    env_txt.close()
-                    prefix2 = make_temp_prefix()
-                    run_command(Commands.CREATE, prefix2, "--file", env_txt.name)
-
-                    assert package_is_installed(prefix2, "python")
-                    assert package_is_installed(prefix2, "six")
-                output2, error2, _ = run_command(Commands.LIST, prefix2, "--explicit")
-                assert not error2
-                urls2 = {url for url in output2.split() if url.startswith("http")}
-                assert urls1 == urls2
-            finally:
-                rm_rf(env_txt.name)
+                assert package_is_installed(prefix2, "python")
+                assert package_is_installed(prefix2, "six")
+            output2, error2, _ = run_command(Commands.LIST, prefix2, "--explicit")
+            assert not error2
+            urls2 = {url for url in output2.split() if url.startswith("http")}
+            assert urls1 == urls2
+        finally:
+            rm_rf(env_txt.name)
