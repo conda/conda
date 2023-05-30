17e689a20d687cee5ec5d10e66587d358dd4f249
Ken Odegard <kodegard@anaconda.com>
2023-05-30 14:48:06 -0500

    Review conda.testing.* module docstrings

--- conda/testing/fixtures.py
+++ conda/testing/fixtures.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
+"""Collection of pytest fixtures used in conda tests."""
 import warnings
 
 import pytest
--- conda/testing/gateways/fixtures.py
+++ conda/testing/gateways/fixtures.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
+"""Collection of pytest fixtures used in conda.gateways tests."""
 import json
 import os
 import socket
