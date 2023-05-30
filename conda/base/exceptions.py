3879463875ab3e5983119ccffc0e384ec978315f
Ken Odegard <kodegard@anaconda.com>
2023-05-30 12:58:48 -0500

    Deprecate conda.base.exceptions

--- conda/base/exceptions.py
+++ conda/base/exceptions.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
-from logging import getLogger
+from ..deprecations import deprecated
 
-log = getLogger(__name__)
+deprecated.module("24.3", "24.9", addendum="Nothing to import.")
