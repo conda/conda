6ff0a623dacbdfd8b63b25898948d28843b87b53
Ken Odegard <kodegard@anaconda.com>
2023-05-30 13:00:00 -0500

    Deprecate conda.common.cuda

--- conda/common/cuda.py
+++ conda/common/cuda.py
@@ -3,6 +3,10 @@
 from ..deprecations import deprecated
 from ..plugins.virtual_packages import cuda
 
+deprecated.module(
+    "23.3", "23.9", addendum="Use `conda.plugins.virtual_packages.cuda` instead."
+)
+
 
 @deprecated(
     "23.3",
