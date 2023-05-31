5f466b19d846d239ecf079550948007170ae5b10
Ken Odegard <kodegard@anaconda.com>
2023-05-31 11:47:58 -0500

    Review conda.trust.* module docstrings

--- conda/trust/signature_verification.py
+++ conda/trust/signature_verification.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
+"""Interface between conda-content-trust and conda."""
 import json
 import warnings
 from functools import lru_cache
