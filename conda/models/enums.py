f624326cb4a7307effb1a40c289201ec77a1f12c
Ken Odegard <kodegard@anaconda.com>
2023-06-02 07:17:35 +0200

    Deprecate `conda.models.leased_path_entry.LeasedPathEntry` & `conda.models.enums.LeasedPathType` (#12735)
    * Deprecate LeasedPathEntry & LeasedPathType
    
    * Add news

--- conda/models/enums.py
+++ conda/models/enums.py
@@ -7,6 +7,7 @@ from platform import machine
 from ..auxlib.decorators import classproperty
 from ..auxlib.ish import dals
 from ..auxlib.type_coercion import TypeCoercionError, boolify
+from ..deprecations import deprecated
 from ..exceptions import CondaUpgradeError
 
 
@@ -124,6 +125,10 @@ class LeasedPathType(Enum):
 
     def __json__(self):
         return self.name
+
+
+deprecated.constant("24.3", "24.9", "LeasedPathType", LeasedPathType)
+del LeasedPathType
 
 
 class PackageType(Enum):
