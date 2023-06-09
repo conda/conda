509588b4af5b6f36ad38b8f2341d59bd77b7faa1
Daniel Holth <dholth@anaconda.com>
2023-06-09 15:10:38 -0500

    add missing import

--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
+import os
 from os.path import exists, isfile
 from tempfile import mktemp
 from unittest import TestCase
