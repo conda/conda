5b9fa3fd492680d20af3313db11aac7a7922ee1d
Ken Odegard <kodegard@anaconda.com>
2023-06-13 07:57:47 -0500

    Revert deprecation

--- conda/cli/python_api.py
+++ conda/cli/python_api.py
@@ -7,13 +7,11 @@ from ..base.constants import SEARCH_PATH
 from ..base.context import context
 from ..common.compat import encode_arguments
 from ..common.io import CaptureTarget, argv, captured
-from ..deprecations import deprecated
 from ..exceptions import conda_exception_handler
 from ..gateways.logging import initialize_std_loggers
 from .conda_argparse import do_call
 from .main import generate_parser
 
-deprecated.module("24.3", "24.9", addendum="Use `conda.testing.conda_cli` instead.")
 log = getLogger(__name__)
 
 
