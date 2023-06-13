34cd4e4681b5b6b658e13a5c6d923300c8c319a6
Jannis Leidel <jannis@leidel.info>
2023-06-13 17:11:13 +0200

    Fix NameError.

--- tests/conda_env/test_create.py
+++ tests/conda_env/test_create.py
@@ -51,18 +51,12 @@ def package_is_installed(prefix, spec):
             % (dashlist(prec.dist_str() for prec in prefix_recs))
         )
     is_installed = bool(len(prefix_recs))
-    if is_installed and pip is True:
+    if is_installed:
         assert prefix_recs[0].package_type in (
             PackageType.VIRTUAL_PYTHON_WHEEL,
             PackageType.VIRTUAL_PYTHON_EGG_MANAGEABLE,
             PackageType.VIRTUAL_PYTHON_EGG_UNMANAGEABLE,
             PackageType.VIRTUAL_PYTHON_EGG_LINK,
-        )
-    if is_installed and pip is False:
-        assert prefix_recs[0].package_type in (
-            None,
-            PackageType.NOARCH_GENERIC,
-            PackageType.NOARCH_PYTHON,
         )
     return is_installed
 
