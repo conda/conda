73a605d9aa44e433844c5676c2184b07ecf37e16
Ken Odegard <kodegard@anaconda.com>
2023-06-20 13:36:26 -0500

    Convert test_link_order to pytest

--- tests/test_link_order.py
+++ tests/test_link_order.py
@@ -1,34 +1,17 @@
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
-import shutil
-import tempfile
-import unittest
-from logging import getLogger
-
 import pytest
 
-from conda.testing.integration import Commands, run_command
-
-log = getLogger(__name__)
+from conda.testing import TmpEnvFixture
 
 
-class TestLinkOrder(unittest.TestCase):
-    def setUp(self):
-        self.prefix = tempfile.mkdtemp()
+@pytest.mark.integration
+def test_link_order_post_link_actions(tmp_env: TmpEnvFixture):
+    with tmp_env("c_post_link_package", "--channel", "conda-test"):
+        pass
 
-    def tearDown(self):
-        shutil.rmtree(self.prefix)
 
-    @pytest.mark.integration
-    def test_link_order_post_link_actions(self):
-        stdout, stderr, _ = run_command(
-            Commands.CREATE, self.prefix, "c_post_link_package", "-c", "conda-test"
-        )
-        assert stderr == ""
-
-    @pytest.mark.integration
-    def test_link_order_post_link_depend(self):
-        stdout, stderr, _ = run_command(
-            Commands.CREATE, self.prefix, "e_post_link_package", "-c", "conda-test"
-        )
-        assert stderr == ""
+@pytest.mark.integration
+def test_link_order_post_link_depend(tmp_env: TmpEnvFixture):
+    with tmp_env("e_post_link_package", "--channel", "conda-test"):
+        pass
