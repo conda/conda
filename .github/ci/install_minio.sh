3e8d0f24caa791a86d5d162f55358dfaf2587de8
Ken Odegard <kodegard@anaconda.com>
2023-06-23 22:44:31 -0500

    Fix install_minio.sh script

--- .github/ci/install_minio.sh
+++ .github/ci/install_minio.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
-# Download MinIO
-MINIOARCH="${TARGETARCH:-$(uname -m)}"
-
-ARCH="$(uname -m)"
-case "${ARCH}" in
-    aarch64) ARCH=arm64;;
-    x86_64) ARCH=amd64;;
-    ppc64el) ARCH=ppc64le;;
-    *) ARCH=$1;
-esac
-curl -sL -o minio "https://dl.minio.io/server/minio/release/$(uname -s)-${ARCH}/${MINIO_RELEASE:-minio}"
+# Check if MinIO was previously downloaded
+if [[ -f minio ]]; then
+    echo "MinIO already downloaded."
+else
+    echo "Downloading MinIO."
+    ARCH="${TARGETARCH:-$(uname -m)}"
+    case "${ARCH}" in
+        aarch64) ARCH=arm64;;
+        x86_64) ARCH=amd64;;
+        ppc64el) ARCH=ppc64le;;
+    esac
+    OS=$(uname -s | tr "[:upper:]" "[:lower:]")
+    curl -sL -o minio "https://dl.min.io/server/minio/release/${OS}-${ARCH}/${MINIO_RELEASE:-minio}"
+fi
 
 # Install MinIO
+echo "Installing MinIO."
 chmod +x minio
-cp minio $CONDA_PREFIX/bin/minio
+cp minio ${CONDA_PREFIX}/bin/minio
