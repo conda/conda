afadfa876d1c48f6a54b0d26c4ef1ef55ca097e8
Katherine Abrikian <kalawac@users.noreply.github.com>
2023-06-27 11:30:23 -0400

    update function for determining if current shell is a posix shell

--- conda/plugins/shells/posix/posix_os_exec_shell.py
+++ conda/plugins/shells/posix/posix_os_exec_shell.py
@@ -3,8 +3,9 @@
 from __future__ import annotations
 
 import os
-import subprocess
 from pathlib import PurePath
+
+import psutil
 
 from conda.activate import native_path_to_unix
 from conda.plugins import CondaShellPlugins, hookimpl
@@ -12,24 +13,19 @@ from conda.plugins import CondaShellPlugins, hookimpl
 POSIX_SHELLS = {"ash", "bash", "dash", "ksh", "sh", "zsh"}
 
 
-def determine_shell(command: str) -> bool:
+def determine_posix_shell() -> bool:
     """
-    Execute the specified command as a Python subprocess, through the shell.
-    Determine whether the final path component of the output is in the set of
-        compatible shells.
+    Determine whether the final path component of the shell process executable is in
+    the set of compatible shells.
     """
-    s = subprocess.run(command, shell=True, capture_output=True)
+    shell_process = psutil.Process(psutil.Process().ppid()).exe()
 
-    output = s.stdout.decode().strip("\n")
-
-    return PurePath(output).name in POSIX_SHELLS
+    return PurePath(shell_process).name in POSIX_SHELLS
 
 
 @hookimpl
 def conda_shell_plugins():
-    if determine_shell("echo $0") or determine_shell(
-        "shellPID=$$; ps -ocomm= $shellPID"
-    ):
+    if determine_posix_shell():
         yield CondaShellPlugins(
             name="posixp",
             summary="Plugin for POSIX shells used for activate, deactivate, and reactivate",
