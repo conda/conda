d4a7ded11c257216429f9ba65739b8bb12915144
Katherine Abrikian <kalawac@users.noreply.github.com>
2023-07-11 18:32:53 -0400

    update hookspec to correspond to hook in use

--- conda/plugins/hookspec.py
+++ conda/plugins/hookspec.py
@@ -141,32 +141,24 @@ class CondaSpecs:
         .. code-block:: python
 
             import os
-            from pathlib import PurePath
-            import psutil
             from conda import plugins
-
-
-            def confirm_example_shell():
-                shell_process = psutil.Process(psutil.Process().ppid()).exe()
-                return PurePath(shell_process).name == "example"
 
 
             @plugins.hookimpl
             def conda_shell_plugins():
-                if confirm_example_shell():
-                    yield plugins.CondaShellPlugins(
-                        name="plugin_name",
-                        summary="Conda shell plugin for example shell",
-                        script_path=os.path.abspath("./posix_script.sh"),
-                        pathsep_join=":".join,
-                        sep="/",
-                        path_conversion=some_function,
-                        script_extension=".sh",
-                        tempfile_extension=None,
-                        command_join="\n",
-                        run_script_tmpl='. "%s"',
-                        unset_var_tmpl="unset %s",
-                        export_var_tmpl="export %s='%s'",
-                        set_var_tmpl="%s='%s'",
-                    )
+                yield plugins.CondaShellPlugins(
+                    name="plugin_name",
+                    summary="Conda shell plugin for example shell",
+                    script_path=os.path.abspath("./posix_script.sh"),
+                    pathsep_join=":".join,
+                    sep="/",
+                    path_conversion=some_function,
+                    script_extension=".sh",
+                    tempfile_extension=None,
+                    command_join="\n",
+                    run_script_tmpl='. "%s"',
+                    unset_var_tmpl="unset %s",
+                    export_var_tmpl="export %s='%s'",
+                    set_var_tmpl="%s='%s'",
+                )
         """
