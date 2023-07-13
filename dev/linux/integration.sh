deadec9b74910ba91e8af8c5e95de3da561a0457
Ken Odegard <kodegard@anaconda.com>
2023-07-12 20:03:25 -0500

    Remove conda-build as testing dependency

--- dev/linux/integration.sh
+++ dev/linux/integration.sh
@@ -8,11 +8,9 @@ git config --global --add safe.directory /opt/conda-src
 TEST_SPLITS="${TEST_SPLITS:-1}"
 TEST_GROUP="${TEST_GROUP:-1}"
 
-sudo su root -c "/opt/conda/bin/conda install -yq conda-build"
 # make sure all test requirements are installed
 sudo /opt/conda/bin/conda install --quiet -y --file tests/requirements.txt
 eval "$(sudo /opt/conda/bin/python -m conda init --dev bash)"
-conda-build tests/test-recipes/activate_deactivate_package tests/test-recipes/pre_link_messages_package
 conda info
 # put temporary files on same filesystem
 export TMP=$HOME/pytesttmp
