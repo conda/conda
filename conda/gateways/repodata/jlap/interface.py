d24b5a57ecb36be808329693999251605c69a247
Daniel Holth <dholth@anaconda.com>
2023-07-26 12:17:48 -0400

    remove cache_path_* arguments

--- conda/gateways/repodata/jlap/interface.py
+++ conda/gateways/repodata/jlap/interface.py
@@ -32,18 +32,11 @@ class JlapRepoInterface(RepoInterface):
         url: str,
         repodata_fn: str | None,
         *,
-        cache_path_json: str | Path,
-        cache_path_state: str | Path,
         cache: RepodataCache,
         **kwargs,
     ) -> None:
         log.debug("Using CondaRepoJLAP")
 
-        # TODO is there a better way to share these paths
-        self._cache_path_json = Path(cache_path_json)
-        self._cache_path_state = Path(cache_path_state)
-
-        # replaces self._cache_path_json/state
         self._cache = cache
 
         self._url = url
