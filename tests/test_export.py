dfbb6ab23c46e5a7b6b6f00103a677d75fbf495d
Ken Odegard <kodegard@anaconda.com>
2023-08-01 14:43:11 -0500

    Update test_export.py

--- tests/test_export.py
+++ tests/test_export.py
@@ -1,108 +1,61 @@
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
-from os.path import exists, join
-
 import pytest
+from pytest import MonkeyPatch
 
-from conda.auxlib.compat import Utf8NamedTemporaryFile
-from conda.gateways.disk.delete import rm_rf
-from conda.testing.integration import (
-    PYTHON_BINARY,
-    Commands,
-    make_temp_env,
-    make_temp_prefix,
-    package_is_installed,
-    run_command,
-)
+from conda.base.context import context, reset_context
+from conda.testing import CondaCLIFixture, PathFactoryFixture, TmpEnvFixture
+from conda.testing.integration import package_is_installed
 
 
 @pytest.mark.integration
-def test_basic():
-    with make_temp_env("python=3") as prefix:
-        assert exists(join(prefix, PYTHON_BINARY))
-        assert package_is_installed(prefix, "python=3")
+def test_export(
+    tmp_env: TmpEnvFixture,
+    conda_cli: CondaCLIFixture,
+    path_factory: PathFactoryFixture,
+    monkeypatch: MonkeyPatch,
+):
+    """Test that `conda list --export` output can be used to create a similar environment."""
+    monkeypatch.setenv("CONDA_CHANNELS", "defaults")
+    reset_context()
+    assert context.channels == ("defaults",)
 
-        output, error, _ = run_command(Commands.LIST, prefix, "-e")
+    # use "cheap" packages with no dependencies
+    with tmp_env("pkgs/main::zlib") as prefix:
+        assert package_is_installed(prefix, "pkgs/main::zlib")
 
-        with Utf8NamedTemporaryFile(mode="w", suffix="txt", delete=False) as env_txt:
-            env_txt.write(output)
-            env_txt.flush()
-            env_txt.close()
-            prefix2 = make_temp_prefix()
-            run_command(Commands.CREATE, prefix2, "--file", env_txt.name)
+        output, _, _ = conda_cli("list", "--prefix", prefix, "--export")
 
-            assert package_is_installed(prefix2, "python")
+        env_txt = path_factory(suffix=".txt")
+        env_txt.write_text(output)
 
-        output2, error, _ = run_command(Commands.LIST, prefix2, "-e")
-        assert output == output2
+        with tmp_env("--file", env_txt) as prefix2:
+            assert package_is_installed(prefix, "pkgs/main::zlib")
 
-
-@pytest.mark.skipif(True, reason="Bring back `conda list --export` #3445")
-@pytest.mark.integration
-def test_multi_channel_export():
-    """
-    When try to import from txt
-    every package should come from same channel
-    """
-    with make_temp_env("python=3") as prefix:
-        assert exists(join(prefix, PYTHON_BINARY))
-        assert package_is_installed(prefix, "python=3")
-
-        run_command(Commands.INSTALL, prefix, "six", "-c", "conda-forge")
-        assert package_is_installed(prefix, "six")
-
-        output, error, _ = run_command(Commands.LIST, prefix, "-e")
-        assert "conda-forge" in output
-
-        try:
-            with Utf8NamedTemporaryFile(
-                mode="w", suffix="txt", delete=False
-            ) as env_txt:
-                env_txt.write(output)
-                env_txt.close()
-                prefix2 = make_temp_prefix()
-                run_command(Commands.CREATE, prefix2, "--file", env_txt.name)
-
-                assert package_is_installed(prefix2, "python")
-            output2, error, _ = run_command(Commands.LIST, prefix2, "-e")
+            output2, _, _ = conda_cli("list", "--prefix", prefix2, "--export")
             assert output == output2
-        finally:
-            rm_rf(env_txt.name)
 
 
 @pytest.mark.integration
-def test_multi_channel_explicit():
-    """
-    When try to import from txt
-    every package should come from same channel
-    """
-    with make_temp_env("python=3") as prefix:
-        assert exists(join(prefix, PYTHON_BINARY))
-        assert package_is_installed(prefix, "python=3")
+def test_explicit(
+    tmp_env: TmpEnvFixture,
+    conda_cli: CondaCLIFixture,
+    path_factory: PathFactoryFixture,
+):
+    """Test that `conda list --explicit` output can be used to recreate an identical environment."""
+    # use "cheap" packages with no dependencies
+    with tmp_env("pkgs/main::zlib", "conda-forge::ca-certificates") as prefix:
+        assert package_is_installed(prefix, "pkgs/main::zlib")
+        assert package_is_installed(prefix, "conda-forge::ca-certificates")
 
-        run_command(Commands.INSTALL, prefix, "six", "-c", "conda-forge")
-        assert package_is_installed(prefix, "conda-forge::six")
+        output, _, _ = conda_cli("list", "--prefix", prefix, "--explicit")
 
-        output, error, _ = run_command(Commands.LIST, prefix, "--explicit")
-        assert not error
-        assert "conda-forge" in output
+        env_txt = path_factory(suffix=".txt")
+        env_txt.write_text(output)
 
-        urls1 = {url for url in output.split() if url.startswith("http")}
+        with tmp_env("--file", env_txt) as prefix2:
+            assert package_is_installed(prefix2, "pkgs/main::zlib")
+            assert package_is_installed(prefix2, "conda-forge::ca-certificates")
 
-        try:
-            with Utf8NamedTemporaryFile(
-                mode="w", suffix="txt", delete=False
-            ) as env_txt:
-                env_txt.write(output)
-                env_txt.close()
-                prefix2 = make_temp_prefix()
-                run_command(Commands.CREATE, prefix2, "--file", env_txt.name)
-
-                assert package_is_installed(prefix2, "python")
-                assert package_is_installed(prefix2, "six")
-            output2, error2, _ = run_command(Commands.LIST, prefix2, "--explicit")
-            assert not error2
-            urls2 = {url for url in output2.split() if url.startswith("http")}
-            assert urls1 == urls2
-        finally:
-            rm_rf(env_txt.name)
+            output2, _, _ = conda_cli("list", "--prefix", prefix2, "--explicit")
+            assert output == output2
