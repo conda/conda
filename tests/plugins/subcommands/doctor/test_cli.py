26bbd3edad04f55834fcb233dd9fd2fb69f42a89
Jannis Leidel <jannis@leidel.info>
2023-08-01 13:32:48 +0200

    Port doctor CLI tests to conda_cli fixture.

--- tests/plugins/subcommands/doctor/test_cli.py
+++ tests/plugins/subcommands/doctor/test_cli.py
@@ -1,42 +1,46 @@
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
-from conda.testing.helpers import run_inprocess_conda_command as run
-from conda.testing.integration import make_temp_env
+import pytest
+
+from conda.testing import CondaCLIFixture, TmpEnvFixture
 
 
-def test_conda_doctor_happy_path():
+def test_conda_doctor_happy_path(conda_cli: CondaCLIFixture):
     """Make sure that we are able to call the ``conda doctor`` command"""
 
-    out, err, code = run(f"conda doctor")
+    out, err, code = conda_cli(f"doctor")
 
     assert not err  # no error message
     assert not code  # successful exit code
 
 
-def test_conda_doctor_happy_path_verbose():
+def test_conda_doctor_happy_path_verbose(conda_cli: CondaCLIFixture):
     """Make sure that we are able to run ``conda doctor`` command with the --verbose flag"""
 
-    out, err, code = run(f"conda doctor --verbose")
+    out, err, code = conda_cli(f"doctor", "--verbose")
 
     assert not err  # no error message
     assert not code  # successful exit code
 
 
-def test_conda_doctor_happy_path_show_help():
+def test_conda_doctor_happy_path_show_help(conda_cli: CondaCLIFixture):
     """Make sure that we are able to run ``conda doctor`` command with the --help flag"""
+    with pytest.raises(SystemExit):
+        out, err, code = conda_cli("doctor", "--help")
 
-    out, err, code = run(f"conda doctor --help")
-
-    assert "Display a health report for your environment." in out
-    assert not err  # no error message
-    assert not code  # successful exit code
+        assert "Display a health report for your environment." in out
+        assert not err  # no error message
+        assert not code  # successful exit code
 
 
-def test_conda_doctor_with_test_environment():
+def test_conda_doctor_with_test_environment(
+    conda_cli: CondaCLIFixture,
+    tmp_env: TmpEnvFixture,
+):
     """Make sure that we are able to call ``conda doctor`` command for a specific environment"""
 
-    with make_temp_env() as prefix:
-        out, err, code = run(f"conda doctor --prefix '{prefix}'")
+    with tmp_env() as prefix:
+        out, err, code = conda_cli("doctor", "--prefix", prefix)
 
         assert "There are no packages with missing files." in out
         assert not err  # no error message
