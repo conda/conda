a0bf8d2ec556cf304b8673dcf3c5829c4416f8b3
Ken Odegard <kodegard@anaconda.com>
2023-08-28 18:52:39 -0500

    Use conda-forge::minio-server

--- dev/windows/setup.bat
+++ dev/windows/setup.bat
@@ -20,13 +20,6 @@ CALL conda activate conda-test-env || goto :error
 python -m conda init --install || goto :error
 python -m conda init cmd.exe --dev || goto :error
 
-:: Download minio server needed for S3 tests and place it in our conda environment so is in PATH
-:: You can pin to an older release by setting MINIO_RELEASE to 'archive/XXXX'
-
-if "%MINIO_RELEASE%x" == "x" set "MINIO_RELEASE=minio.exe"
-powershell.exe -Command "If (-Not (Test-Path 'minio.exe')) { Invoke-WebRequest -Uri 'https://dl.min.io/server/minio/release/windows-amd64/%MINIO_RELEASE%' -OutFile 'minio.exe' | Out-Null }" || goto :error
-copy minio.exe %CONDA_PREFIX%\minio.exe || goto :error
-
 goto :EOF
 
 :error
