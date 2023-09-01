ab68877c044b5dc232578682f5d9ca2b36f380b0
Ken Odegard <kodegard@anaconda.com>
2023-08-31 22:07:23 -0500

    Missing __future__.annotations import

--- conda/core/solve.py
+++ conda/core/solve.py
@@ -1,6 +1,8 @@
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
 """The classic solver implementation."""
+from __future__ import annotations
+
 import copy
 import sys
 from itertools import chain
