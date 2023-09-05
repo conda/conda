e9870ccaaa1ee1c57ff38b075f308e6c051c72a7
Daniel Holth <dholth@anaconda.com>
2023-09-05 18:36:24 -0400

    defer cgi import when ftp is not used (#13014)
    * defer cgi import
    
    * deprecate ftp STOR functions; use function-level instead of module-level import.

--- conda/gateways/connection/adapters/ftp.py
+++ conda/gateways/connection/adapters/ftp.py
@@ -16,12 +16,13 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-import cgi
 import ftplib
 import os
 from base64 import b64decode
 from io import BytesIO, StringIO
 from logging import getLogger
+
+from conda.deprecations import deprecated
 
 from ....common.url import urlparse
 from ....exceptions import AuthenticationError
@@ -132,6 +133,7 @@ class FTPAdapter(BaseAdapter):
 
         return response
 
+    @deprecated("24.3", "24.9")
     def stor(self, path, request):
         """Executes the FTP STOR command on the given path."""
         # First, get the file handle. We assume (bravely)
@@ -266,10 +268,13 @@ def build_response(request, data, code, encoding):
     return response
 
 
+@deprecated("24.3", "24.9")
 def parse_multipart_files(request):
     """Given a prepared request, return a file-like object containing the
     original data. This is pretty hacky.
     """
+    import cgi
+
     # Start by grabbing the pdict.
     _, pdict = cgi.parse_header(request.headers["Content-Type"])
 
