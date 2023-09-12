eb5cadb7344f0531a919447c372d8daf22314bc2
Mahe Iram Khan <forgotten.programme@gmail.com>
2023-09-12 13:58:38 +0530

    improve docstring

--- conda/plugins/subcommands/doctor/__init__.py
+++ conda/plugins/subcommands/doctor/__init__.py
@@ -1,9 +1,9 @@
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
-"""Provide 'conda doctor' environment check feature."""
-# This file is here for packaging, not for importing submodules.
-
-# CLI implementation for `conda doctor`.
+"""Implementation for `conda doctor` subcommand.
+Adds various environment and package checks to detect issues or possible environment
+corruption.
+"""
 
 from __future__ import annotations
 
