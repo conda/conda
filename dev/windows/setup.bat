89e9f8d35071860a3b9c6a40ef98dc268cf235a1
jaimergp <jaimergp@users.noreply.github.com>
2023-09-15 13:20:48 +0200

    Apply suggestions from code review
    Co-authored-by: Jannis Leidel <jannis@leidel.info>

--- dev/windows/setup.bat
+++ dev/windows/setup.bat
@@ -17,7 +17,6 @@ cd \conda_src || goto :error
 CALL \conda_bin\scripts\activate.bat || goto :error
 CALL conda create -n conda-test-env -y python=%PYTHON% pywin32 --file=tests\requirements.txt || goto :error
 CALL conda activate conda-test-env || goto :error
-CALL conda update openssl ca-certificates certifi || goto :error
 python -m conda init --install || goto :error
 python -m conda init cmd.exe --dev || goto :error
 
