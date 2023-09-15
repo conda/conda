a1fff5c0b1604d12581fe800225978ee0b61acf1
jaimergp <jaimergp@users.noreply.github.com>
2023-09-15 17:05:36 +0200

    use libmamba to provision

--- dev/linux/integration.sh
+++ dev/linux/integration.sh
@@ -9,7 +9,7 @@ TEST_SPLITS="${TEST_SPLITS:-1}"
 TEST_GROUP="${TEST_GROUP:-1}"
 
 # make sure all test requirements are installed
-sudo /opt/conda/bin/conda install --quiet -y --file tests/requirements.txt --file tests/requirements-extra.txt
+sudo /opt/conda/bin/conda install --quiet -y --solver=libmamba --file tests/requirements.txt  --file tests/requirements-extra.txt
 eval "$(sudo /opt/conda/bin/python -m conda init --dev bash)"
 conda info
 # put temporary files on same filesystem
