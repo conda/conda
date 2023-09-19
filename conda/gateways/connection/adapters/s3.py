c32ba23b04da28b8c2c0333160b8698e7fdac658
Ken Odegard <kodegard@anaconda.com>
2023-09-19 15:04:58 -0500

    Add future import

--- conda/gateways/connection/adapters/s3.py
+++ conda/gateways/connection/adapters/s3.py
@@ -1,6 +1,8 @@
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
 """Defines S3 transport adapter for CondaSession (requests.Session)."""
+from __future__ import annotations
+
 import json
 from logging import LoggerAdapter, getLogger
 from tempfile import SpooledTemporaryFile
