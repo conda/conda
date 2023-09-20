9e6bfd00aca1f54cefafa23b304292dfa80847ee
jaimergp <jaimergp@users.noreply.github.com>
2023-09-20 15:31:44 +0200

    keep using classic for provisioning for now

--- dev/linux/unit.sh
+++ dev/linux/unit.sh
@@ -9,7 +9,7 @@ TEST_SPLITS="${TEST_SPLITS:-1}"
 TEST_GROUP="${TEST_GROUP:-1}"
 
 # make sure all test requirements are installed
-sudo /opt/conda/bin/conda install --quiet -y --solver=libmamba --file tests/requirements.txt
+sudo /opt/conda/bin/conda install --quiet -y --file tests/requirements.txt
 eval "$(sudo /opt/conda/bin/python -m conda init --dev bash)"
 conda info
 # remove the pkg cache.  We can't hardlink from here anyway.  Having it around causes log problems.
