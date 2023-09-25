2745211e70436a3b7f4bd116fa3c578d4579866e
Ken Odegard <kodegard@anaconda.com>
2023-09-25 15:43:21 -0400

    Use `conda-forge::minio-server` (#13084)
    Co-authored-by: Jannis Leidel <jannis@leidel.info>

--- dev/macos/setup.sh
+++ dev/macos/setup.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 set -ex
 
-# Download the Minio server, needed for S3 tests
-if [[ ! -f minio ]]
-then
-    minio_release="${MINIO_RELEASE:-minio}" # use 'archive/XXXX' for older releases
-    curl -sL -o minio "https://dl.min.io/server/minio/release/darwin-amd64/${minio_release}"
-fi
-chmod +x minio
-sudo cp minio /usr/local/bin/minio
-
 # restoring the default for changeps1 to have parity with dev
 conda config --set changeps1 true
 # install all test requirements
-conda install --yes --name conda-test-env --file tests/requirements.txt
+conda install --yes --name conda-test-env --file tests/requirements.txt --file tests/requirements-extra.txt
 conda update --yes openssl ca-certificates certifi
