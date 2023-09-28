a9bf0d25b1063a0a19626edcc2d770c423e6d9ff
Ken Odegard <kodegard@anaconda.com>
2023-09-28 18:04:42 -0500

    Initial pass

--- dev/macos/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-
-# restoring the default for changeps1 to have parity with dev
-conda config --set changeps1 true
-# install all test requirements
-conda install --yes --name conda-test-env --file tests/requirements.txt --file tests/requirements-extra.txt
-conda update --yes openssl ca-certificates certifi
