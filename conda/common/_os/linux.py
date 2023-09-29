2b1cdf5cef3ef58d8ab2ff9a4d8f0d4ef1099091
Ken Odegard <kodegard@anaconda.com>
2023-09-29 15:37:41 -0500

    Remove Python 2.7 fallback

--- conda/common/_os/linux.py
+++ conda/common/_os/linux.py
@@ -1,8 +1,10 @@
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
+from __future__ import annotations
+
 from functools import lru_cache
 from logging import getLogger
-from os import confstr, confstr_names, readlink, scandir
+from os import confstr, readlink, scandir
 
 from genericpath import exists
 
@@ -12,44 +14,28 @@ log = getLogger(__name__)
 
 
 @lru_cache(maxsize=None)
-def linux_get_libc_version():
+def linux_get_libc_version() -> tuple[str | None, str | None]:
     """If on linux, returns (libc_family, version), otherwise (None, None)."""
     if not on_linux:
         return None, None
 
-    # Python 2.7 does not have either of these keys in confstr_names, so provide
-    # hard-coded defaults and assert if the key is in confstr_names but differs.
-    # These are defined by POSIX anyway so should never change.
-    confstr_names_fallback = {
-        "CS_GNU_LIBC_VERSION": 2,
-        "CS_GNU_LIBPTHREAD_VERSION": 3,
-    }
-
-    val = None
-    for k, v in confstr_names_fallback.items():
-        assert (
-            k not in confstr_names or confstr_names[k] == v
-        ), "confstr_names_fallback for %s is %s yet in confstr_names it is %s" "" % (
-            k,
-            confstr_names_fallback[k],
-            confstr_names[k],
-        )
+    for name in ("CS_GNU_LIBC_VERSION", "CS_GNU_LIBPTHREAD_VERSION"):
         try:
-            val = str(confstr(v))
-        except Exception:  # pragma: no cover
-            pass
-        else:
-            if val:
+            # check if confstr returned None
+            if value := confstr(name):
+                family, version = value.strip().split(" ")
                 break
-
-    if not val:  # pragma: no cover
-        # Weird, play it safe and assume glibc 2.5
+        except ValueError:
+            # ValueError: name is not defined in os.confstr_names
+            # ValueError: value is not of the form "<family> <version>"
+            pass
+    else:
         family, version = "glibc", "2.5"
         log.warning(
-            "Failed to detect libc family and version, assuming %s/%s", family, version
+            "Failed to detect libc family and version, assuming %s/%s",
+            family,
+            version,
         )
-        return family, version
-    family, version = val.split(" ")
 
     # NPTL is just the name of the threading library, even though the
     # version refers to that of uClibc. readlink() can help to try to
@@ -65,11 +51,14 @@ def linux_get_libc_version():
                         family = "uClibc"
                     else:
                         family = "uClibc-ng"
-                    return family, version
-        # This could be some other C library; it is unlikely though.
-        family = "uClibc"
-        log.warning(
-            "Failed to detect non-glibc family, assuming %s (%s)", family, version
-        )
-        return family, version
+                    break
+        else:
+            # This could be some other C library; it is unlikely though.
+            family = "uClibc"
+            log.warning(
+                "Failed to detect non-glibc family, assuming %s/%s",
+                family,
+                version,
+            )
+
     return family, version
