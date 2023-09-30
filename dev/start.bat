c474496f4001020595442c768e1e300ea052aa97
Ken Odegard <kodegard@anaconda.com>
2023-09-30 09:11:23 -0500

    Split requirements.txt into base vs ci specs

--- dev/start.bat
+++ dev/start.bat
@@ -147,7 +147,8 @@
     --override-channels ^
     -c defaults ^
     --file "%_SRC%\tests\requirements.txt" ^
-    pywin32 ^
+    --file "%_SRC%\tests\requirements-ci.txt" ^
+    --file "%_SRC%\tests\requirements-Windows.txt" ^
     "python=%_PYTHON%" > NUL
 @IF NOT %ErrorLevel%==0 (
     @ECHO Error: failed to update %_NAME% 1>&2
