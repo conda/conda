145d8619111435fca54ef3308798c7e78dd59a59
Ken Odegard <kodegard@anaconda.com>
2023-10-24 23:37:59 -0400

    Inline imports in `conda.cli.main_*` and break up `conda.cli.conda_argparse` (#13173)
    * Test all import paths for conda.cli.conda_argparse
    
    * Remove F821 lint ignore
    
    * Inline imports for main_clean
    
    * Inline imports for main_compare
    
    * Inline imports for main_compare
    
    * Inline imports for main_create
    
    * Inline import for main_info
    
    * Inline import for main_init
    
    * Inline import for main_install
    
    * Inline import for main_list
    
    * Inline import for main_notices
    
    * Inline import for main_package
    
    * Inline import for main_pip
    
    * Inline import for main_remove
    
    * Inline import for main_rename
    
    * Inline import for main_run
    
    * Inline imports for main_search
    
    * Inline imports for main_update
    
    * Move each subcommand's parser into main_*
    
    * Remove redundant calls to add_parser_help
    
    * Correct return code
    
    * Fix tests
    
    * Ruff
    
    * Fix failures
    
    * Cleanup
    
    * Update new (de)activate mock modules
    
    * Change comment to docstring
    
    Co-authored-by: Travis Hathaway <travis.j.hathaway@gmail.com>
    
    ---------
    
    Co-authored-by: Bianca Henderson <bhenderson@anaconda.com>
    Co-authored-by: Bianca Henderson <beeankha@gmail.com>
    Co-authored-by: Travis Hathaway <travis.j.hathaway@gmail.com>

--- conda/cli/main_mock_activate.py
+++ conda/cli/main_mock_activate.py
@@ -4,7 +4,18 @@
 
 A mock implementation of the activate shell command for better UX.
 """
+from argparse import SUPPRESS
+
 from .. import CondaError
+
+
+def configure_parser(sub_parsers):
+    p = sub_parsers.add_parser(
+        "activate",
+        help="Activate a conda environment.",
+    )
+    p.set_defaults(func="conda.cli.main_mock_activate.execute")
+    p.add_argument("args", action="store", nargs="*", help=SUPPRESS)
 
 
 def execute(args, parser):
