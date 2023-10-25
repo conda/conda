923d31b23079ad9f12dd143fcbae444c7694c8c7
Bianca Henderson <bhenderson@anaconda.com>
2023-10-25 14:46:48 -0400

    Add news file, update deprecation message

--- conda_env/pip_util.py
+++ conda_env/pip_util.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
-"""DEPRECATED: Use `conda.env.pip_util.py` instead.
+"""DEPRECATED: Use `conda.env.pip_util` instead.
 
 Environment object describing the conda environment.yaml file.
 """
@@ -15,7 +15,7 @@ from conda.env.pip_util import (  # noqa
 )
 from conda.exceptions import CondaEnvException
 
-deprecated.module("24.3", "24.9", addendum="Use `conda.env.pip_util.py` instead.")
+deprecated.module("24.3", "24.9", addendum="Use `conda.env.pip_util` instead.")
 
 
 @deprecated("23.9", "24.3")
