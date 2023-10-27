59d14f000e53497e0843cd8f3e3104613be150ef
Travis Hathaway <travis.j.hathaway@gmail.com>
2023-10-27 12:02:18 +0200

    updating test runner scripts

--- dev/linux/unit.sh
+++ dev/linux/unit.sh
@@ -8,12 +8,6 @@ git config --global --add safe.directory /opt/conda-src
 TEST_SPLITS="${TEST_SPLITS:-1}"
 TEST_GROUP="${TEST_GROUP:-1}"
 
-# make sure all test requirements are installed
-sudo /opt/conda/bin/conda install --quiet -y --file tests/requirements.txt
-eval "$(sudo /opt/conda/bin/python -m conda init --dev bash)"
-conda info
-# remove the pkg cache.  We can't hardlink from here anyway.  Having it around causes log problems.
-sudo rm -rf /opt/conda/pkgs/*-*-*
 # put temporary files on same filesystem
 export TMP=$HOME/pytesttmp
 mkdir -p $TMP
