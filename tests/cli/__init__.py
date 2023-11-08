abd5873b70f4663b1cfb179086b366424780a2fa
Jannis Leidel <jannis@leidel.info>
2023-11-08 17:10:42 +0100

    Increase logger level for CLI tests. (#13291)
    Co-authored-by: Marcel Bargull <mbargull@users.noreply.github.com>

--- tests/cli/__init__.py
+++ tests/cli/__init__.py
@@ -1,2 +1,11 @@
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
+import logging
+
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def urllib3_logger_error(caplog):
+    "Increasing the level to error to prevent retries show up in the stderr"
+    caplog.set_level(logging.ERROR, logger="urllib3.connectionpool")
