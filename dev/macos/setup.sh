63d3af27cd449ab37748ee7d8a9f4d8ffb6d195f
Jannis Leidel <jannis@leidel.info>
2023-11-17 22:51:44 +0100

    more.

--- dev/macos/setup.sh
+++ dev/macos/setup.sh
@@ -4,5 +4,5 @@ set -ex
 # restoring the default for changeps1 to have parity with dev
 conda config --set changeps1 true
 # install all test requirements
-conda install --yes --name conda-test-env --file tests/requirements.txt --file tests/requirements-s3.txt "conda-forge::menuinst>=2" --repodata-fn=repodata.json
-conda update --yes openssl ca-certificates certifi
+conda install --yes --name conda-test-env --file tests/requirements.txt --file tests/requirements-s3.txt "conda-forge::menuinst>=2" sqlite --repodata-fn=repodata.json
+conda update --yes openssl ca-certificates certifi sqlite
