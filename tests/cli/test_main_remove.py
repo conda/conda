1889b098675dcb50a6acce608133fcf5fc87dc7b
Mahe Iram Khan <forgotten.programme@gmail.com>
2023-12-14 01:38:17 +0100

    revert previous change, delete correct test

--- tests/cli/test_main_remove.py
+++ tests/cli/test_main_remove.py
@@ -5,12 +5,34 @@ from os.path import (
     join,
 )
 
+import pytest
+
+from conda.exceptions import (
+    PackagesNotFoundError,
+)
 from conda.gateways.disk.delete import path_is_clean
 from conda.testing import CondaCLIFixture, TmpEnvFixture
 from conda.testing.integration import (
     PYTHON_BINARY,
     package_is_installed,
 )
+
+
+def test_remove_all(tmp_env: TmpEnvFixture, conda_cli: CondaCLIFixture):
+    with tmp_env("python") as prefix:
+        assert exists(join(prefix, PYTHON_BINARY))
+        assert package_is_installed(prefix, "python")
+
+        # regression test for #2154
+        with pytest.raises(PackagesNotFoundError) as exc:
+            conda_cli("remove", f"--prefix={prefix}", "python", "foo", "numpy", "--yes")
+        exception_string = repr(exc.value)
+        assert "PackagesNotFoundError" in exception_string
+        assert "- numpy" in exception_string
+        assert "- foo" in exception_string
+
+        conda_cli("remove", f"--prefix={prefix}", "--all", "--yes")
+        assert path_is_clean(prefix)
 
 
 def test_remove_all_keep_env(tmp_env: TmpEnvFixture, conda_cli: CondaCLIFixture):
