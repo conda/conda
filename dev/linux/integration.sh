5ea696fdd0eb43889212fdb3115254061725928d
Ken Odegard <kodegard@anaconda.com>
2023-12-19 09:14:09 -0600

    Update menuinst constraint

--- dev/linux/integration.sh
+++ dev/linux/integration.sh
@@ -9,7 +9,7 @@ TEST_SPLITS="${TEST_SPLITS:-1}"
 TEST_GROUP="${TEST_GROUP:-1}"
 
 # make sure all test requirements are installed
-sudo /opt/conda/bin/conda install --quiet -y --file tests/requirements.txt --file tests/requirements-s3.txt "conda-forge::menuinst>=2" --repodata-fn=repodata.json
+sudo /opt/conda/bin/conda install --quiet -y --file tests/requirements.txt --file tests/requirements-s3.txt --repodata-fn=repodata.json
 eval "$(sudo /opt/conda/bin/python -m conda init --dev bash)"
 conda info
 # put temporary files on same filesystem
