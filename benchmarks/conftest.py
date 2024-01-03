de67f8d8f29620d09e1115217d5752784303262a
Jannis Leidel <jannis@leidel.info>
2024-01-03 01:06:48 +0100

    Add local plugins.

---
+++ benchmarks/conftest.py
@@ -0,0 +1,7 @@
+pytest_plugins = (
+    # Add testing fixtures and internal pytest plugins here
+    "conda.testing",
+    "conda.testing.gateways.fixtures",
+    "conda.testing.notices.fixtures",
+    "conda.testing.fixtures",
+)
