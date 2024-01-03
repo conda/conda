13cc47a970d03d0a1e29253420a2c3a270c64926
Jannis Leidel <jannis@leidel.info>
2024-01-03 01:10:29 +0100

    Remove it again.

--- benchmarks/conftest.py
+++
@@ -1,7 +0,0 @@
-pytest_plugins = (
-    # Add testing fixtures and internal pytest plugins here
-    "conda.testing",
-    "conda.testing.gateways.fixtures",
-    "conda.testing.notices.fixtures",
-    "conda.testing.fixtures",
-)
