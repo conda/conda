73c19090c76670141d3b13e2727ee7a3196ccb21
Jannis Leidel <jannis@leidel.info>
2024-01-15 21:40:09 +0100

    Add packaging since it's needed.

--- dev/windows/setup.bat
+++ dev/windows/setup.bat
@@ -15,7 +15,7 @@ mklink /J \conda_src "%CONDA_SRC_PATH%" || goto :error
 
 cd \conda_src || goto :error
 CALL \conda_bin\scripts\activate.bat || goto :error
-CALL conda create -n test -y python=%PYTHON% || goto :error
+CALL conda create -n test -y python=%PYTHON% packaging || goto :error
 CALL conda activate test || goto :error
 python -m conda init --install || goto :error
 python -m conda init cmd.exe --dev || goto :error
