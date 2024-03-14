53e79029ee1fbeebe8a3554847012d97782b122b
Ken Odegard <kodegard@anaconda.com>
2024-03-14 11:05:05 -0500

    Ensure we are testing development conda

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,10 +2,15 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # This is just here so that tests is a package, so that dotted relative
 # imports work.
+from pathlib import Path
+
 from conda.gateways.logging import initialize_logging
 
 initialize_logging()
 
+import conda
 from conda.testing import conda_move_to_front_of_PATH
 
 conda_move_to_front_of_PATH()
+
+assert (Path(__file__).parent.parent / "conda" / "__init__.py").samefile(conda.__file__)
