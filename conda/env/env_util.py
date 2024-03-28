cfcd592be0fd368ddc118a61a36e73f4439db1fa
Jonathan Helmus <jjhelmus@gmail.com>
2024-03-28 13:54:38 -0500

    formatting

--- conda/env/env_util.py
+++ conda/env/env_util.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
+
 """Common utilities for the conda env sub-command."""
 from os.path import abspath, expanduser, expandvars
 
