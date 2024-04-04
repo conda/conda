86badadeebe7239d9de492e1478d5ac3be314eec
Ken Odegard <kodegard@anaconda.com>
2024-04-04 13:36:38 -0500

    Change hatch-vcs `version-file` to support cleaner pip editable installs

--- conda/__init__.py
+++ conda/__init__.py
@@ -7,7 +7,22 @@ import sys
 from json import JSONEncoder
 from os.path import abspath, dirname
 
-from .__version__ import __version__
+try:
+    from ._version import __version__
+except ImportError:
+    # _version.py is only created after running `pip install`
+    try:
+        from setuptools_scm import get_version
+
+        __version__ = get_version(root="..", relative_to=__file__)
+    except (ImportError, OSError, LookupError):
+        # ImportError: setuptools_scm isn't installed
+        # OSError: git isn't installed
+        # LookupError: setuptools_scm unable to detect version
+        # Conda abides by CEP-8 which specifies using CalVer, so the dev version is:
+        #     YY.MM.MICRO.devN+gHASH[.dirty]
+        __version__ = "0.0.0.dev0+placeholder"
+
 
 __all__ = (
     "__name__",
