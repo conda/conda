878b5b8cd5a6e6c236a0ea29fd3ad0e27009bd83
Ken Odegard <kodegard@anaconda.com>
2024-04-26 10:37:05 -0500

    Fix errorlevel exits

--- conda/shell/condabin/conda.bat
+++ conda/shell/condabin/conda.bat
@@ -8,14 +8,20 @@
   @SET _CE_CONDA=
   @SET __condaroot=
 )
-@IF [%1]==[activate]   "%~dp0\_conda_activate.bat" %* || @EXIT /B %ERRORLEVEL%
-@IF [%1]==[deactivate] "%~dp0\_conda_activate.bat" %* || @EXIT /B %ERRORLEVEL%
 
+:: "source" _conda_activate.bat script, we do not return from this script
+@IF [%1]==[activate]   "%~dp0\_conda_activate.bat" %*
+@IF [%1]==[deactivate] "%~dp0\_conda_activate.bat" %*
+
+:: call conda subroutine
 @CALL "%CONDA_EXE%" %_CE_M% %_CE_CONDA% %* || @EXIT /B %ERRORLEVEL%
 
+:: "source" _conda_activate.bat script, we do not return from this script
 @IF [%1]==[install]   "%~dp0\_conda_activate.bat" reactivate
 @IF [%1]==[update]    "%~dp0\_conda_activate.bat" reactivate
 @IF [%1]==[upgrade]   "%~dp0\_conda_activate.bat" reactivate
 @IF [%1]==[remove]    "%~dp0\_conda_activate.bat" reactivate
 @IF [%1]==[uninstall] "%~dp0\_conda_activate.bat" reactivate
+
+:: bubble up any errors
 @EXIT /B %ERRORLEVEL%
