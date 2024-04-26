88fa8bf6f34882e9f68e15b879e867acec68d283
Ken Odegard <kodegard@anaconda.com>
2024-04-26 15:00:57 -0500

    Remove EnableDelayedExpansion

--- conda/shell/condabin/_conda_activate.bat
+++ conda/shell/condabin/_conda_activate.bat
@@ -12,26 +12,23 @@ IF DEFINED CONDA_PS1_BACKUP (
     SET CONDA_PS1_BACKUP=
 )
 
-SETLOCAL EnableDelayedExpansion
+:: attempt to find a unique temporary directory to use
 FOR %%A IN ("%TMP%") DO SET TMP=%%~sA
-:: It seems that it is not possible to have "CONDA_EXE=Something With Spaces"
-:: and %* to contain: activate "Something With Spaces does not exist".
-:: MSDOS associates the outer "'s and is unable to run very much at all.
-:: SET CONDA_EXES="%CONDA_EXE%" %_CE_M% %_CE_CONDA%
-:: FOR /F %%i IN ('%CONDA_EXES% shell.cmd.exe %*') DO SET _TEMP_SCRIPT_PATH=%%i not return error
-:: This method will not work if %TMP% contains any spaces.
-FOR /L %%I IN (1,1,100) DO (
-    SET UNIQUE_DIR=%TMP%\conda-!RANDOM!
-    MKDIR !UNIQUE_DIR! >NUL 2>NUL
-    IF [%ERRORLEVEL%]==[0] (
-        SET UNIQUE=!UNIQUE_DIR!\conda.tmp
-        TYPE NUL 1> !UNIQUE!
-        GOTO :TMP_FILE_CREATED
-    )
+SET _I=100
+:CREATE
+IF [%_I%]==[0] (
+    ECHO Failed to create temp directory "%TMP%\conda-<RANDOM>\" 1>&2
+    SET _I=
+    EXIT /B 1
 )
-ECHO Failed to create temp directory "%TMP%\conda-<RANDOM>\" 1>&2
-EXIT /B 1
-:TMP_FILE_CREATED
+SET /A _I-=1
+SET "UNIQUE_DIR=%TMP%\conda-%RANDOM%"
+MKDIR "%UNIQUE_DIR%" >NUL 2>NUL || GOTO :CREATE
+SET _I=
+
+:: found a unique directory
+SET "UNIQUE=%UNIQUE_DIR%\conda.tmp"
+TYPE NUL >%UNIQUE%
 
 :: call conda
 "%CONDA_EXE%" %_CE_M% %_CE_CONDA% shell.cmd.exe %* 1>%UNIQUE% || EXIT /B 1
@@ -39,7 +36,7 @@ EXIT /B 1
 :: get temporary script to run
 FOR /F %%i IN (%UNIQUE%) DO SET _TEMP_SCRIPT_PATH=%%i
 RMDIR /S /Q %UNIQUE_DIR%
-FOR /F "delims=" %%A IN (""!_TEMP_SCRIPT_PATH!"") DO ENDLOCAL & SET _TEMP_SCRIPT_PATH=%%~A
+FOR /F "delims=" %%A IN ("%_TEMP_SCRIPT_PATH%") DO ENDLOCAL & SET _TEMP_SCRIPT_PATH=%%~A
 IF NOT DEFINED _TEMP_SCRIPT_PATH EXIT /B 1
 
 :: call temporary script
