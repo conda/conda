7e87278306859ec41fdbe6cdaba69accb7025d4f
Klaus Zimmermann <klaus.zimmermann@quansight.com>
2024-07-27 08:48:02 +0000

    Add deprecations

--- conda/core/index.py
+++ conda/core/index.py
@@ -633,6 +633,7 @@ def _supplement_index_with_cache(index: dict[Any, Any]) -> None:
             index[pcrec] = pcrec
 
 
+@deprecated("24.9", "25.3", addendum="Use `conda.core.Index._make_virtual_package` instead.")
 def _make_virtual_package(
     name: str, version: str | None = None, build_string: str | None = None
 ) -> PackageRecord:
@@ -741,6 +742,7 @@ def calculate_channel_urls(
     return all_channel_urls(channel_urls, subdirs=subdirs)
 
 
+@deprecated("24.9", "25.3", addendum="Use `conda.core.ReducedIndex` or `conda.core.Index.get_reduced_index` instead.")
 def get_reduced_index(
     prefix: str | None,
     channels: list[str],
