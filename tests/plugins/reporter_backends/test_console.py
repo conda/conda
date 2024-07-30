ece22a786edb87385e42d6fc1305834698f1fed2
Travis Hathaway <travis.j.hathaway@gmail.com>
2024-07-30 14:11:04 +0200

    increases the coverage of conda.plugins.reporter_backends.console to 100

--- tests/plugins/reporter_backends/test_console.py
+++ tests/plugins/reporter_backends/test_console.py
@@ -1,6 +1,9 @@
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
 from contextlib import nullcontext
+from errno import EPIPE
+
+import pytest
 
 from conda.plugins.reporter_backends.console import (
     ConsoleReporterRenderer,
@@ -32,3 +35,73 @@ def test_console_reporter_renderer():
         console_reporter_renderer.progress_bar_context_manager(None)
     )
     assert isinstance(progress_bar_context_manager, nullcontext)
+
+
+def test_console_reporter_renderer_envs_list(mocker):
+    """
+    Test for the case where a ``context.envs_dirs`` directory equals the prefix
+    """
+    mock_context = mocker.patch("conda.plugins.reporter_backends.console.context")
+    mock_context.envs_dirs = ["/tmp"]
+    console_reporter_renderer = ConsoleReporterRenderer()
+
+    output = console_reporter_renderer.envs_list(["/tmp/envs"])
+
+    assert "envs                   /tmp/envs" in output
+
+
+def test_tqdm_progress_bar_os_error(mocker):
+    """
+    Test for the case where an OSError is raised on tqdm object creation
+    """
+    tqdm_mock = mocker.patch.object(TQDMProgressBar, "_tqdm")
+    tqdm_mock.side_effect = OSError("test")
+
+    with pytest.raises(OSError):
+        TQDMProgressBar("test", nullcontext())
+
+
+def test_tqdm_progress_bar_os_error_with_epipe_errno(mocker):
+    """
+    Test for the case where an OSError is raised on tqdm object creation and the error
+    contains an errno that equals EPIPE
+    """
+    os_error = OSError("test")
+    os_error.errno = EPIPE
+    tqdm_mock = mocker.patch.object(TQDMProgressBar, "_tqdm")
+    tqdm_mock.side_effect = os_error
+
+    progress_bar = TQDMProgressBar("test", nullcontext())
+
+    assert not progress_bar.enabled
+
+
+def test_tqdm_progress_bar_update_to_os_error(mocker):
+    """
+    Test for the case where an OSError is raised when update_to is called
+    """
+    tqdm_mock = mocker.patch.object(TQDMProgressBar, "_tqdm")
+    tqdm_mock().update.side_effect = OSError("test")
+
+    with pytest.raises(OSError):
+        progress_bar = TQDMProgressBar("test", nullcontext())
+        progress_bar.update_to(0.5)
+
+
+def test_tqdm_progress_bar_update_to_os_error_with_errno_epipe(mocker):
+    """
+    Test for the case where an OSError is raised when update_to is called and the OSError
+    errno is set to EPIPE
+    """
+    os_error = OSError("test")
+    os_error.errno = EPIPE
+    tqdm_mock = mocker.patch.object(TQDMProgressBar, "_tqdm")
+    tqdm_mock().update.side_effect = os_error
+
+    progress_bar = TQDMProgressBar("test", nullcontext())
+    progress_bar.update_to(0.5)
+    progress_bar.update_to(0.8)
+    progress_bar.refresh()
+    progress_bar.close()
+
+    assert not progress_bar.enabled
