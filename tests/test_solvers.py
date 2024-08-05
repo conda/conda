7ef80b1468c2e89b00314d08a1d3f06935fb62d7
jaimergp <jaimergp@users.noreply.github.com>
2024-08-05 17:30:55 +0000

    fix import

--- tests/test_solvers.py
+++ tests/test_solvers.py
@@ -2,14 +2,20 @@
 # SPDX-License-Identifier: BSD-3-Clause
 from __future__ import annotations
 
-from conda.core.solve import Solver
+from typing import TYPE_CHECKING
+
 from conda.testing.solver_helpers import SolverTests
+
+if TYPE_CHECKING:
+    from conda.core.solve import Solver
 
 
 class TestClassicSolver(SolverTests):
     @property
     def solver_class(self) -> type[Solver]:
-        return Solver
+        from conda.plugins.solvers.classic.solver import ClassicSolver
+
+        return ClassicSolver
 
 
 class TestLibMambaSolver(SolverTests):
