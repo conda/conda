4f5053090dc289e4c1ea861f69c1642b38b7a586
Klaus Zimmermann <klaus.zimmermann@quansight.com>
2024-08-08 14:42:23 +0000

    Add LAST_CHANNEL_URLS comment

--- conda/core/index.py
+++ conda/core/index.py
@@ -122,6 +122,7 @@ class Index(UserDict):
                 for expanded_channel in expanded_channels
             ]
             self.expanded_channels.extend(expanded_channels)
+        # LAST_CHANNEL_URLS is still used in conda-build and must be maintained for the moment.
         LAST_CHANNEL_URLS.clear()
         LAST_CHANNEL_URLS.extend(self.expanded_channels)
         if prefix is None:
