c14e48133320ca74ecfaac2e2e8011f22ea6e396
Travis Hathaway <travis.j.hathaway@gmail.com>
2024-08-09 15:57:29 +0200

    changing actions

--- tests/setup_squid.sh
+++ tests/setup_squid.sh
@@ -1,12 +1,12 @@
 #!/ bin/bash
 
-sudo apt update \
-&& sudo apt upgrade --yes \
-&& DEBIAN_FRONTEND=noninteractive sudo apt install --yes squid \
-&& sudo echo "auth_param basic program /usr/lib/squid/basic_ncsa_auth /etc/squid/passwords
+apt update \
+&& apt upgrade --yes \
+&& DEBIAN_FRONTEND=noninteractive apt install --yes squid \
+&& echo "auth_param basic program /usr/lib/squid/basic_ncsa_auth /etc/squid/passwords
 auth_param basic realm proxy
 acl authenticated proxy_auth REQUIRED
 http_access allow authenticated
 http_port 8118" > /etc/squid/squid.conf \
-&& sudo echo "admin:$apr1$foJbPHfy$K54kqCzIEYgxAGTAtJrgv1" > /etc/squid/passwords \
-&& sudo squid
+&& echo "admin:$apr1$foJbPHfy$K54kqCzIEYgxAGTAtJrgv1" > /etc/squid/passwords \
+&& squid
