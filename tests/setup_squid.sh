48ceb78e33782022757600c1f518aa1943f0b84c
Travis Hathaway <travis.j.hathaway@gmail.com>
2024-08-12 12:57:13 +0200

    updating squid script

--- tests/setup_squid.sh
+++ tests/setup_squid.sh
@@ -8,4 +8,6 @@ acl authenticated proxy_auth REQUIRED
 http_access allow authenticated
 http_port 8118" > /etc/squid/squid.conf \
 && echo "admin:$apr1$foJbPHfy$K54kqCzIEYgxAGTAtJrgv1" > /etc/squid/passwords \
-&& squid -k restart
+&& squid -k restart \
+&& sleep 5 \
+&& curl -s http://localhost:8118
