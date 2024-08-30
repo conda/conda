0ef37c11761c26ada1da2bb3a1f14dd8b1856182
Travis Hathaway <travis.j.hathaway@gmail.com>
2024-08-30 13:18:58 +0200

    moving logic to determine active reporter backend to context object

--- conda/reporters.py
+++ conda/reporters.py
@@ -14,9 +14,6 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from typing import ContextManager
 
-from .base.constants import (
-    DEFAULT_JSON_REPORTER_BACKEND,
-)
 from .base.context import context
 
 if TYPE_CHECKING:
@@ -27,24 +24,12 @@ if TYPE_CHECKING:
 logger = logging.getLogger(__name__)
 
 
-def _get_reporter_backend() -> str:
-    """
-    Determine the current reporter backend being used
-    """
-    if context.json:
-        backend = DEFAULT_JSON_REPORTER_BACKEND
-    else:
-        backend = context.console
-
-    return backend
-
-
 @lru_cache(maxsize=None)
 def _get_render_func(style: str | None = None) -> Callable:
     """
     Retrieves the render function to use
     """
-    backend = _get_reporter_backend()
+    backend = context.console
     reporter = context.plugin_manager.get_reporter_backend(backend)
 
     renderer = reporter.renderer()
@@ -76,8 +61,7 @@ def get_progress_bar(description: str, **kwargs) -> ProgressBarBase:
     """
     Retrieve the progress bar for the currently configured reporter backend
     """
-    backend = _get_reporter_backend()
-    reporter = context.plugin_manager.get_reporter_backend(backend)
+    reporter = context.plugin_manager.get_reporter_backend(context.console)
 
     return reporter.renderer().progress_bar(description, **kwargs)
 
@@ -86,7 +70,6 @@ def get_progress_bar_context_manager() -> ContextManager:
     """
     Retrieve progress bar context manager to use with registered reporter
     """
-    backend = _get_reporter_backend()
-    reporter = context.plugin_manager.get_reporter_backend(backend)
+    reporter = context.plugin_manager.get_reporter_backend(context.console)
 
     return reporter.renderer().progress_bar_context_manager()
