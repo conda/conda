6b3fbb87a94ef8246d0a4d01a14395bcce310975
Ken Odegard <kodegard@anaconda.com>
2024-09-10 16:08:35 -0500

    Add deprecation tests

--- tests/test_auxlib.py
+++ tests/test_auxlib.py
@@ -2,17 +2,18 @@
 # SPDX-License-Identifier: BSD-3-Clause
 from __future__ import annotations
 
+from contextlib import nullcontext
 from enum import Enum
 from typing import TYPE_CHECKING
 
 import pytest
 from frozendict import deepfreeze
 
-from conda.auxlib.collection import make_immutable
+from conda.auxlib import collection, ish
 from conda.base.constants import UpdateModifier
 
 if TYPE_CHECKING:
-    from typing import Any
+    from typing import Any, ModuleType
 
 
 IMMUTABLE = (
@@ -41,4 +42,23 @@ MUTABLE = ([1, 1, 2], {1, 1, 2}, {1: 1, 2: 2})
     ],
 )
 def test_deepfreeze(unfrozen: Any) -> None:
-    assert make_immutable(unfrozen) == deepfreeze(unfrozen, {Enum: lambda x: x})
+    assert collection.make_immutable(unfrozen) == deepfreeze(
+        unfrozen, {Enum: lambda x: x}
+    )
+
+
+@pytest.mark.parametrize(
+    "module,function,raises",
+    [
+        (collection, "AttrDict", None),
+        (ish, "_get_attr", TypeError),
+        (ish, "find_or_none", TypeError),
+        (ish, "find_or_raise", TypeError),
+    ],
+)
+def test_deprecations(
+    module: ModuleType, function: str, raises: type[Exception] | None
+) -> None:
+    raises_context = pytest.raises(raises) if raises else nullcontext()
+    with pytest.deprecated_call(), raises_context:
+        getattr(module, function)()
