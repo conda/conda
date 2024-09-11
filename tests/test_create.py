8c21b24f3ed8a594e228b0ab477a6778caabc983
Bianca Henderson <bhenderson@anaconda.com>
2024-09-10 21:53:36 -0400

    Remove regression test for #3489 as no longer relevant

--- tests/test_create.py
+++ tests/test_create.py
@@ -58,7 +58,6 @@ from conda.exceptions import (
     UnsatisfiableError,
 )
 from conda.gateways.disk.create import compile_multiple_pyc
-from conda.gateways.disk.delete import rm_rf
 from conda.gateways.disk.permissions import make_read_only
 from conda.gateways.subprocess import (
     Response,
@@ -2303,12 +2302,6 @@ def test_force_remove(
 
         conda_cli("remove", f"--prefix={prefix}", "libarchive", "--yes")
         assert not package_is_installed(prefix, "libarchive")
-
-    # regression test for #3489
-    # don't raise for remove --all if environment doesn't exist
-    # split this into a new test
-    rm_rf(prefix, clean_empty_parents=True)
-    conda_cli("remove", f"--prefix={prefix}", "--all")
 
 
 def test_download_only_flag(
