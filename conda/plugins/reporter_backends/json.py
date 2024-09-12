df419df64faafeced26fb3be8b5a72c759f0eb7b
Travis Hathaway <travis.j.hathaway@gmail.com>
2024-09-12 15:29:16 -0700

    adding a confirm_yn implementation for the JSONReporterRenderer class

--- conda/plugins/reporter_backends/json.py
+++ conda/plugins/reporter_backends/json.py
@@ -90,6 +90,13 @@ class JSONReporterRenderer(ReporterRendererBase):
     def spinner(self, message: str, fail_message: str = "failed\n") -> SpinnerBase:
         return JSONSpinner(message, fail_message)
 
+    def confirm_yn(
+        self, message: str = "Proceed", default: str = "yes", dry_run=None
+    ) -> bool:
+        """
+        For this class, we want this method to do nothing
+        """
+
 
 class JSONSpinner(SpinnerBase):
     """
