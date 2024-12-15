75ed683f6821a607a2b861e3e0221d01f8fc0c48
jaimergp <jaimergp@users.noreply.github.com>
2024-12-15 20:12:23 +0100

    Deprecate this one too

--- conda/plugins/solvers.py
+++ conda/plugins/solvers.py
@@ -1,3 +1,16 @@
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
 """Register the classic conda solver."""
+
+from ..deprecations import deprecated
+
+deprecated.module(
+    "25.1.0",
+    "25.7.0",
+    "Use `conda_classic_solver.plugin`.",
+)
+
+
+def conda_solvers():
+    # Deprecated. Use `conda_classic_solver.plugin`
+    pass
