7e70f02ae1457916a6ede23cccf5b03383527a67
Ken Odegard <kodegard@anaconda.com>
2025-01-27 16:52:48 -0600

    Testing for test failure

--- conda/base/context.py
+++ conda/base/context.py
@@ -839,8 +839,8 @@ class Context(Configuration):
             # error-on-undefined.
             return {
                 "CONDA_EXE": os.path.join(sys.prefix, BIN_DIRECTORY, exe),
-                "_CE_M": None,
-                "_CE_CONDA": None,
+                "_CE_M": "",
+                "_CE_CONDA": "",
                 "CONDA_PYTHON_EXE": sys.executable,
             }
 
