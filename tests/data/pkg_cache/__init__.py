da1bd2e84e7657c345d204961d47d4a96d8616db
Ken Odegard <kodegard@anaconda.com>
2025-01-28 12:52:12 -0600

    Revert "`importlib.resources` has been in stdlib since Python 3.9"
    This reverts commit 2b6259395bbf5a26d00f295b45f3e7df6daadbb1.

--- tests/data/pkg_cache/__init__.py
+++ tests/data/pkg_cache/__init__.py
@@ -1,9 +1,14 @@
 # Copyright (C) 2012 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
 import json
-from importlib.resources import files
+import sys
 
 from conda.core.package_cache_data import PackageCacheRecord
+
+if sys.version_info < (3, 12):
+    from importlib_resources import files
+else:
+    from importlib.resources import files
 
 
 def load_data_file(filename):
