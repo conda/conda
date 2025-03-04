a94cc7567ebc32a99dd901389ad07da5b8f7b24b
sophia <scastellarin@quansight.com>
2025-03-04 15:07:09 -0800

    Allow user to specify --name or --prefix as part of conda config

--- conda/cli/main_config.py
+++ conda/cli/main_config.py
@@ -120,6 +120,21 @@ def configure_parser(sub_parsers: _SubParsersAction, **kwargs) -> ArgumentParser
         ),
     )
     location.add_argument("--file", action="store", help="Write to the given file.")
+    location.add_argument(
+       "-n",
+        "--name",
+        action="store",
+        help="Write to the .condarc file for the specified environment name.s",
+        metavar="ENVIRONMENT",
+    )
+    location.add_argument(
+       "-p",
+        "--prefix",
+        action="store",
+        help="Write to the .condarc file for the specified environment prefix.",
+        metavar="PATH",
+    )
+
 
     # XXX: Does this really have to be mutually exclusive. I think the below
     # code will work even if it is a regular group (although combination of
@@ -530,6 +545,8 @@ def execute_config(args, parser):
     from ..common.io import timeout
     from ..common.iterators import groupby_to_dict as groupby
     from ..common.serialize import yaml_round_trip_load
+    from ..gateways.disk.test import is_conda_environment
+    from ..exceptions import EnvironmentLocationNotFound
 
     stdout_write = getLogger("conda.stdout").info
     stderr_write = getLogger("conda.stderr").info
@@ -676,6 +693,11 @@ def execute_config(args, parser):
             rc_path = user_rc_path
     elif args.file:
         rc_path = args.file
+    elif args.prefix or args.name:
+        if is_conda_environment(context.target_prefix):
+            rc_path = join(context.target_prefix, ".condarc")
+        else:
+            raise EnvironmentLocationNotFound(context.target_prefix)
     else:
         rc_path = user_rc_path
 
