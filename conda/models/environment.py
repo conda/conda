d62ee609baea928642d4f5ac50a12f75cedf738d
sophia <scastellarin@quansight.com>
2025-05-21 16:48:23 -0700

    Add a data model for an environment

---
+++ conda/models/environment.py
@@ -0,0 +1,136 @@
+# Copyright (C) 2012 Anaconda, Inc
+# SPDX-License-Identifier: BSD-3-Clause
+"""Conda environment data model"""
+
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from logging import getLogger
+from typing import TYPE_CHECKING
+
+from ..exceptions import CondaValueError
+
+if TYPE_CHECKING:
+    from typing import Any
+
+    from .match_spec import MatchSpec
+    from .records import PackageRecord
+
+
+log = getLogger(__name__)
+
+
+@dataclass
+class Environment:
+    # Prefix the environment is installed into (required).
+    prefix: str
+
+    # The platform this environment may be installed on (required)
+    platform: str
+
+    # Environment level configuration, eg. channels, solver options, etc.
+    config: dict[str, Any] = field(default_factory=dict)
+
+    # Map of other package types that conda can install. For example pypi packages.
+    external_packages: dict[str, list]  = field(default_factory=dict)
+
+    # The complete list of specs for the environment.
+    # eg. after a solve, or from an explicit environemnt spec
+    explicit_specs: list[PackageRecord] = field(default_factory=list)
+
+    # Environment name
+    name: str |  None = None
+
+    # User requested specs for this environment.
+    requested_specs: list[MatchSpec] = field(default_factory=list)
+
+    # Environment variables to be applied to the environment.
+    variables: dict[str, str] = field(default_factory=dict)
+
+    def __post_init__(self):
+        # an environment must have a name of prefix
+        if not self.prefix:
+            raise CondaValueError("'Environment' needs a 'prefix'.")
+
+    @classmethod
+    def merge(cls, *environments):
+        """
+        Keeps first name and/or prefix.
+        Concatenates and deduplicates requirements.
+        Reduces configuration and variables (last key wins).
+        """
+        name = None
+        prefix = None
+        platform = None
+        names = [env.name for env in environments if env.name]
+        prefixes = [env.prefix for env in environments if env.prefix]
+        
+        if names:
+            name = names[0]
+            if len(names) > 1:
+                log.debug("Several names passed %s. Picking first one %s", names, name)
+        
+        if prefixes:
+            prefix = prefixes[0]
+            if len(prefixes) > 1:
+                log.debug(
+                    "Several prefixes passed %s. Picking first one %s", prefixes, prefix
+                )
+        
+        platforms = [env.platform for env in environments if env.platform]
+        # Ensure that all environments have the same platform
+        if len(set(platforms)) == 1:
+            platform = platforms[0]
+        else:
+            raise CondaValueError(f"Conda can not merge environments of different platforms. Recieved environments with plafroms {platforms}")
+
+        requested_specs = list(
+            dict.fromkeys(
+                requirement for env in environments for requirement in env.requested_specs
+            )
+        )
+
+        explicit_specs = list(
+            dict.fromkeys(
+                requirement for env in environments for requirement in env.explicit_specs
+            )
+        )
+
+        variables = {k: v for env in environments for (k, v) in env.variables.items()}
+
+        config = {}
+        external_packages = {}
+        for env in environments:
+            # Config items can be any type, merge them so that lists get 
+            # concatenated, dicts get merged, and primitive types get clobbered.
+            for k, v in env.config.items():
+                if k not in config:
+                    config[k] = v
+                elif isinstance(config[k], list) and isinstance(v, list):
+                    config[k].extend(v)
+                elif isinstance(config[k], dict) and isinstance(v, dict):
+                    config[k].update(v)
+                else:
+                    log.debug("merging configs, clobbering value %s with value %s")
+                    config[k] = v
+
+            # External packages map values are always lists of strings. So,
+            # we'll want to concatentate each list.
+            for k, v in env.external_packages.items():
+                if k in external_packages:
+                    for val in v:
+                        if val not in external_packages[k]:
+                            external_packages[k].append(val)
+                elif isinstance(v, list):
+                    external_packages[k] = v
+
+        return cls(
+            config=config,
+            external_packages= external_packages,
+            explicit_specs=explicit_specs,
+            name=name,
+            platform=platform,
+            prefix=prefix,
+            requested_specs=requested_specs,
+            variables=variables,
+        )
---
+++ tests/models/test_environment.py
@@ -0,0 +1,50 @@
+# Copyright (C) 2012 Anaconda, Inc
+# SPDX-License-Identifier: BSD-3-Clause
+
+import pytest
+
+from conda.models.environment import Environment
+from conda.models.match_spec import MatchSpec
+from conda.exceptions import CondaValueError
+
+
+def test_create_environment_no_prefix():
+    with pytest.raises(CondaValueError):
+        Environment(platform="platform", prefix=None)
+
+
+def test_environments_merge():
+    env1 = Environment(
+        prefix="/path/to/env1",
+        platform="one",
+        config={"primitive_one": "yes", "list_one": [1,2,4]},
+        external_packages={"pip": ["one", "two"], "other": ["three"]},
+        explicit_specs=[MatchSpec("somepackage.conda")],
+        requested_specs=[MatchSpec("numpy"), MatchSpec("pandas")],
+        variables={"PATH": "/usr/bin"}
+    )
+    env2 = Environment(
+        prefix="/path/to/env1",
+        platform="one",
+        config={"primitive_one": "no", "primitive_two": "yes", "list_one": [3]},
+        external_packages={"pip": ["two", "flask"], "a": ["nother"]},
+        explicit_specs=[MatchSpec("somepackageother.conda")],
+        requested_specs=[MatchSpec("numpy"), MatchSpec("flask")],
+        variables={"VAR": "IABLE"}
+    )
+    merged = Environment.merge(env1, env2)
+    assert merged.prefix == "/path/to/env1"
+    assert merged.platform == "one"
+    assert merged.config == {"primitive_one": "no", "primitive_two": "yes", "list_one": [1,2,4,3]}
+    assert merged.external_packages == {"pip": ["one", "two", "flask"], "other": ["three"], "a": ["nother"]}
+    assert set(merged.requested_specs) == set([MatchSpec("pandas"), MatchSpec("flask"), MatchSpec("numpy")])
+    assert set(merged.explicit_specs) == set([MatchSpec("somepackageother.conda"), MatchSpec("somepackage.conda")])
+    assert merged.variables == {"PATH": "/usr/bin", "VAR": "IABLE"}
+
+
+def test_environments_merge_colliding_platform():
+    env1 = Environment(prefix="/path/to/env1", platform="one")
+    env2 = Environment(prefix="/path/to/env2", platform="two")
+    
+    with pytest.raises(CondaValueError):
+        Environment.merge(env1, env2)
