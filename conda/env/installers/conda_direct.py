14c8fe34179dbf455a7d51ddc749a830eeac98f9
Jonathan Helmus <jjhelmus@gmail.com>
2025-05-22 11:59:22 -0500

    add pip_direct env install

--- conda/env/installers/conda_direct.py
+++ conda/env/installers/conda_direct.py
@@ -2,9 +2,16 @@
 # SPDX-License-Identifier: BSD-3-Clause
 """Installer for conda packages from a direct reference (e.g. by a URL)."""
 
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from ...direct import direct
 
+if TYPE_CHECKING:
+    from ...direct import DirectPackages
 
-def install(prefix, specs, args, env, *_, **kwargs):
+
+def install(prefix: str, specs: DirectPackages, *_, **kwargs):
     """Install packages into an environment"""
     return direct(specs, prefix)
