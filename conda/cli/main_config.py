cd04e2e3d33cf6062cf76389f65a7e0a0d961a33
Peyton Murray <peynmurray@gmail.com>
2025-06-02 09:08:58 -0700

    Update conda/cli/main_config.py
    Co-authored-by: jaimergp <jaimergp@users.noreply.github.com>

--- conda/cli/main_config.py
+++ conda/cli/main_config.py
@@ -1008,7 +1008,7 @@ def migrate_pkgs(context, _config: dict):
     if layout != PkgEnvLayout.USER:
         raise CondaError(
             f"The conda configuration for pkg_env_layout is set to {layout}; "
-            "Unless this setting is changed with `conda config set pkg_env_layout user` "
+            "Unless this setting is changed with `conda config --set pkg_env_layout user` "
             "conda will continue to store packages in the root prefix by default."
         )
 
