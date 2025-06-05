c1166b180d89490971bf1f23e1596cdd373cffdd
sophia <scastellarin@quansight.com>
2025-06-05 09:34:12 -0700

    Allow env spec plugins to enable/disable autodetection

--- conda/plugins/manager.py
+++ conda/plugins/manager.py
@@ -565,20 +565,23 @@ class CondaPluginManager(pluggy.PluginManager):
         hooks = self.get_hook_results("environment_specifiers")
         found = []
         for hook in hooks:
-            log.debug("EnvironmentSpec hook: checking %s", hook.name)
-            if hook.environment_spec(source).can_handle():
-                log.debug(
-                    "EnvironmentSpec hook: %s can be %s",
-                    source,
-                    hook.name,
-                )
-                found.append(hook)
+            if hook.enable_autodetection:
+                log.debug("EnvironmentSpec hook: checking %s", hook.name)
+                if hook.environment_spec(source).can_handle():
+                    log.debug(
+                        "EnvironmentSpec hook: %s can be %s",
+                        source,
+                        hook.name,
+                    )
+                    found.append(hook)
+                else:
+                    log.debug(
+                        "EnvironmentSpec hook: %s can NOT be handled by %s",
+                        source,
+                        hook.name,
+                    )
             else:
-                log.debug(
-                    "EnvironmentSpec hook: %s can NOT be handled by %s",
-                    source,
-                    hook.name,
-                )
+               log.debug("EnvironmentSpec hook '%s' does not support autodetection, skipping", hook.name)
 
         if len(found) == 0:
             # raise error if no plugins found that can read the environment file
