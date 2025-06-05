ec954cf360c878413a2931abe76c24fb92faf959
Jannis Leidel <jannis@leidel.info>
2025-06-05 19:09:54 +0200

    Remove it back

--- conda/plugins/environment_specifiers/__init__.py
+++ conda/plugins/environment_specifiers/__init__.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 """Register the built-in environment specifier hook implementations."""
 
-from . import binstar, environment_yml, requirements
+from . import binstar, environment_yml, requirements_txt
 
 #: The list of environment speficier plugins for easier registration with pluggy
-plugins = [binstar, requirements, environment_yml]
+plugins = [binstar, requirements_txt, environment_yml]
