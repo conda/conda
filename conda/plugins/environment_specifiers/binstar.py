6159c73c67a85d7f7d893a05a8b5709a795a32a4
kenodegard <kodegard@anaconda.com>
2025-07-01 16:31:26 -0500

    Update binstar.py

--- conda/plugins/environment_specifiers/binstar.py
+++ conda/plugins/environment_specifiers/binstar.py
@@ -14,7 +14,7 @@ from .. import CondaEnvironmentSpecifier, hookimpl
 def conda_environment_specifiers():
     # FUTURE: conda 26.3+, remove ignore BinstarSpec deprecation
     with warnings.catch_warnings():
-        warnings.filterwarnings("ignore", message="conda.env.specs.binstar.BinstarSpec")
+        warnings.filterwarnings("ignore", message="conda.env.specs.binstar")
         from ...env.specs.binstar import BinstarSpec
 
     yield CondaEnvironmentSpecifier(
