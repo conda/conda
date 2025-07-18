31278c66678a659e72053d0e6056365760c12541
sophia <scastellarin@quansight.com>
2025-07-18 13:35:50 -0700

    Add health check for conda-meta/pinned file
    In this check, we'll inspect each pinned package. If the package
    does not exist in the prefix, that might indicate that the package
    is not written out right. For example, this may catch typos or
    extra punctuation added to package names.

--- conda/plugins/subcommands/doctor/health_checks.py
+++ conda/plugins/subcommands/doctor/health_checks.py
@@ -12,6 +12,7 @@ from requests.exceptions import RequestException
 
 from ....base.context import context
 from ....common.serialize import json, yaml_safe_dump
+from ....common.io import dashlist
 from ....core.envs_manager import get_user_environments_txt_file
 from ....core.prefix_data import PrefixData
 from ....exceptions import CondaError
@@ -238,6 +239,36 @@ def consistent_env_check(prefix: str, verbose: bool) -> None:
         print(f"{OK_MARK} The environment is consistent.\n")
 
 
+def pinned_well_formatted_check(prefix: str, verbose: bool) -> None:
+    prefix_data = PrefixData(prefix_path=prefix)
+
+    try:
+        pinned_specs = prefix_data.get_pinned_specs()
+    except Exception as err:
+        print(f"{X_MARK} Unable to read pinned file at {prefix}/conda-meta/pinned:\n\t{err}")
+        return
+
+    # If there are no pinned specs, exit early
+    if not pinned_specs:
+        print(f"{OK_MARK} No pinned specs found in {prefix}/conda-meta/pinned.")
+        return
+
+    # If there is a pinned package that does not exist in the prefix, that
+    # is an indication that it might be a typo.
+    maybe_malformed =  [
+        pinned for pinned in pinned_specs if not any(prefix_data.query(pinned.name))
+    ]
+
+    # Inform the user of any packages that might be malformed
+    if maybe_malformed:
+        print(f"{X_MARK} The following specs in {prefix}/conda-meta/pinned are maybe malformed:")
+        print(dashlist((spec.name for spec in maybe_malformed), indent=4))
+        return
+
+    # If there are no malformed packages, the pinned file is well formatted
+    print(f"{OK_MARK} The pinned file in {prefix}/conda-meta/pinned is seems well formatted.")
+
+
 @hookimpl
 def conda_health_checks():
     yield CondaHealthCheck(name="Missing Files", action=missing_files)
@@ -248,4 +279,7 @@ def conda_health_checks():
     )
     yield CondaHealthCheck(
         name="Consistent Environment Check", action=consistent_env_check
+    )
+    yield CondaHealthCheck(
+        name="conda-meta/pinned Well Formatted Check", action=pinned_well_formatted_check
     )
