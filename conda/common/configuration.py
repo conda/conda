0b6419aae821e0921c54184ed0bca022a0ac5b26
sophia <scastellarin@quansight.com>
2025-08-01 14:32:24 -0700

    Add config type for environment config

--- conda/common/configuration.py
+++ conda/common/configuration.py
@@ -58,11 +58,13 @@ del _getFreezeConversionMap
 del _register
 
 if TYPE_CHECKING:
+    from argparse import Namespace
     from collections.abc import Hashable, Iterable, Sequence
     from re import Match
     from typing import Any
 
     from ..common.path import PathsType
+    from ..models.environment import EnvironmentConfig
 
 log = getLogger(__name__)
 
@@ -409,6 +411,44 @@ class YamlRawParameter(RawParameter):
                     position=err.position,
                 )
             return cls.make_raw_parameters(filepath, yaml_obj) or EMPTY_MAP
+
+
+class EnvironmentSpecificationRawParameter(RawParameter):
+    """This class represents a raw parameter originating from an environment specification file"""
+
+    def value(self, parameter_obj):
+        if isiterable(self._raw_value):
+            children_values = []
+            for i in range(len(self._raw_value)):
+                children_values.append(
+                    EnvironmentSpecificationRawParameter(self.source, self.key, self._raw_value[i])
+                )
+            return tuple(children_values)
+        else:
+            return deepfreeze(self._raw_value)
+
+    def keyflag(self):
+        return None
+
+    def valueflags(self, parameter_obj):
+        return None if isinstance(parameter_obj, PrimitiveLoadedParameter) else ()
+
+    @classmethod
+    def make_raw_parameters(cls, source, from_map):
+        """
+        Create an EnvironmentSpecificationRawParameter from an environment specification
+        :param source: the path to the environment specification file
+        :param from_map: a map of key-value pairs representing the configuration from the environment file
+        :return: a map of EnvironmentSpecificationRawParameters
+        """
+        if from_map:
+            return {
+                key: cls(
+                    source, key, from_map[key]
+                )
+                for key in from_map
+            }
+        return EMPTY_MAP
 
 
 class DefaultValueRawParameter(RawParameter):
@@ -1388,7 +1428,14 @@ def custom_expandvars(
 
 
 class Configuration(metaclass=ConfigurationType):
-    def __init__(self, search_path=(), app_name=None, argparse_args=None, **kwargs):
+    def __init__(
+        self,
+        search_path: Iterable[PathsType] = (),
+        app_name: str | None = None,
+        argparse_args: Namespace | None = None,
+        env_spec_config: dict[str, EnvironmentConfig] | None = None,
+        **kwargs
+    ):
         # Currently, __init__ does a **full** disk reload of all files.
         # A future improvement would be to cache files that are already loaded.
         self.raw_data = {}
@@ -1397,6 +1444,8 @@ class Configuration(metaclass=ConfigurationType):
         self._validation_errors = defaultdict(list)
 
         self._set_search_path(search_path, **kwargs)
+        if env_spec_config:
+            self._set_environment_spec_data(env_spec_config)
         self._set_env_vars(app_name)
         self._set_argparse_args(argparse_args)
 
@@ -1499,6 +1548,18 @@ class Configuration(metaclass=ConfigurationType):
 
         self._reset_cache()
         return self
+
+    def _set_environment_spec_data(self, env_spec_config: dict[Path, EnvironmentConfig]):
+        for file, config in env_spec_config.items():
+            # Ensure the file is a Path
+            file_path = Path(file)
+            self._set_raw_data(
+                {
+                    file_path: EnvironmentSpecificationRawParameter.make_raw_parameters(
+                        file_path, {k: v for k, v in vars(config).items() if v is not None}
+                    )
+                }
+            )
 
     def _set_raw_data(self, raw_data: Mapping[Hashable, dict]):
         self.raw_data.update(raw_data)
