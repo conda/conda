adba91cf38af4f197182a400408987dad3032c66
Mahe Iram Khan <forgotten.programme@gmail.com>
2025-08-07 09:42:28 +0200

    increase sleep time to account for window

--- tests/gateways/disk/test_lock.py
+++ tests/gateways/disk/test_lock.py
@@ -63,7 +63,7 @@ def lock_wrapper(path):
     try:
         with path.open("r+b") as fd:
             with lock(fd, lock_attempts=1):
-                time.sleep(4)
+                time.sleep(5)
             return "success"
     except LockError:
         return "lock_error"
