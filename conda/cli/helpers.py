89fccb92401e2a31da051215aaf3c9ae9037e813
kenodegard <kodegard@anaconda.com>
2025-08-21 16:27:32 -0500

    Replace custom `--subdir`/`--platform` definition with `add_parser_platform` helper function

--- conda/cli/helpers.py
+++ conda/cli/helpers.py
@@ -563,10 +563,17 @@ def add_parser_default_packages(p: ArgumentParser) -> None:
     )
 
 
-def add_parser_platform(parser):
+def add_parser_platform(parser: ArgumentParser, *, help: str | None = None) -> None:
     from ..base.constants import KNOWN_SUBDIRS
     from ..common.constants import NULL
 
+    help = (
+        help
+        or (
+            "Use packages built for this platform. "
+            "The new environment will be configured to remember this choice. "
+        )
+    ).strip()
     parser.add_argument(
         "--subdir",
         "--platform",
@@ -574,10 +581,11 @@ def add_parser_platform(parser):
         dest="subdir",
         choices=[s for s in KNOWN_SUBDIRS if s != "noarch"],
         metavar="SUBDIR",
-        help="Use packages built for this platform. "
-        "The new environment will be configured to remember this choice. "
-        "Should be formatted like 'osx-64', 'linux-32', 'win-64', and so on. "
-        "Defaults to the current (native) platform.",
+        help=(
+            f"{help}{' ' if help else ''}"
+            f"Should be formatted like 'osx-64', 'linux-32', 'win-64', and so on. "
+            f"Defaults to the current (native) platform."
+        ),
     )
 
 
