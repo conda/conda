70fab5f6e1e11e82c20d6c4ae2cdfd60661698d2
kenodegard <kodegard@anaconda.com>
2025-09-11 10:44:02 -0500

    Update test_exports.py

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -2,15 +2,20 @@
 # SPDX-License-Identifier: BSD-3-Clause
 from __future__ import annotations
 
+from contextlib import nullcontext
 
-# TODO: will need to be re-enabled for `CondaJSONEncoder` deprecation warning in 26.3
-# @pytest.mark.parametrize(
-#     "function,raises",
-#     [
-#         ("EntityEncoder", TypeError),
-#     ],
-# )
-# def test_deprecations(function: str, raises: type[Exception] | None) -> None:
-#     raises_context = pytest.raises(raises) if raises else nullcontext()
-#     with pytest.deprecated_call(), raises_context:
-#         getattr(exports, function)()
+import pytest
+
+from conda import exports
+
+
+@pytest.mark.parametrize(
+    "function,raises",
+    [
+        ("EntityEncoder", None),
+    ],
+)
+def test_deprecations(function: str, raises: type[Exception] | None) -> None:
+    raises_context = pytest.raises(raises) if raises else nullcontext()
+    with pytest.deprecated_call(), raises_context:
+        getattr(exports, function)()
