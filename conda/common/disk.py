a9477c1a864995bc66fd281cb02a26610238c860
Sophia Castellarin <scastellarin@quansight.com>
2025-09-16 02:28:00 -0700

    Remove deprecated modules - `conda.cli.python_api` & `conda.common.disk` (#15219)
    * Remove deprecated module
    
    * Add news
    
    * Also remove conda.common.disk

--- conda/common/disk.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2012 Anaconda, Inc
-# SPDX-License-Identifier: BSD-3-Clause
-"""Common disk utilities."""
-
-from ..deprecations import deprecated
-
-deprecated.module("25.3", "25.9", addendum="Nothing to import.")
