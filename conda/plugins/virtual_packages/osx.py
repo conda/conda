488838b3630e31f39fec48121eeb1e024057b459
Mahe Iram Khan <forgotten.programme@gmail.com>
2025-09-30 15:33:50 +0200

    change windows and osx

--- conda/plugins/virtual_packages/osx.py
+++ conda/plugins/virtual_packages/osx.py
@@ -2,29 +2,26 @@
 # SPDX-License-Identifier: BSD-3-Clause
 """Detect whether this is macOS."""
 
-import os
-
 from ...base.context import context
 from .. import hookimpl
 from ..types import CondaVirtualPackage
+
+
+def osx_version():
+    dist_name, dist_version = context.os_distribution_name_version
+    if dist_name != "OSX":
+        # avoid reporting platform.version() of a different OS
+        # this happens with CONDA_SUBDIR=osx-* in a non macOS machine or when `--platform` is used on the CLI
+        dist_version = "0"
+    return dist_version
 
 
 @hookimpl
 def conda_virtual_packages():
     if not context.subdir.startswith("osx-"):
         return
-
     # 1: __osx (always exported if the target subdir is osx-*)
     yield CondaVirtualPackage("unix", None, None)
-
     # 2: __osx
-    dist_version = os.getenv("CONDA_OVERRIDE_OSX")
-    if dist_version is None:  # no override found, let's detect it
-        dist_name, dist_version = context.os_distribution_name_version
-        if dist_name != "OSX":
-            # avoid reporting platform.version() of a different OS
-            # this happens with CONDA_SUBDIR=osx-* in a non macOS machine
-            dist_version = "0"
-    if dist_version:  # truthy override found
-        yield CondaVirtualPackage("osx", dist_version, None)
+    yield CondaVirtualPackage("osx", osx_version, None)
     # if a falsey override was found, the __osx virtual package is not exported
--- conda/plugins/virtual_packages/windows.py
+++ conda/plugins/virtual_packages/windows.py
@@ -2,24 +2,22 @@
 # SPDX-License-Identifier: BSD-3-Clause
 """Detect whether this is Windows."""
 
-import os
-
 from ...base.context import context
 from .. import hookimpl
 from ..types import CondaVirtualPackage
+
+
+def win_version():
+    dist_name, dist_version = context.os_distribution_name_version
+    if dist_name != "Windows":
+        # avoid reporting platform.version() of other OS
+        # this happens with CONDA_SUBDIR=win-* in a non Windows machine
+        dist_version = "0"
+    return dist_version
 
 
 @hookimpl
 def conda_virtual_packages():
     if not context.subdir.startswith("win-"):
         return
-
-    dist_version = os.getenv("CONDA_OVERRIDE_WIN")
-    if dist_version is None:
-        dist_name, dist_version = context.os_distribution_name_version
-        if dist_name != "Windows":
-            # avoid reporting platform.version() of other OS
-            # this happens with CONDA_SUBDIR=win-* in a non Windows machine
-            dist_version = "0"
-    if dist_version:
-        yield CondaVirtualPackage("win", dist_version, None)
+    yield CondaVirtualPackage("win", win_version, None)
