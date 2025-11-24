7a5cd66db2d9becca68d360601e2309d4f67c8fd
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2025-11-25 05:08:25 +0530

    Fix test

--- tests/cli/test_main_run.py
+++ tests/cli/test_main_run.py
@@ -125,5 +125,5 @@ def test_run_with_empty_command_will_raise(
 ):
     from conda.exceptions import ArgumentError
 
-    with pytest.raises(ArgumentError, match="No command specified"):
+    with pytest.raises(ArgumentError, match="No command has been specified to run"):
         conda_cli("run")
