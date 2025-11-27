cd0844b494a3b64732a85e34838a7a7d9052e725
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2025-11-28 00:29:46 +0530

    Use a logger to print environment activation message

--- conda/cli/common.py
+++ conda/cli/common.py
@@ -306,7 +306,7 @@ def print_activate(env_name_or_prefix):  # pragma: no cover
             #     $ conda deactivate
             """
         )
-        print(message)  # TODO: use logger
+        getLogger("conda.stdout").info(message)
 
 
 def validate_environment_files_consistency(files: list[str]) -> None:
