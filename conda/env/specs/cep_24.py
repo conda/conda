478812bee6425e7ecb1882545e02f4bdc69f7bda
sophia <scastellarin@quansight.com>
2025-12-08 14:56:29 -0800

    Accept env spec file contents

--- conda/env/specs/cep_24.py
+++ conda/env/specs/cep_24.py
@@ -24,9 +24,6 @@ class Cep24YamlFileSpec(EnvironmentSpecBase):
     _environment = None
     extensions = {".yaml", ".yml"}
 
-    def __init__(self, filename: str | None = None, **kwargs):
-        self.filename = filename
-
     def can_handle(self):
         """
         Validates loader can process environment definition.
@@ -39,7 +36,7 @@ class Cep24YamlFileSpec(EnvironmentSpecBase):
         """
         if not self.filename:
             return False
-
+        
         # Extract the file extension (e.g., '.txt' or '' if no extension)
         _, file_ext = os.path.splitext(self.filename)
 
@@ -48,8 +45,7 @@ class Cep24YamlFileSpec(EnvironmentSpecBase):
             return False
 
         try:
-            yamlstr = env.load_file(self.filename)
-            data = yaml_safe_load(yamlstr)
+            data = yaml_safe_load(self.data)
             errors = env.get_schema_errors(data)
             if errors:
                 return False
@@ -61,5 +57,5 @@ class Cep24YamlFileSpec(EnvironmentSpecBase):
     @property
     def env(self) -> Environment:
         if not self._environment:
-            self._environment = env.from_file(self.filename)
+            self._environment = env.from_yaml(self.data)
         return self._environment.to_environment_model()
