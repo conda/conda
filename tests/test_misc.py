8bcfab5699e110a092c7d832bf1f8470ca63caf7
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2025-12-09 21:02:46 +0530

    Bad update, fix `test_explicit_missing_cache_entries`

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -118,7 +118,7 @@ def test_explicit_missing_cache_entries(
 
     with pytest.raises(
         SpecNotFoundInPackageCache,
-        match="Missing package cache records for: test-recipes/noarch::missing==1.0.0=1",
+        match="Missing package cache records for: test-recipes/noarch::missing==1.0.0=0",
     ):
         schema = "file:///" if on_win else "file://"
         noarch = test_recipes_channel / "noarch"
