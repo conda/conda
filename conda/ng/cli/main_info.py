b25c9a704cddd2cf34c1c4b9405f667971126c16
jaimergp <jaimergp@users.noreply.github.com>
2025-12-31 15:04:52 +0100

    Crude 'info' implementation

--- conda/ng/cli/main_info.py
+++ conda/ng/cli/main_info.py
@@ -6,11 +6,29 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from conda import CondaError
-
 if TYPE_CHECKING:
     from argparse import ArgumentParser, Namespace
 
 
 def execute(args: Namespace, parser: ArgumentParser) -> int:
-    raise CondaError("Not implemented yet")
+    from textwrap import dedent
+
+    from rich import box
+    from rich.table import Column
+
+    from conda.cli.main_info import get_info_dict, get_main_info_display
+
+    from .common import create_console, create_table
+
+    table = create_table(
+        Column(justify="right", style="dim"),
+        Column(justify="left"),
+        show_header=False,
+        show_footer=False,
+        box=box.MINIMAL,
+    )
+    for field, value in get_main_info_display(get_info_dict()).items():
+        # HACK get_main_info_display force-"flattens" its multiline values; undo here
+        value = dedent((26 * " ") + str(value))
+        table.add_row(field, value if value != "None" else "[dim]none[/]")
+    create_console().print("", table, "")
