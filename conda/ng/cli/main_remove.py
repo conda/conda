931e5a2afcb960be4f99bd3a42974c72d6014806
jaimergp <jaimergp@users.noreply.github.com>
2026-01-03 18:53:35 +0100

    'remove': implement --force

--- conda/ng/cli/main_remove.py
+++ conda/ng/cli/main_remove.py
@@ -51,33 +51,73 @@ def execute(args: Namespace, parser: ArgumentParser) -> int:
         for pkg in context.plugin_manager.get_virtual_package_records()
     ]
 
+    # conda remove allows globbed names; make sure we don't install those!
+    remove = set()
+    for spec in names:
+        if "*" in str(spec):
+            raise ArgumentError(
+                f"Asterisks in package names are not supported yet: {spec}"
+            )
+            for record_name, record in installed.items():
+                if spec.match(record):
+                    remove.add(record_name)
+        elif spec.name.normalized not in installed:
+            raise ArgumentError(
+                f"Package '{spec}' is not installed. Nothing to remove."
+            )
+        else:
+            remove.add(spec.name.normalized)
+    remove_depends = set()
+    for spec in history:
+        installed_history = installed[spec.name.normalized]
+        for dep in installed_history.depends:
+            dep_spec = MatchSpec(dep)
+            if dep_spec.name.normalized in remove:
+                remove_depends.add(spec.name.normalized)
+
+    if args.force_remove:
+        from rattler import install as rattler_install
+
+        from conda.reporters import confirm_yn
+
+        from .common import cache_dir, create_console
+        from .install import solution_table
+
+        records = [rec for name, rec in installed.items() if name not in remove]
+        n_pkgs = len(installed) - len(records)
+        if not context.quiet and not context.json:
+            create_console().print(
+                solution_table(
+                    records=records,
+                    specs=names,
+                    history=history,
+                    installed=installed.values(),
+                    removing=True,
+                ),
+                "",
+            )
+
+        confirm_yn(
+            f"Force removing {n_pkgs} package(s). "
+            "Environment may be left unusable. Proceed?",
+            dry_run=context.dry_run,
+        )
+
+        rattler_install(
+            records,
+            target_prefix=prefix,
+            cache_dir=cache_dir("pkgs"),
+            installed_packages=installed.values(),
+            show_progress=not context.quiet and not context.json,
+            execute_link_scripts=True,
+        )
+        return 0
+
     conflicts = {}
     t0 = time.perf_counter()
+    last_exception = None
     for _ in range(1, 6):
         try:
-            # conda remove allows globbed names; make sure we don't install those!
-            remove = set()
-            for spec in names:
-                if "*" in str(spec):
-                    raise ArgumentError(
-                        f"Asterisks in package names are not supported yet: {spec}"
-                    )
-                    for record_name, record in installed.items():
-                        if spec.match(record):
-                            remove.add(record_name)
-                elif spec.name.normalized not in installed:
-                    raise ArgumentError(
-                        f"Package '{spec}' is not installed. Nothing to remove."
-                    )
-                else:
-                    remove.add(spec.name.normalized)
-            remove_depends = set()
-            for spec in history:
-                installed_history = installed[spec.name.normalized]
-                for dep in installed_history.depends:
-                    dep_spec = MatchSpec(dep)
-                    if dep_spec.name.normalized in remove:
-                        remove_depends.add(spec.name.normalized)
             conflicting_names = set(conflicts.keys())
             specs = [
                 spec
@@ -99,18 +139,22 @@ def execute(args: Namespace, parser: ArgumentParser) -> int:
                 ],
                 virtual_packages=virtual_packages,
                 constraints=[f"{name}<0.0dev0" for name in remove],
-                report=True,
+                report=not context.quiet and not context.json,
                 dry_run=context.dry_run,
                 removing=True,
                 t0=t0,
             )
         except CondaSolverError as exc:
+            last_exception = exc
             new_conflicts = parse_conflicts(
                 str(exc), conflicts=conflicts, installed=installed.values()
             )
             conflicts.update(new_conflicts)
         else:
             break
+    else:
+        if last_exception:
+            raise last_exception
 
     History(prefix).write_specs(remove_specs=names)
 
