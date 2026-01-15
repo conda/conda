522cc8081f3eccfa34074a925b9ca80326289e67
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2026-01-15 08:02:27 +0530

    Revert "Delete `PYTHONPATH` variable"
    This reverts commit 152fd2b33160fefe69b404bdfdeb8b548c1a6332.

--- tests/core/test_initialize.py
+++ tests/core/test_initialize.py
@@ -436,7 +436,6 @@ def test_make_entry_point_exe(verbose):
 def test_install_conda_sh(verbose, monkeypatch: MonkeyPatch):
     with tempdir() as conda_prefix:
         target_path = join(conda_prefix, "etc", "profile.d", "conda.sh")
-        monkeypatch.delenv("PYTHONPATH", raising=False)
         monkeypatch.setenv("CONDA_DEV", "0")
         reset_context()
         result = install_conda_sh(target_path, conda_prefix)
@@ -484,7 +483,6 @@ def test_install_conda_sh(verbose, monkeypatch: MonkeyPatch):
 def test_install_conda_fish(verbose, monkeypatch: MonkeyPatch):
     with tempdir() as conda_temp_prefix:
         target_path = join(conda_temp_prefix, "etc", "fish", "conf.d", "conda.fish")
-        monkeypatch.delenv("PYTHONPATH", raising=False)
         monkeypatch.setenv("CONDA_DEV", "0")
         reset_context()
         result = install_conda_fish(target_path, context.conda_prefix)
@@ -530,7 +528,6 @@ def test_install_conda_xsh(verbose, monkeypatch: MonkeyPatch):
 
     with tempdir() as conda_temp_prefix:
         target_path = join(conda_temp_prefix, "Lib", "site-packages", "conda.xsh")
-        monkeypatch.delenv("PYTHONPATH", raising=False)
         monkeypatch.setenv("CONDA_DEV", "0")
         reset_context()
         result = install_conda_xsh(target_path, context.conda_prefix)
@@ -566,7 +563,6 @@ def test_install_conda_xsh(verbose, monkeypatch: MonkeyPatch):
 def test_install_conda_csh(verbose, monkeypatch: MonkeyPatch):
     with tempdir() as conda_temp_prefix:
         target_path = join(conda_temp_prefix, "etc", "profile.d", "conda.csh")
-        monkeypatch.delenv("PYTHONPATH", raising=False)
         monkeypatch.setenv("CONDA_DEV", "0")
         reset_context()
         result = install_conda_csh(target_path, context.conda_prefix)
