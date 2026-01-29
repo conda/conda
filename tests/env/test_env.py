53aaebcadb0871e2de1faed5eb97160f0f6cd4d3
sophia <scastellarin@quansight.com>
2026-01-28 16:00:21 -0800

    Deprecate conda.env.env.load_file

--- tests/env/test_env.py
+++ tests/env/test_env.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 import os
 import random
+from contextlib import nullcontext
 from io import StringIO
 from typing import TYPE_CHECKING
 from unittest.mock import patch
@@ -12,6 +13,7 @@ import pytest
 
 from conda.common.serialize import yaml
 from conda.core.prefix_data import PrefixData
+from conda.env import env as env_module
 from conda.env.env import (
     VALID_KEYS,
     EnvironmentYaml,
@@ -524,3 +526,13 @@ def test_prefix_validation(prefix, error_message):
             prefix_validation(prefix)
     else:
         prefix_validation(prefix)
+
+
+@pytest.mark.parametrize(
+    "function,raises",
+    [("load_file", TypeError)],
+)
+def test_env_deprecations(function: str, raises: type[Exception] | None) -> None:
+    raises_context = pytest.raises(raises) if raises else nullcontext()
+    with pytest.deprecated_call(), raises_context:
+        getattr(env_module, function)()
