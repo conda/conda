02936113d72c79f6aac8f40271eb92a8ab27ea9b
Sophia Castellarin <scastellarin@quansight.com>
2026-03-02 22:54:29 -0800

    Add aliases for environment spec plugins (#15701)
    * Add aliases for environment spec plugins
    
    * Add aliases to alight env spec and export plugins
    
    * Update docs for aliases support
    
    * Add news
    
    * Add integration test
    
    * Remove yaml/yml/txt aliases
    
    * Add env.yml alias
    
    * Parameterize tests

--- conda/plugins/environment_specifiers/requirements_txt.py
+++ conda/plugins/environment_specifiers/requirements_txt.py
@@ -15,5 +15,6 @@ def conda_environment_specifiers():
 
     yield CondaEnvironmentSpecifier(
         name="requirements.txt",
+        aliases=("requirements", "reqs"),
         environment_spec=RequirementsSpec,
     )
--- tests/env/test_create.py
+++ tests/env/test_create.py
@@ -439,3 +439,49 @@ def test_create_env_from_environment_yml_does_not_output_duplicate_warning(
     # get an array of length 2 if the string only appears once. If it appears
     # multiple times, the array will have more elements.
     assert len(stdout.split("EnvironmentSectionNotValid")) == 2
+
+
+@pytest.mark.parametrize(
+    "target_format,file_name",
+    [
+        ("environment-yaml", "env.yaml"),
+        ("env.yml", "env.yaml"),
+        ("requirements", "env.txt"),
+        ("reqs", "env.txt"),
+    ],
+)
+@pytest.mark.integration
+def test_export_and_recreate_environment(
+    conda_cli: CondaCLIFixture,
+    tmp_env: TmpEnvFixture,
+    path_factory: PathFactoryFixture,
+    target_format,
+    file_name,
+):
+    """
+    Test that a user can recreate an environment with the same
+    plugin name they used to export the environment.
+    Ref: https://github.com/conda-incubator/conda-lockfiles/issues/79
+    """
+    # Setup a simple environment
+    with tmp_env("ca-certificates") as prefix:
+        env_file_path = path_factory(file_name)
+        stdout, stderr, rc = conda_cli(
+            "export",
+            f"--prefix={prefix}",
+            f"--format={target_format}",
+            f"--file={env_file_path}",
+        )
+        assert rc == 0, "Unable to export env to format {target_format}"
+
+        # recreate the environment
+        recreate_prefix = path_factory()
+        stdout, stderr, rc = conda_cli(
+            "env",
+            "create",
+            f"--prefix={recreate_prefix}",
+            f"--env-spec={target_format}",
+            f"--file={env_file_path}",
+            "--dry-run",
+        )
+        assert rc == 0, "Unable to recreate env from format {target_format}"
