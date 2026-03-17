5b864567d3c9bfc849c32a8210282385efe7f357
sophia <scastellarin@quansight.com>
2026-03-17 14:58:42 -0700

    Consolidate getting an environment in the plugin manager

--- conda/cli/main_env_create.py
+++ conda/cli/main_env_create.py
@@ -103,7 +103,7 @@ def execute(args: Namespace, parser: ArgumentParser) -> int:
     from ..core.prefix_data import PrefixData
     from ..env.env import print_result
     from ..env.installers.base import get_installer
-    from ..exceptions import CondaEnvException, CondaValueError, InvalidInstaller
+    from ..exceptions import CondaEnvException, InvalidInstaller
     from ..gateways.disk.delete import rm_rf
     from .common import validate_file_exists
 
@@ -111,17 +111,10 @@ def execute(args: Namespace, parser: ArgumentParser) -> int:
     validate_file_exists(args.file)
 
     # detect the file format and get the env representation
-    try:
-        env = context.plugin_manager.get_environment(
-            source=args.file, name=context.environment_specifier
-        )
-    except CondaValueError:
-        spec_hook = context.plugin_manager.get_environment_specifier(
-            source=args.file,
-            name=context.environment_specifier,
-        )
-        spec = spec_hook.environment_spec(args.file)
-        env = spec.env
+    env = context.plugin_manager.get_environment(
+        source=args.file,
+        name=context.environment_specifier,
+    )
 
     # FIXME conda code currently requires args to have a name or prefix
     # don't overwrite name if it's given. gh-254
