7d1fc9f98a921fde05c5d284473c884091a66cf1
Ken Odegard <kodegard@anaconda.com>
2026-03-19 16:56:34 -0500

    Refactor

--- tests/plugins/test_transaction_hooks.py
+++ tests/plugins/test_transaction_hooks.py
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
 from __future__ import annotations
 
-import logging
 from typing import TYPE_CHECKING
 
 import pytest
@@ -57,112 +56,112 @@ class DummyPostActionPlugin:
         )
 
 
-@pytest.fixture()
-def transaction_plugin(plugin_manager_with_reporter_backends, mocker):
-    """Test that post transaction actions get called."""
+@pytest.fixture
+def plugin_manager_with_solvers(plugin_manager_with_reporter_backends, mocker):
     # Explicitly load the solver, since this is a dummy plugin manager and not the default
     plugin_manager_with_reporter_backends.load_plugins(
         solvers,
         *package_extractors.plugins,
     )
 
+    return plugin_manager_with_reporter_backends
+
+
+@pytest.fixture
+def pre_transaction_plugin(plugin_manager_with_solvers, mocker):
     pre_plugin = DummyPreActionPlugin()
     mock_pre_verify = mocker.spy(DummyPreTransactionAction, "verify")
     mock_pre_execute = mocker.spy(DummyPreTransactionAction, "execute")
     mock_pre_reverse = mocker.spy(DummyPreTransactionAction, "reverse")
     mock_pre_cleanup = mocker.spy(DummyPreTransactionAction, "cleanup")
 
+    plugin_manager_with_solvers.register(pre_plugin)
+
+    return (mock_pre_verify, mock_pre_execute, mock_pre_reverse, mock_pre_cleanup)
+
+
+@pytest.fixture
+def post_transaction_plugin(plugin_manager_with_solvers, mocker):
     post_plugin = DummyPostActionPlugin()
     mock_post_verify = mocker.spy(DummyPostTransactionAction, "verify")
     mock_post_execute = mocker.spy(DummyPostTransactionAction, "execute")
     mock_post_reverse = mocker.spy(DummyPostTransactionAction, "reverse")
     mock_post_cleanup = mocker.spy(DummyPostTransactionAction, "cleanup")
 
-    plugin_manager_with_reporter_backends.register(pre_plugin)
-    plugin_manager_with_reporter_backends.register(post_plugin)
+    plugin_manager_with_solvers.register(post_plugin)
 
-    return (
-        (mock_pre_verify, mock_pre_execute, mock_pre_reverse, mock_pre_cleanup),
-        (mock_post_verify, mock_post_execute, mock_post_reverse, mock_post_cleanup),
-    )
+    return (mock_post_verify, mock_post_execute, mock_post_reverse, mock_post_cleanup)
 
 
 def test_transaction_hooks_invoked(
     tmp_env,
-    transaction_plugin,
-    caplog,
+    pre_transaction_plugin,
+    post_transaction_plugin,
     test_recipes_channel: Path,
 ):
     """Test that the transaction hooks are invoked as expected."""
-    with caplog.at_level(logging.INFO):
-        with tmp_env("small-executable", "--solver=classic"):
-            pass
+    pre_verify, pre_execute, pre_reverse, pre_cleanup = pre_transaction_plugin
+    post_verify, post_execute, post_reverse, post_cleanup = post_transaction_plugin
 
-    mock_pre, mock_post = transaction_plugin
+    with tmp_env("small-executable", "--solver=classic"):
+        pass
 
-    mock_pre_verify, mock_pre_execute, mock_pre_reverse, mock_pre_cleanup = mock_pre
-    mock_post_verify, mock_post_execute, mock_post_reverse, mock_post_cleanup = (
-        mock_post
-    )
+    pre_verify.assert_called_once()
+    pre_execute.assert_called_once()
+    pre_reverse.assert_not_called()
+    pre_cleanup.assert_called_once()
 
-    mock_pre_verify.assert_called_once()
-    mock_pre_execute.assert_called_once()
-    mock_pre_reverse.assert_not_called()
-    mock_pre_cleanup.assert_called_once()
-
-    mock_post_verify.assert_called_once()
-    mock_post_execute.assert_called_once()
-    mock_post_reverse.assert_not_called()
-    mock_post_cleanup.assert_called_once()
+    post_verify.assert_called_once()
+    post_execute.assert_called_once()
+    post_reverse.assert_not_called()
+    post_cleanup.assert_called_once()
 
 
 def test_pre_transaction_raises_exception(
     tmp_env,
-    transaction_plugin,
+    pre_transaction_plugin,
     test_recipes_channel: Path,
 ):
     """Test that exceptions get bubbled up from inside the pre-transaction hooks."""
     msg = "💥"
 
-    mock_pre, _ = transaction_plugin
-    mock_verify, mock_execute, mock_reverse, mock_cleanup = mock_pre
-    mock_execute.side_effect = Exception(msg)
+    pre_verify, pre_execute, pre_reverse, pre_cleanup = pre_transaction_plugin
+    pre_execute.side_effect = Exception(msg)
 
     with pytest.raises(Exception, match=msg):
         with tmp_env("small-executable", "--solver=classic"):
             pass
 
-    mock_verify.assert_called_once()
-    mock_execute.assert_called_once()
+    pre_verify.assert_called_once()
+    pre_execute.assert_called_once()
 
     # Should this be assert_called_once()?
     # UnlinkLinkTransaction appears to double-rollback on error
-    mock_reverse.assert_called()
+    pre_reverse.assert_called()
 
-    mock_cleanup.assert_not_called()
+    pre_cleanup.assert_not_called()
 
 
 def test_post_transaction_raises_exception(
     tmp_env,
-    transaction_plugin,
+    post_transaction_plugin,
     test_recipes_channel: Path,
 ):
     """Test that exceptions get bubbled up from inside the post-transaction hooks."""
     msg = "💥"
 
-    _, mock_post = transaction_plugin
-    mock_verify, mock_execute, mock_reverse, mock_cleanup = mock_post
-    mock_execute.side_effect = Exception(msg)
+    post_verify, post_execute, post_reverse, post_cleanup = post_transaction_plugin
+    post_execute.side_effect = Exception(msg)
 
     with pytest.raises(Exception, match=msg):
         with tmp_env("small-executable", "--solver=classic"):
             pass
 
-    mock_verify.assert_called_once()
-    mock_execute.assert_called_once()
+    post_verify.assert_called_once()
+    post_execute.assert_called_once()
 
     # Should this be assert_called_once()?
     # UnlinkLinkTransaction appears to double-rollback on error
-    mock_reverse.assert_called()
+    post_reverse.assert_called()
 
-    mock_cleanup.assert_not_called()
+    post_cleanup.assert_not_called()
