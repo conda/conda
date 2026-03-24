da75911f0802cc63867b0175440dc7572d502049
Travis Hathaway <travis.j.hathaway@gmail.com>
2026-03-24 09:20:47 +0100

    Add new metadata fields to environment plugins (`environment_format` and `description`) (#15763)
    * adding initial version of proposed changes
    
    * adding news file\
    ;
    
    * shifting metadata fields to use EnvironmentFormat enum instead of is_lockfile boolean

--- conda/plugins/environment_specifiers/environment_yml.py
+++ conda/plugins/environment_specifiers/environment_yml.py
@@ -17,4 +17,5 @@ def conda_environment_specifiers():
         name="environment.yml",
         environment_spec=YamlFileSpec,
         default_filenames=("environment.yml", "environment.yaml"),
+        description="Standard YAML environment specification with dependencies",
     )
--- conda/plugins/environment_specifiers/explicit.py
+++ conda/plugins/environment_specifiers/explicit.py
@@ -6,7 +6,7 @@ Register the conda env spec for explicit files.
 """
 
 from .. import hookimpl
-from ..types import CondaEnvironmentSpecifier
+from ..types import CondaEnvironmentSpecifier, EnvironmentFormat
 
 
 @hookimpl
@@ -17,4 +17,6 @@ def conda_environment_specifiers():
         name="explicit",
         environment_spec=ExplicitSpec,
         default_filenames=("explicit.txt",),
+        description="Explicit package URLs for fully reproducible environments",
+        environment_format=EnvironmentFormat.lockfile,
     )
