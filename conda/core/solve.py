d97810cf4b29e312d22d75aaaa7d71d472e6305b
Ken Odegard <kodegard@anaconda.com>
2026-03-25 18:04:55 -0500

    Update conda/core/solve.py
    Co-authored-by: Daniel Holth <dholth@anaconda.com>

--- conda/core/solve.py
+++ conda/core/solve.py
@@ -122,7 +122,7 @@ class Solver:
         force_reinstall=NULL,
         should_retry_solve=False,
     ) -> UnlinkLinkTransaction:
-        """Gives an UnlinkLinkTransaction instance that can be used to execute the solution
+        """Return an UnlinkLinkTransaction instance that can be used to execute the solution
         on an environment.
 
         Args:
