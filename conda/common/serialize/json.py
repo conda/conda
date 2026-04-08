f945d0b07af996c38933b883c0b864a6148ceb6c
Jannis Leidel <jannis@leidel.info>
2026-04-08 20:54:22 +0200

    Use stdlib json instead of requests.compat.json (#15866)

--- conda/common/serialize/json.py
+++ conda/common/serialize/json.py
@@ -4,14 +4,12 @@
 
 from __future__ import annotations
 
+import json  # noqa: TID251
 from enum import Enum
 from pathlib import Path
 from typing import TYPE_CHECKING, overload
 
 from frozendict import frozendict
-
-# detect the best json library to use
-from requests.compat import json
 
 if TYPE_CHECKING:
     from io import IO
