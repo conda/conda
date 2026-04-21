0abe72b8d341d272c8af53c33cdf749abe12c12e
Mahe Iram Khan <forgotten.programme@gmail.com>
2026-04-21 12:10:13 +0200

    add test 2

--- tests/plugins/subcommands/doctor/health_checks/test_external_packages.py
+++ tests/plugins/subcommands/doctor/health_checks/test_external_packages.py
@@ -11,9 +11,27 @@ from conda.plugins.subcommands.doctor.health_checks.external_packages import (
 )
 
 if TYPE_CHECKING:
-    from conda.testing.fixtures import TmpEnvFixture
+    from pathlib import Path
+
+    from conda.testing.fixtures import PipCLIFixture, TmpEnvFixture
 
 
 def test_no_external_packages(tmp_env: TmpEnvFixture):
     with tmp_env() as prefix:
         assert find_external_packages(prefix) == []
+
+
+py_ver = "3.10"
+
+
+def test_external_packages(
+    tmp_env: TmpEnvFixture, pip_cli: PipCLIFixture, wheelhouse: Path
+):
+    with tmp_env(f"python={py_ver}", "pip") as prefix:
+        wheel_path = wheelhouse / "small_python_package-1.0.0-py3-none-any.whl"
+        _, _, _ = pip_cli("install", wheel_path, prefix=prefix)
+
+        packages = find_external_packages(prefix)
+
+        assert packages != []
+        assert "small-python-package" == packages[0].name
