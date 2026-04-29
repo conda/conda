d5b892d20fbae2b2293b1c58ac4b7dccc6d5e07d
Daniel Holth <dholth@anaconda.com>
2026-04-29 15:36:59 -0400

    fix reference

--- conda/gateways/shards/__init__.py
+++ conda/gateways/shards/__init__.py
@@ -2,6 +2,6 @@
 Interface to sharded repodata code.
 """
 
-from _conda.shards.shards_subset import RepodataSubset, build_repodata_subset
+from _conda.shards.subset import RepodataSubset, build_repodata_subset
 
 __all__ = ["RepodataSubset", "build_repodata_subset"]
