358bb3696361cf89fb7ac7291cb25f2e2c2cd053
Daniel Holth <dholth@anaconda.com>
2026-05-11 16:26:01 -0400

    empty conda/_private/__init__.py

--- conda/_private/__init__.py
+++ conda/_private/__init__.py
@@ -1,6 +1,9 @@
-`conda._private`. This directory should have an empty `__init__.py` to avoid
-side-effects for any submodules.
+# Copyright (C) 2012 Anaconda, Inc
+# SPDX-License-Identifier: BSD-3-Clause
+"""
+`conda._private`. Keep `__init__.py` empty to avoid side-effects.
 
 If you are an API user, stop! This module has no stability guarantees. Symbols
 under `_conda` can be renamed or removed at any time. Instead, look for
 re-exported API under `conda.*`.
+"""
