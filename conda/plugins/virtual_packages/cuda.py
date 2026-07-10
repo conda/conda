a4bbfabdc88f0e3caeeed0b1e6a2ca541a0a0872
Daniel Holth <208018+dholth@users.noreply.github.com>
2026-07-10 11:10:31 -0400

    move cuda detection subprocess code into conda._private

--- conda/plugins/virtual_packages/cuda.py
+++ conda/plugins/virtual_packages/cuda.py
@@ -4,15 +4,13 @@
 
 from __future__ import annotations
 
-import ctypes
 import functools
-import itertools
 import multiprocessing
-import os
 import platform
 import warnings
-from contextlib import suppress
 from typing import TYPE_CHECKING
+
+from conda._private.cuda import cuda_driver_version_detector_target
 
 from ...auxlib import NULL
 from .. import hookimpl
@@ -58,14 +56,14 @@ def cuda_version():
         )
         return NULL
 
+    # Spawn a subprocess to detect the CUDA version
+    detector = context.Process(
+        target=cuda_driver_version_detector_target,
+        args=(queue,),
+        name="CUDA driver version detector",
+        daemon=True,
+    )
     try:
-        # Spawn a subprocess to detect the CUDA version
-        detector = context.Process(
-            target=_cuda_driver_version_detector_target,
-            args=(queue,),
-            name="CUDA driver version detector",
-            daemon=True,
-        )
         detector.start()
         detector.join(timeout=60.0)
     finally:
@@ -95,88 +93,3 @@ def conda_virtual_packages() -> Iterable[CondaVirtualPackage]:
         override_entity="version",
         # empty_override=NULL,  # falsy override → skip __cuda
     )
-
-
-def _cuda_driver_version_detector_target(queue):
-    """
-    Attempt to detect the version of CUDA present in the operating system in a
-    subprocess.
-
-    On Windows and Linux, the CUDA library is installed by the NVIDIA
-    driver package, and is typically found in the standard library path,
-    rather than with the CUDA SDK (which is optional for running CUDA apps).
-
-    On macOS, the CUDA library is only installed with the CUDA SDK, and
-    might not be in the library path.
-
-    Returns: version string (e.g., '9.2') or None if CUDA is not found.
-             The result is put in the queue rather than a return value.
-    """
-    # Platform-specific libcuda location
-    system = platform.system()
-    if system == "Darwin":
-        lib_filenames = [
-            "libcuda.1.dylib",  # check library path first
-            "libcuda.dylib",
-            "/usr/local/cuda/lib/libcuda.1.dylib",
-            "/usr/local/cuda/lib/libcuda.dylib",
-        ]
-    elif system == "Linux":
-        lib_filenames = [
-            "libcuda.so",  # check library path first
-            "/usr/lib64/nvidia/libcuda.so",  # RHEL/Centos/Fedora
-            "/usr/lib/x86_64-linux-gnu/libcuda.so",  # Ubuntu
-            "/usr/lib/wsl/lib/libcuda.so",  # WSL
-        ]
-        # Also add libraries with version suffix `.1`
-        lib_filenames = list(
-            itertools.chain.from_iterable((f"{lib}.1", lib) for lib in lib_filenames)
-        )
-    elif system == "Windows":
-        bits = platform.architecture()[0].replace("bit", "")  # e.g. "64" or "32"
-        lib_filenames = [f"nvcuda{bits}.dll", "nvcuda.dll"]
-    else:
-        queue.put(None)  # CUDA not available for other operating systems
-        return
-
-    # Open library
-    if system == "Windows":
-        dll = ctypes.windll
-    else:
-        dll = ctypes.cdll
-    for lib_filename in lib_filenames:
-        with suppress(Exception):
-            libcuda = dll.LoadLibrary(lib_filename)
-            break
-    else:
-        queue.put(None)
-        return
-
-    # Empty `CUDA_VISIBLE_DEVICES` can cause `cuInit()` returns `CUDA_ERROR_NO_DEVICE`
-    # Invalid `CUDA_VISIBLE_DEVICES` can cause `cuInit()` returns `CUDA_ERROR_INVALID_DEVICE`
-    # Unset this environment variable to avoid these errors
-    os.environ.pop("CUDA_VISIBLE_DEVICES", None)
-
-    # Get CUDA version
-    try:
-        cuInit = libcuda.cuInit
-        flags = ctypes.c_uint(0)
-        ret = cuInit(flags)
-        if ret != 0:
-            queue.put(None)
-            return
-
-        cuDriverGetVersion = libcuda.cuDriverGetVersion
-        version_int = ctypes.c_int(0)
-        ret = cuDriverGetVersion(ctypes.byref(version_int))
-        if ret != 0:
-            queue.put(None)
-            return
-
-        # Convert version integer to version string
-        value = version_int.value
-        queue.put(f"{value // 1000}.{(value % 1000) // 10}")
-        return
-    except Exception:
-        queue.put(None)
-        return
