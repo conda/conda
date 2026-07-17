94f57ab5b81122203fab827e17ec51000422ccdd
Isuru Fernando <isuruf@gmail.com>
2026-07-17 08:50:22 -0700

    Apply suggestions from code review
    Co-authored-by: jaimergp <jaimergp@users.noreply.github.com>

--- conda/models/enums.py
+++ conda/models/enums.py
@@ -37,11 +37,12 @@ class Arch(Enum):
         if sys.platform == "win32":
             # platform.machine() is wrong when using emulation
             # see https://github.com/python/cpython/issues/98962
-            if sysconfig.get_platform() == "win-amd64":
+            sysconfig_platform = sysconfig.get_platform()
+            if sysconfig_platform == "win-amd64":
                 return cls["x86_64"]
-            elif sysconfig.get_platform() == "win-arm64":
+            elif sysconfig_platform == "win-arm64":
                 return cls["arm64"]
-            elif sysconfig.get_platform() == "win32":
+            elif sysconfig_platform == "win32":
                 return cls["x86"]
         return cls[machine()]
 
