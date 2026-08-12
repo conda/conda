0118452bdb2d01ca389638b207c46d900d4e36e9
Jannis Leidel <jannis@leidel.info>
2026-08-12 17:26:17 +0200

    Add conda plugins list subcommand (#16355)
    * Add conda plugins list subcommand
    
    * Address plugins list review feedback

---
+++ conda/plugins/subcommands/plugins/__init__.py
@@ -0,0 +1,42 @@
+# Copyright (C) 2012 Anaconda, Inc
+# SPDX-License-Identifier: BSD-3-Clause
+"""Implementation for `conda plugins` subcommand."""
+
+from __future__ import annotations
+
+from functools import partial
+from typing import TYPE_CHECKING
+
+from ... import hookimpl
+from ...types import CondaSubcommand
+from . import list
+
+if TYPE_CHECKING:
+    from argparse import ArgumentParser, Namespace
+
+
+SUMMARY = "Manage conda plugins."
+
+
+def configure_parser(parser: ArgumentParser) -> None:
+    subparsers = parser.add_subparsers(
+        title="subcommands",
+        dest="subcommand",
+    )
+
+    list.configure_parser(subparsers.add_parser("list", help=list.HELP))
+    parser.set_defaults(func=partial(parser.parse_args, ["--help"]))
+
+
+def execute(args: Namespace) -> int:
+    return args.func(args)
+
+
+@hookimpl
+def conda_subcommands():
+    yield CondaSubcommand(
+        name="plugins",
+        summary=SUMMARY,
+        action=execute,
+        configure_parser=configure_parser,
+    )
