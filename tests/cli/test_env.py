67b037f4e434db504ef10512dfff933877463359
Mahe Iram Khan <forgotten.programme@gmail.com>
2026-09-23 12:24:33 +0200

    update slow test

--- tests/cli/test_env.py
+++ tests/cli/test_env.py
@@ -307,19 +307,38 @@ def test_conda_env_create_http(
     assert parsed[0].get("name") == "small-executable"
 
 
-@pytest.mark.integration
-def test_update(path_factory: PathFactoryFixture, conda_cli: CondaCLIFixture):
+def test_update(
+    path_factory: PathFactoryFixture,
+    conda_cli: CondaCLIFixture,
+    test_recipes_channel: Path,
+):
+    """Update an environment by adding a package from an environment file."""
     prefix = path_factory()
-    create_env(ENVIRONMENT_CA_CERTIFICATES)
+    channel = str(test_recipes_channel)
+    create_env(
+        yaml.write(
+            {
+                "name": TEST_ENV1,
+                "dependencies": ["small-executable"],
+                "channels": [channel],
+            }
+        )
+    )
     conda_cli("env", "create", f"--prefix={prefix}")
 
-    create_env(ENVIRONMENT_CA_CERTIFICATES_ZLIB)
+    create_env(
+        yaml.write(
+            {
+                "name": TEST_ENV1,
+                "dependencies": ["small-executable", "dependency"],
+                "channels": [channel],
+            }
+        )
+    )
     conda_cli("env", "update", f"--prefix={prefix}")
 
-    stdout, _, _ = conda_cli("list", f"--prefix={prefix}", "zlib", "--json")
-    parsed = json.loads(stdout)
-    assert parsed
-    assert json.loads(stdout)
+    assert package_is_installed(prefix, "small-executable")
+    assert package_is_installed(prefix, "dependency")
 
 
 @pytest.mark.integration
